bd3ce1fc180202b3dcfa25740b0fbe405a45f8d7
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-01-30 15:23:38 +0100

    [github-56][feat]: Precheck selection of user and it correction (remove line's numbers, remove spec symbols (close #56)

--- app/client/src/app/core/modules/controller.selection.text.ts
+++ app/client/src/app/core/modules/controller.selection.text.ts
@@ -1,4 +1,5 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter       } from '@angular/core';
+import { clipboardShortcuts, ClipboardKeysEvent } from './controller.clipboard.shortcuts';
 
 const EVENTS = {
     mousedown   : 'mousedown',
@@ -19,6 +20,8 @@ class TextSelection {
                 target.addEventListener(EVENTS.mousedown,   this[EVENTS.mousedown]);
                 target.addEventListener(EVENTS.mouseup,     this[EVENTS.mouseup]);
                 this.trigger = trigger;
+                clipboardShortcuts.onCopy.subscribe(this.onCopy.bind(this));
+                clipboardShortcuts.onPaste.subscribe(this.onPaste.bind(this));
             }
         }
     }
@@ -36,14 +39,48 @@ class TextSelection {
         this.inProgress = false;
     }
 
+    onCopy(event: ClipboardKeysEvent) {
+        const text  = typeof event.selection.toString === 'function' ? event.selection.toString() : null;
+        const reg   = /\u0001/gi;
+
+        if (text === null) {
+            return false;
+        }
+
+        if (!~text.search(/\u0001/gi)){
+            return false;
+        }
+
+        const element           = document.createElement('P');
+        element.style.opacity   = '0.0001';
+        element.style.position  = 'absolute';
+        element.style.width     = '1px';
+        element.style.height    = '1px';
+        element.style.overflow  = 'hidden';
+        element.innerHTML       = text.replace(/\u0001\d*\u0001/gi, '').replace(/[\n\r]/gi, '</br>');
+        document.body.appendChild(element);
+        const range             = document.createRange();
+        range.selectNode(element);
+        event.selection.empty();
+        event.selection.addRange(range);
+
+        clipboardShortcuts.doCopy();
+
+        event.selection.empty();
+        document.body.removeChild(element);
+    }
+
+    onPaste() {
+
+    }
+
     [EVENTS.mousedown] (event: MouseEvent){
         this.bindWindowListener();
         this.inProgress = true;
     }
 
     [EVENTS.mouseup] (event: MouseEvent){
-        let selection   = window.getSelection(),
-            text        = '';
+        let selection = window.getSelection();
         if (typeof selection.toString === 'function'){
             this.trigger.emit(selection.toString());
         }
