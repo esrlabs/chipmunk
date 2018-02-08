e6f296de65ab2fe10916b56eb641b51b9a1fdaf2
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-02-08 16:35:26 +0100

    [github-84][feat]: settings for manage ASCII colors rendering

---
+++ app/client/src/app/core/components/common/dialogs/app.settings/visual/component.ts
@@ -0,0 +1,76 @@
+import {
+    Component, Input, Output, ViewChild, AfterContentInit, OnInit, ViewContainerRef,
+    ComponentFactoryResolver, ChangeDetectorRef, OnDestroy, EventEmitter
+} from '@angular/core';
+import { SimpleCheckbox     } from '../../../checkboxes/simple/component';
+import { TabController      } from '../../../../common/tabs/tab/class.tab.controller';
+import { Visual             } from '../../../../../modules/controller.settings';
+
+
+@Component({
+    selector    : 'dialog-visual-settings-tab',
+    templateUrl : './template.html',
+})
+
+export class DialogVisualSettingTab extends TabController implements OnDestroy, AfterContentInit, OnInit{
+
+    @Input() visual     : Visual = {
+        prevent_ascii_colors_always         : false,
+        prevent_ascii_colors_on_highlight   : true
+    };
+
+    @Input() register   : Function = null;
+
+    @Output() getData() : Visual {
+        return {
+            prevent_ascii_colors_always         : this._prevent_ascii_colors_always.getValue(),
+            prevent_ascii_colors_on_highlight   : this._prevent_ascii_colors_on_highlight.getValue()
+        };
+    };
+
+    private registered: boolean = false;
+
+    @ViewChild('_prevent_ascii_colors_always'       ) _prevent_ascii_colors_always   : SimpleCheckbox;
+    @ViewChild('_prevent_ascii_colors_on_highlight' ) _prevent_ascii_colors_on_highlight   : SimpleCheckbox;
+
+    constructor(private componentFactoryResolver    : ComponentFactoryResolver,
+                private viewContainerRef            : ViewContainerRef,
+                private changeDetectorRef           : ChangeDetectorRef) {
+        super();
+        this.onTabSelected              = this.onTabSelected.           bind(this);
+        this.onTabDeselected            = this.onTabDeselected.         bind(this);
+    }
+
+    ngOnInit(){
+        this.onSelect   .subscribe(this.onTabSelected);
+        this.onDeselect .subscribe(this.onTabDeselected);
+    }
+
+    ngOnDestroy(){
+        this.onSelect.      unsubscribe();
+        this.onDeselect.    unsubscribe();
+    }
+
+    ngAfterContentInit(){
+        if (this.register !== null && !this.registered) {
+            this.register({
+                getData: this.getData.bind(this),
+                section: 'visual'
+            });
+            this.registered = true;
+        }
+    }
+
+    onTabSelected(){
+
+    }
+
+    onTabDeselected(){
+
+    }
+
+    forceUpdate(){
+        this.changeDetectorRef.detectChanges();
+    }
+
+}
