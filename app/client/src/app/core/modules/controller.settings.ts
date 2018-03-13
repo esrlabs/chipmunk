2f71b24a89d05316558222e85babf8d0a2b0e09e
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-03-13 01:41:40 +0100

    [github-153][feat]: provide new settings of application (in scope of #153)

--- app/client/src/app/core/modules/controller.settings.ts
+++ app/client/src/app/core/modules/controller.settings.ts
@@ -8,7 +8,8 @@ interface IVisualSettings {
     prevent_ascii_colors_always         : boolean,
     prevent_ascii_colors_on_highlight   : boolean,
     do_not_highlight_matches_in_requests: boolean,
-    highlight_search_requests           : boolean
+    highlight_search_requests           : boolean,
+    show_active_search_results_always   : boolean
 }
 
 interface IServerSetting {
