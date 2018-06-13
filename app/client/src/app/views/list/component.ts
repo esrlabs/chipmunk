c9b85e4c79e1e7063422535509c6ccd4e2b19f18
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-06-13 18:21:15 +0200

    [github-203][fix]: export logs with ASCII encoding (close #203)

--- app/client/src/app/views/list/component.ts
+++ app/client/src/app/views/list/component.ts
@@ -917,7 +917,7 @@ export class ViewControllerList extends ViewControllerPattern implements ViewInt
                 let str     = this.rows.map((row)=>{
                         return ANSIClearer(row.params.original);
                     }),
-                    blob    = new Blob([str.join('\n')], {type: 'text/plain'}),
+                    blob    = new Blob([str.join('\n')], { type: 'text/plain; charset=ASCII' }),
                     url     = URL.createObjectURL(blob);
                 this.exportdata.url         = this.sanitizer.bypassSecurityTrustUrl(url);
                 this.exportdata.filename    = 'export_' + (new Date()).getTime() + '.txt';
