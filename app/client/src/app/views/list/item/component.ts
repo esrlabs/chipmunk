781c4534a2fac0b3098ea124f9522036b4329a66
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-08-09 19:43:39 +0200

    [github-xx][fix]: fix for removing line numbers (after electron upgrade)

--- app/client/src/app/views/list/item/component.ts
+++ app/client/src/app/views/list/item/component.ts
@@ -304,7 +304,7 @@ export class ViewControllerListItem implements ListItemInterface, OnDestroy, OnC
     }
 
     removeIndexes(str: string){
-        return str.replace(/\u0020.*\u0020/gi, '');
+        return str.replace(/\u0020.*\u0020/gi, '').replace(/\d*\u0020{1,}/gi, '');
     }
 
     onContextMenu(event: MouseEvent) {
