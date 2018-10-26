4d5f95de5a0d7b75fe0a9e25366fa2d1c958a8f1
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2018-10-26 10:52:03 +0200

    electron app include

--- application/electron/src/main.js
+++ application/electron/src/main.js
@@ -1,8 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
-  // Keep a global reference of the window object, if you don't, the window will
-  // be closed automatically when the JavaScript object is garbage collected.
+
   let win
   
   function createWindow () {
