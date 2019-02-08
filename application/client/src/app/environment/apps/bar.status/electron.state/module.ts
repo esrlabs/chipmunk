0f27048543b33a565999b2099fddff3bd49a119a
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-02-08 13:38:53 +0100

    [github-xx][feat] frame wrapper (render)

---
+++ application/client/src/app/environment/apps/bar.status/electron.state/module.ts
@@ -0,0 +1,20 @@
+import { NgModule                               } from '@angular/core';
+import { CommonModule                           } from '@angular/common';
+
+import { AppsStatusBarElectronStateComponent    } from './component';
+import { StateHistoryComponent                  } from './history/component';
+import { EnvironmentComponentsModule            } from '../../../components/module';
+
+const components = [ AppsStatusBarElectronStateComponent, StateHistoryComponent];
+
+@NgModule({
+    entryComponents : [ ...components ],
+    imports         : [ CommonModule, EnvironmentComponentsModule ],
+    declarations    : [ ...components ],
+    exports         : [ ...components ]
+})
+
+export class ElectronStateModule {
+    constructor() {
+    }
+}
--- application/client/src/app/environment/components/complex/module.ts
+++ application/client/src/app/environment/components/complex/module.ts
@@ -4,13 +4,14 @@ import { CommonModule                           } from '@angular/common';
 import { NotificationsModule                    } from './notifications/module';
 import { TabsModule                             } from './tabs/module';
 import { DockingModule                          } from './docking/module';
+import { WrappersModule                         } from './wrappers/module';
 
 
 @NgModule({
     entryComponents : [  ],
     imports         : [ CommonModule ],
     declarations    : [  ],
-    exports         : [ NotificationsModule, TabsModule, DockingModule ]
+    exports         : [ NotificationsModule, TabsModule, DockingModule, WrappersModule ]
 })
 
 export class EnvironmentComplexModule {
