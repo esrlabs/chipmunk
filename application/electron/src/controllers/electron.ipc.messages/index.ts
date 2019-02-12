e2df8f5773f168b4e0ce659c7c5875375e49aaf9
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-02-13 00:39:21 +0100

    [github-xx][feat] IPC update

--- application/electron/src/controllers/electron.ipc.messages/index.ts
+++ application/electron/src/controllers/electron.ipc.messages/index.ts
@@ -4,13 +4,26 @@ export { HostState, EHostState, IHostState };
 import { HostStateHistory } from './host.state.history';
 export { HostStateHistory };
 
+import { IRenderMountPlugin, RenderMountPlugin } from './render.plugin.mount';
+export { IRenderMountPlugin, RenderMountPlugin };
+
+import { IRenderState, RenderState, ERenderState } from './render.state';
+export { IRenderState, RenderState, ERenderState };
+
 // Common type for expected message implementation
-export type TMessage = HostState | HostStateHistory;
+export type TMessage =  HostState |
+                        HostStateHistory |
+                        RenderMountPlugin |
+                        RenderState;
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * Mapping of host/render events
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  */
 export const Map = {
-    [HostState.signature]: HostState,
-    [HostStateHistory.signature]: HostStateHistory,
+
+    [HostState.signature            ]: HostState,
+    [HostStateHistory.signature     ]: HostStateHistory,
+    [RenderMountPlugin.signature    ]: RenderMountPlugin,
+    [RenderState.signature          ]: RenderState,
+
 };
---
+++ application/electron/src/controllers/electron.ipc.messages/render.state.ts
@@ -0,0 +1,29 @@
+export enum ERenderState {
+    ready = 'ready',        // Render is ready. No any operations
+    busy = 'busy',          // Render isn't ready. Some blocked operations are going
+    working = 'working',    // Render isn't ready, but can be used, because host has background operations
+}
+
+export interface IRenderState {
+    message?: string;
+    state: ERenderState;
+}
+
+export class RenderState {
+
+    public static States = ERenderState;
+    public static signature: string = 'RenderState';
+    public message: string = '';
+    public state: ERenderState = ERenderState.ready;
+
+    constructor(params: IRenderState) {
+        if (typeof params !== 'object' || params === null) {
+            throw new Error(`Incorrect parameters for RenderState message`);
+        }
+        if (typeof params.state !== 'string' || params.state.trim() === '') {
+            throw new Error(`State of render should be defined.`);
+        }
+        this.message = typeof params.message === 'string' ? params.message : '';
+        this.state = params.state;
+    }
+}
