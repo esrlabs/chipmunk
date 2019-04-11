c411881ab42ee6ceb322c467a952379b0d662b87
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-04-11 14:19:00 +0200

    [github][feat] correction of stream reader for search; change format of search results

--- application/electron/src/controllers/controller.stream.search.ts
+++ application/electron/src/controllers/controller.stream.search.ts
@@ -27,64 +27,84 @@ export default class ControllerStreamSearch extends EventEmitter {
     public search(): Promise<IResults> {
         return new Promise((resolve, reject) => {
             const done = () => {
-                if (this._stream !== undefined) {
-                    this._stream.close();
-                    this._stream.removeAllListeners();
-                    this._stream = undefined;
+                if (this._stream === undefined) {
+                    return;
                 }
+                this._stream.close();
+                this._stream.removeAllListeners();
+                this._stream = undefined;
+                if (restFromChunkEnd !== '') {
+                    const res: IResults | Error = this._findInChunk(restFromChunkEnd, result);
+                    if (res instanceof Error) {
+                        this.emit(ControllerStreamSearch.Events.finished);
+                        return reject(new Error(this._logger.warn(`Cannot continue search due error: ${res.message}`)));
+                    }
+                }
+                this.emit(ControllerStreamSearch.Events.finished, result);
+                resolve(result);
             };
+            // To store rest of each chunk
+            let restFromChunkEnd = '';
             // Storage of results
             const result: IResults = {
                 found: 0,
                 regs: {},
-                end: -1,
-                begin: 0,
                 str: '',
             };
             // Offset in file. We need it to correctly calculate numbers of rows
-            let offset = 0;
             // Get file info
             const stat: fs.Stats = fs.statSync(this._file);
             // Create stream to read a target file
             this._stream = fs.createReadStream(this._file, { encoding: 'utf8' });
             this.emit(ControllerStreamSearch.Events.started);
             this._stream.on('data', (chunk: string) => {
-                const fragment: Fragment = new Fragment(offset, 1000000, chunk);
-                const res: IResults | Error = fragment.find(this._requests);
+                // Append to the beggining of chunk rest part from previous
+                chunk = `${restFromChunkEnd}${chunk}`;
+                // Remove last row in chunk because it could be not finished
+                const rows: string[] = chunk.split(/\r?\n|\r/gi);
+                restFromChunkEnd = rows[rows.length - 1];
+                rows.splice(rows.length - 1, 1);
+                chunk = rows.join('\n');
+                // Start search
+                const res: IResults | Error = this._findInChunk(chunk, result);
                 if (res instanceof Error) {
                     done();
                     this.emit(ControllerStreamSearch.Events.finished);
                     return reject(new Error(this._logger.warn(`Cannot continue search due error: ${res.message}`)));
                 }
-                result.found += res.found;
-                result.end = res.end;
-                // result.str += res.str; // Dangerous from memory usage point of view
-                offset = res.end;
-                Object.keys(res.regs).forEach((reg: string) => {
-                    const index: number = parseInt(reg, 10);
-                    if (result.regs[index] === undefined) {
-                        result.regs[index] = res.regs[index];
-                    } else {
-                        result.regs[index].push(...res.regs[index]);
-                    }
-                });
+
+                result.str = ''; // !!!!!! <==== HAS TO BE REMOVED
+                // Emit event about middle results
                 this.emit(ControllerStreamSearch.Events.next, res);
+                // Check: is stream already finished
                 if (this._stream !== undefined && this._stream.bytesRead === stat.size) {
                     // Whole file is read. If stream still is available - event "end" wasn't triggered.
                     done();
-                    this.emit(ControllerStreamSearch.Events.finished, result);
-                    resolve(result);
                 }
             });
             this._stream.on('end', () => {
-                if (this._stream === undefined) {
-                    return;
-                }
                 done();
-                this.emit(ControllerStreamSearch.Events.finished, result);
-                resolve(result);
             });
         });
+    }
+
+    private _findInChunk(chunk: string, results: IResults): Error | IResults {
+        const fragment: Fragment = new Fragment(1000000, chunk);
+        const res: IResults | Error = fragment.find(this._requests);
+        if (res instanceof Error) {
+            return res;
+        }
+        results.found += res.found;
+        results.str += res.str;
+        Object.keys(res.regs).forEach((reg: string) => {
+            const index: number = parseInt(reg, 10);
+            if (results.regs[index] === undefined) {
+                results.regs[index] = res.regs[index];
+            } else {
+                results.regs[index].push(...res.regs[index]);
+            }
+        });
+        return res;
     }
 
 }
