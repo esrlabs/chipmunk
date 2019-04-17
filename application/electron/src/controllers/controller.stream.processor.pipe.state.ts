a93ac6e7244d9a010a88eb862cd67db2b3002200
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-04-18 01:51:11 +0200

    [github-xx][feat] upgrade stream controller; fix bug with byte's reange detection.

--- application/electron/src/controllers/controller.stream.processor.pipe.state.ts
+++ application/electron/src/controllers/controller.stream.processor.pipe.state.ts
@@ -1,19 +1,33 @@
 import ServiceElectron, { IPCMessages as IPCElectronMessages, Subscription } from '../services/service.electron';
+import Logger from '../tools/env.logger';
 
-export default class PipesState {
+const Settings = {
+    notificationDelayOnStream: 500,             // ms, Delay for sending notifications about stream's update to render (client) via IPC, when stream is blocked
+    maxPostponedNotificationMessages: 500,      // How many IPC messages to render (client) should be postponed via timer
+};
+
+export default class PipeState {
 
     private _streamId: string;
     private _names: Map<string, string> = new Map();
     private _size: number = 0;
     private _done: number = 0;
+    private _timer: any;
+    private _postponed: number = 0;
+    private _started: number = 0;
+    private _logger: Logger;
 
     constructor(streamId: string) {
         this._streamId = streamId;
+        this._logger = new Logger(`PipeState: ${this._streamId}`);
     }
 
     public add(id: string, size: number, name: string) {
         if (this._names.has(id)) {
             return;
+        }
+        if (this._started === 0) {
+            this._started = Date.now();
         }
         this._names.set(id, name);
         this._size += size;
@@ -28,6 +42,8 @@ export default class PipesState {
         if (this._names.size === 0) {
             this._size = -1;
             this._done = -1;
+            this._logger.env(`All pipes done in: ${((Date.now() - this._started) / 1000).toFixed(2)}s`);
+            this._started = 0;
         }
         this._send();
     }
@@ -37,10 +53,11 @@ export default class PipesState {
             return;
         }
         this._done += written;
-        this._send();
+        this._notify();
     }
 
     private _send() {
+        clearTimeout(this._timer);
         ServiceElectron.IPC.send(new IPCElectronMessages.StreamPipeState({
             streamId: this._streamId,
             size: this._size,
@@ -48,4 +65,18 @@ export default class PipesState {
             items: Array.from(this._names.values()),
         }));
     }
+
+    private _notify() {
+        clearTimeout(this._timer);
+        if (this._postponed < Settings.maxPostponedNotificationMessages) {
+            this._postponed += 1;
+            this._timer = setTimeout(() => {
+                this._send();
+            }, Settings.notificationDelayOnStream);
+        } else {
+            this._postponed = 0;
+            this._send();
+        }
+    }
+
 }
