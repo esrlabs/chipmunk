f8f30ae033575a9b09ba9dc636d6f2b0119efe83
Oliver Mueller <oliver.mueller@esrlabs.com>
2019-05-06 20:01:04 +0200

    [](chore): testing travis integration

---
+++ src/ci/before_deploy.sh
@@ -0,0 +1,29 @@
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross rustc --bin $CRATE_NAME --target $TARGET --release -- -C lto
+
+    cp target/$TARGET/release/$CRATE_NAME $stage/ || cp target/$TARGET/release/$CRATE_NAME.exe $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
