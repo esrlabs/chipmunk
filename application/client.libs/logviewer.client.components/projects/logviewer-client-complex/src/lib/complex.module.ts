0dae2bf1a708b04f843e4f2401b5f051a6af0449
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-05-10 17:56:01 +0200

    [github-xx][feat] new scroll area controller (beta)

--- application/client.libs/logviewer.client.components/projects/logviewer-client-complex/src/lib/complex.module.ts
+++ application/client.libs/logviewer.client.components/projects/logviewer-client-complex/src/lib/complex.module.ts
@@ -4,13 +4,14 @@ import { CommonModule                           } from '@angular/common';
 import { TabsModule                             } from './tabs/module';
 import { DockingModule                          } from './docking/module';
 import { InfinityOutputModule                   } from './infinityoutput/module';
+import { ScrollBoxModule                        } from './scrollbox/module';
 
 
 @NgModule({
     entryComponents : [  ],
     imports         : [ CommonModule ],
     declarations    : [  ],
-    exports         : [ TabsModule, DockingModule, InfinityOutputModule ]
+    exports         : [ TabsModule, DockingModule, InfinityOutputModule, ScrollBoxModule ]
 })
 
 export class ComplexModule { }
---
+++ application/client.libs/logviewer.client.components/projects/logviewer-client-complex/src/lib/scrollbox/module.ts
@@ -0,0 +1,23 @@
+import { NgModule                               } from '@angular/core';
+import { CommonModule                           } from '@angular/common';
+
+import { ComplexScrollBoxComponent              } from './component';
+import { ComplexScrollBoxSBVComponent           } from './sbv/component';
+
+import { PrimitiveModule                        } from 'logviewer-client-primitive';
+import { ContainersModule                       } from 'logviewer-client-containers';
+
+const entryComponents = [ ComplexScrollBoxComponent, ComplexScrollBoxSBVComponent ];
+const components = [ ...entryComponents ];
+
+@NgModule({
+    entryComponents : [ ...entryComponents ],
+    imports         : [ CommonModule, PrimitiveModule, ContainersModule ],
+    declarations    : [ ...components ],
+    exports         : [ ...components ]
+})
+
+export class ScrollBoxModule {
+    constructor() {
+    }
+}
