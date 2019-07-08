ad3cc71ba96bfbe3db03a8ed11cc8b97baed4073
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-07-08 11:34:56 +0200

    [github-323][fix] change color match calculation logic

--- application/client.core/src/app/environment/theme/colors.ts
+++ application/client.core/src/app/environment/theme/colors.ts
@@ -36,3 +36,33 @@ export function shadeColor(color: string, percent: number) {
     colorsCache.set(key, result);
     return result;
 }
+
+export function getContrastColor (hex: string, bw: boolean = false) {
+    function padZero(str: string, len?: number) {
+        len = len || 2;
+        const zeros = new Array(len).join('0');
+        return (zeros + str).slice(-len);
+    }
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+        // http://stackoverflow.com/a/3943023/112731
+        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+            ? '#000000'
+            : '#FFFFFF';
+    }
+    // pad each with zeros and return
+    return '#' + padZero((255 - r).toString(16)) + padZero((255 - g).toString(16)) + padZero((255 - b).toString(16));
+    // https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+}
