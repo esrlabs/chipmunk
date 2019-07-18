5128821e311aa8f1c64bf9c096780c5e22889ed0
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-07-18 17:32:53 +0200

    [github-xx][feat] restore columns state

--- application/client.core/src/app/environment/components/views/row/columns/controller.columns.ts
+++ application/client.core/src/app/environment/components/views/row/columns/controller.columns.ts
@@ -1,6 +1,7 @@
 // tslint:disable:no-inferrable-types
 
 import { Subject, Observable } from 'rxjs';
+import * as Toolkit from 'logviewer.client.toolkit';
 
 export const CDefaults = {
     width: 100,
@@ -17,9 +18,12 @@ export interface IColumn {
 
 export interface IColumns { [key: number]: IColumn; }
 
+const CLocalStorageKey = '__logviewer.row.render.custom.columns';
+
 export class ControllerColumns {
 
     private _columns: IColumns = {};
+    private _hash: string;
     private _subjects: {
         onResized: Subject<IColumns>,
         onVisibility: Subject<IColumns>,
@@ -29,15 +33,15 @@ export class ControllerColumns {
     };
 
     constructor(widths: Array<{ width: number, min: number}>, headers: string[]) {
-        headers.forEach((header: string, index: number) => {
-            this._columns[index] = {
-                width: widths[index].width,
-                minWidth: widths[index].min,
-                header: header,
-                visible: true,
-                index: index,
-            };
-        });
+        this._hash = Toolkit.hash(headers.join(','));
+        this._columns = this._getStored(headers);
+        if (this._columns === undefined) {
+            this._initColumns(widths, headers);
+        }
+    }
+
+    public destroy() {
+
     }
 
     public getColumns(): IColumns {
@@ -59,6 +63,7 @@ export class ControllerColumns {
             return;
         }
         this._columns[index].width = width > this._columns[index].minWidth ? width : this._columns[index].minWidth;
+        this._setStored();
         this._subjects.onResized.next(this._columns);
     }
 
@@ -69,7 +74,49 @@ export class ControllerColumns {
             }
             this._columns[index].visible = visibile;
         });
+        this._setStored();
         this._subjects.onVisibility.next(this._columns);
     }
+
+    private _getStored(headers: string[]): IColumns | undefined {
+        let columns: IColumns;
+        try {
+            columns = JSON.parse(localStorage.getItem(this._getStorageKey()));
+        } catch (e) {
+            localStorage.removeItem(this._getStorageKey());
+            return undefined;
+        }
+        if (typeof columns !== 'object' || columns === null) {
+            localStorage.removeItem(this._getStorageKey());
+            return undefined;
+        }
+        if (Object.keys(columns).length !== headers.length) {
+            localStorage.removeItem(this._getStorageKey());
+            return undefined;
+        }
+        return columns;
+    }
+
+    private _setStored() {
+        localStorage.setItem(this._getStorageKey(), JSON.stringify(this._columns));
+    }
+
+    private _initColumns(widths: Array<{ width: number, min: number}>, headers: string[]) {
+        this._columns = {};
+        headers.forEach((header: string, index: number) => {
+            this._columns[index] = {
+                width: widths[index].width,
+                minWidth: widths[index].min,
+                header: header,
+                visible: true,
+                index: index,
+            };
+        });
+    }
+
+    private _getStorageKey(): string {
+        return `${CLocalStorageKey}:${this._hash}`;
+    }
+
 }
 
