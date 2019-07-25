a72832dcb4387b51a5767060a69d7ada3fbfedec
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-07-25 23:19:20 +0200

    [github-xx][feat] added install script

--- rakefile.rb
+++ rakefile.rb
@@ -17,3 +17,26 @@ task :start do
     sh "npm run electron"
   end
 end
+
+desc "install"
+task :install do
+  cd "application/client.core" do
+    sh "npm install"
+  end
+  cd "application/client.libs/logviewer.client.components" do
+    sh "npm install"
+  end
+  cd "application/client.plugins" do
+    sh "npm install"
+  end
+  cd "application/electron" do
+    sh "npm install"
+    sh "npm run build-ts"
+    sh "mkdir ./dist/compiled/plugins & exit 0"
+    sh "mkdir ./dist/compiled/client & exit 0"
+  end
+  cd "application" do
+    sh "jake client:all --skip-npm"
+    sh "jake electron:quick --skip-npm"
+  end
+end
