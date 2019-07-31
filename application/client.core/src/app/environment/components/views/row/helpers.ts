16e73316bdaf382736702fcfec7b56e6de90aa89
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-07-31 15:16:01 +0200

    [github-xx][feat] cleanup clipboard output

--- application/client.core/src/app/environment/components/views/row/helpers.ts
+++ application/client.core/src/app/environment/components/views/row/helpers.ts
@@ -1,3 +1,7 @@
 export function removeRowNumber(str: string): string {
     return str.replace(/^\d*\u0008/gm, '');
 }
+
+export function cleanupOutput(str: string): string {
+    return removeRowNumber(str).replace(/\u0004/gi, '').replace(/\u0005/gi, '');
+}
