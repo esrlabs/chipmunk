1090992a94af83a82120565a1123ff4447e2fd91
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-09-10 23:23:32 +0200

    [github-xx][feat] add feature pcre2 to ripgrep

--- rakefile.rb
+++ rakefile.rb
@@ -414,7 +414,7 @@ task :buildripgrep do
 
   cd "application/apps/ripgrep" do
     puts 'Build ripgrep'
-    sh "cargo build --release"
+    sh "cargo build --release --features 'pcre2'"
   end
 
   puts "Check old version of app: #{INCLUDED_APPS_FOLDER}/#{APP_FILE}"
