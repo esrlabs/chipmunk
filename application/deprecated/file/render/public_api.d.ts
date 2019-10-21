b00c1b1ebaeb8e69cb8fb9f9e20306ed2d350737
Oliver Mueller <oliver.mueller@esrlabs.com>
2019-10-21 10:28:25 +0200

    [](chore) removed unused files
    deleted unused rust code

--- application/deprecated/file/render/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/views/sidebar.vertical/component';
-export * from './lib/views/taskbar.state/component';
-export * from './lib/module';
--- application/platform/node/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as FS from './fs';
-import * as Env from './process.env';
-import * as Spawn from './process.spawn';
-
-import Logger from './env.logger';
-
-export { FS, Env, Spawn, Logger };
