18abe5908de5e3f6d2ac78cfc320dec113c66f42
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-10-22 19:44:43 +0200

    [#400][feat] new kind of spinner (circle)

---
+++ application/client.libs/logviewer.client.components/projects/logviewer-client-primitive/src/lib/spinners/circle/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-primitive-spinner-circle',
+    templateUrl: './template.html',
+    styleUrls: ['./styles.less']
+})
+
+export class SpinnerCircleComponent {
+
+}
