d2774728cf617c6ec4d38503ae4e2c52d8a80211
fur-gan <56543045+fur-gan@users.noreply.github.com>
2019-10-31 14:23:19 +0100

    [](fix) Use self-defined HTML elements

--- application/sandbox/serial/render/lib/views/sidebar.vertical/component.d.ts
+++ application/sandbox/serial/render/lib/views/sidebar.vertical/component.d.ts
@@ -2,15 +2,23 @@ import { OnDestroy, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { IPortInfo, IPortState } from '../../common/interface.portinfo';
 import { IOptions } from '../../common/interface.options';
 import { SidebarVerticalPortOptionsWriteComponent } from './port.options.write/component';
+
+import { InputStandardComponent } from 'logviewer-client-primitive';
 import * as Toolkit from 'chipmunk.client.toolkit';
+
 interface IConnected {
     port: IPortInfo;
     options: IOptions;
     state: IPortState;
 }
+interface IPortListItem {
+    comPort: IPortInfo;
+    caption: string;
+}
 export declare class SidebarVerticalComponent implements AfterViewInit, OnDestroy {
     private _cdRef;
     _optionsCom: SidebarVerticalPortOptionsWriteComponent;
+    _inputCom: InputStandardComponent;
     api: Toolkit.IAPI;
     session: string;
     sessions: Toolkit.ControllerSessionsEvents;
@@ -18,6 +26,7 @@ export declare class SidebarVerticalComponent implements AfterViewInit, OnDestro
     private _logger;
     private _destroyed;
     private _chosenPort;
+    private _portItem;
     _ng_ports: IPortInfo[];
     _ng_connected: IConnected[];
     _ng_selected: IPortInfo | undefined;
@@ -25,6 +34,7 @@ export declare class SidebarVerticalComponent implements AfterViewInit, OnDestro
     _ng_error: string | undefined;
     _ng_options: boolean;
     _ng_msg: string;
+    _ng_portList: IPortListItem[];
     constructor(_cdRef: ChangeDetectorRef);
     ngOnDestroy(): void;
     ngAfterViewInit(): void;
@@ -53,11 +63,12 @@ export declare class SidebarVerticalComponent implements AfterViewInit, OnDestro
     _ng_sendMessage(value: string, event: KeyboardEvent): void;
     private _createDropdownElement;
     private _removeDropdownElement;
-    _ng_updateSelection(): void;
-    private _updateDropdown;
+    private _updateSelection;
+    _ng_change(value: string): void;
     private _updateDisconnectPort;
     private _updateConnectPort;
-    _ng_saveOption(): void;
+    private _checkOption;
+    private _saveOption;
     private _loadOption;
 }
 export {};
