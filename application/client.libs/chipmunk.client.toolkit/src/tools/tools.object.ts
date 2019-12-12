3d73695b7f0de4993efbef2000f689be8c4fa089
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2019-12-12 20:34:06 +0100

    [](feat) chipmunk.client.toolkit -> 0.0.74

--- application/client.libs/chipmunk.client.toolkit/src/tools/tools.object.ts
+++ application/client.libs/chipmunk.client.toolkit/src/tools/tools.object.ts
@@ -1,14 +1,25 @@
+function isObject(smth: any): boolean {
+    if (typeof smth !== 'object' || smth === null) {
+        return false;
+    } else if (smth instanceof Array) {
+        return false;
+    } else if (typeof smth.constructor === 'function' && typeof smth.constructor.name === 'string' && smth.constructor.name.toLowerCase() !== 'object') {
+        return false;
+    }
+    return true;
+}
+
 export function copy(obj: any): any {
     if (obj instanceof Array) {
         return obj.map(item => copy(item));
-    } else if (typeof obj !== 'object' || obj === null) {
+    } else if (!isObject(obj)) {
         return obj;
     }
     const _obj = Object.assign({}, obj);
     Object.keys(_obj).forEach((prop: string) => {
         if (_obj[prop] instanceof Array) {
             _obj[prop] = _obj[prop].map(item => copy(item));
-        } else if (typeof _obj[prop] === 'object' && _obj[prop] !== null) {
+        } else if (isObject(obj)) {
             _obj[prop] = copy(_obj[prop]);
         }
     });
