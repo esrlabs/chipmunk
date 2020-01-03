3c56e6b9796169829331e2cd10a113b96ca92069
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-01-03 10:00:35 +0100

    [](feat) global log level (client side)

--- application/client.core/src/main.ts
+++ application/client.core/src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { setGlobalLogLevel, ELogLevels } from 'chipmunk.client.toolkit';
+
+setGlobalLogLevel(ELogLevels.WARNING);
 
 if (environment.production) {
   enableProdMode();
--- application/client.libs/chipmunk.client.toolkit/src/tools/index.ts
+++ application/client.libs/chipmunk.client.toolkit/src/tools/index.ts
@@ -2,7 +2,7 @@ import guid from './tools.guid';
 import Emitter from './tools.emitter';
 import Queue from './tools.queue';
 import Logger from './tools.logger';
-import { LoggerParameters } from './tools.logger.parameters';
+import { LoggerParameters, setGlobalLogLevel, ELogLevels } from './tools.logger.parameters';
 import Subscription, { THandler } from './tools.subscription';
 import Subject from './tools.subject';
 import { sequences } from './tools.sequences';
@@ -10,4 +10,22 @@ import * as regTools from './tools.regexp';
 import { hash } from './tools.hash';
 import { basename, dirname } from './tools.path';
 import { copy } from './tools.object';
-export { guid, hash, Emitter, Logger, LoggerParameters, Subscription, Subject, THandler, sequences, regTools, Queue, basename, dirname, copy };
+
+export {
+    guid,
+    hash,
+    Emitter,
+    Logger,
+    LoggerParameters,
+    Subscription,
+    Subject,
+    THandler,
+    sequences,
+    regTools,
+    Queue,
+    basename,
+    dirname,
+    copy,
+    setGlobalLogLevel,
+    ELogLevels,
+};
