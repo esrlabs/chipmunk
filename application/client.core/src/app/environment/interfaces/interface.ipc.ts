e42f3aad4035fa6396b143ca2031e7b6edd0c75f
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-01-31 15:28:08 +0100

    [](feat) link to common interfaces

---
+++ application/client.core/src/app/environment/interfaces/interface.ipc.ts
@@ -0,0 +1,8 @@
+import * as IPCMessages from '../../../../../common/ipc/electron.ipc.messages/index';
+
+export {
+    ISearchExpressionFlags,
+    ISearchExpression
+} from '../../../../../common/ipc/electron.ipc.messages/search.request';
+
+export { IPCMessages };
