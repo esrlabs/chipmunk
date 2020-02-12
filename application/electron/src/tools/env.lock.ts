042242e8a1ae404dc00e3fb7054bdc620086d46d
DmitryAstafyev <dvastafyev@gmail.com>
2020-02-12 12:21:40 +0100

    [](refact) update logs controller

---
+++ application/electron/src/tools/env.lock.ts
@@ -0,0 +1,21 @@
+export class Lock {
+
+    private _locked: boolean = false;
+
+    constructor(locked: boolean = false) {
+        this._locked = locked;
+    }
+
+    public lock() {
+        this._locked = true;
+    }
+
+    public unlock() {
+        this._locked = false;
+    }
+
+    public isLocked(): boolean {
+        return this._locked;
+    }
+
+}
--- application/electron/src/tools/env.logger.parameters.ts
+++ application/electron/src/tools/env.logger.parameters.ts
@@ -1,46 +1,11 @@
-export enum ELogLevels {
-    INFO = 'INFO',
-    DEBUG = 'DEBUG',
-    WARNING = 'WARNING',
-    VERBOS = 'VERBOS',
-    ERROR = 'ERROR',
-    ENV = 'ENV',
-    WTF = 'WTF',
-}
+import {
+    TOutputFunc,
+    ELogLevels,
+} from './env.logger.service';
 
-const DEFAUT_ALLOWED_CONSOLE = {
-    DEBUG: true,
-    ENV: true,
-    ERROR: true,
-    INFO: true,
-    VERBOS: false,
-    WARNING: true,
-    WTF: true,
-};
+import LogsService from './env.logger.service';
 
-const LOGS_LEVEL_TABLE = {
-    ENV: [ELogLevels.ENV, ELogLevels.VERBOS, ELogLevels.DEBUG, ELogLevels.INFO, ELogLevels.WARNING, ELogLevels.ERROR],
-    VERBOS: [ELogLevels.VERBOS, ELogLevels.DEBUG, ELogLevels.INFO, ELogLevels.WARNING, ELogLevels.ERROR],
-    DEBUG: [ELogLevels.DEBUG, ELogLevels.INFO, ELogLevels.WARNING, ELogLevels.ERROR],
-    INFO: [ELogLevels.INFO, ELogLevels.WARNING, ELogLevels.ERROR],
-    WARNING: [ELogLevels.WARNING, ELogLevels.ERROR],
-    ERROR: [ELogLevels.ERROR],
-    WTF: [ELogLevels.WTF, ELogLevels.ERROR],
-};
-
-type TOutputFunc = (...args: any[]) => any;
-
-let level: ELogLevels | undefined;
-
-export function setGlobalLogLevel(lev: ELogLevels) {
-    if (typeof lev !== 'string') {
-        return;
-    }
-    if (LOGS_LEVEL_TABLE[lev] === undefined) {
-        return;
-    }
-    level = lev;
-}
+export { ELogLevels, LogsService };
 
 /**
  * @class
@@ -53,30 +18,17 @@ export function setGlobalLogLevel(lev: ELogLevels) {
 export class LoggerParameters {
 
     public console: boolean = true;
-    public allowedConsole: {[key: string]: boolean} = {};
     public output: TOutputFunc | null = null;
 
     constructor(
         {
             console         = true,
             output          = null,
-            allowedConsole  = DEFAUT_ALLOWED_CONSOLE,
         }: {
             console?: boolean,
             output?: TOutputFunc | null,
-            allowedConsole?: {[key: string]: boolean },
         }) {
         this.console = console;
         this.output = output;
-        this.allowedConsole = allowedConsole;
-        if (level !== undefined && LOGS_LEVEL_TABLE[level] !== undefined) {
-            Object.keys(this.allowedConsole).forEach((key: string) => {
-                if (level === undefined) {
-                    return;
-                }
-                this.allowedConsole[key] = LOGS_LEVEL_TABLE[level].indexOf(key as ELogLevels) !== -1;
-
-            });
-        }
     }
 }
