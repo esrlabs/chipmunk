b90e973c7734f1ab0ec53fa2b3da87e3243d89bd
Oliver Mueller <oliver.mueller@esrlabs.com>
2020-03-29 20:32:48 +0200

    [](fix) add better errors for UDP DLT connections

--- application/apps/indexer/dlt/src/dlt_net.rs
+++ application/apps/indexer/dlt/src/dlt_net.rs
@@ -6,7 +6,6 @@ use crate::dlt_parse::*;
 use crate::fibex::FibexMetadata;
 use crate::filtering;
 use async_std::net::{Ipv4Addr, UdpSocket};
-use async_std::task;
 use crossbeam_channel as cc;
 use failure::err_msg;
 use futures::stream::StreamExt;
@@ -62,7 +61,7 @@ pub async fn index_from_socket2(
     initial_line_nr: usize,
     shutdown_receiver: async_std::sync::Receiver<()>,
 ) -> Result<(), ConnectionError> {
-    trace!("index_from_socket for socket conf: {:?}", socket_config);
+    debug!("index_from_socket: with socket conf: {:?}", socket_config);
     let (out_file, current_out_file_size) = utils::get_out_file_and_size(true, out_path)?;
     let tmp_dlt_file = create_dlt_session_file(&session_id)?;
 
@@ -72,13 +71,24 @@ pub async fn index_from_socket2(
     let mut buf_writer = BufWriter::with_capacity(10 * 1024 * 1024, out_file);
     let s = format!("{}:{}", socket_config.bind_addr, socket_config.port);
     let bind_addr_and_port: SocketAddr = s.parse()?;
-    let socket = UdpSocket::bind(bind_addr_and_port).await?;
+    debug!("create UDP socket by binding to: {}", bind_addr_and_port);
+    let socket = UdpSocket::bind(bind_addr_and_port).await.map_err(|e| {
+        warn!("error trying to bind to {}: {}", bind_addr_and_port, e);
+        err_msg(format!(
+            "you cannot not bind a UDP socket to {}",
+            socket_config.bind_addr
+        ))
+    })?;
     if let Some(multicast_info) = socket_config.multicast_addr {
         let multi_addr = multicast_info.multiaddr.parse()?;
         let inter = match multicast_info.interface {
             Some(s) => s.parse()?,
             None => Ipv4Addr::new(0, 0, 0, 0),
         };
+        debug!(
+            "joining UDP multicast group: {} on interface: {}",
+            multi_addr, inter
+        );
         if let Err(e) = socket.join_multicast_v4(multi_addr, inter) {
             return Err(ConnectionError::UnableToConnect {
                 reason: format!("error joining multicast group: {}", e),
@@ -154,109 +164,7 @@ pub async fn index_from_socket2(
     tmp_writer.flush()?;
     Ok(())
 }
-#[allow(clippy::too_many_arguments)]
-pub fn index_from_socket(
-    socket_config: SocketConfig,
-    filter_config: Option<filtering::ProcessedDltFilterConfig>,
-    update_channel: cc::Sender<ChunkResults>,
-    fibex_metadata: Option<Rc<FibexMetadata>>,
-    tag: &str,
-    out_path: &std::path::PathBuf,
-    initial_line_nr: usize,
-    shutdown_receiver: async_std::sync::Receiver<()>,
-) -> Result<(), ConnectionError> {
-    trace!("index_from_socket for socket conf: {:?}", socket_config);
-    let (out_file, current_out_file_size) = utils::get_out_file_and_size(true, out_path)?;
-    let mut chunk_factory = ChunkFactory::new(0, current_out_file_size);
-    let mut line_nr = initial_line_nr;
-    let mut buf_writer = BufWriter::with_capacity(10 * 1024 * 1024, out_file);
-    let mut chunk_count = 0usize;
-    let mut last_byte_index = 0usize;
-    task::block_on(async {
-        let s = format!("{}:{}", socket_config.bind_addr, socket_config.port);
-        let bind_addr_and_port: SocketAddr = s.parse()?;
-        let socket = UdpSocket::bind(bind_addr_and_port).await?;
-        if let Some(multicast_info) = socket_config.multicast_addr {
-            let multi_addr = multicast_info.multiaddr.parse()?;
-            let inter = match multicast_info.interface {
-                Some(s) => s.parse()?,
-                None => Ipv4Addr::new(0, 0, 0, 0),
-            };
-            if let Err(e) = socket.join_multicast_v4(multi_addr, inter) {
-                return Err(ConnectionError::UnableToConnect {
-                    reason: format!("error joining multicast group: {}", e),
-                });
-            }
-        }
-        trace!("created socket...");
-        // send (0,0),(0,0) to indicate connection established
-        let _ = update_channel.send(Ok(IndexingProgress::GotItem {
-            item: Chunk {
-                r: (0, 0),
-                b: (0, 0),
-            },
-        }));
-        let udp_msg_producer = UdpMessageProducer {
-            socket,
-            update_channel: update_channel.clone(),
-            fibex_metadata,
-            filter_config,
-        };
-        // listen for both a shutdown request and incomming messages
-        // to do this we need to select over streams of the same type
-        // the type we use to unify is this Event enum
-        enum Event {
-            Shutdown,
-            Msg(Result<Option<Message>, DltParseError>),
-        }
-        let shutdown_stream = shutdown_receiver.map(|_| {
-            debug!("shutdown_receiver event");
-            Event::Shutdown
-        });
-        let message_stream = udp_msg_producer.map(Event::Msg);
-        let mut event_stream = futures::stream::select(message_stream, shutdown_stream);
-        while let Some(event) = event_stream.next().await {
-            let maybe_msg = match event {
-                Event::Shutdown => {
-                    debug!("received shutdown through future channel");
-                    let _ = update_channel.send(Ok(IndexingProgress::Stopped));
-                    break;
-                }
-                Event::Msg(Ok(maybe_msg)) => maybe_msg,
-                Event::Msg(Err(DltParseError::ParsingHickup { reason })) => {
-                    let _ = update_channel.send(Err(Notification {
-                        severity: Severity::WARNING,
-                        content: format!("parsing faild for one message: {}", reason),
-                        line: None,
-                    }));
-                    None
-                }
-                Event::Msg(Err(DltParseError::Unrecoverable { .. })) => break,
-                Event::Msg(Err(DltParseError::IncompleteParse { .. })) => break,
-            };
-            match maybe_msg {
-                Some(msg) => {
-                    trace!("got msg ...({} bytes)", msg.as_bytes().len());
-                    let written_bytes_len =
-                        utils::create_tagged_line_d(tag, &mut buf_writer, &msg, line_nr, true)?;
-                    line_nr += 1;
-                    if let Some(chunk) =
-                        chunk_factory.create_chunk_if_needed(line_nr, written_bytes_len)
-                    {
-                        chunk_count += 1;
-                        last_byte_index = chunk.b.1;
-                        buf_writer.flush()?;
-                        let _ = update_channel.send(Ok(IndexingProgress::GotItem { item: chunk }));
-                    }
-                }
-                None => {
-                    trace!("msg was filtered");
-                }
-            }
-        }
-        Ok(())
-    })
-}
+
 #[allow(clippy::too_many_arguments)]
 pub async fn create_index_and_mapping_dlt_from_socket(
     session_id: String,
