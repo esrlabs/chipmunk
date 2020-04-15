b82fe95ed76feb6a3bd7a924cd79dd98882ddd18
DmitryAstafyev <dvastafyev@gmail.com>
2020-04-15 17:38:33 +0200

    [](782) addition OS information

--- application/electron/src/controllers/electron/menu.functions/handler.item.about.ts
+++ application/electron/src/controllers/electron/menu.functions/handler.item.about.ts
@@ -3,10 +3,12 @@ import { IPCMessages } from '../../../services/service.electron';
 import ServiceElectron from '../../../services/service.electron';
 import ServicePackage from '../../../services/service.package';
 
+import * as os from 'os';
+
 export default function about() {
     ServiceElectron.IPC.send(new IPCMessages.TabCustomAbout({
         version: ServicePackage.get().version,
         dependencies: ServicePackage.get().chipmunk.versions,
-        platform: process.platform,
+        platform: `${os.platform()}(${os.arch()}) / ${os.type()} / ${os.release()}`,
     }));
 }
