fc125a8b2708b721260886529bbf1f7a54d01d7c
DmitryAstafyev <dvastafyev@gmail.com>
2020-04-28 19:40:14 +0200

    [794](refact) update concat sidebar view (close #794)

--- application/client.core/src/app/environment/components/sidebar/merge/module.ts
+++ application/client.core/src/app/environment/components/sidebar/merge/module.ts
@@ -12,7 +12,7 @@ import { PrimitiveModule, ContainersModule      } from 'chipmunk-client-material
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
