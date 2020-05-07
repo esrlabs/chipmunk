cedb56340ef67189e9d1c35b8849def478a50792
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-05-07 16:14:28 +0200

    [806](feat) addition interfaces for settings

--- application/common/settings/field.ts
+++ application/common/settings/field.ts
@@ -1,4 +1,4 @@
-export enum ESettingsFieldControll {
+export enum ESettingController {
     checkbox = 'checkbox',
     dropdown = 'dropdown',
     list = 'list',
@@ -6,11 +6,75 @@ export enum ESettingsFieldControll {
     numinput = 'numinput',
 }
 
-export interface ISettingsField<T> {
-    name: string;
-    description: string;
-    element: ESettingsFieldControll;
-    value: T;
-    default: T;
-    values: T[];
+export enum ESettingKey {
+    GlobalGroup = 'global',
+    UpdateGroup = 'global.updates',
 }
+
+export interface IEntry {
+    key: string;
+    path: string;
+    name: string;
+    desc: string;
+}
+
+export interface IGroup extends IEntry {
+    // We need it just for naming
+}
+
+export interface IField<T> extends IEntry {
+    controller: ESettingController;
+    value: T;
+    validate: (value: T) => boolean;
+    default: T;
+    options: T[];
+}
+
+export interface IStore {
+    [key: string]: IField<any> | IGroup;
+}
+
+interface IEntryDesc {
+    name: string;
+    type: string;
+}
+
+const CEntryProps: IEntryDesc[] = [
+    { name: 'key', type: 'string' },
+    { name: 'path', type: 'string' },
+    { name: 'name', type: 'string' },
+    { name: 'desc', type: 'string' },
+];
+
+export function validate(entry: IEntry): Error | undefined {
+    if (typeof entry !== 'object' || entry === null) {
+        return new Error(`"entry" is not valid.`);
+    }
+    let error: Error | undefined;
+    CEntryProps.forEach((_entry: IEntryDesc) => {
+        if (error !== undefined) {
+            return;
+        }
+        if (typeof entry[_entry.name] !== typeof _entry.type) {
+            error = new Error(`Key "${_entry.name}" has wrong type "${typeof entry[_entry.name]}", but expected "${_entry.type}"`);
+        }
+    });
+    return error;
+}
+
+export function register(store: IStore, entry: IEntry): void {
+    
+};
+
+export function find(store: IStore, entry: IEntry): IStore | Error {
+    if (typeof store !== 'object' || store === null) {
+        return new Error(`"store" is not valid.`);
+    }
+    if (typeof entry !== 'object' || entry === null) {
+        return new Error(`"entry" is not valid.`);
+    }
+    if (entry.path === '') {
+
+    }
+};
+
