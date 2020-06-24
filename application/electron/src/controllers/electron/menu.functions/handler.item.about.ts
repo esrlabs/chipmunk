89fe1c6df8b610326652854189adb91e5c25fe8e
fur-gan <56543045+fur-gan@users.noreply.github.com>
2020-06-24 09:24:12 +0200

    [#797](refact) Apply review changes

--- application/electron/src/controllers/electron/menu.functions/handler.item.about.ts
+++ application/electron/src/controllers/electron/menu.functions/handler.item.about.ts
@@ -7,28 +7,30 @@ import * as os from 'os';
 
 const logger: Logger = new Logger('HandlerItemAbout');
 
+function _getRelease(raw: string) {
+    const regex = /\d{1,}\.\d{1,}\.\d{1,}/;
+    const match = raw.match(regex);
+    return (match === null) ? '' : match[0];
+}
+
+function _send(release: string) {
+    ServiceElectron.IPC.send(new IPCMessages.TabCustomAbout({
+        version: ServicePackage.get().version,
+        dependencies: ServicePackage.get().chipmunk.versions,
+        platform: `${os.platform()}(${os.arch()}) / ${os.type()} / ${release}`,
+    }));
+}
+
 export default function about() {
     const platform: string = os.platform();
     if (platform === 'darwin' || platform === 'linux') {
         return exec('uname -v', (error: ExecException | null, stdout: string, stderr: string) => {
-            if (error) {
-                logger.error(error.message);
-                return;
+            if (error || stderr.trim() === '') {
+                error ? logger.error(error.message) : logger.error(stderr);
+                return _send('');
             }
-            if (stderr.trim() === '') {
-                logger.error(stderr);
-            }
-            const regex = /\d{1,}\.\d{1,}\.\d{1,}/;
-            ServiceElectron.IPC.send(new IPCMessages.TabCustomAbout({
-                version: ServicePackage.get().version,
-                dependencies: ServicePackage.get().chipmunk.versions,
-                platform: `${os.platform()}(${os.arch()}) / ${os.type()} / ${stdout.match(regex)}`,
-            }));
+            _send(_getRelease(stdout));
         });
     }
-    ServiceElectron.IPC.send(new IPCMessages.TabCustomAbout({
-        version: ServicePackage.get().version,
-        dependencies: ServicePackage.get().chipmunk.versions,
-        platform: `${os.platform()}(${os.arch()}) / ${os.type()} / ${os.release()}`,
-    }));
+    _send(os.release());
 }
