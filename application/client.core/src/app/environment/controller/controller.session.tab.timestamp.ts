36d23ba9a1f5f23c8aa1cd44601fed65605fc7ce
DmitryAstafyev <dvastafyev@gmail.com>
2020-07-02 10:04:08 +0200

    [#835](fix) drop zoom if all ranges are removed

--- application/client.core/src/app/environment/controller/controller.session.tab.timestamp.ts
+++ application/client.core/src/app/environment/controller/controller.session.tab.timestamp.ts
@@ -602,13 +602,13 @@ export class ControllerSessionTabTimestamp {
             }
         });
         this._state.duration = Math.abs(this._state.min - this._state.max);
-        if (this._mode === EChartMode.aligned) {
-            return;
-        }
-        if ((this._state.max - this._cursor.right) - (this._state.min + this._cursor.left) < 0) {
+        if (((this._state.max - this._cursor.right) - (this._state.min + this._cursor.left) < 0) || 
+            (this._ranges.length === 0 && (this._cursor.left + this._cursor.right) !== 0)) {
             this._cursor.left = 0;
             this._cursor.right = 0;
-            this._subjects.zoom.next();
+            if (this._mode !== EChartMode.aligned) {
+                this._subjects.zoom.next();
+            }
         }
     }
 
