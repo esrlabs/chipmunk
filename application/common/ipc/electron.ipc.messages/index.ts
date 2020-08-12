5856efdf7d3b94aadcca7d043a7e9beab59d45b5
Dmitry Astafyev <DVAstafyev@gmail.com>
2020-08-12 17:20:28 +0200

    Revert "[#739](feat) Add shortcuts to save filter/chart"
    This reverts commit 214e64e5ce6f447098b8ad95706393c44d32f037.

--- application/common/ipc/electron.ipc.messages/index.ts
+++ application/common/ipc/electron.ipc.messages/index.ts
@@ -163,12 +163,6 @@ export { IStreamPtyResizeResponse, StreamPtyResizeResponse };
 import { ISearchResultMap, SearchResultMap, ISearchResultMapData } from './search.results.map';
 export { ISearchResultMap, SearchResultMap, ISearchResultMapData };
 
-import { SearchOSRequest } from './search.os.request';
-export { SearchOSRequest };
-
-import { SearchOSResponse, ISearchOSResponse } from './search.os.response';
-export { SearchOSResponse, ISearchOSResponse};
-
 import { ISearchChunk, SearchChunk } from './search.chunk';
 export { ISearchChunk, SearchChunk };
 
@@ -594,8 +588,6 @@ export type TMessage =  AppRestartRequest |
                         SearchRequestResults |
                         SearchResultMapState |
                         SearchUpdated |
-                        SearchOSRequest |
-                        SearchOSResponse |
                         SearchChunk |
                         SearchRecentRequest |
                         SearchRecentResponse |
@@ -779,8 +771,6 @@ export const Map = {
     [SearchRequestCancelResponse.signature      ]: SearchRequestCancelResponse,
     [SearchRequestResults.signature             ]: SearchRequestResults,
     [SearchResultMapState.signature             ]: SearchResultMapState,
-    [SearchOSRequest.signature                  ]: SearchOSRequest,
-    [SearchOSResponse.signature                 ]: SearchOSResponse,
     [SearchChunk.signature                      ]: SearchChunk,
     [SearchUpdated.signature                    ]: SearchUpdated,
     [SearchRecentRequest.signature              ]: SearchRecentRequest,
