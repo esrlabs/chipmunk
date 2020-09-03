8f0fe21899ac0d72853f3cadd0031e9d6bca409a
fur-gan <56543045+fur-gan@users.noreply.github.com>
2020-09-03 10:09:41 +0200

    [#894](fix) Fix drag&drop for filters placeholder

--- application/client.core/src/app/environment/components/sidebar/search.manager/filters/placeholder/component.ts
+++ application/client.core/src/app/environment/components/sidebar/search.manager/filters/placeholder/component.ts
@@ -1,11 +1,11 @@
-import { Component, OnDestroy, AfterContentInit, Input } from '@angular/core';
+import { Component, OnDestroy, Input, ViewChild } from '@angular/core';
 import { FilterRequest } from '../../../../../controller/controller.session.tab.search.filters.request';
 import { Subscription } from 'rxjs';
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Provider } from '../../providers/provider';
+import { SidebarAppSearchManagerListDirective } from '../../directives/list.directive';
+import SearchManagerService, { EListID, TRequest } from '../../service/service';
 import { Entity } from '../../providers/entity';
-
-import SearchManagerService, { EListID } from '../../service/service';
 
 @Component({
     selector: 'app-sidebar-app-searchmanager-filters',
@@ -13,16 +13,16 @@ import SearchManagerService, { EListID } from '../../service/service';
     styleUrls: ['./styles.less'],
 })
 
-export class SidebarAppSearchManagerFiltersPlaceholderComponent implements OnDestroy, AfterContentInit {
+export class SidebarAppSearchManagerFiltersPlaceholderComponent implements OnDestroy {
 
     @Input() provider: Provider<FilterRequest>;
+
+    @ViewChild(SidebarAppSearchManagerListDirective) listDirective: SidebarAppSearchManagerListDirective;
 
     public _ng_listID: EListID = EListID.filtersList;
     public _ng_empty = 'No filters are stored';
 
     private _subscriptions: { [key: string]: Subscription } = {};
-    private _droppedOut: boolean;
-    private _ignore: boolean;
 
     public ngOnDestroy() {
         Object.keys(this._subscriptions).forEach((key: string) => {
@@ -30,40 +30,12 @@ export class SidebarAppSearchManagerFiltersPlaceholderComponent implements OnDes
         });
     }
 
-    public ngAfterContentInit() {
-        this._subscriptions.mouseOver = SearchManagerService.observable.mouseOver.subscribe(this._onMouseOver.bind(this));
-        this._subscriptions.mouseOverGlobal = SearchManagerService.observable.mouseOverGlobal.subscribe(this._onMouseOverGlobal.bind(this));
-    }
-
-    public _ng_onItemDragged(event: CdkDragDrop<FilterRequest[]>) {
+    public _ng_onItemDragged(event: CdkDragDrop<Entity<TRequest>[]>) {
         SearchManagerService.onDragStart(false);
-        if (this._droppedOut) {
+        if (this.listDirective.droppedOut) {
             return;
         }
-        const prev = event.previousContainer;
-        const index = event.previousIndex;
-        if (prev.data !== undefined && (prev.data as any).disabled !== undefined) {
-            const outside: Entity<any> | undefined = (prev.data as any).disabled[event.previousIndex] !== undefined ? (prev.data as any).disabled[index] : undefined;
-            if (outside !== undefined && typeof outside.getEntity().getEntity === 'function' && outside.getEntity().getEntity() instanceof FilterRequest) {
-                this.provider.getSession().getSessionSearch().getDisabledAPI().getStorage().remove(outside.getEntity());
-                this.provider.getSession().getSessionSearch().getFiltersAPI().getStorage().add(outside.getEntity().getEntity(), event.currentIndex);
-            }
-        } else {
-            this.provider.reorder({ prev: event.previousIndex, curt: event.currentIndex });
-        }
-    }
-
-    private _onMouseOver(listID: EListID) {
-        this._ignore = true;
-        this._droppedOut = false;
-    }
-
-    private _onMouseOverGlobal() {
-        if (!this._ignore) {
-            this._droppedOut = true;
-        } else {
-            this._ignore = false;
-        }
+        this.provider.itemDragged(event);
     }
 
 }
