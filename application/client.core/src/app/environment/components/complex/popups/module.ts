74da22b4557405e85f3e0d3fdac0be86873786a6
DmitryAstafyev <dvastafyev@gmail.com>
2020-09-11 09:31:54 +0200

    Remove popup modifications

--- application/client.core/src/app/environment/components/complex/popups/module.ts
+++ application/client.core/src/app/environment/components/complex/popups/module.ts
@@ -3,14 +3,12 @@ import { CommonModule                           } from '@angular/common';
 
 import { PopupsComponent                        } from './component';
 import { PopupComponent                         } from './popup/component';
-import { ComplexPopupsDirective                 } from './directive/popups.directive';
 
 import { PrimitiveModule, ContainersModule      } from 'chipmunk-client-material';
 import { MatButtonModule } from '@angular/material/button';
 
 const entryComponents = [ PopupComponent ];
 const components = [ PopupsComponent, ...entryComponents ];
-const directives = [ ComplexPopupsDirective ];
 
 @NgModule({
     entryComponents : [ ...entryComponents ],
@@ -20,8 +18,8 @@ const directives = [ ComplexPopupsDirective ];
         ContainersModule,
         MatButtonModule
     ],
-    declarations    : [ ...components, ...directives ],
-    exports         : [ ...components, ...directives ]
+    declarations    : [ ...components ],
+    exports         : [ ...components ]
 })
 
 export class PopupsModule {
