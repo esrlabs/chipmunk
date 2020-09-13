ad2378e001c0ba8fcba0b45ff4022e502e0e9c78
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-09-13 22:15:17 +0200

    Apply updates of tookit on client.core

---
+++ application/client.core/src/app/environment/services/standalone/row.modifiers/modifiers.processor.ts
@@ -0,0 +1,56 @@
+
+import { Modifier, Priorities, EType, IHTMLInjection, IModifierRange } from 'chipmunk.client.toolkit';
+
+export class ModifierProcessor {
+
+    private _modifiers: Modifier[];
+    private _injections: IHTMLInjection[] = [];
+
+    constructor(modifiers: Modifier[]) {
+        this._modifiers = modifiers;
+    }
+
+    public parse(row: string): string {
+        this._injections = [];
+        Priorities.forEach((type: EType, index: number) => {
+            const subordinateTypes: EType[] = index !== Priorities.length - 1 ? Priorities.slice(index + 1, Priorities.length) : [];
+            const masters: Modifier[] = this._modifiers.filter(m => m.type() === type);
+            const subordinates: Modifier[] = this._modifiers.filter(m => subordinateTypes.indexOf(m.type()) !== -1);
+            if (masters.length === 0) {
+                return;
+            } else {
+                masters.sort((a, b) => a.getGroupPriority() > b.getGroupPriority() ? 1 : -1);
+                masters.forEach((master: Modifier, n: number) => {
+                    for (let i = n + 1; i <= masters.length - 1; i += 1) {
+                        masters[i].obey(master.getRanges());
+                    }
+                });
+            }
+            if (subordinates.length === 0) {
+                return;
+            } else {
+                subordinates.sort((a, b) => a.getGroupPriority() > b.getGroupPriority() ? 1 : -1);
+                masters.forEach((master: Modifier) => {
+                    subordinates.forEach((subordinate: Modifier) => {
+                        subordinate.obey(master.getRanges());
+                    });
+                });
+            }
+        });
+        this._modifiers.forEach((modifier: Modifier) => {
+            this._injections.push(...modifier.getInjections());
+        });
+        this._injections.sort((a: IHTMLInjection, b: IHTMLInjection) => {
+            return a.offset < b.offset ? 1 : -1;
+        });
+        this._injections.forEach((inj: IHTMLInjection) => {
+            row = row.substring(0, inj.offset) + inj.injection + row.substring(inj.offset, row.length);
+        });
+        return row;
+    }
+
+    public wasChanged(): boolean {
+        return this._injections.length > 0;
+    }
+
+}
