204a21dd938b1021e78fa64081c484af5bab1efc
DmitryAstafyev <dvastafyev@gmail.com>
2020-11-16 13:02:39 +0100

    Add comments and resolve require issue

--- application/apps/indexer-neon/src/api/common.ts
+++
@@ -1,5 +0,0 @@
-import { IChunk, INeonNotification, ITicks } from "../util/progress";
-
-export type ProgressEventHandler = (event: ITicks) => void;
-export type ChunkHandler = (event: IChunk) => void;
-export type NotificationHandler = (event: INeonNotification) => void;
