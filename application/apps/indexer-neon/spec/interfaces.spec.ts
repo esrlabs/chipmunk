4fa8a07a0777dc531103d2ecaedaa9dfc6fbbf33
DmitryAstafyev <dvastafyev@gmail.com>
2020-11-16 13:02:39 +0100

    Add comments and resolve require issue

--- application/apps/indexer-neon/spec/interfaces.spec.ts
+++ application/apps/indexer-neon/spec/interfaces.spec.ts
@@ -7,6 +7,19 @@
 // Manual start of defined test:
 // ./node_modules/.bin/jasmine-ts src/something.spec.ts
 
+// If you have error like next:
+//
+// Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './dist' is not defined by "exports" in ./application/apps/indexer-neon/node_modules/ts-node/package.json
+//
+// Yeu have to resolve it by hands. Open "./application/apps/indexer-neon/node_modules/ts-node/package.json"
+// Add there ("./dist": "./dist/index.js") into "exports" sections. Like this:
+//
+// "exports": {
+//     ".": "./dist/index.js",
+//     "./dist": "./dist/index.js",
+//     ...
+// }
+
 // Get rid of default Jasmine timeout
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 900000;
 
