78aab703c0f6cc0ff09770fdb86508ba3418eed0
DmitryAstafyev <dvastafyev@gmail.com>
2020-11-25 15:19:22 +0100

    Add jasmine test to electron level

---
+++ application/electron/spec/example.spec.ts
@@ -0,0 +1,37 @@
+// tslint:disable
+
+// We need to provide path to TypeScript types definitions
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
+
+// Manual start of defined test:
+// ./node_modules/.bin/jasmine-ts src/something.spec.ts
+
+// If you have error like next:
+//
+// Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './dist' is not defined by "exports" in ./application/apps/indexer-neon/node_modules/ts-node/package.json
+//
+// You have to resolve it by hands. Open "./application/apps/indexer-neon/node_modules/ts-node/package.json"
+// Add there ("./dist": "./dist/index.js") into "exports" sections. Like this:
+//
+// "exports": {
+//     ".": "./dist/index.js",
+//     "./dist": "./dist/index.js",
+//     ...
+// }
+
+// Get rid of default Jasmine timeout
+// jasmine.DEFAULT_TIMEOUT_INTERVAL = 900000;
+
+describe('Example of test', () => {
+
+    it('Example done', (done: Function)=> {
+        done();
+    });
+
+    it('Example fail', (done: Function)=> {
+        fail(`Because it's a test`);
+        done();
+    });
+
+});
