ca3bd1c87fc1b7ba8bd722d7e33da665f52bcb6a
fur-gan <56543045+fur-gan@users.noreply.github.com>
2020-11-26 06:24:42 +0100

    [#960](fix) Focus on dialog button

--- application/client.core/src/app/environment/directives/module.ts
+++ application/client.core/src/app/environment/directives/module.ts
@@ -1,10 +1,11 @@
 import { NgModule                           } from '@angular/core';
 import { MatAutocompleteTriggerDirective    } from './material.autocomplete.directive';
 import { MatDragDropResetFeatureDirective   } from './material.dragdrop.directive';
+import { FocusDefaultButtonDirective        } from './focus.button.directive';
 
 @NgModule({
-    declarations    : [ MatAutocompleteTriggerDirective, MatDragDropResetFeatureDirective ],
-    exports         : [ MatAutocompleteTriggerDirective, MatDragDropResetFeatureDirective ]
+    declarations    : [ MatAutocompleteTriggerDirective, MatDragDropResetFeatureDirective, FocusDefaultButtonDirective ],
+    exports         : [ MatAutocompleteTriggerDirective, MatDragDropResetFeatureDirective, FocusDefaultButtonDirective ]
 })
 
 export class AppDirectiviesModule {
