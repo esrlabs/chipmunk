6feef9656e4ca250f5132bce8975fb9f8ac5d6d3
fur-gan <56543045+fur-gan@users.noreply.github.com>
2021-02-18 12:07:35 +0100

    [#1068](fix) Move navigation along with focus

--- application/client.core/src/app/environment/directives/module.ts
+++ application/client.core/src/app/environment/directives/module.ts
@@ -1,11 +1,10 @@
 import { NgModule                           } from '@angular/core';
-import { MatAutocompleteTriggerDirective    } from './material.autocomplete.directive';
 import { MatDragDropResetFeatureDirective   } from './material.dragdrop.directive';
 import { FocusDefaultButtonDirective        } from './focus.button.directive';
 
 @NgModule({
-    declarations    : [ MatAutocompleteTriggerDirective, MatDragDropResetFeatureDirective, FocusDefaultButtonDirective ],
-    exports         : [ MatAutocompleteTriggerDirective, MatDragDropResetFeatureDirective, FocusDefaultButtonDirective ]
+    declarations    : [ MatDragDropResetFeatureDirective, FocusDefaultButtonDirective ],
+    exports         : [ MatDragDropResetFeatureDirective, FocusDefaultButtonDirective ]
 })
 
 export class AppDirectiviesModule {
