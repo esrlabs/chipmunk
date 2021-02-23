5665c0139c92cb77d7ec79072127094b820f3843
DmitryAstafyev <dvastafyev@gmail.com>
2021-02-23 17:01:31 +0100

    Added ShellProcessListEvent event

---
+++ application/common/ipc/electron.ipc.messages/shell.process.list.event.ts
@@ -0,0 +1,28 @@
+import { IShellProcess } from './shell.process.list.response';
+
+export interface IShellProcessListEvent {
+    session: string;
+    processes: IShellProcess[];
+}
+
+export class ShellProcessListEvent {
+
+    public static signature: string = 'ShellProcessListEvent';
+    public signature: string = ShellProcessListEvent.signature;
+    public session: string;
+    public processes: IShellProcess[];
+
+    constructor(params: IShellProcessListEvent) {
+        if (typeof params !== 'object' || params === null) {
+            throw new Error(`Incorrect parameters for ShellProcessListEvent message`);
+        }
+        if (typeof params.session !== 'string' || params.session.trim() === '') {
+            throw new Error(`Expecting session to be a string`);
+        }
+        if (!(params.processes instanceof Array)) {
+            throw new Error(`Expecting shells to be an Array<string>`);
+        }
+        this.session = params.session;
+        this.processes = params.processes;
+    }
+}
