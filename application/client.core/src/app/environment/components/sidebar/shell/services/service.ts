b16403b93e88bf326a59350c26a1ea33fb584752
fur-gan <56543045+fur-gan@users.noreply.github.com>
2021-03-01 12:24:06 +0100

    [#1055](refact) Move BE communication to service

--- application/client.core/src/app/environment/components/sidebar/shell/services/service.ts
+++ application/client.core/src/app/environment/components/sidebar/shell/services/service.ts
@@ -1,6 +1,7 @@
 import { IShellProcess } from '../../../../../../../../common/ipc/electron.ipc.messages';
 import { Session } from '../../../../controller/session/session';
 import { Subscription } from 'rxjs';
+import { IPair } from '../../../../thirdparty/code/engine';
 
 import TabsSessionsService from '../../../../services/service.sessions.tabs';
 import EventsSessionService from '../../../../services/standalone/service.events.session';
@@ -8,6 +9,13 @@ import ElectronIpcService, { IPCMessages } from '../../../../services/service.el
 import SourcesService from '../../../../services/service.sources';
 
 import * as Toolkit from 'chipmunk.client.toolkit';
+
+export interface IInformation {
+    env: { [key: string]: string};
+    shells: string[];
+    shell: string;
+    pwd: string;
+}
 
 export class ShellService {
 
@@ -23,12 +31,83 @@ export class ShellService {
         this._subscriptions.shellProcessListEvent = ElectronIpcService.subscribe(IPCMessages.ShellProcessListEvent, this._onListUpdate.bind(this));
     }
 
-    public removeProcess(session: string, guid: string) {
-        if (this._processes[session] !== undefined) {
-            this._processes[session] = this._processes[session].filter((process: IShellProcess) => {
-                return process.guid !== guid;
+    public terminate(process: IShellProcess): Promise<void> {
+        const session: string = this._session.getGuid();
+        return new Promise((resolve, reject) => {
+            ElectronIpcService.request(new IPCMessages.ShellProcessKillRequest({ session: session, guid: process.guid }), IPCMessages.ShellProcessKillResponse).then((response: IPCMessages.ShellProcessKillResponse) => {
+            if (response.error !== undefined) {
+                reject(this._logger.error(`Fail to terminate process "${process.command}" due error: ${response.error}`));
+            } else {
+                this._removeProcess(session, process.guid);
+                resolve();
+            }
+            }).catch((error: Error) => {
+                reject(this._logger.error(`Fail to terminate process "${process.command}" due error: ${error.message}`));
             });
-        }
+        });
+    }
+
+    public getEnv(): Promise<IInformation> {
+        return new Promise((resolve, reject) => {
+            ElectronIpcService.request(new IPCMessages.ShellEnvRequest({ session: this._session.getGuid() }), IPCMessages.ShellEnvResponse).then((response: IPCMessages.ShellEnvResponse) => {
+                if (response.error !== undefined) {
+                    reject(this._logger.error(`Failed to reqeust environment information due to Error: ${response.error}`));
+                } else {
+                    resolve({
+                        env: Object.assign({}, response.env),
+                        shells: [...response.shells],
+                        shell: response.shell,
+                        pwd: response.pwd,
+                    });
+                }
+            }).catch((error: Error) => {
+                reject(this._logger.error(`Failed to reqeust environment information due to Error: ${error}`));
+            });
+        });
+    }
+
+    public clearRecent(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            ElectronIpcService.request(new IPCMessages.ShellRecentCommandsClearRequest(), IPCMessages.ShellRecentCommandsClearResponse).then((response: IPCMessages.ShellRecentCommandsClearResponse) => {
+                if (response.error) {
+                    return reject(this._logger.error(`Fail to reset recent commands due error: ${response.error}`));
+                }
+                resolve();
+            }).catch((error: Error) => {
+                reject(this._logger.error(`Fail send request to reset recent commands due error: ${error.message}`));
+            });
+        });
+    }
+
+    public runCommand(command: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            ElectronIpcService.request(new IPCMessages.ShellProcessRunRequest({ session: this._session.getGuid(), command: command }), IPCMessages.ShellProcessRunResponse).then((response: IPCMessages.ShellProcessRunResponse) => {
+                if (response.error !== undefined) {
+                    return reject(this._logger.error(`Failed to run command due Error: ${response.error}`));
+                }
+                resolve();
+            }).catch((error: Error) => {
+                reject(this._logger.error(`Failed to run command due Error: ${error.message}`));
+            });
+        });
+    }
+
+    public loadRecentCommands(): Promise<IPair[]> {
+        return new Promise((resolve, reject) => {
+            ElectronIpcService.request(new IPCMessages.ShellRecentCommandsRequest(), IPCMessages.ShellRecentCommandsResponse).then((response: IPCMessages.ShellRecentCommandsResponse) => {
+                resolve(response.commands.map((recent: string) => {
+                    return {
+                        id: '',
+                        caption: ' ',
+                        description: recent,
+                        tcaption: ' ',
+                        tdescription: recent
+                    };
+                }));
+            }).catch((error: Error) => {
+                reject(this._logger.error(`Fail to get list of recent commands due error: ${error.message}`));
+            });
+        });
     }
 
     public get session(): Session | undefined {
@@ -38,6 +117,14 @@ export class ShellService {
     public get processes(): IShellProcess[] {
         const procs = this._processes[this._session.getGuid()];
         return procs === undefined ? [] : procs;
+    }
+
+    private _removeProcess(session: string, guid: string) {
+        if (this._processes[session] !== undefined) {
+            this._processes[session] = this._processes[session].filter((process: IShellProcess) => {
+                return process.guid !== guid;
+            });
+        }
     }
 
     private _onListUpdate(response: IPCMessages.ShellProcessListEvent) {
