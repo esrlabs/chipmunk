5d3fa97e98284e7567ef5160aa1d1079b4867cd2
fur-gan <56543045+fur-gan@users.noreply.github.com>
2021-03-02 11:21:02 +0100

    [#1090](feat) Show spinner while chart generating

--- application/client.core/src/app/environment/components/views/chart/module.ts
+++ application/client.core/src/app/environment/components/views/chart/module.ts
@@ -1,5 +1,6 @@
 import { NgModule                                   } from '@angular/core';
 import { CommonModule                               } from '@angular/common';
+import { MatProgressSpinnerModule                   } from '@angular/material/progress-spinner';
 
 import { ViewChartComponent                         } from './component';
 import { ViewChartCanvasComponent                   } from './chart/component';
@@ -12,7 +13,7 @@ const components = [ ...entryComponents ];
 
 @NgModule({
     entryComponents : [ ...entryComponents ],
-    imports         : [ CommonModule, PrimitiveModule, ContainersModule ],
+    imports         : [ CommonModule, PrimitiveModule, ContainersModule, MatProgressSpinnerModule ],
     declarations    : [ ...components ],
     exports         : [ ...components ]
 })
