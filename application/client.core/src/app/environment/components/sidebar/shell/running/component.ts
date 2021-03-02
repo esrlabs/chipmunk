68a321972bd0c1f014126b839c3d03d4a8546df8
DmitryAstafyev <dvastafyev@gmail.com>
2021-03-02 10:44:22 +0100

    Add tooltip on running process

--- application/client.core/src/app/environment/components/sidebar/shell/running/component.ts
+++ application/client.core/src/app/environment/components/sidebar/shell/running/component.ts
@@ -1,5 +1,4 @@
-import { IShellProcess} from '../../../../../../../../common/ipc/electron.ipc.messages';
-import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ShellService } from '../services/service';
 import { Session } from '../../../../controller/session/session';
@@ -12,26 +11,35 @@ import EventsSessionService from '../../../../services/standalone/service.events
 
 import * as Toolkit from 'chipmunk.client.toolkit';
 
+interface IRunningProcessInfo {
+    running: string;
+    received: string;
+}
+
 @Component({
     selector: 'app-sidebar-app-shell-running',
     templateUrl: './template.html',
-    styleUrls: ['./styles.less']
+    styleUrls: ['./styles.less'],
 })
-
 export class SidebarAppShellRunningComponent implements OnDestroy, OnInit {
-
     @Input() public service: ShellService;
 
-    public _ng_running: IShellProcess[] = [];
+    public _ng_running: IPCMessages.IShellProcess[] = [];
 
     private _sessionID: string;
     private _subscriptions: { [key: string]: Toolkit.Subscription | Subscription } = {};
     private _logger: Toolkit.Logger = new Toolkit.Logger('SidebarAppShellRunningComponent');
+    private _updating: boolean = false;
 
     constructor() {
         this._sessionID = TabsSessionsService.getActive().getGuid();
-        this._subscriptions.onSessionChange = EventsSessionService.getObservable().onSessionChange.subscribe(this._onSessionChange.bind(this));
-        this._subscriptions.ShellProcessListEvent = ElectronIpcService.subscribe(IPCMessages.ShellProcessListEvent, this._onListUpdate.bind(this));
+        this._subscriptions.onSessionChange = EventsSessionService.getObservable().onSessionChange.subscribe(
+            this._onSessionChange.bind(this),
+        );
+        this._subscriptions.ShellProcessListEvent = ElectronIpcService.subscribe(
+            IPCMessages.ShellProcessListEvent,
+            this._onListUpdate.bind(this),
+        );
     }
 
     public ngOnInit() {
@@ -44,11 +52,27 @@ export class SidebarAppShellRunningComponent implements OnDestroy, OnInit {
         });
     }
 
-    public _ng_tooltip(process: IShellProcess): string {
-        return `Started at: ${new Date(process.stat.created).toLocaleTimeString()}`;
+    public _ng_info(process: IPCMessages.IShellProcess): IRunningProcessInfo {
+        function getRecievedAmount(recieved: number): string {
+            if (recieved < 1024) {
+                return `${recieved} bytes`;
+            } else if (recieved / 1024 < 1024) {
+                return `${(recieved / 1024).toFixed(2)} kB`;
+            } else if (recieved / 1024 / 1024 < 1024 * 1024) {
+                return `${(recieved / 1024 / 1024).toFixed(2)} Mb`;
+            } else {
+                return `${(recieved / 1024 / 1024 / 1024).toFixed(2)} Gb`;
+            }
+        }
+        // Update data in background
+        this._update(process.guid);
+        return {
+            running: `${((Date.now() - process.stat.created) / 1000).toFixed(2)} s`,
+            received: `${getRecievedAmount(process.stat.recieved)}`,
+        };
     }
 
-    public _ng_onContexMenu(event: MouseEvent, process: IShellProcess) {
+    public _ng_onContexMenu(event: MouseEvent, process: IPCMessages.IShellProcess) {
         const items: IMenuItem[] = [
             {
                 caption: 'Terminate',
@@ -56,7 +80,7 @@ export class SidebarAppShellRunningComponent implements OnDestroy, OnInit {
                     this.service.terminate(process).catch((error: string) => {
                         this._logger.error(error);
                     });
-                }
+                },
             },
         ];
         ContextMenuService.show({
@@ -66,14 +90,33 @@ export class SidebarAppShellRunningComponent implements OnDestroy, OnInit {
         });
     }
 
+    private _update(guid: string) {
+        if (!this._updating) {
+            this._updating = true;
+            this.service.getDetails(guid).then((details: IPCMessages.IShellProcess) => {
+                const index: number = this._ng_running.findIndex(p => p.guid === guid);
+                if (index === -1) {
+                    return;
+                }
+                this._ng_running[index].stat = details.stat;
+                this._updating = false;
+            }).catch((err: Error) => {
+                this._logger.warn(`Fail to request details of process "${guid}" due error: ${err.message}`);
+            });
+        }
+    }
+
     private _restoreSession() {
-        this.service.getRunning(this._sessionID).then((response: IPCMessages.ShellProcessListResponse) => {
-            if (response.session === this._sessionID) {
-                this._ng_running = this._colored(response.processes);
-            }
-        }).catch((error: string) => {
-            this._logger.error(error);
-        });
+        this.service
+            .getRunning(this._sessionID)
+            .then((response: IPCMessages.ShellProcessListResponse) => {
+                if (response.session === this._sessionID) {
+                    this._ng_running = this._colored(response.processes);
+                }
+            })
+            .catch((error: string) => {
+                this._logger.error(error);
+            });
     }
 
     private _onListUpdate(response: IPCMessages.ShellProcessListEvent) {
@@ -82,8 +125,8 @@ export class SidebarAppShellRunningComponent implements OnDestroy, OnInit {
         }
     }
 
-    private _colored(processes: IShellProcess[]): IShellProcess[] {
-        processes.forEach((process: IShellProcess) => {
+    private _colored(processes: IPCMessages.IShellProcess[]): IPCMessages.IShellProcess[] {
+        processes.forEach((process: IPCMessages.IShellProcess) => {
             process.meta.color = SourcesService.getSourceColor(process.meta.sourceId);
         });
         return processes;
@@ -95,5 +138,4 @@ export class SidebarAppShellRunningComponent implements OnDestroy, OnInit {
             this._restoreSession();
         }
     }
-
 }
