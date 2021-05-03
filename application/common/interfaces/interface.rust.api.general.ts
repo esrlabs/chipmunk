8db247703f4f33e543b73e6d3c88a482c4d56581
Oliver Mueller <oliver.mueller@esrlabs.com>
2021-05-03 17:21:18 +0200

    Upgrade time ranges detection implementation to new API

--- application/common/interfaces/interface.rust.api.general.ts
+++ application/common/interfaces/interface.rust.api.general.ts
@@ -24,7 +24,53 @@ export interface IGrabbedContent {
 	grabbed_elements: IGrabbedElement[],
 }
 
+/**
+ * Output for @grabStreamChunk method of session
+ * (application/apps/indexer-neon/src/native/native.session.ts)
+ */
 export interface IGrabbedElement {
-    source_id: string,
+	source_id: string,
     content: string,
+}
+
+/**
+ * Output for @grabSearchChunk method of session
+ * (application/apps/indexer-neon/src/native/native.session.ts)
+ */
+export interface IGrabbedSearchElement {
+	source_id: string,	// Id of source
+	position: number, 	// Original position in stream
+    content: string, 	// Row value
+}
+
+/**
+ * Output for @search method of session
+ * (application/apps/indexer-neon/src/native/native.session.ts)
+ */
+export interface IResultSearchElement {
+	position: number,	// Original position in stream
+	filters: number[],  // Indexes of matched filters, fit to indexes, which was 
+						// provided with search(filters: IFilter[]) 
+						// (application/apps/indexer-neon/src/native/native.session.ts)
+    content: string,    // Row value
+}
+
+
+/**
+ * Output for @extract method of session
+ * (application/apps/indexer-neon/src/native/native.session.ts)
+ */
+export interface IExtractDTFormatResult {
+    format: string;
+    reg: string;
+    timestamp: string;
+}
+
+/**
+ * Input for @extract method of session
+ * (application/apps/indexer-neon/src/native/native.session.ts)
+ */
+export interface IExtractDTFormatOptions {
+	input: string;
+	format?: string;
 }
