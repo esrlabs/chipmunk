2cb6e76751644b06072d5ab40c6c557d557b19e2
Oliver Mueller <oliver.mueller@esrlabs.com>
2021-06-14 10:59:15 +0200

    After pack hook

--- application/electron/scripts/afterPackHook.ts
+++ application/electron/scripts/afterPackHook.ts
@@ -106,32 +106,12 @@ function copyFolderExcept(srcFolder: string, destFolder: string, exceptions: str
 
 function taskCopying() {
     const dest = getNodeModulesPath();
-    const srcFolder = path.join(APPS_DIR, 'rustcore');
+    const srcFolder = path.join(APPS_DIR, 'rustcore/ts-bindings');
     const destFolder = `${dest}/rustcore`;
     if (!fs.existsSync(destFolder)) {
         fs.mkdirSync(destFolder);
     }
-    copyFolderExcept(srcFolder, destFolder, ["node_modules", "native", ".vscode"]);
-    const destNative = path.join(destFolder, "native");
-    const destNativeTarget = path.join(destNative, "target");
-    const destNativeRelease = path.join(destNativeTarget, "release");
-    if (!fs.existsSync(destNative)) {
-        fs.mkdirSync(destNative);
-    }
-    if (!fs.existsSync(destNativeTarget)) {
-        fs.mkdirSync(destNativeTarget);
-    }
-    if (!fs.existsSync(destNativeRelease)) {
-        fs.mkdirSync(destNativeRelease);
-    }
-    ["artifacts.json", "index.node"].forEach((fileName: string) => {
-        fs.copyFileSync(
-            path.join(srcFolder, "native", fileName),
-            path.join(destFolder, "native", fileName),
-        );
-    });
-    const neonResources = path.resolve(path.join(srcFolder, "native", "target", "release"));
-    copyFolderExcept(neonResources, destNativeRelease, ["build", "deps"]);
+    copyFolderExcept(srcFolder, destFolder, ["node_modules", ".vscode"]);
 }
 
 export default async (context: any) => {
