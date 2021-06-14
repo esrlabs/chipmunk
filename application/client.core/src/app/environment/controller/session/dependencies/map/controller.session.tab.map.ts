ee736bb7332277efad8f73c87c8d23dbd1879248
Oliver Mueller <oliver.mueller@esrlabs.com>
2021-06-14 11:05:49 +0200

    Implement search/map support

--- application/client.core/src/app/environment/controller/session/dependencies/map/controller.session.tab.map.ts
+++ application/client.core/src/app/environment/controller/session/dependencies/map/controller.session.tab.map.ts
@@ -6,24 +6,32 @@ import { ControllerSessionTabStream } from '../stream/controller.session.tab.str
 import { IPositionData } from '../output/controller.session.tab.stream.output';
 import { Lock } from '../../../helpers/lock';
 import { Dependency, SessionGetter } from '../session.dependency';
+import { CommonInterfaces } from '../../../../interfaces/interface.common';
 
 import ServiceElectronIpc from '../../../../services/service.electron.ipc';
 
 import * as Toolkit from 'chipmunk.client.toolkit';
 
-export interface IMapPoint {
-    column: number;
-    position: number;
-    color: string;
-    description: string;
-    reg?: string;
-    regs?: string[];
-}
-
 export interface IMapState {
     count: number;
     position: number;
     rowsInView: number;
+}
+
+export interface IMapItem {
+    filters: { [key: number]: {
+        color: string,
+        weight: number,
+        index: number,
+    } };
+    dominant: number;
+}
+
+export interface IMap {
+    filters: number;
+    columns: number;
+    max: number;
+    items: IMapItem[];
 }
 
 export interface IControllerSessionTabMap {
@@ -39,11 +47,6 @@ export interface IColumn {
     index: number;
 }
 
-export interface IMap {
-    points: IMapPoint[];
-    columns: number;
-}
-
 const CSettings = {
     columnWideWidth: 16,
     columnNarroweWidth: 8,
@@ -52,7 +55,7 @@ const CSettings = {
 
 export class ControllerSessionTabMap implements Dependency {
 
-    public static SearchResultMapRequestDelay: number = 250;
+    public static MAP_REQUEST_DELAY: number = 50;
 
     private _guid: string;
     private _logger: Toolkit.Logger;
@@ -65,22 +68,20 @@ export class ControllerSessionTabMap implements Dependency {
     private _expanded: boolean = false;
     private _width: number = CSettings.columnNarroweWidth;
     private _cached: {
-        src: IPC.ISearchResultMapData | undefined,
         map: IMap;
+        src: CommonInterfaces.API.ISearchMap,
         pending: {
             scale: number;
-            expanded: boolean;
             timer: any;
             requested: number;
             progress: boolean;
         };
     } = {
-        src: undefined,
-        map: { points: [], columns: 0 },
+        map: { items: [], columns: 0, filters: 0, max: 0 },
+        src: [],
         pending: {
             progress: false,
             scale: -1,
-            expanded: false,
             timer: -1,
             requested: -1,
         },
@@ -91,14 +92,12 @@ export class ControllerSessionTabMap implements Dependency {
         onPositionUpdate: Subject<IMapState>,
         onRepaint: Subject<void>,
         onRepainted: Subject<void>,
-        onRestyle: Subject<FilterRequest>,
         onMapRecalculated: Subject<IMap>,
     } = {
         onStateUpdate: new Subject(),
         onPositionUpdate: new Subject(),
         onRepaint: new Subject(),
         onRepainted: new Subject(),
-        onRestyle: new Subject(),
         onMapRecalculated: new Subject(),
     };
     private _session: SessionGetter;
@@ -111,10 +110,10 @@ export class ControllerSessionTabMap implements Dependency {
 
     public init(): Promise<void> {
         return new Promise((resolve, reject) => {
-            this._subscriptions.SearchResultMapUpdated = ServiceElectronIpc.subscribe(IPC.SearchResultMapUpdated, this._ipc_SearchResultMapUpdated.bind(this));
             this._subscriptions.StreamUpdated = ServiceElectronIpc.subscribe(IPC.StreamUpdated, this._ipc_onStreamUpdated.bind(this));
             this._subscriptions.onSearchDropped = this._session().getSessionSearch().getFiltersAPI().getObservable().dropped.subscribe(this._onSearchDropped.bind(this));
             this._subscriptions.onSearchStarted = this._session().getSessionSearch().getFiltersAPI().getObservable().searching.subscribe(this._onSearchStarted.bind(this));
+            this._subscriptions.onSearchComplited = this._session().getSessionSearch().getFiltersAPI().getObservable().complited.subscribe(this._onSearchComplited.bind(this));
             this._subscriptions.onPositionChanged = this._session().getStreamOutput().getObservable().onPositionChanged.subscribe(this._onPositionChanged.bind(this));
             this._subscriptions.onFiltersStyleUpdate = this._session().getSessionSearch().getFiltersAPI().getStorage().getObservable().changed.subscribe(this._onFiltersStyleUpdate.bind(this));
             resolve();
@@ -152,7 +151,6 @@ export class ControllerSessionTabMap implements Dependency {
         onPositionUpdate: Observable<IMapState>,
         onRepaint: Observable<void>,
         onRepainted: Observable<void>,
-        onRestyle: Observable<FilterRequest>,
         onMapRecalculated: Observable<IMap>,
     } {
         return {
@@ -160,7 +158,6 @@ export class ControllerSessionTabMap implements Dependency {
             onPositionUpdate: this._subjects.onPositionUpdate.asObservable(),
             onRepaint: this._subjects.onRepaint.asObservable(),
             onRepainted: this._subjects.onRepainted.asObservable(),
-            onRestyle: this._subjects.onRestyle.asObservable(),
             onMapRecalculated: this._subjects.onMapRecalculated.asObservable(),
         };
     }
@@ -179,7 +176,8 @@ export class ControllerSessionTabMap implements Dependency {
 
     public expanding() {
         this._expanded = !this._expanded;
-        this._cached.map = this._extractMap(this._expanded, this._cached.src.map);
+        this._cached.map = this._convert(this._cached.src);
+        this._subjects.onMapRecalculated.next(this._cached.map);
     }
 
     public getSettings(): {
@@ -198,25 +196,48 @@ export class ControllerSessionTabMap implements Dependency {
         return this._width;
     }
 
-    public requestMapCalculation(scale: number, force: boolean) {
+    public getClosedMatchRow(row: number): { index: number, position: number } | undefined {
+        return undefined;
+        // const points: IMapPoint[] = this._cached.map.points;
+        // if (points.length === 0) {
+        //     return;
+        // }
+        // if (isNaN(row) || !isFinite(row)) {
+        //     this._logger.warn(`Value of target row is incorrect.`);
+        // }
+        // const target: { index: number, position: number } = { index: 0, position: points[0].position };
+        // let distance: number = Math.abs(row - target.position);
+        // points.forEach((point: IMapPoint, i: number) => {
+        //     const _distance: number = Math.abs(row - point.position);
+        //     if (_distance < distance) {
+        //         distance = _distance;
+        //         target.position = point.position;
+        //         target.index = i;
+        //     }
+        // });
+        // return target;
+    }
+
+    public update(scale: number, force: boolean) {
         const request = () => {
             this._cached.pending.progress = true;
-            const expandedReq = this._cached.pending.expanded;
+            const requestedScale = this._cached.pending.scale;
             ServiceElectronIpc.request(new IPC.SearchResultMapRequest({
                 streamId: this._guid,
-                scale: this._cached.pending.scale,
-                details: expandedReq,
+                scale: requestedScale,
             }), IPC.SearchResultMapResponse).then((response: IPC.SearchResultMapResponse) => {
-                this._cached.src = response.getData();
-                this._cached.map = this._extractMap(expandedReq, this._cached.src.map);
-                this._subjects.onMapRecalculated.next({
-                    points: this._cached.map.points,
-                    columns: this._cached.map.columns,
-                });
+                if (requestedScale !== response.map.length) {
+                    this._logger.warn(`Map has diffrent to requested scale. Requested for ${requestedScale}, has been gotten for ${response.map.length}`);
+                    this._cached.pending.progress = false;
+                    return;
+                }
+                this._cached.src = response.map;
+                this._cached.map = this._convert(response.map);
+                this._subjects.onMapRecalculated.next(this._cached.map);
                 this._cached.pending.progress = false;
                 if (this._cached.pending.scale !== -1) {
                     // While IPC message was in progress we get new request.
-                    this.requestMapCalculation(this._cached.pending.scale, false);
+                    this.update(this._cached.pending.scale, false);
                 }
             }).catch((err: Error) => {
                 this._logger.warn(`Fail delivery search result map due error: ${err.message}`);
@@ -228,13 +249,12 @@ export class ControllerSessionTabMap implements Dependency {
             this._cached.pending.requested = Date.now();
         }
         this._cached.pending.scale = scale;
-        this._cached.pending.expanded = this._expanded;
         if (this._cached.pending.progress) {
             return;
         }
         clearTimeout(this._cached.pending.timer);
         const timeout: number = Date.now() - this._cached.pending.requested;
-        if (timeout > ControllerSessionTabMap.SearchResultMapRequestDelay || force) {
+        if (timeout > ControllerSessionTabMap.MAP_REQUEST_DELAY || force) {
             request();
         } else {
             this._cached.pending.timer = setTimeout(() => {
@@ -243,108 +263,103 @@ export class ControllerSessionTabMap implements Dependency {
         }
     }
 
-    public getMatchesMap(scale: number, range: { begin: number, end: number }): Promise<{
-        [key: number]: {
-            [key: string]: number;
-        };
-    }> {
+    public getMap(scale: number, range: { begin: number, end: number }): Promise<IMap> {
         return new Promise((resolve, reject) => {
             ServiceElectronIpc.request(new IPC.SearchResultMapRequest({
                 streamId: this._guid,
                 scale: scale,
                 range: range,
-                details: true
             }), IPC.SearchResultMapResponse).then((response: IPC.SearchResultMapResponse) => {
-                resolve(response.getData().map);
+                resolve(this._convert(response.map, true));
             }).catch((err: Error) => {
                 reject(new Error(this._logger.warn(`Fail delivery search result map due error: ${err.message}`)));
             });
         });
     }
 
-    private _extractMap(expanded: boolean, scaled: { [key: number]: { [key: string ]: number } }): IMap {
-        function max(matches: { [match: string]: number }): string {
-            let v: number = 0;
-            let n: string = '';
-            Object.keys(matches).forEach((key: string) => {
-                if (v < matches[key]) {
-                    v = matches[key];
-                    n = key;
+    private _convert(map: CommonInterfaces.API.ISearchMap, expanding?: boolean): IMap {
+        const expanded = expanding !== undefined ? expanding : this._expanded;
+        const stored = this._session().getSessionSearch().getFiltersAPI().getStorage().get();
+        const single = this._session().getSessionSearch().getFiltersAPI().isSingle();
+        let max: number = -1;
+        const mapItems = map.map((matches: number[][]) => {
+            let maxMatches: number = -1;
+            const mapItem: IMapItem = {
+                filters: {},
+                dominant: -1,
+            };
+            matches.forEach((match: number[]) => {
+                // match[0] - index of filter
+                // match[1] - number of filter's matches
+                if (match.length !== 2 && match.length !== 0) {
+                    this._logger.warn(`Unexpected length of match array. Expecting 2, but gets ${match.length}`);
+                    return;
+                }
+                if (match.length === 0) {
+                    // Empty position - no matches at all
+                    return;
+                }
+                const filterIndex: number = match[0];
+                if (!single && stored.length <= filterIndex) {
+                    this._logger.warn(`Filter with index = ${filterIndex} isn't found in storage (length = ${stored.length})`);
+                    return;
+                }
+                if (expanded) {
+                    mapItem.filters[filterIndex] = {
+                        color: single ? '' : (stored[filterIndex] !== undefined ? stored[filterIndex].getBackground() : ''),
+                        weight: match[1],
+                        index: filterIndex,
+                    };
+                }
+                if (maxMatches < match[1]) {
+                    maxMatches = match[1];
+                    mapItem.dominant = filterIndex;
+                }
+                if (max < match[1]) {
+                    max = match[1];
                 }
             });
-            return n;
-        }
-        const results: IMap = {
-            points: [],
-            columns: 0,
-        };
-        const map: { [key: string]: FilterRequest } = {};
-        this._session().getSessionSearch().getFiltersAPI().getStorage().get().forEach((request: FilterRequest) => {
-            map[request.asDesc().request] = request;
+            if (!expanded && mapItem.dominant !== -1) {
+                mapItem.filters[0] = {
+                    color: single ? '' : (stored[mapItem.dominant] !== undefined ? stored[mapItem.dominant].getBackground() : ''),
+                    weight: maxMatches,
+                    index: mapItem.dominant,
+                };
+                mapItem.dominant = 0;
+            }
+            return mapItem;
         });
-        let column: number = 0;
-        if (expanded) {
-            // Expanded
-            const columns: { [key: string]: number } = {};
-            Object.keys(scaled).forEach((position: number | string) => {
-                const matches: { [match: string]: number } = scaled[position];
-                Object.keys(matches).forEach((match: string) => {
-                    if (columns[match] === undefined) {
-                        columns[match] = column;
-                        column += 1;
-                    }
-                    const point: IMapPoint = {
-                        position: typeof position === 'number' ? position : parseInt(position, 10),
-                        color: map[match] === undefined ? '' : (map[match].getBackground() !== '' ? map[match].getBackground() : map[match].getColor()),
-                        column: columns[match],
-                        description: match,
-                        reg: match,
-                        regs: [match],
-                    };
-                    results.points.push(point);
-                });
-            });
-            results.columns = column;
-        } else {
-            // Single
-            Object.keys(scaled).forEach((position: number | string) => {
-                const matches: { [match: string]: number } = scaled[position];
-                const hotest: string = max(matches);
-                if (hotest !== '') {
-                    const all: string[] = Object.keys(matches);
-                    const point: IMapPoint = {
-                        position: typeof position === 'number' ? position : parseInt(position, 10),
-                        color: map[hotest] === undefined ? '' : (map[hotest].getBackground() !== '' ? map[hotest].getBackground() : map[hotest].getColor()),
-                        column: 0,
-                        description: all.join(', '),
-                        reg: hotest,
-                        regs: all,
-                    };
-                    results.points.push(point);
-                }
-            });
-            results.columns = 1;
-        }
-        return results;
+        return {
+            filters: (single || !expanded) ? 1 : stored.length,
+            columns: (single || !expanded) ? 1 : stored.length,
+            items: mapItems,
+            max: max,
+        };
     }
 
     private _onFiltersStyleUpdate(event: IFilterUpdateEvent) {
         if (!event.updated.colors) {
             return;
         }
-        this._subjects.onRestyle.next(event.filter);
+        this._cached.map = this._convert(this._cached.src);
+        this._subjects.onMapRecalculated.next(this._cached.map);
     }
 
     private _onSearchDropped() {
         // Lock update workflow
         this._lock.lock();
         // Trigger event
-        this._saveTriggerStateUpdate();
+        this._updated();
     }
 
     private _onSearchStarted() {
         // Unlock update workflow
         this._lock.unlock();
+    }
+
+    private _onSearchComplited() {
+        // Trigger event
+        this._updated();
     }
 
     private _onPositionChanged(position: IPositionData) {
@@ -354,7 +369,7 @@ export class ControllerSessionTabMap implements Dependency {
         this._subjects.onPositionUpdate.next(this._state);
     }
 
-    private _saveTriggerStateUpdate() {
+    private _updated() {
         this._subjects.onStateUpdate.next({
             count: this._state.count,
             position: this._state.position,
@@ -362,15 +377,11 @@ export class ControllerSessionTabMap implements Dependency {
         });
     }
 
-    private _ipc_SearchResultMapUpdated(message: IPC.SearchResultMapUpdated) {
-        this._saveTriggerStateUpdate();
-    }
-
     private _ipc_onStreamUpdated(message: IPC.StreamUpdated) {
         if (message.guid !== this._guid) {
             return;
         }
         this._state.count = message.rows;
-        this._saveTriggerStateUpdate();
+        this._updated();
     }
 }
