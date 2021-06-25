12a6e5e0b14deed9fd3ed415df7f39ccfe9e6bb5
DmitryAstafyev <dvastafyev@gmail.com>
2021-06-25 14:21:51 +0200

    Fix style issue of DLT stat dialog

--- application/client.core/src/app/environment/components/dialogs/module.ts
+++ application/client.core/src/app/environment/components/dialogs/module.ts
@@ -24,6 +24,7 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
@@ -70,6 +71,7 @@ const CDialogs = [
         MatSortModule,
         MatTableModule,
         MatProgressBarModule,
+        MatProgressSpinnerModule,
         MatCheckboxModule,
         MatButtonModule,
         MatSelectModule,
