e4b20976510d210f0a3736fefe1efe324fb91d9a
fur-gan <56543045+fur-gan@users.noreply.github.com>
2021-09-15 09:40:56 +0200

    [#1232](feat) Add process filter + refresh buttons
    [#1232](feat) Add process filter + refresh buttons
    
    [#1232](feat) Add process search + refresh buttons
    
    [#1232](feat) Add search for processes
    
    [#1232](feat) Refresh button processes and devices
    
    [#1232](feat) Change process search style
    
    [#1232](feat) Scroll to top on filtering
    
    [#1232](refact) Error strings to error object

--- application/client.core/src/app/environment/components/sidebar/adb/services/service.ts
+++ application/client.core/src/app/environment/components/sidebar/adb/services/service.ts
@@ -1,5 +1,5 @@
 import { Observable, Subject, Subscription } from 'rxjs';
-import { IAdbDevice, IAdbProcess, IAdbSession } from '../../../../../../../../common/interfaces/interface.adb';
+import { IAdbDevice, IAdbProcess } from '../../../../../../../../common/interfaces/interface.adb';
 
 import ElectronIpcService, { IPCMessages } from '../../../../services/service.electron.ipc';
 
@@ -17,17 +17,19 @@ export enum EAdbStatus {
 }
 
 export class SidebarAppAdbService {
-
     private _status: EAdbStatus = EAdbStatus.init;
     private _subscriptions: { [key: string]: Toolkit.Subscription | Subscription } = {};
     private _subjects: {
-        onAmount: Subject<IAmount>,
+        onAmount: Subject<IAmount>;
     } = {
         onAmount: new Subject<IAmount>(),
     };
 
     constructor() {
-        this._subscriptions.AdbStreamUpdated = ElectronIpcService.subscribe(IPCMessages.AdbStreamUpdated, this._onAdbStreamUpdated.bind(this));
+        this._subscriptions.AdbStreamUpdated = ElectronIpcService.subscribe(
+            IPCMessages.AdbStreamUpdated,
+            this._onAdbStreamUpdated.bind(this),
+        );
     }
 
     public destroy() {
@@ -37,7 +39,7 @@ export class SidebarAppAdbService {
     }
 
     public getObservable(): {
-        onAmount: Observable<IAmount>,
+        onAmount: Observable<IAmount>;
     } {
         return {
             onAmount: this._subjects.onAmount.asObservable(),
@@ -54,73 +56,100 @@ export class SidebarAppAdbService {
 
     public getDevices(request: IPCMessages.IAdbDevicesRequest): Promise<IAdbDevice[]> {
         return new Promise((resolve, reject) => {
-            ElectronIpcService.request(new IPCMessages.AdbDevicesRequest(request), IPCMessages.AdbDevicesResponse).then((response: IPCMessages.AdbDevicesResponse) => {
-                if (response.error !== undefined) {
+            ElectronIpcService.request(
+                new IPCMessages.AdbDevicesRequest(request),
+                IPCMessages.AdbDevicesResponse,
+            )
+                .then((response: IPCMessages.AdbDevicesResponse) => {
+                    if (response.error !== undefined) {
+                        this._status = EAdbStatus.error;
+                        return reject(new Error(response.error));
+                    }
+                    this._status = EAdbStatus.ready;
+                    resolve(response.devices);
+                })
+                .catch((error: Error) => {
                     this._status = EAdbStatus.error;
-                    return reject(response.error);
-                }
-                this._status = EAdbStatus.ready;
-                resolve(response.devices);
-            }).catch((error: Error) => {
-                this._status = EAdbStatus.error;
-                reject(error);
-            });
+                    reject(error);
+                });
         });
     }
 
     public getProcesses(request: IPCMessages.IAdbProcessesRequest): Promise<IAdbProcess[]> {
         return new Promise((resolve, reject) => {
-            ElectronIpcService.request(new IPCMessages.AdbProcessesRequest(request), IPCMessages.AdbProcessesResponse).then((response: IPCMessages.AdbProcessesResponse) => {
-                if (response.error !== undefined) {
-                    return reject(response.error);
-                }
-                resolve(response.processes);
-            }).catch((error: Error) => {
-                reject(error.message);
-            });
+            ElectronIpcService.request(
+                new IPCMessages.AdbProcessesRequest(request),
+                IPCMessages.AdbProcessesResponse,
+            )
+                .then((response: IPCMessages.AdbProcessesResponse) => {
+                    if (response.error !== undefined) {
+                        return reject(new Error(response.error));
+                    }
+                    resolve(response.processes);
+                })
+                .catch((error: Error) => {
+                    reject(error);
+                });
         });
     }
 
     public start(request: IPCMessages.IAdbStartRequest): Promise<void> {
         return new Promise((resolve, reject) => {
-            ElectronIpcService.request(new IPCMessages.AdbStartRequest(request), IPCMessages.AdbStartResponse).then((response: IPCMessages.AdbStartResponse) => {
-                resolve();
-            }).catch((error: Error) => {
-                reject(error.message);
-            });
+            ElectronIpcService.request(
+                new IPCMessages.AdbStartRequest(request),
+                IPCMessages.AdbStartResponse,
+            )
+                .then((response: IPCMessages.AdbStartResponse) => {
+                    resolve();
+                })
+                .catch((error: Error) => {
+                    reject(error);
+                });
         });
     }
 
     public stop(request: IPCMessages.IAdbStopRequest): Promise<void> {
         return new Promise((resolve, reject) => {
-            ElectronIpcService.request(new IPCMessages.AdbStopRequest(request), IPCMessages.AdbStopResponse).then((response: IPCMessages.AdbStopResponse) => {
-                if (response.error !== undefined) {
-                    return reject(response.error);
-                }
-                resolve();
-            }).catch((error: Error) => {
-                reject(error);
-            });
+            ElectronIpcService.request(
+                new IPCMessages.AdbStopRequest(request),
+                IPCMessages.AdbStopResponse,
+            )
+                .then((response: IPCMessages.AdbStopResponse) => {
+                    if (response.error !== undefined) {
+                        return reject(new Error(response.error));
+                    }
+                    resolve();
+                })
+                .catch((error: Error) => {
+                    reject(error);
+                });
         });
     }
 
     public change(request: IPCMessages.IAdbStartRequest): Promise<void> {
         return new Promise((resolve, reject) => {
-            this.start(request).then(() => {
-                resolve();
-            }).catch((error: string) => {
-                reject(error);
-            });
+            this.start(request)
+                .then(() => {
+                    resolve();
+                })
+                .catch((error: Error) => {
+                    reject(error);
+                });
         });
     }
 
     public restore(request: IPCMessages.IAdbLoadRequest): Promise<IPCMessages.AdbLoadResponse> {
         return new Promise((resolve, reject) => {
-            ElectronIpcService.request(new IPCMessages.AdbLoadRequest(request), IPCMessages.AdbLoadResponse).then((response: IPCMessages.AdbLoadResponse) => {
-                resolve(response);
-            }).catch((error: Error) => {
-                reject(error.message);
-            });
+            ElectronIpcService.request(
+                new IPCMessages.AdbLoadRequest(request),
+                IPCMessages.AdbLoadResponse,
+            )
+                .then((response: IPCMessages.AdbLoadResponse) => {
+                    resolve(response);
+                })
+                .catch((error: Error) => {
+                    reject(error);
+                });
         });
     }
 
@@ -130,14 +159,16 @@ export class SidebarAppAdbService {
         } else if (amount / 1024 < 1024) {
             return `${(amount / 1024).toFixed(2)} kB`;
         } else if (amount / 1024 / 1024 < 1024 * 1024) {
-            return`${(amount / 1024 / 1024).toFixed(4)} Mb`;
+            return `${(amount / 1024 / 1024).toFixed(4)} Mb`;
         } else {
             return `${(amount / 1024 / 1024 / 1024).toFixed(5)} Gb`;
         }
     }
 
     private _onAdbStreamUpdated(response: IPCMessages.AdbStreamUpdated) {
-        this._subjects.onAmount.next({ session: response.guid, amount: this.bytesToString(response.amount) })
+        this._subjects.onAmount.next({
+            session: response.guid,
+            amount: this.bytesToString(response.amount),
+        });
     }
-
 }
--- application/electron/src/controllers/stream.adb/controller.ts
+++ application/electron/src/controllers/stream.adb/controller.ts
@@ -10,7 +10,6 @@ import Logger from '../../tools/env.logger';
 import * as Tools from '../../tools/index';
 
 export default class ControllerStreamAdb {
-
     private _logger: Logger;
     private _guid: string;
     private _process?: Process;
@@ -27,21 +26,62 @@ export default class ControllerStreamAdb {
     constructor(guid: string) {
         this._guid = guid;
         this._logger = new Logger(`ControllerStreamAdb: ${guid}`);
-        ServiceElectron.IPC.subscribe(IPC.AdbStartRequest, (this._ipc_AdbStartRequest.bind(this) as any)).then((subscription: Subscription) => {
-            this._subscriptions.AdbStartRequest = subscription;
-        }).catch((err: Error) => this._logger.error(`Fail to subscribe to AdbStartRequest due error: ${err.message}`));
-        ServiceElectron.IPC.subscribe(IPC.AdbProcessesRequest, (this._ipc_AdbProcessesRequest.bind(this) as any)).then((subscription: Subscription) => {
-            this._subscriptions.AdbProcessesRequest = subscription;
-        }).catch((err: Error) => this._logger.error(`Fail to subscribe to AdbProcessesRequest due error: ${err.message}`));
-        ServiceElectron.IPC.subscribe(IPC.AdbDevicesRequest, (this._ipc_AdbDevicesRequest.bind(this) as any)).then((subscription: Subscription) => {
-            this._subscriptions.AdbDevicesRequest = subscription;
-        }).catch((err: Error) => this._logger.error(`Fail to subscribe to AdbDevicesRequest due error: ${err.message}`));
-        ServiceElectron.IPC.subscribe(IPC.AdbLoadRequest, (this._ipc_AdbLoadRequest.bind(this) as any)).then((subscription: Subscription) => {
-            this._subscriptions.AdbLoadRequest = subscription;
-        }).catch((err: Error) => this._logger.error(`Fail to subscribe to AdbLoadRequest due error: ${err.message}`));
-        ServiceElectron.IPC.subscribe(IPC.AdbStopRequest, (this._ipc_AdbStopRequest.bind(this) as any)).then((subscription: Subscription) => {
-            this._subscriptions.AdbStopRequest = subscription;
-        }).catch((err: Error) =>  this._logger.error(`Fail to subscribe to AdbStopRequest due error: ${err.message}`));
+        ServiceElectron.IPC.subscribe(
+            IPC.AdbStartRequest,
+            this._ipc_AdbStartRequest.bind(this) as any,
+        )
+            .then((subscription: Subscription) => {
+                this._subscriptions.AdbStartRequest = subscription;
+            })
+            .catch((err: Error) =>
+                this._logger.error(
+                    `Fail to subscribe to AdbStartRequest due error: ${err.message}`,
+                ),
+            );
+        ServiceElectron.IPC.subscribe(
+            IPC.AdbProcessesRequest,
+            this._ipc_AdbProcessesRequest.bind(this) as any,
+        )
+            .then((subscription: Subscription) => {
+                this._subscriptions.AdbProcessesRequest = subscription;
+            })
+            .catch((err: Error) =>
+                this._logger.error(
+                    `Fail to subscribe to AdbProcessesRequest due error: ${err.message}`,
+                ),
+            );
+        ServiceElectron.IPC.subscribe(
+            IPC.AdbDevicesRequest,
+            this._ipc_AdbDevicesRequest.bind(this) as any,
+        )
+            .then((subscription: Subscription) => {
+                this._subscriptions.AdbDevicesRequest = subscription;
+            })
+            .catch((err: Error) =>
+                this._logger.error(
+                    `Fail to subscribe to AdbDevicesRequest due error: ${err.message}`,
+                ),
+            );
+        ServiceElectron.IPC.subscribe(
+            IPC.AdbLoadRequest,
+            this._ipc_AdbLoadRequest.bind(this) as any,
+        )
+            .then((subscription: Subscription) => {
+                this._subscriptions.AdbLoadRequest = subscription;
+            })
+            .catch((err: Error) =>
+                this._logger.error(`Fail to subscribe to AdbLoadRequest due error: ${err.message}`),
+            );
+        ServiceElectron.IPC.subscribe(
+            IPC.AdbStopRequest,
+            this._ipc_AdbStopRequest.bind(this) as any,
+        )
+            .then((subscription: Subscription) => {
+                this._subscriptions.AdbStopRequest = subscription;
+            })
+            .catch((err: Error) =>
+                this._logger.error(`Fail to subscribe to AdbStopRequest due error: ${err.message}`),
+            );
     }
 
     public destroy(): Promise<void> {
@@ -54,7 +94,10 @@ export default class ControllerStreamAdb {
         });
     }
 
-    private _ipc_AdbStartRequest(request: IPC.AdbStartRequest, response: (response: IPC.AdbStartResponse) => Promise<void>) {
+    private _ipc_AdbStartRequest(
+        request: IPC.AdbStartRequest,
+        response: (response: IPC.AdbStartResponse) => Promise<void>,
+    ) {
         if (request.session !== this._guid) {
             return;
         }
@@ -81,7 +124,10 @@ export default class ControllerStreamAdb {
         response(new IPC.AdbStartResponse({ guid: guid }));
     }
 
-    private _ipc_AdbStopRequest(request: IPC.AdbStopRequest, response: (response: IPC.AdbStopResponse) => Promise<void>) {
+    private _ipc_AdbStopRequest(
+        request: IPC.AdbStopRequest,
+        response: (response: IPC.AdbStopResponse) => Promise<void>,
+    ) {
         if (request.session !== this._guid) {
             return;
         }
@@ -89,77 +135,103 @@ export default class ControllerStreamAdb {
         this._session.device = undefined;
         this._session.pid = undefined;
         this._session.recieved = 0;
-        response(new IPC.AdbStopResponse({ error: error !== undefined ? error.message : undefined }));
+        response(
+            new IPC.AdbStopResponse({ error: error !== undefined ? error.message : undefined }),
+        );
     }
 
-    private _ipc_AdbProcessesRequest(request: IPC.AdbProcessesRequest, response: (response: IPC.AdbProcessesResponse) => Promise<void>) {
+    private _ipc_AdbProcessesRequest(
+        request: IPC.AdbProcessesRequest,
+        response: (response: IPC.AdbProcessesResponse) => Promise<void>,
+    ) {
         if (request.session !== this._guid) {
             return;
         }
-        exec(`adb -s ${request.device} shell ps -A`, this._getExecOpts(), (error: ExecException | null, stdout: string, stderr: string) => {
-            if (error || stderr) {
-                return response(new IPC.AdbDevicesResponse({ error: error ? error.message : stderr }));
-            }
-
-            let match: RegExpExecArray | null;
-            const split: string[] = stdout.split(/[\n\r]/g);
-            const processes: IAdbProcess[] = [];
-            const reg = /(\w+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\w+)\s+\[??(.+)\]?\s*$/g;
-            split.forEach((line: string) => {
-                match = reg.exec(line);
-                if (match !== null) {
-                    processes.push({
-                        user: match[1].toString(),
-                        pid: this._toNumber(match[2]),
-                        ppid: this._toNumber(match[3]),
-                        vsz: this._toNumber(match[4]),
-                        rss: this._toNumber(match[5]),
-                        wchan: this._toNumber(match[6]),
-                        addr: this._toNumber(match[7]),
-                        s: match[8],
-                        name: match[9],
-                    });
+        exec(
+            `adb -s ${request.device} shell ps -A`,
+            this._getExecOpts(),
+            (error: ExecException | null, stdout: string, stderr: string) => {
+                if (error || stderr) {
+                    return response(
+                        new IPC.AdbDevicesResponse({ error: error ? error.message : stderr }),
+                    );
                 }
-            });
-            this._session.processes = processes;
-            return response(new IPC.AdbProcessesResponse({processes: processes }));
-        });
+
+                let match: RegExpExecArray | null;
+                const split: string[] = stdout.split(/[\n\r]/g);
+                const processes: IAdbProcess[] = [];
+                const reg =
+                    /(\w+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\w+)\s+\[??(.+)\]?\s*$/g;
+                split.forEach((line: string) => {
+                    match = reg.exec(line);
+                    if (match !== null) {
+                        processes.push({
+                            user: match[1].toString(),
+                            pid: this._toNumber(match[2]),
+                            ppid: this._toNumber(match[3]),
+                            vsz: this._toNumber(match[4]),
+                            rss: this._toNumber(match[5]),
+                            wchan: this._toNumber(match[6]),
+                            addr: this._toNumber(match[7]),
+                            s: match[8],
+                            name: match[9],
+                        });
+                    }
+                });
+                this._session.processes = processes;
+                return response(new IPC.AdbProcessesResponse({ processes: processes }));
+            },
+        );
     }
 
-    private _ipc_AdbDevicesRequest(request: IPC.AdbDevicesRequest, response: (response: IPC.AdbDevicesResponse) => Promise<void>) {
+    private _ipc_AdbDevicesRequest(
+        request: IPC.AdbDevicesRequest,
+        response: (response: IPC.AdbDevicesResponse) => Promise<void>,
+    ) {
         if (request.session !== this._guid) {
             return;
         }
-        exec('adb devices', this._getExecOpts() , (error: ExecException | null, stdout: string, stderr: string) => {
-            if (error || stderr) {
-                return response(new IPC.AdbDevicesResponse({ error: error ? error.message : stderr }));
-            }
-            let match: RegExpExecArray | null;
-            const split: string[] = stdout.split(/[\n\r]/g);
-            const devices: IAdbDevice[] = [];
-            const reg = /(.*)\t(.*)/g;
-            split.forEach((line: string) => {
-                match = reg.exec(line);
-                if (match !== null) {
-                    devices.push({
-                        name: match[1],
-                        type: match[3],
-                    });
+        exec(
+            'adb devices',
+            this._getExecOpts(),
+            (error: ExecException | null, stdout: string, stderr: string) => {
+                if (error || stderr) {
+                    return response(
+                        new IPC.AdbDevicesResponse({ error: error ? error.message : stderr }),
+                    );
                 }
-            });
-            this._session.devices = devices;
-            return response(new IPC.AdbDevicesResponse({ devices: devices }));
-        });
+                let match: RegExpExecArray | null;
+                const split: string[] = stdout.split(/[\n\r]/g);
+                const devices: IAdbDevice[] = [];
+                const reg = /(.*)\t(.*)/g;
+                split.forEach((line: string) => {
+                    match = reg.exec(line);
+                    if (match !== null) {
+                        devices.push({
+                            name: match[1],
+                            type: match[3],
+                        });
+                    }
+                });
+                this._session.devices = devices;
+                return response(new IPC.AdbDevicesResponse({ devices: devices }));
+            },
+        );
     }
 
-    private _ipc_AdbLoadRequest(request: IPC.AdbLoadRequest, response: (response: IPC.AdbLoadResponse) => Promise<void>) {
+    private _ipc_AdbLoadRequest(
+        request: IPC.AdbLoadRequest,
+        response: (response: IPC.AdbLoadResponse) => Promise<void>,
+    ) {
         if (request.session !== this._guid) {
             return;
         }
-        response(new IPC.AdbLoadResponse({
-            session: this._guid,
-            data: this._session
-        }));
+        response(
+            new IPC.AdbLoadResponse({
+                session: this._guid,
+                data: this._session,
+            }),
+        );
     }
 
     private _toNumber(input: string): number | undefined {
