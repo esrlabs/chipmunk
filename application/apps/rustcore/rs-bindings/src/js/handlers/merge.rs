99dcb9cf94ad179ef4977944a7e31fced51e1779
DmitryAstafyev <dvastafyev@gmail.com>
2021-11-10 11:27:27 +0100

    Progress for concat/merge

--- application/apps/rustcore/rs-bindings/src/js/handlers/merge.rs
+++ application/apps/rustcore/rs-bindings/src/js/handlers/merge.rs
@@ -1,12 +1,12 @@
 use super::assign;
 use crate::js::session::{
-    events::{NativeError, NativeErrorKind},
+    events::{CallbackEvent, NativeError, NativeErrorKind},
     operations::{OperationAPI, OperationResult},
     state::SessionStateAPI,
     SupportedFileType,
 };
 use crossbeam_channel as cc;
-use indexer_base::progress::Severity;
+use indexer_base::progress::{IndexingProgress, Progress, Severity, Ticks};
 use merging::merger::{merge_files_use_config, FileMergeOptions};
 use std::path::Path;
 
@@ -20,13 +20,13 @@ pub async fn handle(
     source_id: String,
     state: SessionStateAPI,
 ) -> OperationResult<()> {
-    let (tx, _rx) = cc::unbounded();
+    let (tx_progress, rx_progress) = cc::unbounded();
     merge_files_use_config(
         files,
         out_path,
         append,
         500,
-        tx,
+        tx_progress,
         Some(operation_api.get_cancellation_token()),
     )
     .map_err(|err| NativeError {
@@ -34,5 +34,32 @@ pub async fn handle(
         kind: NativeErrorKind::OperationSearch,
         message: Some(format!("Failed to merge files: {}", err)),
     })?;
+    while let Ok(msg) = rx_progress.recv() {
+        match msg {
+            Ok(msg) => match msg {
+                IndexingProgress::Stopped | IndexingProgress::Finished => {
+                    operation_api.emit(CallbackEvent::Progress {
+                        uuid: operation_api.id(),
+                        progress: Progress::Stopped,
+                    });
+                }
+                IndexingProgress::Progress { ticks } => {
+                    let (count, total) = ticks;
+                    operation_api.emit(CallbackEvent::Progress {
+                        uuid: operation_api.id(),
+                        progress: Progress::Ticks(Ticks { count, total }),
+                    });
+                }
+                IndexingProgress::GotItem { item } => {
+                    let (_, rows): (usize, usize) = item.r;
+                    operation_api.emit(CallbackEvent::StreamUpdated(rows as u64));
+                }
+            },
+            Err(notification) => operation_api.emit(CallbackEvent::Progress {
+                uuid: operation_api.id(),
+                progress: Progress::Notification(notification),
+            }),
+        }
+    }
     assign::handle(operation_api, out_path, source_type, source_id, state).await
 }
