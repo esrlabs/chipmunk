7d0b127fb050c6961712e1adc9094d2a98a5cb3e
Oliver Mueller <oliver.mueller@esrlabs.com>
2021-11-30 18:22:39 +0100

    updated cargo dependencies

--- application/apps/indexer/session/src/lib.rs
+++ application/apps/indexer/session/src/lib.rs
@@ -3,12 +3,3 @@ mod handlers;
 pub mod operations;
 pub mod session;
 pub mod state;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
