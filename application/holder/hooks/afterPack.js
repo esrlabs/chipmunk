63d5a3ff740781b067b80457f6bd4a059fb8e28c
DmitryAstafyev <DVAstafyev@gmail.com>
2022-07-22 14:05:03 +0200

    Build mac bumdle using hooks

--- application/holder/hooks/afterPack.js
+++ application/holder/hooks/afterPack.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const LAUNCHERS = path.normalize(path.join(__dirname, '../../apps/launchers/target/release/'));
-const RELEASE_BUILD_FOLDER = path.normalize(
+const RELEASE_BIN_FOLDER = path.normalize(
     path.join(
         __dirname,
         `../release/${(() => {
@@ -17,11 +16,24 @@ const RELEASE_BUILD_FOLDER = path.normalize(
         })()}`,
     ),
 );
+const RELEASE_BINRES_FOLDER = path.normalize(
+    path.join(
+        __dirname,
+        `../release/${(() => {
+            switch (process.platform) {
+                case 'darwin':
+                    return 'mac/chipmunk.app/Contents/Resources/bin';
+                case 'linux':
+                    return 'linux-unpacked/Resources/bin';
+                default:
+                    return 'win-unpacked/Resources/bin';
+            }
+        })()}`,
+    ),
+);
 const LAUNCHER_NAME = 'launcher';
 const APPLICATION_NAME = 'chipmunk';
 const APP_NAME = 'app';
-const UPDATER_NAME = 'updater';
-const CLI_NAME = 'cm';
 
 function getExecutable(filename) {
     const extention = process.platform === 'win32' ? '.exe' : '';
@@ -29,29 +41,13 @@ function getExecutable(filename) {
 }
 
 function delivery() {
-    const appFile = path.normalize(
-        path.join(RELEASE_BUILD_FOLDER, getExecutable(APPLICATION_NAME)),
-    );
-    const launcherFile = path.normalize(path.join(LAUNCHERS, getExecutable(LAUNCHER_NAME)));
     fs.renameSync(
-        appFile,
-        path.normalize(path.join(RELEASE_BUILD_FOLDER, getExecutable(APP_NAME))),
-    );
-    fs.renameSync(
-        launcherFile,
-        path.normalize(path.join(LAUNCHERS, getExecutable(APPLICATION_NAME))),
+        path.normalize(path.join(RELEASE_BIN_FOLDER, getExecutable(APPLICATION_NAME))),
+        path.normalize(path.join(RELEASE_BIN_FOLDER, getExecutable(APP_NAME))),
     );
     fs.copyFileSync(
-        path.normalize(path.join(LAUNCHERS, getExecutable(APPLICATION_NAME))),
-        path.normalize(path.join(RELEASE_BUILD_FOLDER, getExecutable(APPLICATION_NAME))),
-    );
-    fs.copyFileSync(
-        path.normalize(path.join(LAUNCHERS, getExecutable(UPDATER_NAME))),
-        path.normalize(path.join(RELEASE_BUILD_FOLDER, getExecutable(UPDATER_NAME))),
-    );
-    fs.copyFileSync(
-        path.normalize(path.join(LAUNCHERS, getExecutable(CLI_NAME))),
-        path.normalize(path.join(RELEASE_BUILD_FOLDER, getExecutable(CLI_NAME))),
+        path.normalize(path.join(RELEASE_BINRES_FOLDER, getExecutable(LAUNCHER_NAME))),
+        path.normalize(path.join(RELEASE_BIN_FOLDER, getExecutable(APPLICATION_NAME))),
     );
 }
 
