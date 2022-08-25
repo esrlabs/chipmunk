6c735185630737d265e8db8857bb396e15d0a957
DmitryAstafyev <dvastafyev@gmail.com>
2022-08-25 15:16:25 +0200

    Rollback processes/commands implementation

--- application/apps/indexer/sources/src/command/process.rs
+++ application/apps/indexer/sources/src/command/process.rs
@@ -1,14 +1,12 @@
-use crate::{
-    command::sde::{SdeRequest, SdeResponse, WriteResponse},
-    ByteSource, Error as SourceError, ReloadInfo, SourceFilter,
-};
+use crate::{ByteSource, Error as SourceError, ReloadInfo, SourceFilter};
 use async_trait::async_trait;
 use buf_redux::Buffer;
+use futures;
 use std::{collections::HashMap, process::Stdio};
 use thiserror::Error;
 use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
-    process::{Child, ChildStderr, ChildStdin, ChildStdout, Command},
+    io::{AsyncBufReadExt, BufReader},
+    process::{Child, ChildStderr, ChildStdout, Command},
     select,
 };
 
@@ -24,9 +22,16 @@ pub struct ProcessSource {
     process: Child,
     buffer: Buffer,
     amount: usize,
-    stdout_reader: Option<BufReader<ChildStdout>>,
-    stderr_reader: Option<BufReader<ChildStderr>>,
-    stdin_writer: Option<BufWriter<ChildStdin>>,
+    stdout: BufReader<ChildStdout>,
+    stderr: BufReader<ChildStderr>,
+}
+
+impl Drop for ProcessSource {
+    fn drop(&mut self) {
+        if let Err(err) = futures::executor::block_on(self.process.kill()) {
+            error!("Fail to kill child process: {}", err);
+        }
+    }
 }
 
 impl ProcessSource {
@@ -35,20 +40,29 @@ impl ProcessSource {
         args: Vec<String>,
         envs: HashMap<String, String>,
     ) -> Result<Self, ProcessError> {
+        let mut process = Command::new(command)
+            .args(args)
+            .envs(envs)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .map_err(|e| ProcessError::Setup(format!("{}", e)))?;
+        let stdout = process
+            .stdout
+            .take()
+            .map(BufReader::new)
+            .ok_or_else(|| ProcessError::Setup(String::from("Fail to get stdout handle")))?;
+        let stderr = process
+            .stderr
+            .take()
+            .map(BufReader::new)
+            .ok_or_else(|| ProcessError::Setup(String::from("Fail to get stderr handle")))?;
         Ok(Self {
-            process: Command::new(command)
-                .args(args)
-                .envs(envs)
-                .stdout(Stdio::piped())
-                .stderr(Stdio::piped())
-                .stdin(Stdio::piped())
-                .spawn()
-                .map_err(|e| ProcessError::Setup(format!("{}", e)))?,
+            process,
             buffer: Buffer::new(),
             amount: 0,
-            stdout_reader: None,
-            stderr_reader: None,
-            stdin_writer: None,
+            stdout,
+            stderr,
         })
     }
 }
@@ -59,80 +73,28 @@ impl ByteSource for ProcessSource {
         &mut self,
         _filter: Option<&SourceFilter>,
     ) -> Result<Option<ReloadInfo>, SourceError> {
-        if self.stdout_reader.is_none() {
-            match self.process.stdout.take() {
-                Some(stdout) => self.stdout_reader = Some(BufReader::new(stdout)),
-                None => {
-                    return Err(SourceError::Unrecoverable(
-                        "Detecting stdout failed".to_string(),
-                    ))
-                }
-            }
+        let output = select! {
+            buffer = async {
+                let mut out = String::new();
+                self.stdout.read_line(&mut out).await.map_err(|e| SourceError::Unrecoverable(format!(
+                    "Reading line from stdout failed: {}",
+                    e
+                ))).map(|_| out)
+            } => buffer,
+            buffer = async {
+                let mut out = String::new();
+                self.stderr.read_line(&mut out).await.map_err(|e| SourceError::Unrecoverable(format!(
+                    "Reading line from stdout failed: {}",
+                    e
+                ))).map(|_| out)
+            } => buffer,
+        }?;
+        self.amount = output.len();
+        if self.amount == 0 {
+            return Ok(None);
         }
-        if self.stderr_reader.is_none() {
-            match self.process.stderr.take() {
-                Some(stderr) => self.stderr_reader = Some(BufReader::new(stderr)),
-                None => {
-                    return Err(SourceError::Unrecoverable(
-                        "Detecting stderr failed".to_string(),
-                    ))
-                }
-            }
-        }
-        match select! {
-            stdout_buffer = async {
-                let mut stdout_buffer = String::new();
-                match &mut self.stdout_reader {
-                    Some(reader) => {
-                        match reader.read_line(&mut stdout_buffer).await {
-                            Ok(_) => {
-                                Ok(stdout_buffer)
-                            },
-                            Err(err) => {
-                                Err(SourceError::Unrecoverable(format!(
-                                    "Reading line from stdout failed: {}",
-                                    err
-                                )))
-                            }
-                        }
-                    },
-                    None => Err(SourceError::Unrecoverable(
-                            "Stdout reader not found".to_string(),
-                        ))
-                }
-            } => stdout_buffer,
-            stderr_buffer = async {
-                let mut stderr_buffer = String::new();
-                match &mut self.stderr_reader {
-                    Some(reader) => {
-                        match reader.read_line(&mut stderr_buffer).await {
-                            Ok(_) => {
-                                Ok(stderr_buffer)
-                            },
-                            Err(err) => {
-                                Err(SourceError::Unrecoverable(format!(
-                                    "Reading line from stderr failed: {}",
-                                    err
-                                )))
-                            },
-                        }
-                    },
-                    None => Err(SourceError::Unrecoverable(
-                            "Stderr reader not found".to_string(),
-                        ))
-                }
-            } => stderr_buffer,
-        } {
-            Ok(read) => {
-                self.amount = read.len();
-                if self.amount == 0 {
-                    return Ok(None);
-                }
-                self.buffer.copy_from_slice(read.as_bytes());
-                Ok(Some(ReloadInfo::new(self.amount, self.amount, 0, None)))
-            }
-            Err(err) => Err(err),
-        }
+        self.buffer.copy_from_slice(output.as_bytes());
+        Ok(Some(ReloadInfo::new(self.amount, self.amount, 0, None)))
     }
 
     fn current_slice(&self) -> &[u8] {
@@ -149,44 +111,6 @@ impl ByteSource for ProcessSource {
 
     fn is_empty(&self) -> bool {
         self.len() == 0
-    }
-
-    async fn income(&mut self, msg: String) -> Result<String, String> {
-        let request = serde_json::from_str::<SdeRequest>(&msg)
-            .map_err(|e| format!("Fail to deserialize message: {}", e))?;
-        if self.stdin_writer.is_none() {
-            match self.process.stdin.take() {
-                Some(stdin) => self.stdin_writer = Some(BufWriter::new(stdin)),
-                None => return Err(String::from("No stdin available")),
-            }
-        }
-        let response = if let Some(writer) = self.stdin_writer.as_mut() {
-            let response = match request {
-                SdeRequest::WriteText(str) => {
-                    let written = writer
-                        .write(str.as_bytes())
-                        .await
-                        .map_err(|e| format!("Fail to write string into stdin: {}", e))?;
-                    SdeResponse::WriteText(WriteResponse { bytes: written })
-                }
-                SdeRequest::WriteBytes(bytes) => {
-                    let written = writer
-                        .write(&bytes)
-                        .await
-                        .map_err(|e| format!("Fail to write bytes into stdin: {}", e))?;
-                    SdeResponse::WriteText(WriteResponse { bytes: written })
-                }
-            };
-            writer
-                .flush()
-                .await
-                .map_err(|e| format!("Fail to write string into stdin: {}", e))?;
-            response
-        } else {
-            SdeResponse::Error(String::from("No access to stdin"))
-        };
-        serde_json::to_string(&response)
-            .map_err(|e| format!("Fail to convert response to JSON: {}", e))
     }
 }
 
