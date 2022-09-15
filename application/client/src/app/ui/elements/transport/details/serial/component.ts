754d613d711ff7c870b41ee010cfca6ad2b6c907
fur-gan <56543045+fur-gan@users.noreply.github.com>
2022-09-15 09:53:04 +0200

    [#1499](feat) Support sending messages on serial
    [#1499](feat) Support sending messages on serial
    
    [#1499](refact) Clippy fix
    
    [#1499](refact) Review changes

--- application/client/src/app/ui/elements/transport/details/serial/component.ts
+++ application/client/src/app/ui/elements/transport/details/serial/component.ts
@@ -1,9 +1,15 @@
-import { Component, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, ChangeDetectorRef, Input, ViewChild } from '@angular/core';
 import { Ilc, IlcInterface } from '@env/decorators/component';
 import { ChangesDetector } from '@ui/env/extentions/changes';
 import { Session } from '@service/session/session';
 import { SerialTransportSettings } from '@platform/types/transport/serial';
 import { ObserveOperation } from '@service/session/dependencies/observe/operation';
+import { SdeRequest, SdeResponse } from '@platform/types/sde/commands';
+import {
+    Options as AutocompleteOptions,
+    AutocompleteInput,
+} from '@elements/autocomplete/component';
+import { Subject } from '@platform/env/subscription';
 
 @Component({
     selector: 'app-transport-serial-details',
@@ -16,8 +22,47 @@ export class TransportSerial extends ChangesDetector {
     @Input() public source!: SerialTransportSettings;
     @Input() public session!: Session;
 
+    @ViewChild('message') public input!: AutocompleteInput;
+
+    public readonly options: AutocompleteOptions = {
+        name: 'SerialRecentSentMessageList',
+        storage: 'serial_sending_recent',
+        defaults: '',
+        placeholder: 'Enter message to send',
+        label: 'Enter message to send',
+        recent: new Subject<void>(),
+    };
+
     constructor(cdRef: ChangeDetectorRef) {
         super(cdRef);
+    }
+
+    public ngEnter(event: string): void {
+        if (this.observe === undefined) {
+            return;
+        }
+        if (event === '') {
+            return;
+        }
+        this.input.disable();
+        this.observe
+            .sendIntoSde<SdeRequest, SdeResponse>({
+                WriteText: `${event}\n`,
+            })
+            .then((res) => {
+                if (res.Error !== undefined) {
+                    this.log().error(`Fail to process SDE: ${res.Error}`);
+                    return;
+                }
+                this.options.recent.emit();
+                this.input.set('');
+            })
+            .catch((err) => {
+                this.log().error(`Fail to send SDE: ${err.message}`);
+            })
+            .finally(() => {
+                this.input.enable().focus();
+            });
     }
 }
 export interface TransportSerial extends IlcInterface {}
