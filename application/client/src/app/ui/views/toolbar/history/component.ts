8ee82beddcae0de1e4fada45aec5c0e543f5fd87
DmitryAstafyev <DVAstafyev@gmail.com>
2022-09-29 09:52:45 +0200

    History sorting

--- application/client/src/app/ui/views/toolbar/history/component.ts
+++ application/client/src/app/ui/views/toolbar/history/component.ts
@@ -6,6 +6,7 @@ import { IMenuItem, contextmenu } from '@ui/service/contextmenu';
 import { ChangesDetector } from '@ui/env/extentions/changes';
 import { HistorySession, Suitable, SuitableGroup } from '@service/history/session';
 import { Collections } from '@service/history/collections';
+import { MatSelectChange } from '@angular/material/select';
 
 import * as dom from '@ui/env/dom';
 
@@ -21,10 +22,13 @@ export class History extends ChangesDetector implements AfterContentInit {
 
     public history!: HistorySession;
     public groups: SuitableGroup[] = [];
+    public filters: { caption: string; value: number }[] = [];
+    public show: number = -2;
 
     constructor(cdRef: ChangeDetectorRef) {
         super(cdRef);
     }
+
     @HostListener('contextmenu', ['$event']) onContextMenu(event: MouseEvent) {
         const items: IMenuItem[] = [
             {
@@ -94,20 +98,78 @@ export class History extends ChangesDetector implements AfterContentInit {
             return;
         }
         this.history = history;
-        history.subjects.get().suitable.subscribe((collections: Suitable) => {
-            this.update(collections);
-            this.detectChanges();
+        history.subjects.get().suitable.subscribe((_collections: Suitable) => {
+            this.list().update().detectChanges();
         });
-        this.update();
+        this.list().update();
     }
 
     public use(collection: Collections) {
         this.history.apply(collection);
     }
 
-    protected update(collections?: Suitable): History {
-        const suitable = collections === undefined ? this.history.find().suitable() : collections;
-        this.groups = suitable.asGroups();
+    public onListFilterChange(_event: MatSelectChange) {
+        this.list().update().detectChanges();
+    }
+
+    protected update(): History {
+        const groups: SuitableGroup[] = (() => {
+            switch (this.show) {
+                case -3:
+                    return [{ caption: 'All', rank: 0, collections: this.history.find().all() }];
+                case -2:
+                    return this.history.find().suitable().asGroups();
+                case -1:
+                    return [
+                        {
+                            caption: 'All Named Presets',
+                            rank: 0,
+                            collections: this.history.find().named(),
+                        },
+                    ];
+                default:
+                    return this.history.find().suitable().asGroups();
+            }
+        })();
+        if (this.show < 0) {
+            this.groups = groups;
+            return this;
+        }
+        const unnamed: SuitableGroup = { caption: 'Unnamed group', rank: 1000, collections: [] };
+        this.groups = [];
+        groups.forEach((group) => {
+            if (group.caption !== undefined) {
+                this.groups.push(group);
+            } else {
+                unnamed.collections = unnamed.collections.concat(group.collections);
+            }
+        });
+        if (unnamed.collections.length > 0) {
+            this.groups.push(unnamed);
+        }
+        this.groups = this.groups.filter((g) => g.rank === this.show);
+        return this;
+    }
+
+    protected list(): History {
+        const suitable = this.history.find().suitable();
+        const groups = suitable.asGroups();
+        this.filters = [
+            { caption: 'All', value: -3 },
+            { caption: 'All Suitable', value: -2 },
+            { caption: 'Named Presets', value: -1 },
+        ];
+        const unnamed: SuitableGroup = { caption: 'Unnamed group', rank: 1000, collections: [] };
+        groups.forEach((group) => {
+            if (group.caption !== undefined) {
+                this.filters.push({ caption: group.caption, value: group.rank });
+            } else {
+                unnamed.collections = unnamed.collections.concat(group.collections);
+            }
+        });
+        if (unnamed.collections.length > 0) {
+            this.filters.push({ caption: 'Unnamed group', value: 1000 });
+        }
         return this;
     }
 }
