524638580bbb2d2d0e441b6c2cbf2cff1b8a7538
DmitryAstafyev <dvastafyev@gmail.com>
2022-10-05 13:31:41 +0200

    Update bookmark implementation

--- application/client/src/app/service/session/dependencies/bookmark/bookmark.ts
+++ application/client/src/app/service/session/dependencies/bookmark/bookmark.ts
@@ -1,4 +1,4 @@
-import { EntryConvertable, Entry, Recognizable } from '@platform/types/storage/entry';
+import { Recognizable } from '@platform/types/storage/entry';
 import { StaticRowInputs, Row, Owner } from '@schema/content/row';
 import { Subject } from '@platform/env/subscription';
 import { error } from '@platform/env/logger';
@@ -9,29 +9,8 @@ import { Equal } from '@platform/types/env/types';
 
 import * as obj from '@platform/env/obj';
 
-export class Bookmark
-    extends Json<Bookmark>
-    implements Recognizable, EntryConvertable, Equal<Bookmark>
-{
+export class Bookmark extends Json<Bookmark> implements Recognizable, Equal<Bookmark> {
     public static KEY: string = 'bookmark';
-
-    public static from(input: Entry | string): Bookmark | Error {
-        let entry: Entry | Error;
-        if (typeof input === 'string') {
-            entry = EntryConvertable.from(input);
-        } else {
-            entry = input;
-        }
-        if (entry instanceof Error) {
-            return entry;
-        }
-        const bookmark = new Bookmark();
-        const error = bookmark.entry().from(entry);
-        if (error instanceof Error) {
-            return error;
-        }
-        return bookmark;
-    }
 
     public static fromJson(json: string): Bookmark | Error {
         try {
@@ -113,43 +92,6 @@ export class Bookmark
             },
             key: (): string => {
                 return Bookmark.KEY;
-            },
-        };
-    }
-
-    public entry(): {
-        to(): Entry;
-        from(entry: Entry): Error | undefined;
-        hash(): string;
-        uuid(): string;
-        updated(): Subject<void>;
-    } {
-        return {
-            to: (): Entry => {
-                return {
-                    uuid: this.row.stream.toString(),
-                    content: JSON.stringify(this.row),
-                };
-            },
-            from: (entry: Entry): Error | undefined => {
-                try {
-                    const def: StaticRowInputs = JSON.parse(entry.content);
-                    def.stream = obj.getAsValidNumber(def, 'stream');
-                    def.content = obj.getAsString(def, 'content');
-                    def.source = obj.getAsValidNumber(def, 'source');
-                } catch (e) {
-                    return new Error(error(e));
-                }
-                return undefined;
-            },
-            hash: (): string => {
-                return this.row.stream.toString();
-            },
-            uuid: (): string => {
-                return this.row.stream.toString();
-            },
-            updated: (): Subject<void> => {
-                return this._updated;
             },
         };
     }
