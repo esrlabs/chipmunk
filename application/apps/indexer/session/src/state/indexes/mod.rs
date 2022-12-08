3b248f14af0477c7ee56dd5a2b7336aee4167054
DmitryAstafyev <dvastafyev@gmail.com>
2022-12-08 18:22:38 +0100

    Resolve clippy issues

--- application/apps/indexer/session/src/state/indexes/mod.rs
+++ application/apps/indexer/session/src/state/indexes/mod.rs
@@ -1,9 +1,10 @@
+#[allow(dead_code)]
 pub mod controller;
 pub mod frame;
 pub mod index;
 pub mod map;
 pub mod nature;
 #[cfg(test)]
-pub mod tests_indexes;
-#[cfg(test)]
 pub mod tests_controller;
+#[cfg(test)]
+pub mod tests_indexes;
