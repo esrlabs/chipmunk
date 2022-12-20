171379a4231613d74b1e069e2eb96675307c4658
DmitryAstafyev <dvastafyev@gmail.com>
2022-12-20 16:27:31 +0100

    Highlight error (foldersinput)

--- application/client/src/app/ui/elements/folderinput/error.ts
+++ application/client/src/app/ui/elements/folderinput/error.ts
@@ -1,41 +1,38 @@
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Subject } from '@platform/env/subscription';
 import { FormControl } from '@angular/forms';
+import { bridge } from '@service/bridge';
+import { scope } from '@platform/env/scope';
 
-export abstract class ErrorState extends ErrorStateMatcher {
+export class ErrorState extends ErrorStateMatcher {
+    public subject: Subject<void> = new Subject();
+    protected exist: boolean = false;
     protected value: string = '';
 
     public override isErrorState(control: FormControl | null): boolean {
-        if (control !== null) {
+        if (control !== null && typeof control.value === 'string') {
             const updated = this.value !== control.value;
             this.value = control.value;
-            updated && this.validate();
+            updated &&
+                this.check().catch((err: Error) => {
+                    scope
+                        .getLogger(`ErrorState (FolderInput)`)
+                        .error(`Fail to check path: ${err.message}`);
+                });
         }
         return this.is();
     }
 
-    abstract msg(): string;
-    abstract is(): boolean;
-    abstract observer(): Subject<void>;
-    abstract validate(): void;
-}
-
-export class NullErrorState extends ErrorState {
-    protected subject: Subject<void> = new Subject();
-
-    public msg(): string {
-        return '';
-    }
-
     public is(): boolean {
-        return false;
+        return !this.exist;
     }
 
-    public observer(): Subject<void> {
-        return this.subject;
-    }
-
-    public validate(): void {
-        return undefined;
+    public async check(): Promise<void> {
+        if (this.value.trim() === '') {
+            this.exist = false;
+        } else {
+            this.exist = await bridge.files().exists(this.value);
+        }
+        this.subject.emit();
     }
 }
