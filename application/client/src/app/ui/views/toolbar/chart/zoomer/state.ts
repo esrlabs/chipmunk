80100a00333f9890b4c14f1f01dc3642514cc15d
fur-gan <56543045+fur-gan@users.noreply.github.com>
2022-12-21 17:10:54 +0100

    Currently viewed rows rectangle

--- application/client/src/app/ui/views/toolbar/chart/zoomer/state.ts
+++ application/client/src/app/ui/views/toolbar/chart/zoomer/state.ts
@@ -1,11 +1,16 @@
 import { AdvancedState, EChartName } from '../abstract/advanced';
+import { IRange } from '@platform/types/range';
 
 export class State extends AdvancedState {
     public isCursorVisible: boolean = true;
+    public _ng_rectWidth: number = -1;
+    public _ng_rectLeft: number = 0;
+    public readonly _ng_borderWidth: number = 2;
 
     private _width: number = 0;
 
     public init() {
+        this._positionChangeSetup();
         this._resize();
         this._subscribe();
         this._parent.env().subscriber.register(
@@ -73,5 +78,33 @@ export class State extends AdvancedState {
             }
             this._parent.detectChanges();
         }
+    }
+
+    private _onPositionChange(range: IRange) {
+        const linesDiff: number = range.to - range.from;
+        const linesTotal: number = this._session.stream.len();
+        const linesDiffPercent: number = linesDiff / linesTotal;
+        const totalWidth: number = this._element.getBoundingClientRect().width;
+        const fromPercent: number = range.from / linesTotal;
+        const border: number = 2 * this._ng_borderWidth;
+
+        this._ng_rectWidth = Math.round(linesDiffPercent * totalWidth);
+        this._ng_rectLeft = Math.round(fromPercent * totalWidth);
+        if (this._ng_rectLeft + this._ng_rectWidth + border > totalWidth) {
+            this._ng_rectLeft = totalWidth - this._ng_rectWidth - border;
+        }
+        this._parent.detectChanges();
+    }
+
+    private _positionChangeSetup() {
+        const initialFrame = this._session.cursor.frame().get();
+        initialFrame !== undefined && this._onPositionChange(initialFrame);
+        this._parent
+            .env()
+            .subscriber.register(
+                this._session.cursor.subjects
+                    .get()
+                    .frame.subscribe(this._onPositionChange.bind(this)),
+            );
     }
 }
