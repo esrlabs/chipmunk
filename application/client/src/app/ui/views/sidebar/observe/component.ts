2b2cda47e89409e3ad9c2c9891003c8baa446237
DmitryAstafyev <DVAstafyev@gmail.com>
2022-12-23 11:28:48 +0100

    Buttons attach file/stream fix

--- application/client/src/app/ui/views/sidebar/observe/component.ts
+++ application/client/src/app/ui/views/sidebar/observe/component.ts
@@ -4,7 +4,7 @@ import { ObserveOperation } from '@service/session/dependencies/observe/operatio
 import { Ilc, IlcInterface } from '@env/decorators/component';
 import { Initial } from '@env/decorators/initial';
 import { ChangesDetector } from '@ui/env/extentions/changes';
-import { DataSource } from '@platform/types/observe';
+import { DataSource, ParserName, Parser } from '@platform/types/observe';
 import { Alias, getRenderAlias } from '@schema/render/tools';
 import { SourceHolder } from './holder';
 
@@ -90,62 +90,116 @@ export class ObserveList extends ChangesDetector implements AfterContentInit {
         }
     }
 
-    public ngAttachStream() {
-        const render = getRenderAlias(this.session);
-        if (render instanceof Error) {
-            this.log().error(render.message);
-            return;
-        }
-        const assigned = this.ilc()
-            .services.system.opener.stream(undefined, undefined, undefined)
-            .assign(this.session);
-        switch (render) {
-            case Alias.dlt:
-                assigned.dlt().catch((err: Error) => {
-                    this.log().error(`Fail to open DLT stream; error: ${err.message}`);
-                });
-                break;
-            case Alias.text:
-                assigned.text().catch((err: Error) => {
-                    this.log().error(`Fail to open DLT stream; error: ${err.message}`);
-                });
-                break;
-        }
+    protected last(): {
+        any(): SourceHolder | undefined;
+        running(): SourceHolder | undefined;
+    } {
+        return {
+            any: (): SourceHolder | undefined => {
+                if (this.observed.running.length > 0) {
+                    return this.observed.running[this.observed.running.length - 1];
+                } else if (this.observed.done.length > 0) {
+                    return this.observed.done[this.observed.done.length - 1];
+                }
+                return undefined;
+            },
+            running: (): SourceHolder | undefined => {
+                if (this.observed.running.length > 0) {
+                    return this.observed.running[this.observed.running.length - 1];
+                }
+                return undefined;
+            },
+        };
     }
 
-    public ngAddFile() {
-        const render = getRenderAlias(this.session);
-        if (render instanceof Error) {
-            this.log().error(render.message);
-            return;
-        }
-        (() => {
-            const select = this.ilc().services.system.bridge.files().select;
-            switch (render) {
-                case Alias.dlt:
-                    return select.dlt();
-                case Alias.text:
-                    return select.text();
-            }
-        })().then((files) => {
-            if (files.length !== 1) {
-                this.log().error(`Fail to open file: invalid count of files`);
-                return;
-            }
-            const assigned = this.ilc().services.system.opener.file(files[0]).assign(this.session);
-            switch (render) {
-                case Alias.dlt:
-                    assigned.dlt().catch((err: Error) => {
-                        this.log().error(`Fail to open DLT file; error: ${err.message}`);
-                    });
-                    break;
-                case Alias.text:
-                    assigned.text().catch((err: Error) => {
-                        this.log().error(`Fail to open DLT file; error: ${err.message}`);
-                    });
-                    break;
-            }
-        });
+    public attachableAs(): {
+        stream(): boolean;
+        file(): boolean;
+    } {
+        return {
+            stream: (): boolean => {
+                const source = this.last().any();
+                return source === undefined
+                    ? false
+                    : !(source.source.asSourceDefinition() instanceof Error);
+            },
+            file: (): boolean => {
+                if (this.attachableAs().stream()) {
+                    return false;
+                }
+                const running = this.last().running();
+                if (running !== undefined && running.source.parser.Text !== undefined) {
+                    return false;
+                }
+                return true;
+            },
+        };
+    }
+
+    public attach(): {
+        stream(): void;
+        file(): void;
+    } {
+        return {
+            stream: (): void => {
+                const render = getRenderAlias(this.session);
+                if (render instanceof Error) {
+                    this.log().error(render.message);
+                    return;
+                }
+                const assigned = this.ilc()
+                    .services.system.opener.stream(undefined, undefined, undefined)
+                    .assign(this.session);
+                switch (render) {
+                    case Alias.dlt:
+                        assigned.dlt().catch((err: Error) => {
+                            this.log().error(`Fail to open DLT stream; error: ${err.message}`);
+                        });
+                        break;
+                    case Alias.text:
+                        assigned.text().catch((err: Error) => {
+                            this.log().error(`Fail to open DLT stream; error: ${err.message}`);
+                        });
+                        break;
+                }
+            },
+            file: (): void => {
+                const render = getRenderAlias(this.session);
+                if (render instanceof Error) {
+                    this.log().error(render.message);
+                    return;
+                }
+                (() => {
+                    const select = this.ilc().services.system.bridge.files().select;
+                    switch (render) {
+                        case Alias.dlt:
+                            return select.dlt();
+                        case Alias.text:
+                            return select.text();
+                    }
+                })().then((files) => {
+                    if (files.length !== 1) {
+                        this.log().error(`Fail to open file: invalid count of files`);
+                        return;
+                    }
+                    const assigned = this.ilc()
+                        .services.system.opener.file(files[0])
+                        .assign(this.session);
+                    switch (render) {
+                        case Alias.dlt:
+                            assigned.dlt().catch((err: Error) => {
+                                this.log().error(`Fail to open DLT file; error: ${err.message}`);
+                            });
+                            break;
+                        case Alias.text:
+                            assigned.text().catch((err: Error) => {
+                                this.log().error(`Fail to open DLT file; error: ${err.message}`);
+                            });
+                            break;
+                    }
+                });
+            },
+        };
     }
 
     public isAttachingDisabled(): boolean {
@@ -153,7 +207,7 @@ export class ObserveList extends ChangesDetector implements AfterContentInit {
             return false;
         }
         const source = this.observed.running[0].source;
-        return source.asFile() !== undefined && source.parser.Text === undefined;
+        return source.asSourceDefinition() instanceof Error;
     }
 }
 export interface ObserveList extends IlcInterface {}
