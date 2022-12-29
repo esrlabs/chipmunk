9acb855f09fdac0ab614e3d1e8817f5f8fa41258
fur-gan <56543045+fur-gan@users.noreply.github.com>
2022-12-29 15:28:23 +0100

    [](fix) Small chart fix and some refactoring
    [](refact) Proper error message output
    
    [](refact) Refactor and fixes
    
    [](refact) Refactor changes

--- application/client/src/app/ui/views/toolbar/chart/chart/state.ts
+++ application/client/src/app/ui/views/toolbar/chart/chart/state.ts
@@ -1,7 +1,6 @@
 import { Chart } from 'chart.js';
 import { IPosition, IPositionChange } from '../service';
 import { Owner } from '@schema/content/row';
-import { getPropByPath } from '@platform/env/obj';
 import { IRange } from '@platform/types/range';
 import { AdvancedState, EChartName } from '../abstract/advanced';
 
@@ -54,13 +53,8 @@ export class State extends AdvancedState {
     }
 
     public onClick(event: MouseEvent) {
-        const position: number | Error = this._calculatePosition(event);
-        if (position instanceof Error) {
-            this._parent.log().error(position);
-        } else {
-            position >= 0 &&
-                this._session.cursor.select(position, Owner.Chart, undefined, undefined);
-        }
+        const position: number = this._calculatePosition(event);
+        position >= 0 && this._session.cursor.select(position, Owner.Chart, undefined, undefined);
     }
 
     public onMouseMove(event: MouseEvent) {
@@ -71,12 +65,8 @@ export class State extends AdvancedState {
         }
         if (event.offsetY > 0) {
             this.tooltipTop = this._height / 2;
-            const position: number | Error = this._calculatePosition(event);
-            if (position instanceof Error) {
-                this._parent.log().error(position);
-            } else {
-                this.tooltip = position >= 0 ? `${position}` : '';
-            }
+            const position: number = this._calculatePosition(event);
+            this.tooltip = position >= 0 ? `${position}` : '';
         }
     }
 
@@ -100,7 +90,7 @@ export class State extends AdvancedState {
                 this._draw(EChartName.chartFilters);
             })
             .catch((err: Error) => {
-                this._parent.log().error(err);
+                this._parent.log().error(err.message);
             })
             .finally(() => {
                 this._loading = false;
@@ -111,82 +101,37 @@ export class State extends AdvancedState {
         this._height = this._element.getBoundingClientRect().height;
     }
 
-    private _calculatePosition(event: MouseEvent): number | Error {
+    private _calculatePosition(event: MouseEvent): number {
         if (event.target === undefined) {
             return -1;
         }
-        let position: number | undefined = this._getPositionByChartPointData(event);
-        if (position === undefined) {
-            const streamLen: number = this._session.stream.len();
-            const pos: IPosition = this._service.getPosition(this._session.uuid()).position;
-            if (!this._isPositionViable(pos)) {
-                return new Error('Failed to calculate position due to invalid data');
-            }
-            const width: number = pos.full === 0 ? this._width : pos.full;
-            if (streamLen > width) {
-                const rangeRate: number = streamLen / width;
-                const rangeBegin: number = Math.floor(pos.left * rangeRate);
-                const rangeEnd: number = Math.floor((pos.left + pos.width) * rangeRate);
-                const rows = rangeEnd - rangeBegin;
-                const rate: number = width / rows;
-                const offsetX: number = event.offsetX;
-                position = Math.floor(offsetX / rate) + rangeBegin;
-            } else {
-                if (this._range === undefined) {
-                    return -1;
-                }
-                const rows = this._range.to - this._range.from;
-                const rate: number = width / rows;
-                const offsetX: number = event.offsetX;
-                position = Math.floor(offsetX / rate) + this._range.from;
-            }
+        let pos: IPosition = this._service.getPosition(this._session.uuid()).position;
+        if (!this._isPositionViable(pos)) {
+            pos = {
+                left: 0,
+                width: 0,
+                full: this._width,
+            };
         }
-        return position;
-    }
-
-    private _getPositionByChartPointData(event: MouseEvent): number | undefined {
-        let position: number | undefined;
-        // Chart for regex will be added later
-        [this._filters].forEach((chart: Chart | undefined) => {
-            if (position !== undefined || chart === undefined || event.x < chart.chartArea.left) {
-                return;
+        const streamLen: number = this._session.stream.len();
+        const width: number = pos.full === 0 ? this._width : pos.full;
+        if (streamLen > width) {
+            const rangeRate: number = streamLen / width;
+            const rangeBegin: number = Math.floor(pos.left * rangeRate);
+            const rangeEnd: number = Math.floor((pos.left + pos.width) * rangeRate);
+            const rows = rangeEnd - rangeBegin;
+            const rate: number = width / rows;
+            const offsetX: number = event.offsetX;
+            return Math.floor(offsetX / rate) + rangeBegin;
+        } else {
+            if (this._range === undefined) {
+                return -1;
             }
-            // This feature of chartjs isn't documented well,
-            // so that's why here we have many checks
-            const e: any[] = chart.getElementsAtEventForMode(
-                event,
-                'nearest',
-                { intersect: true },
-                false,
-            );
-            if (!(e instanceof Array) || e.length === 0) {
-                return;
-            }
-            let label: any = getPropByPath(e[0], '_model.label');
-            if (label !== undefined) {
-                position = parseInt(label.replace(/\s-\s\d*/gi, ''), 10);
-            } else {
-                label = getPropByPath(e[0], '_chart.tooltip._model.body');
-                if (
-                    label instanceof Array &&
-                    label.length > 0 &&
-                    label[0].lines instanceof Array &&
-                    label[0].lines.length > 0
-                ) {
-                    label = typeof label[0].lines[0] === 'string' ? label[0].lines[0] : undefined;
-                    if (label !== undefined) {
-                        position = parseInt(
-                            label.replace(/[()]/gi, '').replace(/,\s\d*/gi, ''),
-                            10,
-                        );
-                    }
-                }
-            }
-            if (position !== undefined && (isNaN(position) || !isFinite(position))) {
-                position = undefined;
-            }
-        });
-        return position;
+            const rows = this._range.to - this._range.from;
+            const rate: number = width / rows;
+            const offsetX: number = event.offsetX;
+            return Math.floor(offsetX / rate) + this._range.from;
+        }
     }
 
     private _onChange(event: IPositionChange) {
@@ -212,7 +157,7 @@ export class State extends AdvancedState {
                 this._map.length > 0 && this._draw(EChartName.chartFilters);
             })
             .catch((err: Error) => {
-                this._parent.log().error(err);
+                this._parent.log().error(err.message);
             });
     }
 
