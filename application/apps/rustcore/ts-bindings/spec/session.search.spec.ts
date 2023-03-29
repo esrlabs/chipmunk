6fb0d04aee515369505f819d76177e0a7449c3ab
DmitryAstafyev <dvastafyev@gmail.com>
2023-03-29 17:24:33 +0200

    Addition search tests on ts-binding level

--- application/apps/rustcore/ts-bindings/spec/session.search.spec.ts
+++ application/apps/rustcore/ts-bindings/spec/session.search.spec.ts
@@ -668,4 +668,263 @@ describe('Search', function () {
                 );
             });
     });
+
+    it(config.regular.list[6], function (done) {
+        if (ingore(1, done)) {
+            return;
+        }
+        const logger = getLogger(config.regular.list[6]);
+        Session.create()
+            .then((session: Session) => {
+                // Set provider into debug mode
+                session.debug(true, config.regular.list[6]);
+                const stream = session.getStream();
+                const search = session.getSearch();
+                const events = session.getEvents();
+                if (events instanceof Error) {
+                    finish(session, done, events);
+                    return;
+                }
+                if (stream instanceof Error) {
+                    return finish(session, done, stream);
+                }
+                if (search instanceof Error) {
+                    return finish(session, done, search);
+                }
+                const tmpobj = createSampleFile(
+                    501,
+                    logger,
+                    (i: number) =>
+                        `[${i}]:: ${
+                            i % 100 === 0 || i <= 5
+                                ? `some match A ${i % 6 === 0 ? 'B' : ''} line data\n`
+                                : `some line data\n`
+                        }`,
+                );
+                stream
+                    .observe(Observe.DataSource.file(tmpobj.name).text())
+                    .on('confirmed', () => {
+                        search
+                            .search([
+                                {
+                                    filter: 'match A',
+                                    flags: { reg: true, word: false, cases: false },
+                                },
+                                {
+                                    filter: 'match [A,B]',
+                                    flags: { reg: true, word: false, cases: false },
+                                },
+                            ])
+                            .then((_) => {
+                                search
+                                    .getMap(501)
+                                    .then((map) => {
+                                        expect(map[0].length).toEqual(2);
+                                        expect(map[0][0][0]).toEqual(0);
+                                        expect(map[0][1][0]).toEqual(1);
+                                        expect(map[500][0][0]).toEqual(0);
+                                        expect(map[500][1][0]).toEqual(1);
+                                        logger.verbose(map);
+                                        search
+                                            .grab(0, 11)
+                                            .then((result: IGrabbedElement[]) => {
+                                                expect(result.map((i) => i.content)).toEqual([
+                                                    '[0]:: some match A B line data',
+                                                    '[1]:: some match A  line data',
+                                                    '[2]:: some match A  line data',
+                                                    '[3]:: some match A  line data',
+                                                    '[4]:: some match A  line data',
+                                                    '[5]:: some match A  line data',
+                                                    '[100]:: some match A  line data',
+                                                    '[200]:: some match A  line data',
+                                                    '[300]:: some match A B line data',
+                                                    '[400]:: some match A  line data',
+                                                    '[500]:: some match A  line data',
+                                                ]);
+                                                expect(result.map((i) => i.position)).toEqual([
+                                                    0, // 0
+                                                    1, // 1
+                                                    2, // 2
+                                                    3, // 3
+                                                    4, // 4
+                                                    5, // 5
+                                                    100, // 6
+                                                    200, // 7
+                                                    300, // 8
+                                                    400, // 9
+                                                    500, // 10
+                                                ]);
+                                                search
+                                                    .len()
+                                                    .then((len: number) => {
+                                                        expect(len).toEqual(11);
+                                                        expect(searchStreamUpdated).toEqual(true);
+                                                        finish(session, done);
+                                                    })
+                                                    .catch((err: Error) => {
+                                                        finish(session, done, err);
+                                                    });
+                                            })
+                                            .catch((err: Error) => {
+                                                finish(
+                                                    session,
+                                                    done,
+                                                    new Error(
+                                                        `Fail to grab data due error: ${
+                                                            err instanceof Error ? err.message : err
+                                                        }`,
+                                                    ),
+                                                );
+                                            });
+                                    })
+                                    .catch(finish.bind(null, session, done));
+                            })
+                            .catch(finish.bind(null, session, done));
+                    })
+                    .catch(finish.bind(null, session, done));
+                let searchStreamUpdated = false;
+                events.SearchUpdated.subscribe((event) => {
+                    searchStreamUpdated = true;
+                });
+            })
+            .catch((err: Error) => {
+                finish(
+                    undefined,
+                    done,
+                    new Error(
+                        `Fail to create session due error: ${
+                            err instanceof Error ? err.message : err
+                        }`,
+                    ),
+                );
+            });
+    });
+
+    it(config.regular.list[7], function (done) {
+        if (ingore(1, done)) {
+            return;
+        }
+        const logger = getLogger(config.regular.list[7]);
+        Session.create()
+            .then((session: Session) => {
+                // Set provider into debug mode
+                session.debug(true, config.regular.list[7]);
+                const stream = session.getStream();
+                const search = session.getSearch();
+                const events = session.getEvents();
+                if (events instanceof Error) {
+                    finish(session, done, events);
+                    return;
+                }
+                if (stream instanceof Error) {
+                    return finish(session, done, stream);
+                }
+                if (search instanceof Error) {
+                    return finish(session, done, search);
+                }
+                const tmpobj = createSampleFile(
+                    5000,
+                    logger,
+                    (i: number) =>
+                        `[${i}]:: ${
+                            i % 100 === 0 || i <= 5
+                                ? `some match ${
+                                      i % 6 === 0
+                                          ? 'B'
+                                          : i % 4 === 0
+                                          ? 'C'
+                                          : i % 3 === 0
+                                          ? 'D'
+                                          : 'A'
+                                  } line data\n`
+                                : `some line data\n`
+                        }`,
+                );
+                stream
+                    .observe(Observe.DataSource.file(tmpobj.name).text())
+                    .on('confirmed', () => {
+                        const calls = ['match A', 'match D', 'match C', 'match B'];
+                        let canceled = 0;
+                        calls.forEach((filter) => {
+                            search
+                                .search([
+                                    {
+                                        filter,
+                                        flags: { reg: true, word: false, cases: false },
+                                    },
+                                ])
+                                .then((_) => {
+                                    expect(canceled).toEqual(3);
+                                    search
+                                        .grab(0, 16)
+                                        .then((result: IGrabbedElement[]) => {
+                                            expect(result.map((i) => i.content)).toEqual([
+                                                '[0]:: some match B line data',
+                                                '[300]:: some match B line data',
+                                                '[600]:: some match B line data',
+                                                '[900]:: some match B line data',
+                                                '[1200]:: some match B line data',
+                                                '[1500]:: some match B line data',
+                                                '[1800]:: some match B line data',
+                                                '[2100]:: some match B line data',
+                                                '[2400]:: some match B line data',
+                                                '[2700]:: some match B line data',
+                                                '[3000]:: some match B line data',
+                                                '[3300]:: some match B line data',
+                                                '[3600]:: some match B line data',
+                                                '[3900]:: some match B line data',
+                                                '[4200]:: some match B line data',
+                                                '[4500]:: some match B line data',
+                                            ]);
+                                            expect(result.map((i) => i.position)).toEqual([
+                                                0, 300, 600, 900, 1200, 1500, 1800, 2100, 2400,
+                                                2700, 3000, 3300, 3600, 3900, 4200, 4500,
+                                            ]);
+                                            search
+                                                .len()
+                                                .then((len: number) => {
+                                                    expect(len).toEqual(17);
+                                                    expect(searchStreamUpdated).toEqual(true);
+                                                    finish(session, done);
+                                                })
+                                                .catch((err: Error) => {
+                                                    finish(session, done, err);
+                                                });
+                                        })
+                                        .catch((err: Error) => {
+                                            finish(
+                                                session,
+                                                done,
+                                                new Error(
+                                                    `Fail to grab data due error: ${
+                                                        err instanceof Error ? err.message : err
+                                                    }`,
+                                                ),
+                                            );
+                                        });
+                                })
+                                .canceled(() => {
+                                    canceled += 1;
+                                })
+                                .catch(finish.bind(null, session, done));
+                        });
+                    })
+                    .catch(finish.bind(null, session, done));
+                let searchStreamUpdated = false;
+                events.SearchUpdated.subscribe((event) => {
+                    searchStreamUpdated = true;
+                });
+            })
+            .catch((err: Error) => {
+                finish(
+                    undefined,
+                    done,
+                    new Error(
+                        `Fail to create session due error: ${
+                            err instanceof Error ? err.message : err
+                        }`,
+                    ),
+                );
+            });
+    });
 });
