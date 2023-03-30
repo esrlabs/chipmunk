e30675fa5b2a578c25fe29677c2b22093d318939
DmitryAstafyev <dvastafyev@gmail.com>
2023-03-30 16:34:23 +0200

    Fix spinner on file indexing

--- application/client/src/app/ui/views/dialogs/locker/module.ts
+++ application/client/src/app/ui/views/dialogs/locker/module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { LockerMessage } from './component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     entryComponents: [LockerMessage],
-    imports: [CommonModule, MatProgressSpinnerModule, MatIconModule, MatButtonModule],
+    imports: [
+        CommonModule,
+        MatProgressSpinnerModule,
+        MatIconModule,
+        MatButtonModule,
+        MatProgressBarModule,
+    ],
     declarations: [LockerMessage],
     exports: [LockerMessage],
     bootstrap: [LockerMessage],
