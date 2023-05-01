3ab323577c9a81b99846d5929250111bb2e0bcfc
DmitryAstafyev <dvastafyev@gmail.com>
2023-05-01 17:12:08 +0200

    Allow clipboard copy for text attachment

--- application/client/src/app/ui/views/sidebar/attachments/attachment/previews/text/component.ts
+++ application/client/src/app/ui/views/sidebar/attachments/attachment/previews/text/component.ts
@@ -1,8 +1,15 @@
-import { Component, Input, AfterContentInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import {
+    Component,
+    Input,
+    AfterContentInit,
+    ChangeDetectorRef,
+    ChangeDetectionStrategy,
+} from '@angular/core';
 import { Ilc, IlcInterface } from '@env/decorators/component';
 import { Attachment } from '@platform/types/content';
 import { URLFileReader } from '@env/urlfilereader';
 import { ChangesDetector } from '@ui/env/extentions/changes';
+import { unique } from '@platform/env/sequence';
 
 const MAX_LINES_COUNT = 1000;
 
@@ -17,6 +24,8 @@ export class Preview extends ChangesDetector implements AfterContentInit {
     @Input() attachment!: Attachment;
 
     public lines: string[] = [];
+
+    public uuid: string = unique();
 
     constructor(cdRef: ChangeDetectorRef) {
         super(cdRef);
@@ -41,6 +50,22 @@ export class Preview extends ChangesDetector implements AfterContentInit {
             .catch((err: Error) => {
                 this.log().error(`Fail to get a text for ${this.attachment.name}: ${err.message}.`);
             });
+        this.env().subscriber.register(
+            this.ilc().services.system.hotkeys.listen('Ctrl + C', () => {
+                const active = document.activeElement;
+                if (active === null) {
+                    return;
+                }
+                if (active.getAttribute('uuid') !== this.uuid) {
+                    return;
+                }
+                const selection = document.getSelection();
+                if (selection === null) {
+                    return;
+                }
+                navigator.clipboard.writeText(selection.toString());
+            }),
+        );
     }
 }
 export interface Preview extends IlcInterface {}
