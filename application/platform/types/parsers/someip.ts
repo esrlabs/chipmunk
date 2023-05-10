d24953b541c0151000191a0fa75393e3b8ae8bcc
DmitryAstafyev <dvastafyev@gmail.com>
2023-05-10 22:15:16 +0200

    Refactoring rest levels

--- application/platform/types/parsers/someip.ts
+++ application/platform/types/parsers/someip.ts
@@ -32,3 +32,17 @@ export interface SomeipStatisticDetail {
     id: number;
     num: number;
 }
+export interface SomeIpParserSettings {
+    dummy: string;
+}
+
+export function optionsToParserSettings(options: ISomeIpOptions): SomeIpParserSettings {
+    return {
+        dummy: options.dummy,
+    };
+}
+export function parserSettingsToOptions(options: SomeIpParserSettings): ISomeIpOptions {
+    return {
+        dummy: options.dummy,
+    };
+}
