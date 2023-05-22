643079b907d826136532bc6d8915f67a31c2e872
DmitryAstafyev <dvastafyev@gmail.com>
2023-05-22 16:59:24 +0200

    Fix unit tests (search values)

--- application/apps/indexer/processor/src/search/searchers/tests_values.rs
+++ application/apps/indexer/processor/src/search/searchers/tests_values.rs
@@ -15,7 +15,7 @@ use super::values::ValueSearchHolder;
 fn extracted(
     content: &str,
     filters: Vec<String>,
-) -> Result<HashMap<u64, Vec<(u8, String)>>, std::io::Error> {
+) -> Result<HashMap<u8, Vec<(u64, f64)>>, std::io::Error> {
     let mut tmp_file = tempfile::NamedTempFile::new()?;
     let input_file = tmp_file.as_file_mut();
     input_file.write_all(content.as_bytes())?;
@@ -31,24 +31,24 @@ fn extracted(
 
 #[test]
 fn test_value_search() -> Result<(), std::io::Error> {
-    let mut sum: usize = 0;
+    let mut sum: f64 = 0.0;
     let mut rows: Vec<String> = vec![];
     for n in 0..100 {
         rows.push(if n % 10 == 0 {
-            sum += n;
+            sum += n as f64;
             format!("{n} test log entry CPU={n}%; some content")
         } else {
             format!("{n} test log entry; some content")
         });
     }
-    let values = extracted(&rows.join("\n"), vec![String::from("CPU=(\\d{2,})")])?;
-    let mut control: usize = 0;
-    values.iter().for_each(|(pos, values)| {
-        assert_eq!(1, values.len());
-        let (_term, value) = &values[0];
-        let value = value.parse::<usize>().unwrap();
-        control += value;
-        assert_eq!(*pos as usize, value);
+    let terms = extracted(&rows.join("\n"), vec![String::from("CPU=(\\d{2,})")])?;
+    let mut control: f64 = 0f64;
+    assert_eq!(1, terms.len());
+    terms.iter().for_each(|(_term, values)| {
+        values.iter().for_each(|(pos, value)| {
+            control += value;
+            assert_eq!(*pos as f64, *value);
+        });
     });
     assert_eq!(control, sum);
     Ok(())
@@ -56,39 +56,39 @@ fn test_value_search() -> Result<(), std::io::Error> {
 
 #[test]
 fn test_value_search2() -> Result<(), std::io::Error> {
-    let mut sum_0: usize = 0;
-    let mut sum_1: usize = 0;
+    let mut sum_0: f64 = 0f64;
+    let mut sum_1: f64 = 0f64;
     let mut rows: Vec<String> = vec![];
     for n in 0..100 {
         rows.push(if n % 10 == 0 {
-            sum_0 += n;
-            sum_1 += n;
+            sum_0 += n as f64;
+            sum_1 += n as f64;
             format!("{n} test log entry CPU={n}%; TEMP={n}C some content")
         } else if n % 5 == 0 {
-            sum_1 += n;
+            sum_1 += n as f64;
             format!("{n} test log entry TEMP={n}C some content")
         } else {
             format!("{n} test log entry; some content")
         });
     }
-    let values = extracted(
+    let terms = extracted(
         &rows.join("\n"),
         vec![
             String::from("CPU=(\\d{1,})%"),
             String::from("TEMP=(\\d{1,})C"),
         ],
     )?;
-    let mut control_0: usize = 0;
-    let mut control_1: usize = 0;
-    values.iter().for_each(|(pos, values)| {
+    let mut control_0: f64 = 0f64;
+    let mut control_1: f64 = 0f64;
+    terms.iter().for_each(|(term, values)| {
         assert!(!values.is_empty());
-        for (term, value) in values.iter() {
+        for (pos, value) in values.iter() {
             if *term == 0 {
-                control_0 += value.parse::<usize>().unwrap();
+                control_0 += value;
             } else if *term == 1 {
-                control_1 += value.parse::<usize>().unwrap();
+                control_1 += value;
             }
-            assert_eq!(*pos as usize, value.parse::<usize>().unwrap());
+            assert_eq!(*pos as f64, *value);
         }
     });
     assert_eq!(control_0, sum_0);
