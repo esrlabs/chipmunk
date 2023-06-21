8d48ed6557319b398cc130132f1b388ab6e3a41c
DmitryAstafyev <dvastafyev@gmail.com>
2023-06-21 17:42:39 +0200

    Update action and api for observe view

---
+++ application/holder/src/service/sessions/requests/observe/start.ts
@@ -0,0 +1,40 @@
+import { CancelablePromise } from 'platform/env/promise';
+import { sessions } from '@service/sessions';
+import { Logger } from 'platform/log';
+
+import * as Requests from 'platform/ipc/request';
+
+export const handler = Requests.InjectLogger<
+    Requests.Observe.Start.Request,
+    CancelablePromise<Requests.Observe.Start.Response>
+>(
+    (
+        log: Logger,
+        request: Requests.Observe.Start.Request,
+    ): CancelablePromise<Requests.Observe.Start.Response> => {
+        return new CancelablePromise((resolve, reject) => {
+            const stored = sessions.get(request.session);
+            if (stored === undefined) {
+                return reject(new Error(`Session doesn't exist`));
+            }
+            stored
+                .observe()
+                .start(request.observe)
+                .then(() => {
+                    resolve(
+                        new Requests.Observe.Start.Response({
+                            session: stored.session.getUUID(),
+                        }),
+                    );
+                })
+                .catch((err: Error) => {
+                    resolve(
+                        new Requests.Observe.Start.Response({
+                            session: stored.session.getUUID(),
+                            error: err.message,
+                        }),
+                    );
+                });
+        });
+    },
+);
