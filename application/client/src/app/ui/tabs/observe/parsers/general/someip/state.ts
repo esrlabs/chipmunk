264d937236a01362cbefa7beb16327e9357b5bac
DmitryAstafyev <dvastafyev@gmail.com>
2023-06-22 18:41:08 +0200

    SomeIp general view update

--- application/client/src/app/ui/tabs/observe/parsers/general/someip/state.ts
+++ application/client/src/app/ui/tabs/observe/parsers/general/someip/state.ts
@@ -2,10 +2,23 @@ import { File } from '@platform/types/files';
 import { bridge } from '@service/bridge';
 import { State as Base } from '../../state';
 
-// import * as SomeIp from '@platform/types/observe/parser/someip';
+import * as SomeIp from '@platform/types/observe/parser/someip';
 
 export class State extends Base {
     public fibex: File[] = [];
+
+    public update(): State {
+        const conf = this.observe.parser.as<SomeIp.Configuration>(SomeIp.Configuration);
+        if (conf === undefined) {
+            return this;
+        }
+        if (this.fibex.length !== 0) {
+            conf.configuration.fibex_file_paths = this.fibex.map((f) => f.filename);
+        } else {
+            conf.configuration.fibex_file_paths = undefined;
+        }
+        return this;
+    }
 
     public addFibexFile() {
         bridge
@@ -23,11 +36,12 @@ export class State extends Base {
                 this.ref.log().error(`Fail to open xml (fibex) file(s): ${err.message}`);
             })
             .finally(() => {
-                this.ref.detectChanges();
+                this.update().ref.detectChanges();
             });
     }
 
     public removeFibex(file: File) {
         this.fibex = this.fibex.filter((f) => f.filename !== file.filename);
+        this.update().ref.detectChanges();
     }
 }
