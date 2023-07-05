df31e2359718d11f6640f507b29c6a1dc0d4f165
DmitryAstafyev <dvastafyev@gmail.com>
2023-07-05 09:02:42 +0200

    Add view for text parser

--- application/client/src/app/ui/tabs/observe/parsers/general/module.ts
+++ application/client/src/app/ui/tabs/observe/parsers/general/module.ts
@@ -3,20 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { DltGeneralConfigurationModule } from './dlt/module';
 import { SomeIpGeneralConfigurationModule } from './someip/module';
+import { TextGeneralConfigurationModule } from './text/module';
 import { ParserGeneralConfiguration } from './component';
 
 @NgModule({
-    imports: [CommonModule, DltGeneralConfigurationModule, SomeIpGeneralConfigurationModule],
+    imports: [CommonModule, DltGeneralConfigurationModule, SomeIpGeneralConfigurationModule, TextGeneralConfigurationModule],
     declarations: [ParserGeneralConfiguration],
     exports: [
         ParserGeneralConfiguration,
         DltGeneralConfigurationModule,
         SomeIpGeneralConfigurationModule,
+        TextGeneralConfigurationModule,
     ],
     bootstrap: [
         ParserGeneralConfiguration,
         DltGeneralConfigurationModule,
         SomeIpGeneralConfigurationModule,
+        TextGeneralConfigurationModule,
     ],
 })
 export class ParserGeneralConfigurationModule {}
