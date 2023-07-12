e16ba8c2cc9b5e727b7fd36bb41e922d7620e830
DmitryAstafyev <dvastafyev@gmail.com>
2023-07-12 11:43:14 +0200

    Cleanup code

--- application/client/src/app/ui/tabs/observe/origin/stream/transport/setup/bases/udp/component.ts
+++ application/client/src/app/ui/tabs/observe/origin/stream/transport/setup/bases/udp/component.ts
@@ -29,32 +29,14 @@ export class SetupBase extends ChangesDetector implements OnDestroy, AfterConten
 
     public ngAfterContentInit(): void {
         this.state = new State(this.action, this.configuration);
-        // this.env().subscriber.register(
-        //     this.state.subjects.get().updated.subscribe(this.verify.bind(this)),
-        // );
-        // this.env().subscriber.register(
-        //     this.action.subjects.get().applied.subscribe(() => {
-        //         this.state.drop();
-        //         this.verify();
-        //     }),
-        // );
-        this.verify();
     }
 
     public addMulticast() {
         this.state.addMulticast();
-        this.verify();
     }
 
     public removeMulticast(index: number) {
         this.state.removeMulticast(index);
-        this.verify();
-    }
-
-    public verify() {
-        // setTimeout(() => {
-        //     this.action.setDisabled(!this.state.isValid());
-        // });
     }
 }
 export interface SetupBase extends IlcInterface {}
