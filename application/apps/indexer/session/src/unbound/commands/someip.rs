efdd0508aad212ad80a289f2cde46bd223a20b5e
DmitryAstafyev <dvastafyev@gmail.com>
2023-07-14 23:59:58 +0200

    Extra logs for someip statistic

--- application/apps/indexer/session/src/unbound/commands/someip.rs
+++ application/apps/indexer/session/src/unbound/commands/someip.rs
@@ -1,5 +1,6 @@
 use super::CommandOutcome;
 use crate::{events::ComputationError, unbound::signal::Signal};
+use log::{error, warn};
 use std::path::Path;
 
 use parsers::someip::{read_someip_statistic_from_pcapng, SomeipStatistic};
@@ -10,6 +11,7 @@ pub fn get_someip_statistic(
 ) -> Result<CommandOutcome<String>, ComputationError> {
     let mut statistic = SomeipStatistic::new();
     let mut error: Option<String> = None;
+    warn!("Getting statistic for: {files:?}");
     files.iter().for_each(|file| {
         if error.is_some() {
             return;
@@ -27,9 +29,11 @@ pub fn get_someip_statistic(
         }
     });
     if let Some(err) = error {
+        error!("Fail to get statistic for: {files:?}");
         return Err(ComputationError::IoOperation(err));
     }
     if signal.is_cancelling() {
+        warn!("Operation of geting statistic for: {files:?} has been cancelled");
         return Ok(CommandOutcome::Cancelled);
     }
     Ok(CommandOutcome::Finished(
