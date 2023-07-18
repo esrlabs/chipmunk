f5356f25243f8dd868d3356da4d37207f0530af0
DmitryAstafyev <dvastafyev@gmail.com>
2023-07-18 14:50:52 +0200

    Update handeling of observe-object changes

--- application/apps/rustcore/ts-bindings/spec/session.observing.spec.ts
+++ application/apps/rustcore/ts-bindings/spec/session.observing.spec.ts
@@ -9,6 +9,65 @@ import * as Factory from 'platform/types/observe/factory';
 
 const config = readConfigurationFile().get().tests.observing;
 
+function checkContext(
+    observe: $.Observe,
+    flags: { file: boolean; stream: boolean; concat: boolean },
+) {
+    expect(
+        observe.origin.as<$.Origin.Stream.Configuration>($.Origin.Stream.Configuration) instanceof
+            $.Origin.Stream.Configuration,
+    ).toEqual(flags.stream);
+    expect(
+        observe.origin.as<$.Origin.File.Configuration>($.Origin.File.Configuration) instanceof
+            $.Origin.File.Configuration,
+    ).toEqual(flags.file);
+    expect(
+        observe.origin.as<$.Origin.Concat.Configuration>($.Origin.Concat.Configuration) instanceof
+            $.Origin.Concat.Configuration,
+    ).toEqual(flags.concat);
+}
+
+function checkStream(
+    observe: $.Observe,
+    flags: { udp: boolean; tcp: boolean; serial: boolean; process: boolean },
+) {
+    expect(
+        observe.origin.as<$.Origin.Stream.Stream.UDP.Configuration>(
+            $.Origin.Stream.Stream.UDP.Configuration,
+        ) instanceof $.Origin.Stream.Stream.UDP.Configuration,
+    ).toEqual(flags.udp);
+    expect(
+        observe.origin.as<$.Origin.Stream.Stream.TCP.Configuration>(
+            $.Origin.Stream.Stream.TCP.Configuration,
+        ) instanceof $.Origin.Stream.Stream.TCP.Configuration,
+    ).toEqual(flags.tcp);
+    expect(
+        observe.origin.as<$.Origin.Stream.Stream.Process.Configuration>(
+            $.Origin.Stream.Stream.Process.Configuration,
+        ) instanceof $.Origin.Stream.Stream.Process.Configuration,
+    ).toEqual(flags.process);
+    expect(
+        observe.origin.as<$.Origin.Stream.Stream.Serial.Configuration>(
+            $.Origin.Stream.Stream.Serial.Configuration,
+        ) instanceof $.Origin.Stream.Stream.Serial.Configuration,
+    ).toEqual(flags.serial);
+}
+
+function checkParser(observe: $.Observe, flags: { text: boolean; dlt: boolean; someip: boolean }) {
+    expect(
+        observe.parser.as<$.Parser.Text.Configuration>($.Parser.Text.Configuration) instanceof
+            $.Parser.Text.Configuration,
+    ).toEqual(flags.text);
+    expect(
+        observe.parser.as<$.Parser.Dlt.Configuration>($.Parser.Dlt.Configuration) instanceof
+            $.Parser.Dlt.Configuration,
+    ).toEqual(flags.dlt);
+    expect(
+        observe.parser.as<$.Parser.SomeIp.Configuration>($.Parser.SomeIp.Configuration) instanceof
+            $.Parser.SomeIp.Configuration,
+    ).toEqual(flags.someip);
+}
+
 describe('Platform: observing', function () {
     it(config.regular.list[1], function () {
         return runner(config.regular, 1, async (logger, done, collector) => {
@@ -66,7 +125,6 @@ describe('Platform: observing', function () {
             // Note: splice of array gives multiple changes:
             // - change of position of each element
             // - change of array's length
-            console.log(`exit`);
             expect(changes).toEqual(19);
             finish(undefined, done);
         });
@@ -74,58 +132,48 @@ describe('Platform: observing', function () {
 
     it(config.regular.list[2], function () {
         return runner(config.regular, 2, async (logger, done, collector) => {
-            function checks(
-                observe: $.Observe,
-                flags: { udp: boolean; tcp: boolean; serial: boolean; process: boolean },
-            ) {
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.UDP.Configuration>(
-                        $.Origin.Stream.Stream.UDP.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.UDP.Configuration,
-                ).toEqual(flags.udp);
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.TCP.Configuration>(
-                        $.Origin.Stream.Stream.TCP.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.TCP.Configuration,
-                ).toEqual(flags.tcp);
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.Process.Configuration>(
-                        $.Origin.Stream.Stream.Process.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.Process.Configuration,
-                ).toEqual(flags.process);
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.Serial.Configuration>(
-                        $.Origin.Stream.Stream.Serial.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.Serial.Configuration,
-                ).toEqual(flags.serial);
-            }
             const observe = new Factory.Stream().asDlt().tcp().get();
-            checks(observe, { udp: false, tcp: true, serial: false, process: false });
+            checkStream(observe, { udp: false, tcp: true, serial: false, process: false });
             const stream = observe.origin.as<$.Origin.Stream.Configuration>(
                 $.Origin.Stream.Configuration,
-            );
+            ) as $.Origin.Stream.Configuration;
             expect(stream instanceof $.Origin.Stream.Configuration).toEqual(true);
-            stream?.change(
+            stream.change(
                 new $.Origin.Stream.Stream.UDP.Configuration(
                     $.Origin.Stream.Stream.UDP.Configuration.initial(),
                     undefined,
                 ),
             );
-            checks(observe, { udp: true, tcp: false, serial: false, process: false });
-            stream?.change(
+            expect(
+                stream.as<$.Origin.Stream.Stream.UDP.Configuration>(
+                    $.Origin.Stream.Stream.UDP.Configuration,
+                ) instanceof $.Origin.Stream.Stream.UDP.Configuration,
+            ).toEqual(true);
+            checkStream(observe, { udp: true, tcp: false, serial: false, process: false });
+            stream.change(
                 new $.Origin.Stream.Stream.Serial.Configuration(
                     $.Origin.Stream.Stream.Serial.Configuration.initial(),
                     undefined,
                 ),
             );
-            checks(observe, { udp: false, tcp: false, serial: true, process: false });
-            stream?.change(
+            checkStream(observe, { udp: false, tcp: false, serial: true, process: false });
+            expect(
+                stream.as<$.Origin.Stream.Stream.Serial.Configuration>(
+                    $.Origin.Stream.Stream.Serial.Configuration,
+                ) instanceof $.Origin.Stream.Stream.Serial.Configuration,
+            ).toEqual(true);
+            stream.change(
                 new $.Origin.Stream.Stream.Process.Configuration(
                     $.Origin.Stream.Stream.Process.Configuration.initial(),
                     undefined,
                 ),
             );
-            checks(observe, { udp: false, tcp: false, serial: false, process: true });
+            checkStream(observe, { udp: false, tcp: false, serial: false, process: true });
+            expect(
+                stream.as<$.Origin.Stream.Stream.Process.Configuration>(
+                    $.Origin.Stream.Stream.Process.Configuration,
+                ) instanceof $.Origin.Stream.Stream.Process.Configuration,
+            ).toEqual(true);
             // Command for process isn't defined; so validation should give error
             expect(observe.validate() instanceof Error).toEqual(true);
             const process = observe.origin.as<$.Origin.Stream.Stream.Process.Configuration>(
@@ -139,13 +187,18 @@ describe('Platform: observing', function () {
             // Set invalid command
             process.configuration.command = '';
             expect(observe.validate() instanceof Error).toEqual(true);
-            stream?.change(
+            stream.change(
                 new $.Origin.Stream.Stream.TCP.Configuration(
                     $.Origin.Stream.Stream.TCP.Configuration.initial(),
                     undefined,
                 ),
             );
-            checks(observe, { udp: false, tcp: true, serial: false, process: false });
+            checkStream(observe, { udp: false, tcp: true, serial: false, process: false });
+            expect(
+                stream.as<$.Origin.Stream.Stream.TCP.Configuration>(
+                    $.Origin.Stream.Stream.TCP.Configuration,
+                ) instanceof $.Origin.Stream.Stream.TCP.Configuration,
+            ).toEqual(true);
             const tcp = observe.origin.as<$.Origin.Stream.Stream.TCP.Configuration>(
                 $.Origin.Stream.Stream.TCP.Configuration,
             ) as $.Origin.Stream.Stream.TCP.Configuration;
@@ -155,13 +208,13 @@ describe('Platform: observing', function () {
             tcp.configuration.bind_addr = '0.0.0.0';
             // Valid IP address setup, no errors should be
             expect(observe.validate() instanceof Error).toEqual(false);
-            stream?.change(
+            stream.change(
                 new $.Origin.Stream.Stream.UDP.Configuration(
                     $.Origin.Stream.Stream.UDP.Configuration.initial(),
                     undefined,
                 ),
             );
-            checks(observe, { udp: true, tcp: false, serial: false, process: false });
+            checkStream(observe, { udp: true, tcp: false, serial: false, process: false });
             const udp = observe.origin.as<$.Origin.Stream.Stream.UDP.Configuration>(
                 $.Origin.Stream.Stream.UDP.Configuration,
             ) as $.Origin.Stream.Stream.UDP.Configuration;
@@ -187,28 +240,8 @@ describe('Platform: observing', function () {
 
     it(config.regular.list[3], function () {
         return runner(config.regular, 3, async (logger, done, collector) => {
-            function checks(
-                observe: $.Observe,
-                flags: { file: boolean; stream: boolean; concat: boolean },
-            ) {
-                expect(
-                    observe.origin.as<$.Origin.Stream.Configuration>(
-                        $.Origin.Stream.Configuration,
-                    ) instanceof $.Origin.Stream.Configuration,
-                ).toEqual(flags.stream);
-                expect(
-                    observe.origin.as<$.Origin.File.Configuration>(
-                        $.Origin.File.Configuration,
-                    ) instanceof $.Origin.File.Configuration,
-                ).toEqual(flags.file);
-                expect(
-                    observe.origin.as<$.Origin.Concat.Configuration>(
-                        $.Origin.Concat.Configuration,
-                    ) instanceof $.Origin.Concat.Configuration,
-                ).toEqual(flags.concat);
-            }
             const observe = new Factory.File().type($.Types.File.FileType.Text).asText().get();
-            checks(observe, { file: true, concat: false, stream: false });
+            checkContext(observe, { file: true, concat: false, stream: false });
             let file = observe.origin.as<$.Origin.File.Configuration>(
                 $.Origin.File.Configuration,
             ) as $.Origin.File.Configuration;
@@ -233,28 +266,8 @@ describe('Platform: observing', function () {
 
     it(config.regular.list[4], function () {
         return runner(config.regular, 4, async (logger, done, collector) => {
-            function checks(
-                observe: $.Observe,
-                flags: { text: boolean; dlt: boolean; someip: boolean },
-            ) {
-                expect(
-                    observe.parser.as<$.Parser.Text.Configuration>(
-                        $.Parser.Text.Configuration,
-                    ) instanceof $.Parser.Text.Configuration,
-                ).toEqual(flags.text);
-                expect(
-                    observe.parser.as<$.Parser.Dlt.Configuration>(
-                        $.Parser.Dlt.Configuration,
-                    ) instanceof $.Parser.Dlt.Configuration,
-                ).toEqual(flags.dlt);
-                expect(
-                    observe.parser.as<$.Parser.SomeIp.Configuration>(
-                        $.Parser.SomeIp.Configuration,
-                    ) instanceof $.Parser.SomeIp.Configuration,
-                ).toEqual(flags.someip);
-            }
             const observe = new Factory.File().type($.Types.File.FileType.Text).asText().get();
-            checks(observe, { text: true, dlt: false, someip: false });
+            checkParser(observe, { text: true, dlt: false, someip: false });
             let file = observe.origin.as<$.Origin.File.Configuration>(
                 $.Origin.File.Configuration,
             ) as $.Origin.File.Configuration;
@@ -263,7 +276,7 @@ describe('Platform: observing', function () {
             observe.parser.change(
                 new $.Parser.Dlt.Configuration($.Parser.Dlt.Configuration.initial(), undefined),
             );
-            checks(observe, { text: false, dlt: true, someip: false });
+            checkParser(observe, { text: false, dlt: true, someip: false });
             expect(observe.validate() instanceof Error).toEqual(true);
             file.set().type($.Types.File.FileType.PcapNG);
             observe.parser.change(
@@ -272,7 +285,7 @@ describe('Platform: observing', function () {
                     undefined,
                 ),
             );
-            checks(observe, { text: false, dlt: false, someip: true });
+            checkParser(observe, { text: false, dlt: false, someip: true });
             expect(observe.validate() instanceof Error).toEqual(true);
             file.set().filename('some_file');
             expect(observe.validate() instanceof Error).toEqual(false);
@@ -282,34 +295,9 @@ describe('Platform: observing', function () {
 
     it(config.regular.list[5], function () {
         return runner(config.regular, 5, async (logger, done, collector) => {
-            function checks(
-                observe: $.Observe,
-                flags: { udp: boolean; tcp: boolean; serial: boolean; process: boolean },
-            ) {
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.UDP.Configuration>(
-                        $.Origin.Stream.Stream.UDP.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.UDP.Configuration,
-                ).toEqual(flags.udp);
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.TCP.Configuration>(
-                        $.Origin.Stream.Stream.TCP.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.TCP.Configuration,
-                ).toEqual(flags.tcp);
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.Process.Configuration>(
-                        $.Origin.Stream.Stream.Process.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.Process.Configuration,
-                ).toEqual(flags.process);
-                expect(
-                    observe.origin.as<$.Origin.Stream.Stream.Serial.Configuration>(
-                        $.Origin.Stream.Stream.Serial.Configuration,
-                    ) instanceof $.Origin.Stream.Stream.Serial.Configuration,
-                ).toEqual(flags.serial);
-            }
             const observe = new Factory.Stream().asDlt().tcp().get();
-            checks(observe, { udp: false, tcp: true, serial: false, process: false });
-            observe.watcher.subscribe(() => {
+            checkStream(observe, { udp: false, tcp: true, serial: false, process: false });
+            observe.subscribe(() => {
                 if (currentAction >= actions.length) {
                     console.log(`[warn] Update out of actions (this is NOT an error)`);
                     return;
@@ -322,9 +310,9 @@ describe('Platform: observing', function () {
                     action: () => {
                         const stream = observe.origin.as<$.Origin.Stream.Configuration>(
                             $.Origin.Stream.Configuration,
-                        );
+                        ) as $.Origin.Stream.Configuration;
                         expect(stream instanceof $.Origin.Stream.Configuration).toEqual(true);
-                        stream?.change(
+                        stream.change(
                             new $.Origin.Stream.Stream.UDP.Configuration(
                                 $.Origin.Stream.Stream.UDP.Configuration.initial(),
                                 undefined,
@@ -332,7 +320,12 @@ describe('Platform: observing', function () {
                         );
                     },
                     check: () => {
-                        checks(observe, { udp: true, tcp: false, serial: false, process: false });
+                        checkStream(observe, {
+                            udp: true,
+                            tcp: false,
+                            serial: false,
+                            process: false,
+                        });
                         expect(observe.validate() instanceof Error).toEqual(true);
                     },
                 },
