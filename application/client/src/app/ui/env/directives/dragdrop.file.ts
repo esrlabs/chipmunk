a65ecf23ae66794534331266e1588d5f2807303a
DmitryAstafyev <dvastafyev@gmail.com>
2023-09-12 12:07:08 +0200

    Fix drag and drop files support

--- application/client/src/app/ui/env/directives/dragdrop.file.ts
+++ application/client/src/app/ui/env/directives/dragdrop.file.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
+import { Directive, OnDestroy, Output, EventEmitter, ElementRef } from '@angular/core';
 import { bridge } from '@service/bridge';
 import { File as OwnFileDef } from '@platform/types/files';
 import { stop } from '@ui/env/dom';
@@ -11,27 +11,78 @@ export interface GlobalFileDef extends File {
 @Directive({
     selector: '[appMatDragDropFileFeature]',
 })
-export class MatDragDropFileFeatureDirective {
+export class MatDragDropFileFeatureDirective implements OnDestroy {
     @Output() dropped: EventEmitter<OwnFileDef[]> = new EventEmitter();
 
-    @HostListener('dragover', ['$event']) dragover(event: MouseEvent) {
+    protected drop(event: DragEvent): void {
         stop(event);
-    }
-
-    @HostListener('dragleave', ['$event']) dragleave(event: MouseEvent) {
-        stop(event);
-    }
-
-    @HostListener('drop', ['$event']) drop(event: DragEvent) {
-        if (event.dataTransfer === null || event.dataTransfer === undefined) {
+        const files: string[] = this.getFiles(event);
+        if (files.length === 0) {
             return;
         }
-        const files: FileList = event.dataTransfer.files;
         bridge
             .files()
-            .getByPath(Array.from(files).map((f) => (f as GlobalFileDef).path))
+            .getByPath(files)
             .then((files) => {
                 this.dropped.emit(files);
             });
+    }
+
+    protected stop(event: MouseEvent): boolean {
+        return stop(event);
+    }
+
+    protected getFiles(event: DragEvent): string[] {
+        if (event.dataTransfer === null || event.dataTransfer === undefined) {
+            return [];
+        }
+        const path = (entity: unknown): string => {
+            return typeof (entity as { [key: string]: string })['path'] === 'string'
+                ? (entity as { [key: string]: string })['path']
+                : (entity as { [key: string]: string })['name'];
+        };
+        const files = (() => {
+            if (event.dataTransfer.files) {
+                return Array.from(event.dataTransfer.files).map((f) => path(f));
+            } else if (event.dataTransfer.items) {
+                return (
+                    Array.from(event.dataTransfer.items)
+                        .map((item: DataTransferItem) => {
+                            if (item.kind === 'file') {
+                                return item.getAsFile();
+                            } else {
+                                return undefined;
+                            }
+                        })
+                        .filter((f) => f !== undefined) as File[]
+                ).map((f) => path(f));
+            } else {
+                return [];
+            }
+        })();
+        return files;
+    }
+
+    constructor(protected readonly element: ElementRef<HTMLDivElement>) {
+        // Note: The files property of DataTransfer objects can only be accessed from within
+        // the drop event. For all other events, the files property will be empty — because
+        // its underlying data store will be in a protected mode.
+        // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/files
+        //
+        // Using of @HostBinding doesn't work always smooth and looks like
+        // "protected mode" is a reason.
+        //
+        // That's why we are binding event's handlers directly to DOM elements
+        this.stop = this.stop.bind(this);
+        this.drop = this.drop.bind(this);
+        this.element.nativeElement.addEventListener('dragover', this.stop);
+        this.element.nativeElement.addEventListener('dragleave', this.stop);
+        this.element.nativeElement.addEventListener('drop', this.drop);
+    }
+
+    public ngOnDestroy(): void {
+        this.element.nativeElement.removeEventListener('dragover', this.stop);
+        this.element.nativeElement.removeEventListener('dragleave', this.stop);
+        this.element.nativeElement.removeEventListener('drop', this.drop);
     }
 }
