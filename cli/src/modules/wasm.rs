6dc897d59bb1218323a7c1a5a655da06d0390e41
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-30 22:30:27 +0100

    CLI Wasm: Prepare the implementation to run tests
    - Method to run the tests has been implemented to use when running the
      tests is built in the cli app

--- cli/src/modules/wasm.rs
+++ cli/src/modules/wasm.rs
@@ -1,7 +1,11 @@
 use super::{Kind, Manager};
-use crate::{Target, LOCATION};
+use crate::{
+    spawner::{spawn, SpawnResult},
+    Target, LOCATION,
+};
 use async_trait::async_trait;
-use std::path::PathBuf;
+use futures::future::join_all;
+use std::{io, path::PathBuf};
 
 const PATH: &str = "application/apps/rustcore/wasm-bindings";
 
@@ -36,5 +40,23 @@ impl Manager for Module {
         let env = if prod { "--release" } else { "--dev" };
 
         Some(format!("wasm-pack build {env} --target bundler"))
+    }
+}
+
+impl Module {
+    // TODO: Use this implementation when testing is implemented in the system
+    #[allow(dead_code)]
+    pub async fn run_tests(&self) -> Vec<Result<SpawnResult, io::Error>> {
+        let path_karma = self.cwd().join("spec");
+
+        join_all([
+            spawn(
+                "wasm-pack test --node",
+                Some(self.cwd()),
+                Some("wasm-pack test wasm-bindings"),
+            ),
+            spawn("npm run test", Some(path_karma), Some("npm test wasm")),
+        ])
+        .await
     }
 }
