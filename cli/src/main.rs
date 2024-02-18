65b43c70c6f2dfd91e51e9c0c9707a1b6e634ff7
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-02-18 21:53:12 +0100

    Changes on the CLI arguments & Commands...
    - Targets changed to positional arguments with the index 1, so the users
      enter their targets directly after the command
    - Unnecessary clone for the targets removed by creating a methods to
      extract the targets from the optional arguments
    - Release flag moved to build command since it's used their only & It's
      name changed to production to free up the short argument -r for the
      report
    - report argument added to CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,7 +9,10 @@ use clap::{Parser, Subcommand};
 use futures::future::join_all;
 use location::Location;
 use modules::Manager;
-use std::io::{Error, ErrorKind};
+use std::{
+    io::{Error, ErrorKind},
+    path::PathBuf,
+};
 use target::Target;
 use tracker::Tracker;
 
@@ -22,15 +25,15 @@ lazy_static! {
     static ref TRACKER: Tracker = Tracker::new();
 }
 
+static REPORT_HELP_TEXT: &str =
+    "Write report from command logs to the given file or to stdout if no file is defined";
+static REPORT_VALUE_NAME: &str = "FILE-PATH";
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Build release version
-    #[arg(short, long, default_value_t = false)]
-    release: bool,
-
     #[command(subcommand)]
-    command: Option<Command>,
+    command: Command,
 }
 
 #[derive(Subcommand, Debug, Clone)]
@@ -38,102 +41,125 @@ enum Command {
     /// Runs linting & clippy
     Lint {
         /// Target to lint, by default whole application will be linted
-        #[arg(short, long, num_args(0..))]
+        #[arg(index = 1)]
         target: Option<Vec<Target>>,
+
+        #[arg(short, long, value_name = REPORT_VALUE_NAME, help = REPORT_HELP_TEXT)]
+        report: Option<Option<PathBuf>>,
     },
     /// Build
     Build {
         /// Target to build, by default whole application will be built
-        #[arg(short, long, num_args(0..))]
+        #[arg(index = 1)]
         target: Option<Vec<Target>>,
+
+        /// Build release version
+        #[arg(short, long, default_value_t = false)]
+        production: bool,
+
+        #[arg(short, long, value_name = REPORT_VALUE_NAME, help = REPORT_HELP_TEXT)]
+        report: Option<Option<PathBuf>>,
     },
     /// Clean
     Clean {
         /// Target to clean, by default whole application will be cleaned
-        #[arg(short, long, num_args(0..))]
+        #[arg(index = 1)]
         target: Option<Vec<Target>>,
+
+        #[arg(short, long, value_name = REPORT_VALUE_NAME, help = REPORT_HELP_TEXT)]
+        report: Option<Option<PathBuf>>,
     },
     /// Run tests
     Test {
         /// Target to test, by default whole application will be tested
-        #[arg(short, long, num_args(0..))]
+        #[arg(index = 1)]
         target: Option<Vec<Target>>,
+
+        #[arg(short, long, value_name = REPORT_VALUE_NAME, help = REPORT_HELP_TEXT)]
+        report: Option<Option<PathBuf>>,
     },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let cli = Cli::parse();
-    let production = cli.release;
-    if let Some(ref command) = cli.command {
-        let targets: Vec<Box<dyn Manager + Sync + Send>> = if let Some(mut list) =
-            match command.clone() {
-                Command::Lint { target } => target,
-                Command::Build { target } => target,
-                Command::Clean { target } => target,
-                Command::Test { target } => target,
-            } {
-            list.dedup();
-            list.iter().map(|target| target.get()).collect()
-        } else {
-            Target::all()
-        };
-        let results = match command {
-            Command::Lint { target: _ } => {
-                join_all(
-                    targets
-                        .iter()
-                        .map(|module| module.check())
-                        .collect::<Vec<_>>(),
-                )
-                .await
-            }
-            Command::Build { target: _ } => {
-                join_all(
-                    targets
-                        .iter()
-                        .map(|module| module.build(production))
-                        .collect::<Vec<_>>(),
-                )
-                .await
-            }
-            Command::Clean { target: _ } => {
-                join_all(
-                    targets
-                        .iter()
-                        .map(|module| module.reset())
-                        .collect::<Vec<_>>(),
-                )
-                .await
-            }
-            Command::Test { target: _ } => {
-                join_all(
-                    targets
-                        .iter()
-                        .map(|module| module.test())
-                        .collect::<Vec<_>>(),
-                )
-                .await
-            }
-        };
-        TRACKER.shutdown().await?;
-        let mut success: bool = true;
-        results.iter().for_each(|res| match res {
-            Ok(status) => {
-                if !status.status.success() {
-                    eprintln!("Failed with errors");
-                    println!("{}:\n{}", status.job, status.stderr.join(""));
-                    success = false;
-                }
-            }
-            Err(err) => {
-                eprintln!("Builder error: {err}");
+    let command = cli.command;
+    let results = match command {
+        Command::Lint { target, report } => {
+            dbg!(report);
+            let targets = get_targets_or_default(target);
+            join_all(
+                targets
+                    .iter()
+                    .map(|module| module.check())
+                    .collect::<Vec<_>>(),
+            )
+            .await
+        }
+        Command::Build {
+            target,
+            production,
+            report,
+        } => {
+            dbg!(report);
+            let targets = get_targets_or_default(target);
+            join_all(
+                targets
+                    .iter()
+                    .map(|module| module.build(production))
+                    .collect::<Vec<_>>(),
+            )
+            .await
+        }
+        Command::Clean { target, report } => {
+            dbg!(report);
+            let targets = get_targets_or_default(target);
+            join_all(
+                targets
+                    .iter()
+                    .map(|module| module.reset())
+                    .collect::<Vec<_>>(),
+            )
+            .await
+        }
+        Command::Test { target, report } => {
+            dbg!(report);
+            let targets = get_targets_or_default(target);
+            join_all(
+                targets
+                    .iter()
+                    .map(|module| module.test())
+                    .collect::<Vec<_>>(),
+            )
+            .await
+        }
+    };
+    TRACKER.shutdown().await?;
+    let mut success: bool = true;
+    results.iter().for_each(|res| match res {
+        Ok(status) => {
+            if !status.status.success() {
+                eprintln!("Failed with errors");
+                println!("{}:\n{}", status.job, status.stderr.join(""));
                 success = false;
             }
-        });
-        if !success {
-            return Err(Error::new(ErrorKind::Other, "Some task were failed"));
         }
+        Err(err) => {
+            eprintln!("Builder error: {err}");
+            success = false;
+        }
+    });
+    if !success {
+        return Err(Error::new(ErrorKind::Other, "Some task were failed"));
     }
     Ok(())
+}
+
+fn get_targets_or_default(targets: Option<Vec<Target>>) -> Vec<Box<dyn Manager + Sync + Send>> {
+    if let Some(mut list) = targets {
+        list.dedup();
+        list.iter().map(|target| target.get()).collect()
+    } else {
+        Target::all()
+    }
 }
