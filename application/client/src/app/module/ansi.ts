0a6a64b007423fa66a0a30a7dc2d35bd74eea32d
DmitryAstafyev <dvastafyev@gmail.com>
2024-03-19 23:29:17 +0100

    Update ansi colors parser

--- application/client/src/app/module/ansi.ts
+++ application/client/src/app/module/ansi.ts
@@ -2,9 +2,31 @@ import { error } from '@platform/log/utils';
 
 import * as wasm from '@loader/wasm';
 
+let mapper: any = undefined;
+
 export function ansiToHtml(input: string): string | Error {
     try {
         return wasm.getBindings().convert(input);
+    } catch (e) {
+        return new Error(error(e));
+    }
+}
+
+export interface Slot {
+    from: number;
+    to: number;
+    color: string | null;
+    background: string | null;
+    bold: boolean;
+    italic: boolean;
+}
+
+export function getAnsiMap(input: string): Slot[] | Error {
+    if (mapper === undefined) {
+        mapper = wasm.getBindings().AnsiMapper.new();
+    }
+    try {
+        return mapper.get_map(input);
     } catch (e) {
         return new Error(error(e));
     }
@@ -15,5 +37,13 @@ export function escapeAnsi(input: string): string | Error {
         return wasm.getBindings().escape(input);
     } catch (e) {
         return new Error(error(e));
+    }
+}
+
+export function safeEscapeAnsi(input: string): string {
+    try {
+        return wasm.getBindings().escape(input);
+    } catch (e) {
+        return input;
     }
 }
