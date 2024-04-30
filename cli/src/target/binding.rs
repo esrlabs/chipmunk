21703edcb54b2b26f9569f529183e4e190b8c767
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-04-30 12:57:49 +0200

    Build CLI: Fix script file name on Windows
    Script file to build electron can get the extension '*.cmd' on Windows

--- cli/src/target/binding.rs
+++ cli/src/target/binding.rs
@@ -10,7 +10,15 @@ pub fn get_build_cmd(prod: bool) -> String {
     let mut path = Target::Wrapper.cwd();
     path.push("node_modules");
     path.push(".bin");
-    path.push("electron-build-env");
+
+    if cfg!(windows) {
+        // The script files can get the extension '*.cmd' on Windows
+        let electron_build_env_path =
+            which::which_in("electron-build-env", Some(&path), &path).unwrap();
+        path = electron_build_env_path;
+    } else {
+        path.push("electron-build-env");
+    }
 
     format!(
         "{} nj-cli build{}",
