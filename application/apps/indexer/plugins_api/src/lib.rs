e5b2e1c95e15850ffaf4ba0d1df00d5b8d8539a1
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-07-12 16:32:35 +0200

    Plugin: Fixes & Adjustments to Parser Plugin Client
    Apply fixes and changes for the parser client after trying to use it
    with real external plugin. Changes include:
    - Separate parser and bytesource with features to prevent generate!
      Macro from being called multiple times.
    - Move add function on the host to it's own interface that will be
      imported separately to avoid compilation errors because of duplicate
      definitions of types that occur when using the crate with separate
      projects as plugins only + Make the needed adjustments on the host
      side.
    - Make generated bindings public but hide them in documentations to
      because they are needed with export Macro
    - Move Prototyping code to test module temporally.

--- application/apps/indexer/plugins_api/src/lib.rs
+++ application/apps/indexer/plugins_api/src/lib.rs
@@ -1,2 +1,10 @@
+// Calling generate! Macro multiple time on the same crate causes compilation errors with `cargo
+// component` in release mode.
+
+#[cfg(feature = "bytesource")]
+/// Provides types, methods and macros to write plugins that provide byte source functionality
 pub mod bytesource;
+
+#[cfg(feature = "parser")]
+/// Provides types, methods and macros to write plugins that provide parser functionality
 pub mod parser;
