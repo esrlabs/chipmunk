ab0f28273cd1977654dc66aedc9eeee57476a53b
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-07-12 16:32:35 +0200

    Plugin: Fixes & Adjustments to Parser Plugin Client
    Apply fixes and changes for the parser client after trying to use it
    with real external plugin. Changes include:
    - Separate parser and bytesource with features to prevent generate!
      Macro from being called multiple times.
    - Move add function on the host to it's own interface that will be
      imported separately to avoid compilation errors because of duplicate
      definitions of types that occur when using the crate with separate
      projects as plugins only + Make the needed adjustments on the host
      side.
    - Make generated bindings public but hide them in documentations to
      because they are needed with export Macro
    - Move Prototyping code to test module temporally.

--- application/apps/indexer/plugins_api/src/parser/mod.rs
+++ application/apps/indexer/plugins_api/src/parser/mod.rs
@@ -3,36 +3,29 @@
 
 use std::path::{Path, PathBuf};
 
-use self::bindings::exports::chipmunk::plugin::parser::Guest;
-
-mod bindings {
-    use super::*;
-
+// Module must be public because the generated types and macros are used within `parser_export!`
+// macro + macros can't be re-exported via pub use
+#[doc(hidden)]
+pub mod internal_bindings {
     wit_bindgen::generate!({
         path: "wit/v_0.1.0",
         world: "parse-plugin",
-        export_macro_name: "export_intern",
         // Export macro is used withing the exported `parser_export!` macro and must be public
         pub_export_macro: true,
         // Bindings for export macro must be set, because it won't be called from withing the
         // same module where `generate!` is called
-        default_bindings_module: "crate::parser::bindings",
+        default_bindings_module: "$crate::parser::internal_bindings",
     });
 }
 
 // External exports for users
-pub use bindings::chipmunk::plugin::{
+pub use internal_bindings::chipmunk::plugin::{
     parse_types::{Attachment, ParseError, ParseReturn, ParseYield, ParserConfig},
     shared_types::InitError,
 };
-// Exports needed in macro but not needed by users
-#[doc(hidden)]
-pub use bindings::{
-    add as add_res_intern, export_intern,
-    exports::chipmunk::plugin::parser::Guest as PluginInternalGuest,
-};
 
-/// Chipmunk Parser Plugin
+/// Chipmunk Parser for plugins. This trait must be implemented from types that need to be exported
+/// as parser plugin to be used within Chipmunk
 pub trait Parser {
     fn create(
         general_configs: ParserConfig,
@@ -40,6 +33,7 @@ pub trait Parser {
     ) -> Result<Self, InitError>
     where
         Self: Sized;
+
     fn parse(
         &mut self,
         data: &[u8],
@@ -47,31 +41,8 @@ pub trait Parser {
     ) -> impl IntoIterator<Item = Result<ParseReturn, ParseError>> + Send;
 }
 
-struct Dummy;
-
-impl Parser for Dummy {
-    fn create(
-        general_configs: ParserConfig,
-        config_path: Option<PathBuf>,
-    ) -> Result<Self, InitError>
-    where
-        Self: Sized,
-    {
-        todo!()
-    }
-
-    fn parse(
-        &mut self,
-        data: &[u8],
-        timestamp: Option<u64>,
-    ) -> impl IntoIterator<Item = Result<ParseReturn, ParseError>> + Send {
-        Vec::new()
-    }
-}
-
-//************************* Macro start **********************************
-
 #[macro_export]
+/// TODO AAZ: Macro docs with example
 macro_rules! parser_export {
     ($par:ty) => {
         static mut PARSER: ::std::option::Option<$par> = ::std::option::Option::None;
@@ -79,7 +50,9 @@ macro_rules! parser_export {
         // Name intentionally lengthened to avoid conflict with user's own types
         struct InternalPluginParserGuest;
 
-        impl $crate::parser::PluginInternalGuest for InternalPluginParserGuest {
+        impl $crate::parser::internal_bindings::exports::chipmunk::plugin::parser::Guest
+            for InternalPluginParserGuest
+        {
             /// Initialize the parser with the given configurations
             fn init(
                 general_configs: $crate::parser::ParserConfig,
@@ -115,13 +88,42 @@ macro_rules! parser_export {
                 // once on the same time on host
                 let parser = unsafe { PARSER.as_mut().expect("parser already initialized") };
                 for item in parser.parse(&data, timestamp) {
-                    $crate::parser::add_res_intern(item.as_ref());
+                    $crate::parser::internal_bindings::chipmunk::plugin::host_add::add(
+                        item.as_ref(),
+                    );
                 }
             }
         }
 
-        $crate::parser::export_intern!(InternalPluginParserGuest);
+        $crate::parser::internal_bindings::export!(InternalPluginParserGuest);
     };
 }
 
-parser_export!(Dummy);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Dummy;
+
+    impl Parser for Dummy {
+        fn create(
+            general_configs: ParserConfig,
+            config_path: Option<PathBuf>,
+        ) -> Result<Self, InitError>
+        where
+            Self: Sized,
+        {
+            todo!()
+        }
+
+        fn parse(
+            &mut self,
+            data: &[u8],
+            timestamp: Option<u64>,
+        ) -> impl IntoIterator<Item = Result<ParseReturn, ParseError>> + Send {
+            Vec::new()
+        }
+    }
+
+    parser_export!(Dummy);
+}
