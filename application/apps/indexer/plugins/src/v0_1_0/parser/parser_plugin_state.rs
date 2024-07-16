097f2632b8b881801ddc37f4690fa644e69a8a23
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-07-16 17:04:43 +0200

    Plugins API: Adding logging functionality
    - Implement logging on parser host.
    - Log Level will be always the current leg level in chipmunk since we
      use the same log functionality to log plugins' messages

--- application/apps/indexer/plugins/src/v0_1_0/parser/parser_plugin_state.rs
+++ application/apps/indexer/plugins/src/v0_1_0/parser/parser_plugin_state.rs
@@ -1,7 +1,11 @@
 use wasmtime_wasi::{ResourceTable, WasiCtx, WasiView};
 
 use super::bindings::{
-    chipmunk::plugin::{host_add::Host, parse_types, shared_types},
+    chipmunk::plugin::{
+        host_add::Host,
+        logging::{self, Level},
+        parse_types, shared_types,
+    },
     ParseError, ParseReturn,
 };
 
@@ -41,3 +45,18 @@ impl Host for ParserPluginState {
 impl parse_types::Host for ParserPluginState {}
 
 impl shared_types::Host for ParserPluginState {}
+
+impl logging::Host for ParserPluginState {
+    /// Log the given message with current log level if log level is allowed
+    fn log(&mut self, level: Level, msg: String) -> () {
+        const PARSER_LOG_TARGET: &str = "parser_plugin";
+
+        match level {
+            Level::Error => log::error!(target: PARSER_LOG_TARGET, "{msg}"),
+            Level::Warn => log::warn!(target: PARSER_LOG_TARGET, "{msg}"),
+            Level::Info => log::info!(target: PARSER_LOG_TARGET, "{msg}"),
+            Level::Debug => log::debug!(target: PARSER_LOG_TARGET, "{msg}"),
+            Level::Trace => log::trace!(target: PARSER_LOG_TARGET, "{msg}"),
+        }
+    }
+}
