e0ae1eaa09b2b64e26868d1ddb602a1c48b8d94e
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-07-16 14:11:23 +0200

    Plugins API: Adding logging functionality
    - Provide custom logger that implements `log::Log` trait and register
      it using boxed logger.
    - This enables the users from using the general log macros from `log`
      crate and the message will be validated on the client side then sent
      to the host.
    - Added log level to the custom configurations for the parser in the wit
      file and implement it on the client side only for now.

--- application/apps/indexer/plugins_api/build.rs
+++ application/apps/indexer/plugins_api/build.rs
@@ -2,7 +2,10 @@ fn main() {
     // Make sure one feature at most is enabled. Enabling more than one feature leads to linking
     // and validation errors since the macro [`wit_bidngen::generat!`] will be called multiple
     // times generating conflicting types.
-    #[cfg(all(feature = "parser", feature = "bytesource"))]
+    //
+    // This check is enabled on release mode only to allow rust analyzer to work with all targets
+    // while in development
+    #[cfg(all(not(debug_assertions), feature = "parser", feature = "bytesource"))]
     compile_error!(
         "Only one feature can be enabled at a time.\n\
         Enabling more than one feature will generate conflicting types"
---
+++ application/apps/indexer/plugins_api/src/plugin_logger.rs
@@ -0,0 +1,59 @@
+use log::{Log, Metadata, Record};
+
+/// Provides methods to send logs to the WASM host.
+///
+/// This trait is made to be used internally only and it is not part of the crate's public API
+/// and is subject to change at any time.
+pub trait LogSend {
+    fn send_msg(&self, level: log::Level, msg: &str);
+}
+
+#[derive(Debug)]
+/// Struct that implements [`log::Log`] trait to enable the users to use rust standard
+/// log macros to send log messages to the host.
+///
+/// This struct is made for internal use only and it is not part of the crate's public API
+/// and is subject to change at any time.
+pub struct PluginLogger<C>
+where
+    C: LogSend,
+{
+    sender: C,
+    max_level: log::Level,
+}
+
+impl<C> PluginLogger<C>
+where
+    C: LogSend,
+{
+    pub fn new(sender: C, max_level: impl Into<log::Level>) -> Self {
+        let max_level = max_level.into();
+        Self { sender, max_level }
+    }
+}
+
+impl<C> Log for PluginLogger<C>
+where
+    C: LogSend + Send + Sync,
+{
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= self.max_level
+    }
+
+    fn log(&self, record: &Record) {
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+
+        if let Some(msg) = record.args().as_str() {
+            self.sender.send_msg(record.level(), msg);
+        } else {
+            let msg = record.args().to_string();
+            self.sender.send_msg(record.level(), msg.as_str());
+        };
+    }
+
+    fn flush(&self) {
+        // we don't need to flush here because we are sending the messages to the host directly.
+    }
+}
