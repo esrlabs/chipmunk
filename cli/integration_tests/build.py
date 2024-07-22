9cdb416d8eb3970adf97f5ff77e4c31b5203b016
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-07-22 15:29:22 +0200

    fBuild CLI: Integration tests for Checksum Checks
    - Tests to insure if files change in a target directory then the target
      and all other targets depending on it will be rebuilt.
    - Tests creates a file in platform and checks that platform and all
      other targets until app(electron) targets are rebuilt

--- cli/integration_tests/build.py
+++ cli/integration_tests/build.py
@@ -2,7 +2,9 @@ import subprocess
 from pathlib import Path
 from utls import get_root
 from datetime import datetime
+from typing import Dict
 
+# Build command to be used in all build tests
 BUILD_COMMAND = [
     "cargo",
     "run",
@@ -15,9 +17,9 @@ BUILD_COMMAND = [
     "app",
 ]
 
-# These paths must exist after build build is done.
+# These paths must exist after build command has been finished.
 # The paths are relative starting from `chipmunk_root/application`
-APP_PATHS_TO_CHECK = [
+APP_PATHS_FOR_BUILD_CHECK = [
     # Core
     "apps/indexer/target",
     # Shared
@@ -25,7 +27,6 @@ APP_PATHS_TO_CHECK = [
     "platform/node_modules",
     # Binding
     "apps/rustcore/rs-bindings/dist",
-    # These sub paths must not exist after clean build is done.
     "apps/rustcore/rs-bindings/target",
     # Wrapper
     "apps/rustcore/ts-bindings/dist",
@@ -51,11 +52,14 @@ CHECKSUM_FILE_NAME = ".build_chksum_dev"
 def get_build_paths(root_dir: Path) -> list[Path]:
     """Provides the paths for the directories that must be created after running the build command"""
     application_dir = root_dir.joinpath("application")
-    build_paths = [application_dir.joinpath(sub_dir) for sub_dir in APP_PATHS_TO_CHECK]
+    build_paths = [
+        application_dir.joinpath(sub_dir) for sub_dir in APP_PATHS_FOR_BUILD_CHECK
+    ]
     return build_paths
 
 
-def run_build_command():
+def _build_general_check():
+    """Runs Build command for app targets and checks if all build directories + checksum file are created"""
     print("Running build command...")
     root_dir = get_root()
     # The path for the file where build checksums are saved.
@@ -70,6 +74,7 @@ def run_build_command():
     )
     subprocess.run(BUILD_COMMAND, check=True)
 
+    # Check if all the targets directory exists after running build command.
     print("Checking created directories...")
     for path in get_build_paths(root_dir):
         if not path.exists():
@@ -79,7 +84,7 @@ def run_build_command():
 
     # Checksum Records file Checks: File must exist after build,
     # and it must have a more resent modification date compared to before build
-    print("Checking Checksum Records file...")
+    print("Checking Checksum Records file changed...")
 
     assert (
         checksum_path.exists()
@@ -91,12 +96,107 @@ def run_build_command():
         checksum_modified_after > checksum_modified_before
     ), f"Checksum file modification date after build isn't greater as before build.\
          Before: {checksum_modified_before} , After: {checksum_modified_after}"
+    print("Checking if Checksum Records file changed Succeeded")
 
-    print("Checking Checksum Records file Succeeded")
 
-    # All Done
-    print("*** Check for Build Command Succeeded ***")
+############   CHECKSUM RECORDS TESTS    ###########
+
+# Name of the temporary file which will be used for testing if file changes will lead to rebuild
+# the direct target and all other targets that depend on here.
+# This file should be created then deleted after the test is done.
+TEMP_FILE_NAME = "tmp_for_cli_test.txt"
+
+# Content of temporary file for testing checksum checks.
+# It should provide a clear message for the users to delete the file if the file somehow didn't
+# get deleted after the test has finished.
+TEMP_FILE_CONTENT = """This file is created to test the build CLI tool only and it should be deleted after each test.
+Please delete this file manually if it still exists after build CLI tests are done, 
+and please consider opening an issue if you can reproduce this behavior"""
+
+# Paths of files and directory in platform target and all other targets depending on it.
+# The modification date for this files will be read before the checksum test starts, then it will be compared after
+# the build command has finished to insure that the those targets have been rebuilt.
+APP_PATHS_FOR_CHECKSUM_CHECK = [
+    # Shared
+    "platform/dist/lib.js",
+    # Binding
+    "apps/rustcore/rs-bindings/dist/index.node",
+    # Wrapper
+    "apps/rustcore/ts-bindings/dist/index.js",
+    # Client
+    "client/dist",
+    # App
+    "holder/dist/app.js",
+]
+
+
+def _build_checksum_check():
+    """!!!This function must run directly after a full build!!!
+    It creates a dummy file in platform directory and checks that all dependencies (Binding, Wrapper, Client, App)
+    has been newly built
+    """
+    root_dir = get_root()
+    application_dir = root_dir.joinpath("application")
+
+    # Get and validate checksum file
+    chksum_file = root_dir.joinpath(CHECKSUM_FILE_NAME)
+    assert (
+        chksum_file.exists()
+    ), f"Checksum File must exist before running checksum tests. File Path: {chksum_file}"
+
+    # Save modification date for build paths before start to compare them later.
+    modifi_before_start: Dict[Path, datetime] = {}
+    for sub_path in APP_PATHS_FOR_CHECKSUM_CHECK:
+        sub_path = application_dir.joinpath(sub_path)
+        assert (
+            sub_path.exists()
+        ), f"Build Path must exist before checksum tests starts. Path {sub_path}"
+        modifi_date = datetime.fromtimestamp(sub_path.stat().st_mtime)
+        modifi_before_start[sub_path] = modifi_date
+
+    # Define temporary file path in platform directory to insure it will be rebuilt
+    # with all other targets depending on it.
+    temp_file_path = application_dir.joinpath(f"platform/{TEMP_FILE_NAME}")
+    assert (
+        not temp_file_path.exists()
+    ), f"Temporary file can't exist before checksum test start. File Path: {temp_file_path}"
+
+    try:
+        # Create temporary file in platform directory
+        with open(temp_file_path, "w") as f:
+            f.write(TEMP_FILE_CONTENT)
+
+        # Run build command
+        subprocess.run(BUILD_COMMAND, check=True)
+
+        # Compare modification date for involved targets
+        for path, modifi_before in modifi_before_start.items():
+            modifi_after = datetime.fromtimestamp(path.stat().st_mtime)
+            assert (
+                modifi_after > modifi_before
+            ), f"Target modified date after must be more recent than before.\n\
+            Target Path: {path}.\n\
+            Before: {modifi_before}, After: {modifi_after}"
+    finally:
+        # Insure temporary file is deleted
+        if temp_file_path.exists():
+            temp_file_path.unlink()
+
+
+################   RUN ALL TESTS   ######################
+
+
+def run_build_tests():
+    print("Running General Checks for Build Command...")
+    _build_general_check()
+    print("*** General Check for Build Command Succeeded ***")
+
+    print("---------------------------------------------------------------")
+
+    print("Running Checksum Checks for Build Command...")
+    _build_checksum_check()
+    print("*** Checksum Check for Build Command Succeeded ***")
 
 
 if __name__ == "__main__":
-    run_build_command()
+    run_build_tests()
