cbd2977fbdfbf540061f7ac37919f872b12ff080
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-08-23 11:13:06 +0200

    Build CLI Copy: Add missing job & Move job to correct order
    * Move Copying Client files to App to before start building App.
    * Add missing copying for the file 'package.json' to dest directory
      after App build is done.
    * Fix typo

--- cli/src/target/app.rs
+++ cli/src/target/app.rs
@@ -1,56 +1,42 @@
 use std::fs;
 
-use anyhow::{bail, Context};
+use anyhow::{ensure, Context};
 
 use crate::{fstools, jobs_runner::JobDefinition, spawner::SpawnResult, tracker::get_tracker};
 
-use super::{client::get_dist_path, Target};
+use super::Target;
 
-pub async fn copy_client_to_app(job_def: JobDefinition) -> Result<SpawnResult, anyhow::Error> {
-    let prod = job_def.job_type.is_production().unwrap_or(false);
-    let src = get_dist_path(prod);
-    let dest = Target::App.cwd().join("dist");
-    if !src.exists() {
-        bail!("Not found: {}", src.display());
-    }
+pub async fn copy_package_file_to_dist(
+    job_def: JobDefinition,
+) -> Result<SpawnResult, anyhow::Error> {
+    const PACKAGE_FILE_NAME: &str = "package.json";
+
+    let package_file_path = Target::App.cwd().join(PACKAGE_FILE_NAME);
+
+    ensure!(
+        package_file_path.exists(),
+        "Package.json File doesn't exist in App. Path: {}",
+        package_file_path.display()
+    );
     let tracker = get_tracker();
-    if !dest.exists() {
-        let msg = format!("creating directory: {}", dest.display());
+
+    let dist = Target::App.cwd().join("dist");
+    if !dist.exists() {
+        let msg = format!("Creating directory: {}", dist.display());
         tracker.msg(job_def, msg);
 
-        fs::create_dir(&dest)
-            .with_context(|| format!("Error while creating directory: {}", dest.display()))?;
-    }
-    let prev = dest.join("client");
-    if prev.exists() {
-        let msg = format!("removing directory: {}", prev.display());
-        tracker.msg(job_def, msg);
-
-        fstools::rm_folder(job_def, &prev)?;
+        fs::create_dir(&dist)
+            .with_context(|| format!("Error while creating directory: {}", dist.display()))?;
     }
 
-    fstools::cp_folder(job_def, src.clone(), dest.clone()).await?;
-
-    let rename_from = dest.join(
-        src.file_name()
-            .context("Fail to parse client artifacts path")?,
-    );
-    let rename_to = dest.join("client");
-
-    let msg = format!(
-        "renaming '{}' to '{}'",
-        rename_from.display(),
-        rename_to.display()
-    );
-    tracker.msg(job_def, msg);
-
-    std::fs::rename(&rename_from, &rename_to)
-        .with_context(|| format!("Error while renaming {}", rename_from.display()))?;
+    fstools::cp_file(job_def, package_file_path, dist.join(PACKAGE_FILE_NAME))?;
 
     let report_logs = tracker.get_logs(job_def).await?.unwrap_or_default();
 
-    Ok(SpawnResult::create_for_fs(
-        "Copy App Build Artifacts".into(),
+    let spawn_res = SpawnResult::create_for_fs(
+        "Copy `package.json` file to holder/dest directory".into(),
         report_logs,
-    ))
+    );
+
+    Ok(spawn_res)
 }
--- cli/src/target/client.rs
+++ cli/src/target/client.rs
@@ -1,4 +1,8 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
+
+use anyhow::{bail, Context};
+
+use crate::{fstools, jobs_runner::JobDefinition, spawner::SpawnResult, tracker::get_tracker};
 
 use super::Target;
 
@@ -7,4 +11,53 @@ pub fn get_dist_path(prod: bool) -> PathBuf {
         .cwd()
         .join("dist")
         .join(if prod { "release" } else { "debug" })
+}
+
+pub async fn copy_client_to_app(job_def: JobDefinition) -> Result<SpawnResult, anyhow::Error> {
+    let prod = job_def.job_type.is_production().unwrap_or(false);
+    let src = get_dist_path(prod);
+    let dest = Target::App.cwd().join("dist");
+    if !src.exists() {
+        bail!("Not found: {}", src.display());
+    }
+    let tracker = get_tracker();
+    if !dest.exists() {
+        let msg = format!("creating directory: {}", dest.display());
+        tracker.msg(job_def, msg);
+
+        fs::create_dir(&dest)
+            .with_context(|| format!("Error while creating directory: {}", dest.display()))?;
+    }
+    let prev = dest.join("client");
+    if prev.exists() {
+        let msg = format!("removing directory: {}", prev.display());
+        tracker.msg(job_def, msg);
+
+        fstools::rm_folder(job_def, &prev)?;
+    }
+
+    fstools::cp_folder(job_def, src.clone(), dest.clone()).await?;
+
+    let rename_from = dest.join(
+        src.file_name()
+            .context("Fail to parse client artifacts path")?,
+    );
+    let rename_to = dest.join("client");
+
+    let msg = format!(
+        "renaming '{}' to '{}'",
+        rename_from.display(),
+        rename_to.display()
+    );
+    tracker.msg(job_def, msg);
+
+    std::fs::rename(&rename_from, &rename_to)
+        .with_context(|| format!("Error while renaming {}", rename_from.display()))?;
+
+    let report_logs = tracker.get_logs(job_def).await?.unwrap_or_default();
+
+    Ok(SpawnResult::create_for_fs(
+        "Copy App Build Artifacts".into(),
+        report_logs,
+    ))
 }
