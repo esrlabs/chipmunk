4ba9fbe2453b7a5e2b04e4fbbbc12d3dc9a934a7
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-08-29 09:59:20 +0200

    Bench: Starting with benchmarks for producer...
    * Define bench for producer with async support
    * Comment out `deny(unused_crate_dependencies)` because this lint can't
      differentiate between dev and none dev dependencies.

---
+++ application/apps/indexer/sources/benches/producer_benchmarks.rs
@@ -0,0 +1,19 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+async fn produce(val: usize, val_2: usize) {
+    //TODO AAZ:
+    assert_eq!(val, val_2);
+}
+
+fn producer_benchmark(c: &mut Criterion) {
+    let val = 1024;
+
+    c.bench_with_input(BenchmarkId::new("producer", val), &val, |bencher, &v| {
+        bencher
+            .to_async(tokio::runtime::Runtime::new().unwrap())
+            .iter(|| produce(v, v));
+    });
+}
+
+criterion_group!(benches, producer_benchmark);
+criterion_main!(benches);
