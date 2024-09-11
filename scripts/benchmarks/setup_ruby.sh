f8d2e8239b9f8b466e74b34663e0e2d1e53d43b4
Sameer Srivastava <itsmesamster@gmail.com>
2024-09-11 16:01:31 +0200

    Use cli tool to run benchmarks instead of rake

---
+++ scripts/benchmarks/setup_ruby.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Need to install ruby separately because github action for installing ruby does not work on self hosted runner.
+whoami
+npm install -g corepack
+corepack enable
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install -y yarn software-properties-common
+apt-add-repository -y ppa:rael-gc/rvm
+sudo apt-get update && sudo apt-get install -y rvm
+echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
+source /etc/profile.d/rvm.sh
+rvm install ruby-3.1.2
+ruby --version
+sudo chown -R $(whoami) /usr/local
+rvm use 3.1.2 --default
+export PATH="/usr/share/rvm:$PATH"
+gem install dotenv json octokit tmpdir fileutils
