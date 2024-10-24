82dbcc29aab4ef624c7a39e81c41bcea1eb96b8b
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-10-24 09:36:38 +0200

    Remove `jemalloc` allocator benchmarks
    `jemalloc` support on windows is not stable yet.

--- application/apps/indexer/sources/benches/mocks_multi_producer_jemalloc.rs
+++
@@ -1,9 +0,0 @@
-//! Benchmarks for producer loop with mock parser and byte source using `jemalloc` memory allocator,
-//! which provides the best performance and is used in Chipmunk app currently.
-//!
-//! The mock of [`parsers::Parser`] will return iterator with multiple value replicating the
-//! behavior of the potential plugins in Chipmunk.
-
-mod macros;
-
-mocks_producer_multi!(mocks_multi_producer_jemalloc, tikv_jemallocator::Jemalloc);
