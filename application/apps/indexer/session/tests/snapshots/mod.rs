4cc9fc5a9864d72296267ef9656b4eba1c81cb86
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-10-25 15:07:42 +0200

    Snapshots Tests (In Progress): Sessions output
    * Create a struct to represent and load the temp output files generated
      by an observe session, with serialization functions to be used in
      snapshot testing.
    * Ensure the generated session files are removed after running the test
    * Renaming & Refactoring

--- application/apps/indexer/session/tests/snapshots/mod.rs
+++ application/apps/indexer/session/tests/snapshots/mod.rs
@@ -4,18 +4,23 @@
 mod utls;
 
 use pretty_assertions::{assert_eq, assert_ne};
+use scopeguard::defer;
 use sources::factory::{DltParserSettings, FileFormat, ParserType};
 use utls::*;
 
 #[tokio::test]
 async fn export_dlt() {
     let input = "../../../developing/resources/attachments.dlt";
-    let session_file = run_export(
+    let session_main_file = run_observe_session(
         input,
         FileFormat::Binary,
         ParserType::Dlt(DltParserSettings::default()),
     )
     .await;
 
-    panic!("Session file: {}", session_file.display());
+    defer! { cleanup_session_file(&session_main_file)};
+
+    let session_files = SessionFiles::from_session_file(&session_main_file);
+
+    panic!("{:#?}", session_files);
 }
--- application/apps/indexer/session/tests/snapshots/utls.rs
+++ application/apps/indexer/session/tests/snapshots/utls.rs
@@ -1,11 +1,103 @@
+use serde::{Deserialize, Serialize};
 use session::{events::CallbackEvent, session::Session};
 use sources::factory::{FileFormat, ObserveOptions, ParserType};
 use std::path::PathBuf;
 use uuid::Uuid;
 
-pub struct MyStruct;
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+/// Represents the files generated while parsing a file in an observe session.
+/// # Note:
+/// This struct it made for testing purposes with snapshots.
+pub struct SessionFiles {
+    /// The content of the main session file, normally with name `{uuid}.session`
+    /// Content saved as line for better comparison in testing.
+    pub session_file_lines: Vec<String>,
+    /// The generated files for the attachments in the attachments directory with the name `{uuid}`
+    pub attachments: Vec<AttachmentInfo>,
+}
 
-pub async fn run_export<P: Into<PathBuf>>(
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+/// Represents an attachment file in process observe session.
+pub struct AttachmentInfo {
+    pub filename: String,
+    /// Content of the file when it has textual content. For binary format it will stay empty.
+    /// Content saved as line for better comparison in testing.
+    pub content_lines: Vec<String>,
+}
+
+impl SessionFiles {
+    /// Creates a session files infos reading the main session file and the attachments if exists.
+    pub fn from_session_file(session_file: &PathBuf) -> Self {
+        assert!(
+            session_file.exists(),
+            "Session file doesn't exist. Path: {}",
+            session_file.display()
+        );
+
+        let session_file_lines = std::fs::read_to_string(&session_file)
+            .expect("Session file can be read to string without problems")
+            .lines()
+            .map(ToString::to_string)
+            .collect();
+
+        let session_dir = session_dir_form_file(&session_file);
+
+        let mut attachments = Vec::new();
+
+        if session_dir.is_dir() {
+            let atts_iter = session_dir
+                .read_dir()
+                .unwrap()
+                .flat_map(Result::ok)
+                .map(|entry| entry.path())
+                .map(|entry_path| AttachmentInfo {
+                    filename: entry_path
+                        .file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string(),
+                    content_lines: std::fs::read_to_string(entry_path)
+                        .map(|content| content.lines().map(ToString::to_string).collect())
+                        .unwrap_or_default(),
+                });
+
+            attachments.extend(atts_iter);
+        }
+
+        Self {
+            session_file_lines,
+            attachments,
+        }
+    }
+}
+
+/// Remove session main file and attachments for the given session file.
+pub fn cleanup_session_file(session_file: &PathBuf) {
+    std::fs::remove_file(session_file)
+        .expect("Removing generated temporary session file shouldn't fail");
+    let session_dir = session_dir_form_file(session_file);
+    if session_dir.exists() {
+        std::fs::remove_dir_all(session_dir)
+            .expect("Removing generated temporary session attachments directory shouldn't fial");
+    }
+}
+
+/// Provide the name of the attachments directory for the given session main file.
+fn session_dir_form_file(session_file: &PathBuf) -> PathBuf {
+    const SESSION_FILE_SUFFIX: &str = ".session";
+
+    session_file
+        .to_str()
+        .and_then(|file| file.strip_suffix(SESSION_FILE_SUFFIX))
+        .map(PathBuf::from)
+        .unwrap()
+}
+
+/// Runs a processor observe session generating the session files in Chipmunk temporary directory
+/// and returning the path of the main session file.
+/// # Note:
+/// This function it made for test purposes with snapshots.
+pub async fn run_observe_session<P: Into<PathBuf>>(
     input: P,
     file_format: FileFormat,
     parser_type: ParserType,
@@ -40,8 +132,6 @@ pub async fn run_export<P: Into<PathBuf>>(
     }
 
     let file = session.get_state().get_session_file().await.unwrap();
-    //TODO AAZ: Remove after Debug
-    println!("SESSOIN FILE: {}", file.display());
 
     file
 }
