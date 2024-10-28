687f086768bda3353112197ddb843b7e690206f7
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-10-28 13:11:11 +0100

    Snapshot test for SomeIP with Pcapng source

--- application/apps/indexer/session/tests/snapshots/mod.rs
+++ application/apps/indexer/session/tests/snapshots/mod.rs
@@ -3,7 +3,7 @@ mod utls;
 use std::path::PathBuf;
 
 use scopeguard::defer;
-use sources::factory::{DltParserSettings, FileFormat, ParserType};
+use sources::factory::{DltParserSettings, FileFormat, ParserType, SomeIpParserSettings};
 use utls::*;
 
 #[tokio::test]
@@ -57,6 +57,41 @@ async fn observe_dlt_with_someip_session() {
 
     insta::with_settings!({
         description => "Snapshot for DLT file with SomeIP network trace.",
+        info => &parser_settings,
+        omit_expression => true,
+        prepend_module_to_snapshot => false,
+    }, {
+        insta::assert_yaml_snapshot!(session_files);
+    });
+}
+
+#[tokio::test]
+async fn observe_someip_bcapng_session() {
+    let input = "../../../developing/resources/someip.pcapng";
+    let fibex_file = "../../../developing/resources/someip.xml";
+
+    assert!(
+        PathBuf::from(fibex_file).exists(),
+        "Fibex file path doesn't exist. Path: {fibex_file}"
+    );
+
+    let parser_settings = SomeIpParserSettings {
+        fibex_file_paths: Some(vec![String::from(fibex_file)]),
+    };
+
+    let session_main_file = run_observe_session(
+        input,
+        FileFormat::PcapNG,
+        ParserType::SomeIp(parser_settings.clone()),
+    )
+    .await;
+
+    defer! { cleanup_session_files(&session_main_file)};
+
+    let session_files = SessionFiles::from_session_file(&session_main_file);
+
+    insta::with_settings!({
+        description => "Snapshot for SomeIP file with Pcapng byte source.",
         info => &parser_settings,
         omit_expression => true,
         prepend_module_to_snapshot => false,
