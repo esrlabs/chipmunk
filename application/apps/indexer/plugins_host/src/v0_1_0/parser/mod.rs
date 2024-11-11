e511858ef173ff4ea37b22abb268430330868911
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-11-11 16:01:55 +0100

    Plugins Parser: Remove method `parsing_with_add`
    * The approach with `parsing_with_add` method is confusing because:
      * parse call won't return the results and it depends on an unwritten
        contract to use the provided add() method on the host.
      * It also provide a security risk since bad plugins can call the
        function unlimited times without any specified context.
      * There is no clean way for plugins to decide if they want to break
        the parsing and return error even if they have already parsed some
        items. (Like unrecoverable errors case)
      * The performance gain isn't considerable in the user experience about
        0.5% percent in the parsing time, so it's not worth to worsen the
        public API and have security risks because of it.

--- application/apps/indexer/plugins_host/src/v0_1_0/parser/mod.rs
+++ application/apps/indexer/plugins_host/src/v0_1_0/parser/mod.rs
@@ -15,10 +15,7 @@ use crate::{
     PluginHostInitError, PluginParseMessage, PluginType, WasmPlugin,
 };
 
-use self::{
-    bindings::{ParsePlugin, ParseReturn},
-    parser_plugin_state::ParserPluginState,
-};
+use self::{bindings::ParsePlugin, parser_plugin_state::ParserPluginState};
 
 pub struct PluginParser {
     store: Store<ParserPluginState>,
@@ -73,11 +70,11 @@ impl PluginParser {
             plugin_bindings,
         })
     }
+}
 
-    #[allow(unused)]
-    #[inline]
-    /// Call parse function that returns the result as a collection.
-    fn parse_with_list(
+use parsers as p;
+impl p::Parser<PluginParseMessage> for PluginParser {
+    fn parse(
         &mut self,
         input: &[u8],
         timestamp: Option<u64>,
@@ -99,103 +96,9 @@ impl PluginParser {
             }
         };
 
-        let res = parse_results.into_iter().map(guest_to_host_parse_results);
+        let res = parse_results
+            .into_iter()
+            .map(|item| (item.consumed as usize, item.value.map(|v| v.into())));
         Ok(res)
-    }
-
-    #[allow(unused)]
-    #[inline]
-    /// Call parse function that adds the results directly at the host using the add method.
-    fn parse_with_add(
-        &mut self,
-        input: &[u8],
-        timestamp: Option<u64>,
-    ) -> Result<impl Iterator<Item = (usize, Option<p::ParseYield<PluginParseMessage>>)>, p::Error>
-    {
-        debug_assert!(
-            self.store.data_mut().results_queue.is_empty(),
-            "Host results most be empty at the start of parse call"
-        );
-
-        let parse_res = futures::executor::block_on(
-            self.plugin_bindings
-                .chipmunk_plugin_parser()
-                .call_parse_with_add(&mut self.store, input, timestamp),
-        )
-        .map_err(|call_err| {
-            p::Error::Unrecoverable(format!(
-                "Call parse on the plugin failed. Error: {call_err}"
-            ))
-        })?;
-
-        if let Err(parse_err) = parse_res {
-            // TODO AAZ: Verify the logic here.
-            // Check if plugins has returned an error and submitted item with `add()` function in
-            // the same call.
-            // In case the error is unrecoverable then ignore the item and return the error.
-            // Otherwise return the items on this call since the recoverable errors will be
-            // reproduced on the next parse call.
-            if !self.store.data().results_queue.is_empty() {
-                match parse_err {
-                    bindings::ParseError::Unrecoverable(err) => {
-                        log::error!("Unrecoverable message sent from plugin with newly submitted parsed items in the same call. \
-                           Items will be ignored and error will be returned.");
-
-                        self.store.data_mut().results_queue.clear();
-
-                        return Err(p::Error::Unrecoverable(err));
-                    }
-                    bindings::ParseError::Parse(_)
-                    | bindings::ParseError::Incomplete
-                    | bindings::ParseError::Eof => {
-                        log::warn!("Plugin returned a recoverable error and submitted items in that same call. \
-                            Error will be ignored and items will be returned");
-                    }
-                };
-            }
-        }
-
-        let parsed_items = std::mem::take(&mut self.store.data_mut().results_queue);
-
-        let res = parsed_items.into_iter().map(guest_to_host_parse_results);
-
-        Ok(res)
-    }
-}
-
-fn guest_to_host_parse_results(
-    parse_res: ParseReturn,
-) -> (usize, Option<p::ParseYield<PluginParseMessage>>) {
-    (
-        parse_res.consumed as usize,
-        parse_res.value.map(|v| v.into()),
-    )
-}
-
-use parsers as p;
-impl p::Parser<PluginParseMessage> for PluginParser {
-    fn parse(
-        &mut self,
-        input: &[u8],
-        timestamp: Option<u64>,
-    ) -> Result<impl Iterator<Item = (usize, Option<p::ParseYield<PluginParseMessage>>)>, p::Error>
-    {
-        // TODO AAZ: We keep both functions for now until we can benchmark them properly.
-        // Update: Parsing with add provide slightly better performance currently. But there is no
-        // clear winner here because parse with list provides better API but has slightly worse
-        // performance.
-        //
-        // ### Benchmarks results for 500 MB DLT file with default allocator ###
-        //
-        // # Performance with add method:
-        // plugin_parser_producer  time:   [8.7054 s 8.7257 s 8.7566 s]
-        // plugin_parser_producer  time:   [8.7059 s 8.7216 s 8.7412 s]
-        //
-        // # Performance with list:
-        // plugin_parser_producer  time:   [9.0579 s 9.0758 s 9.0898 s]
-        // plugin_parser_producer  time:   [9.0844 s 9.0964 s 9.1101 s]
-        //
-        self.parse_with_add(input, timestamp)
-        // self.parse_with_list(input, timestamp)
     }
 }
