de90eb883466340758478a52c3497a64e75014ef
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-11-13 17:21:59 +0100

    Enable custom memory allocators by feature flag
    * Add feature flag to enable using custom memory allocators
      (`jemalloc` on unix and `mimalloc` on Windows)
    * This feature won't be enabled by default since those allocators aren't
      as stable as the default ones.

--- application/apps/rustcore/rs-bindings/src/lib.rs
+++ application/apps/rustcore/rs-bindings/src/lib.rs
@@ -1,10 +1,21 @@
 mod js;
 mod logging;
 
-// Using the mimalloc allocator resulted in an 20% performance improvement on windows.
+// ****************************************
+// *** Memory Allocators Configurations ***
+// ****************************************
+
 // NOTE: In a Rust project, the memory allocator can only be set once,
 // and it applies to the entire project, including all dependencies.
 // We chose to configure the allocator in the highest-level library where the runtime is also set.
-#[cfg(windows)]
+
+// Using the mimalloc allocator resulted in an 20% performance improvement on windows
+// But there are report of memory leaks and big difference in memory consumption using it.
+#[cfg(all(feature = "custom-alloc", windows))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+// Jemalloc currently supports UNIX based operating systems only and provides better performance.
+#[cfg(all(feature = "custom-alloc", unix))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
