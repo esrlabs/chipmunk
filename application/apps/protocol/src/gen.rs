37259d06829b646162df91f040441b20908c41da
DmitryAstafyev <dvastafyev@gmail.com>
2024-11-29 15:17:54 +0100

    Update protocol wasm module

--- application/apps/protocol/src/gen.rs
+++ application/apps/protocol/src/gen.rs
@@ -1,15 +1,16 @@
 #[macro_export]
 macro_rules! gen_encode_decode_fns {
-    ($element_ref:expr) => {
+    // All regular use cases: gen_encode_decode_fns!(ObserveOptions);
+    ($type:ident) => {
         paste::item! {
             #[wasm_bindgen]
             #[allow(non_snake_case)]
-            pub fn [< decode $element_ref >](buf: &[u8]) -> Result<JsValue, E> {
+            pub fn [<decode $type>](buf: &[u8]) -> Result<JsValue, E> {
                 let serializer = Serializer::new()
                     .serialize_missing_as_null(true)
                     .serialize_maps_as_objects(true)
                     .serialize_large_number_types_as_bigints(false);
-                $element_ref::decode(buf)
+                $type::decode(buf)
                     .map_err(E::DecodeError)?
                     .serialize(&serializer)
                     .map_err(|e| E::DecodeError(e.to_string()))
@@ -17,8 +18,86 @@ macro_rules! gen_encode_decode_fns {
 
             #[wasm_bindgen]
             #[allow(non_snake_case)]
-            pub fn [< encode $element_ref >](val: JsValue) -> Result<Vec<u8>, E> {
-                from_value::<$element_ref>(val)?
+            pub fn [<encode $type>](val: JsValue) -> Result<Vec<u8>, E> {
+                from_value::<$type>(val)?
+                    .encode()
+                    .map_err(E::DecodeError)
+            }
+        }
+    };
+
+    // Subtype returns void: gen_encode_decode_fns!(CommandOutcome<()>);
+    ($type:ident<()>) => {
+        paste::item! {
+            #[wasm_bindgen]
+            #[allow(non_snake_case)]
+            pub fn [<decode $type WithVoid>](buf: &[u8]) -> Result<JsValue, E> {
+                let serializer = Serializer::new()
+                    .serialize_missing_as_null(true)
+                    .serialize_maps_as_objects(true)
+                    .serialize_large_number_types_as_bigints(false);
+                $type::<()>::decode(buf)
+                    .map_err(E::DecodeError)?
+                    .serialize(&serializer)
+                    .map_err(|e| E::DecodeError(e.to_string()))
+            }
+
+            #[wasm_bindgen]
+            #[allow(non_snake_case)]
+            pub fn [<encode $type WithVoid>](val: JsValue) -> Result<Vec<u8>, E> {
+                from_value::<$type::<()>>(val)?
+                    .encode()
+                    .map_err(E::DecodeError)
+            }
+        }
+    };
+
+    // With subtypes: gen_encode_decode_fns!(CommandOutcome<String>);
+    ($type:ident<$generic:ident>) => {
+        paste::item! {
+            #[wasm_bindgen]
+            #[allow(non_snake_case)]
+            pub fn [<decode $type With $generic>](buf: &[u8]) -> Result<JsValue, E> {
+                let serializer = Serializer::new()
+                    .serialize_missing_as_null(true)
+                    .serialize_maps_as_objects(true)
+                    .serialize_large_number_types_as_bigints(false);
+                $type::<$generic>::decode(buf)
+                    .map_err(E::DecodeError)?
+                    .serialize(&serializer)
+                    .map_err(|e| E::DecodeError(e.to_string()))
+            }
+
+            #[wasm_bindgen]
+            #[allow(non_snake_case)]
+            pub fn [<encode $type With $generic>](val: JsValue) -> Result<Vec<u8>, E> {
+                from_value::<$type::<$generic>>(val)?
+                    .encode()
+                    .map_err(E::DecodeError)
+            }
+        }
+    };
+
+    // With nested subtypes: gen_encode_decode_fns!(CommandOutcome<Option<String>>);
+    ($type:ident<$generic:ident<$nested:ident>>) => {
+        paste::item! {
+            #[wasm_bindgen]
+            #[allow(non_snake_case)]
+            pub fn [<decode $type With $generic $nested>](buf: &[u8]) -> Result<JsValue, E> {
+                let serializer = Serializer::new()
+                    .serialize_missing_as_null(true)
+                    .serialize_maps_as_objects(true)
+                    .serialize_large_number_types_as_bigints(false);
+                $type::<$generic<$nested>>::decode(buf)
+                    .map_err(E::DecodeError)?
+                    .serialize(&serializer)
+                    .map_err(|e| E::DecodeError(e.to_string()))
+            }
+
+            #[wasm_bindgen]
+            #[allow(non_snake_case)]
+            pub fn [<encode $type With $generic $nested>](val: JsValue) -> Result<Vec<u8>, E> {
+                from_value::<$type::<$generic<$nested>>>(val)?
                     .encode()
                     .map_err(E::DecodeError)
             }
