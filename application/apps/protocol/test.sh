ba11fb87331394f2ed7605b2a8f3dd7bc4a64d4f
DmitryAstafyev <dvastafyev@gmail.com>
2024-12-05 17:06:52 +0100

    Add protocol test runner

---
+++ application/apps/protocol/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+reset
+echo "
+====== WARNING ===============================================================
+This script performs an operation to DELETE ALL the contents of 
+the folder specified in the environment variable CHIPMUNK_PROTOCOL_TEST_OUTPUT.
+Before proceeding, make sure that the CHIPMUNK_PROTOCOL_TEST_OUTPUT variable
+contains the correct path. If the CHIPMUNK_PROTOCOL_TEST_OUTPUT variable is
+not defined, test data will be written to /\$TMP/stypes_tests.
+====== WARNING ===============================================================
+"
+read -p "Do you want to continue? (y/N): " response
+
+response=${response,,} 
+if [[ "$response" != "y" ]]; then
+    echo "Operation aborted."
+    exit 1
+fi
+
+echo "Build wasm module"
+cargo clean
+wasm-pack build --target nodejs
+
+echo "Create test use-cases"
+cd ../indexer/stypes
+export CHIPMUNK_PROTOCOL_TEST_OUTPUT="/tmp/stypes_test/"
+cargo test --release -- --nocapture
+
+echo "Run tests"
+export JASMIN_TEST_BLOCKS_LOGS=on
+cd ../../rustcore/ts-bindings
+rm -rf ./node_modules
+rm -rf ./spec/build
+rake bindings:test:protocol
+ 
