b6af726d93b35615aebe81c7b746fbad3ce40c6c
DmitryAstafyev <dvastafyev@gmail.com>
2024-12-15 18:13:34 +0100

    Extract operations related types into mod

--- application/apps/indexer/stypes/src/miscellaneous/extending.rs
+++ application/apps/indexer/stypes/src/miscellaneous/extending.rs
@@ -1,5 +1,4 @@
 use crate::*;
-use regex::Regex;
 
 impl GrabbedElement {
     /// Sets the `nature` field of the `GrabbedElement`.
@@ -22,35 +21,5 @@ impl FilterMatch {
     /// - A new `FilterMatch` instance with the specified index and filters.
     pub fn new(index: u64, filters: Vec<u8>) -> Self {
         Self { index, filters }
-    }
-}
-
-impl ExtractedMatchValue {
-    pub fn new(index: u64, input: &str, filters: &[Regex]) -> Self {
-        Self {
-            index,
-            values: ExtractedMatchValue::extract(input, filters),
-        }
-    }
-
-    pub fn extract(input: &str, filters: &[Regex]) -> Vec<(usize, Vec<String>)> {
-        let mut values: Vec<(usize, Vec<String>)> = vec![];
-        for (filter_index, filter) in filters.iter().enumerate() {
-            for caps in filter.captures_iter(input) {
-                let mut matches: Vec<String> = caps
-                    .iter()
-                    .flatten()
-                    .map(|m| m.as_str().to_owned())
-                    .collect();
-                if matches.len() <= 1 {
-                    // warn here
-                } else {
-                    // 0 always - whole match
-                    matches.remove(0);
-                    values.push((filter_index, matches));
-                }
-            }
-        }
-        values
     }
 }
--- application/apps/indexer/stypes/src/miscellaneous/mod.rs
+++ application/apps/indexer/stypes/src/miscellaneous/mod.rs
@@ -9,88 +9,14 @@ mod proptest;
 
 use crate::*;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct NearestPosition {
-    pub index: u64,    // Position in search results
-    pub position: u64, // Position in original stream/file
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ResultNearestPosition(pub Option<NearestPosition>);
-
-///(row_number, min_value_in_range, max_value_in_range, value)
-/// value - can be last value in range or some kind of average
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct Point {
-    pub row: u64,
-    pub min: f64,
-    pub max: f64,
-    pub y_value: f64,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ResultSearchValues(pub HashMap<u8, Vec<Point>>);
-
-/// Scaled chart data
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ResultScaledDistribution(pub Vec<Vec<(u8, u16)>>);
-
-/// Used to delivery results of extracting values. That's used in the scope
-/// of chart feature
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ExtractedMatchValue {
-    /// The index of log entry (row number)
-    pub index: u64,
-    /// List of matches:
-    /// `usize` - index of filter
-    /// `Vec<String>` - list of extracted values
-    pub values: Vec<(usize, Vec<String>)>,
-}
-
-/// The list of `ExtractedMatchValue`
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ResultExtractedMatchValues(pub Vec<ExtractedMatchValue>);
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ResultU64(pub u64);
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct ResultBool(pub bool);
-
-/// Used only for debug session lifecycle
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[extend::encode_decode]
-#[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
-pub struct SleepResult {
-    pub sleep_well: bool,
-}
-
 /// Representation of ranges. We cannot use std ranges as soon as no way
 /// to derive Serialize, Deserialize
 #[derive(Clone, Serialize, Deserialize, Debug)]
 #[extend::encode_decode]
 #[cfg_attr(test, derive(TS), ts(export, export_to = "miscellaneous.ts"))]
 pub struct Range {
-    start: u64,
-    end: u64,
+    pub start: u64,
+    pub end: u64,
 }
 
 /// A list of ranges to read.
