0abbab383fe88efc9e40c37273a9a5b5215fb0ed
DmitryAstafyev <dvastafyev@gmail.com>
2025-01-16 15:11:50 +0100

    Resolve lint issue

--- application/client/src/app/ui/tabs/module.ts
+++ application/client/src/app/ui/tabs/module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObserveModule } from '@ui/tabs/observe/module';
 import { MultipleFilesModule } from '@ui/tabs/multiplefiles/module';
-import { DialogsModule } from '@ui/views/dialogs/module';
 import { SettingsModule } from '@ui/tabs/settings/module';
 import { ChangelogModule } from '@ui/tabs/changelogs/module';
 import { HelpModule } from '@ui/tabs/help/module';
