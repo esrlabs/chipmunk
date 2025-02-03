f46b264fad00870cee45a3817502ba129e06d7fb
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-02-03 12:00:16 +0100

    CLI: Use nested command for more generic approach.
    * Use multiple levels of nested commands providing a generic way to
      configure each variant in a generic way.
    * First nested command is the parser command which have another nested
      command to specify the input source.
    * Changes in the code accordingly removing code duplication and
      improving code structure.

--- chipmunk-cli/src/lib.rs
+++ chipmunk-cli/src/lib.rs
@@ -1,14 +1,10 @@
-use anyhow::Context;
 use clap::Parser as _;
-use std::{fs::File, io::BufReader, time::Duration};
+use cli_args::OutputFormat;
 use tokio_util::sync::CancellationToken;
 
-use cli_args::InputSource;
-use parsers::dlt::DltParser;
-use session::format::{binary::BinaryMessageWriter, text::MessageTextWriter};
-use sources::{
-    binary::raw::BinaryByteSource,
-    socket::{tcp::TcpSource, udp::UdpSource, ReconnectInfo},
+use session::{
+    format::{binary::BinaryMessageWriter, text::MessageTextWriter},
+    start_session,
 };
 
 mod cli_args;
@@ -18,120 +14,28 @@ pub async fn run_app(cancel_token: CancellationToken) -> anyhow::Result<()> {
     let cli = cli_args::Cli::parse();
     cli.validate()?;
 
-    let (state_tx, state_rx) = tokio::sync::mpsc::unbounded_channel();
+    match cli.parser {
+        cli_args::Parser::Dlt { input } => {
+            let with_headers = match &input {
+                cli_args::InputSource::Tcp { .. } | cli_args::InputSource::Udp { .. } => false,
+                cli_args::InputSource::File { .. } => true,
+            };
 
-    match cli.input {
-        InputSource::Tcp {
-            address,
-            update_interval,
-            max_reconnect_count,
-            reconnect_interval,
-        } => {
-            let reconnect = max_reconnect_count.and_then(|max| {
-                // provide reconnect infos when max count exists and bigger than zero.
-                (max > 0).then(|| {
-                    ReconnectInfo::new(
-                        max,
-                        Duration::from_millis(reconnect_interval),
-                        Some(state_tx),
-                    )
-                })
-            });
-
-            let update_interval = Duration::from_millis(update_interval);
-            let source = TcpSource::new(address, reconnect)
-                .await
-                .context("Initializing TCP connection failed")?;
-
-            let parser = DltParser::new(None, None, None, None, false);
+            let parser = session::parser::dlt::create_parser(with_headers);
 
             match cli.output_format {
-                cli_args::OutputFormat::Binary => {
-                    session::socket::run_session(
-                        parser,
-                        source,
-                        cli.output_path,
-                        BinaryMessageWriter::default(),
-                        state_rx,
-                        update_interval,
-                        cancel_token,
-                    )
-                    .await?
+                OutputFormat::Binary => {
+                    let msg_writer = BinaryMessageWriter::default();
+
+                    start_session(parser, input, msg_writer, cli.output_path, cancel_token).await?;
                 }
-                cli_args::OutputFormat::Text => {
-                    session::socket::run_session(
-                        parser,
-                        source,
-                        cli.output_path,
-                        MessageTextWriter::new(cli.text_columns_separator, cli.text_args_separator),
-                        state_rx,
-                        update_interval,
-                        cancel_token,
-                    )
-                    .await?
+                OutputFormat::Text => {
+                    let msg_writer =
+                        MessageTextWriter::new(cli.text_columns_separator, cli.text_args_separator);
+
+                    start_session(parser, input, msg_writer, cli.output_path, cancel_token).await?;
                 }
-            }
-        }
-        InputSource::Udp { address } => {
-            let source = UdpSource::new(address, Vec::new())
-                .await
-                .context("Initializing UDP connection failed")?;
-            let parser = DltParser::new(None, None, None, None, false);
-            let temp_interval = Duration::from_millis(1000);
-            match cli.output_format {
-                cli_args::OutputFormat::Binary => {
-                    session::socket::run_session(
-                        parser,
-                        source,
-                        cli.output_path,
-                        BinaryMessageWriter::default(),
-                        state_rx,
-                        temp_interval,
-                        cancel_token,
-                    )
-                    .await?
-                }
-                cli_args::OutputFormat::Text => {
-                    session::socket::run_session(
-                        parser,
-                        source,
-                        cli.output_path,
-                        MessageTextWriter::new(cli.text_columns_separator, cli.text_args_separator),
-                        state_rx,
-                        temp_interval,
-                        cancel_token,
-                    )
-                    .await?
-                }
-            }
-        }
-        InputSource::File { path } => {
-            let file = File::open(&path).context("Opening input file failed")?;
-            let reader = BufReader::new(&file);
-            let source = BinaryByteSource::new(reader);
-            let parser = DltParser::new(None, None, None, None, true);
-            match cli.output_format {
-                cli_args::OutputFormat::Binary => {
-                    session::file::run_session(
-                        parser,
-                        source,
-                        cli.output_path,
-                        BinaryMessageWriter::default(),
-                        cancel_token,
-                    )
-                    .await?;
-                }
-                cli_args::OutputFormat::Text => {
-                    session::file::run_session(
-                        parser,
-                        source,
-                        cli.output_path,
-                        MessageTextWriter::new(cli.text_columns_separator, cli.text_args_separator),
-                        cancel_token,
-                    )
-                    .await?;
-                }
-            }
+            };
         }
     }
 
