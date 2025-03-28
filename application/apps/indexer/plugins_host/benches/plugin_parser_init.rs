3da011c45034c28633a6ef06c9c60c88ac9c0a2a
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-03-28 15:05:49 +0100

    Plugins benchmarks configurations via toml files
    * Plugins path and configurations can be provided to plugins benchmarks
      via toml config files.
    * Provide benchmarks config files for all example plugins and template.
    * Extend plugins readme file with infos about benchmarking.

--- application/apps/indexer/plugins_host/benches/plugin_parser_init.rs
+++ application/apps/indexer/plugins_host/benches/plugin_parser_init.rs
@@ -1,35 +1,22 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use std::{hint::black_box, path::PathBuf};
-use stypes::{PluginConfigItem, PluginConfigValue};
+use std::hint::black_box;
 
-// Use the same environment variable for input source from other benchmarks
-pub const PLUGIN_PATH_ENV_VAR: &str = "CHIPMUNK_BENCH_SOURCE";
+mod plugin_utls;
 
 /// This benchmark covers loading, compiling and validating a parser plugin
 fn plugin_parser_init(c: &mut Criterion) {
-    let plugin_file = match std::env::var(PLUGIN_PATH_ENV_VAR) {
-        Ok(input) =>PathBuf::from(input),
-        Err(err) => panic!("Error while retrieving plugin file.\n\
-            Please ensure to provide the path of plugin file for benchmarks via command line arguments \
-            if you are using chipmunk build cli tool\n\
-            or via the environment variable: '{PLUGIN_PATH_ENV_VAR}' if you are running the benchmark directly.\n\
-            Error Info: {err}"),
-    };
+    let plug_config = plugin_utls::get_plugin_config();
 
     assert!(
-        plugin_file.exists(),
+        plug_config.binary_path.exists(),
         "Provided plugin file path doesn't exist. Path: {}",
-        plugin_file.display()
+        plug_config.binary_path.display()
     );
 
-    //TODO: Deliver plugin configurations for benchmarks.
-    //For now we are delivering hard-coded configurations of string parser plugin.
-    let plugin_configs = get_string_parser_configs();
-
     let settings = black_box(stypes::PluginParserSettings::new(
-        plugin_file,
+        plug_config.binary_path,
         stypes::PluginParserGeneralSettings::default(),
-        plugin_configs,
+        plug_config.config,
     ));
 
     c.bench_function("plugin_parser_init", |bencher| {
@@ -49,16 +36,6 @@ fn plugin_parser_init(c: &mut Criterion) {
                 black_box(&parser);
             })
     });
-}
-
-#[allow(dead_code)]
-fn get_string_parser_configs() -> Vec<PluginConfigItem> {
-    const LOSSY_ID: &str = "lossy";
-    const PREFIX_ID: &str = "prefix";
-    vec![
-        PluginConfigItem::new(LOSSY_ID, PluginConfigValue::Boolean(false)),
-        PluginConfigItem::new(PREFIX_ID, PluginConfigValue::Text(String::default())),
-    ]
 }
 
 criterion_group! {
