8dfa4cdfdec5aa7e4d4c566538ab494bb05ea01f
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-03-31 11:27:53 +0200

    Remove custom allocators benchmarks + Macros
    * Remove custom allocators to reduce build time since those benchmarks
      are hardly relevant
    * Remove macros built to reuse code since it's used in one place only
      now.

--- application/apps/indexer/sources/benches/mocks_multi_parallel_jemalloc.rs
+++
@@ -1,9 +0,0 @@
-//!Benchmarks for producer loop running multiple times in parallel with mock
-//!parser and byte source using 'jemalloc' memory allocator.
-//!
-//!The mock of [`parsers::Parser`] will return iterator with multiple value replicating the
-//!behavior of the potential plugins in Chipmunk.
-
-mod macros;
-
-mocks_producer_multi_parallel!(mocks_multi_parallel_jemalloc, tikv_jemallocator::Jemalloc);
