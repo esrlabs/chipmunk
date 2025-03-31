60947b9ecae701a7c48acc98fb852342af2f25a8
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-03-31 11:27:53 +0200

    Remove custom allocators benchmarks + Macros
    * Remove custom allocators to reduce build time since those benchmarks
      are hardly relevant
    * Remove macros built to reuse code since it's used in one place only
      now.

--- application/apps/indexer/sources/benches/mocks_once_producer.rs
+++ application/apps/indexer/sources/benches/mocks_once_producer.rs
@@ -1,8 +1,50 @@
-//! Benchmarks for producer loop with mock parser and byte source using rust standard memory allocator.
+//! Benchmarks for producer loop with mock parser and byte source.
 //!
 //! The mock of [`parsers::Parser`] will return [`std::iter::once()`] replicating the behavior of
 //! the current built-in parsers in Chipmunk.
 
-mod macros;
+use bench_utls::{bench_standrad_config, run_producer};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use mocks::{mock_parser::MockParser, mock_source::MockByteSource};
+use sources::producer::MessageProducer;
+use std::hint::black_box;
+mod bench_utls;
+mod mocks;
 
-mocks_producer_once!(mocks_once_producer, std::alloc::System);
+/// Runs Benchmarks replicating the producer loop within Chipmunk sessions, using mocks for
+/// [`parsers::Parser`] and [`sources::ByteSource`] to ensure that the measurements is for the
+/// producer loop only.
+///
+/// The mock of [`parsers::Parser`] will return [`std::iter::once()`] replicating the behavior of
+/// the current built-in parsers in Chipmunk.
+///
+/// NOTE: This benchmark suffers unfortunately from a lot of noise because we are running it with
+/// asynchronous runtime. This test is configured to reduce this amount of noise as possible,
+/// However it would be better to run it multiple time for double checking.
+fn mocks_once_producer(c: &mut Criterion) {
+    let max_parse_calls = 50000;
+    c.bench_with_input(
+        BenchmarkId::new("mocks_once_producer", max_parse_calls),
+        &(max_parse_calls),
+        |bencher, &max| {
+            bencher
+                .to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter_batched(
+                    || {
+                        let parser = MockParser::new_once(max);
+                        let byte_source = MockByteSource::new();
+                        let producer = MessageProducer::new(parser, byte_source, black_box(None));
+                        producer
+                    },
+                    |producer| run_producer(producer),
+                    criterion::BatchSize::SmallInput,
+                )
+        },
+    );
+}
+criterion_group! {
+    name = benches;
+    config = bench_standrad_config();
+    targets = mocks_once_producer
+}
+criterion_main!(benches);
