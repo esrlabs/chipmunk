3966c07ed085cbad6ecb845ad6ea15f16c6d1ec5
DmitryAstafyev <dvastafyev@gmail.com>
2025-04-02 12:58:47 +0200

    Add basic docs and small renaming

--- application/apps/indexer/components/src/lib.rs
+++ application/apps/indexer/components/src/lib.rs
@@ -1,139 +1,12 @@
+mod scheme;
 mod tys;
 
-use tokio::select;
-use tokio_util::sync::CancellationToken;
-pub use tys::*;
-
 use std::collections::HashMap;
+use tokio_util::sync::CancellationToken;
 use uuid::Uuid;
 
-#[derive(Debug)]
-pub enum LazyLoadingResult {
-    Feilds(Vec<StaticFieldResult>),
-    Cancelled,
-}
-
-#[derive(Debug, Clone)]
-pub struct LazyLoadingTaskMeta {
-    pub uuid: Uuid,
-    pub ident: stypes::Ident,
-    pub cancel: CancellationToken,
-    pub fields: Vec<String>,
-}
-
-impl LazyLoadingTaskMeta {
-    pub fn contains(&self, fields: &[String]) -> bool {
-        self.fields.iter().any(|id| fields.contains(id))
-    }
-    pub fn owner(&self) -> Uuid {
-        self.ident.uuid
-    }
-}
-
-pub struct LazyLoadingTask {
-    meta: LazyLoadingTaskMeta,
-    task: Option<LazyFieldsTask>,
-}
-
-impl LazyLoadingTask {
-    pub fn new(
-        ident: stypes::Ident,
-        task: LazyFieldsTask,
-        cancel: &CancellationToken,
-        fields: Vec<String>,
-    ) -> Self {
-        Self {
-            meta: LazyLoadingTaskMeta {
-                uuid: Uuid::new_v4(),
-                ident,
-                cancel: cancel.clone(),
-                fields,
-            },
-            task: Some(task),
-        }
-    }
-
-    pub fn get_meta(&self) -> LazyLoadingTaskMeta {
-        self.meta.clone()
-    }
-
-    pub async fn wait(&mut self) -> Result<LazyLoadingResult, stypes::NativeError> {
-        let Some(task) = self.task.take() else {
-            return Err(stypes::NativeError {
-                severity: stypes::Severity::ERROR,
-                kind: stypes::NativeErrorKind::Configuration,
-                message: Some(format!(
-                    "Settings already requested for {}",
-                    self.meta.ident.name
-                )),
-            });
-        };
-        // TODO: cancel safe
-        select! {
-            _ = self.meta.cancel.cancelled() => {
-                // TODO: the question - shell we controll cancellation about parser/source,
-                // or we can deligate it? I think we should controll it above.
-                Ok(LazyLoadingResult::Cancelled)
-            }
-            fields = task => {
-                let fields = fields.map_err(|_| stypes::NativeError {
-                    severity: stypes::Severity::ERROR,
-                    kind: stypes::NativeErrorKind::ChannelError,
-                    message: Some(format!(
-                        "Fail to get settings for {}",
-                        self.meta.ident.name
-                    )),
-                })?;
-                // Check one more time in case if cancel was catched first on level of parser/source
-                if self.meta.cancel.is_cancelled() {
-                    Ok(LazyLoadingResult::Cancelled)
-                } else {
-                    Ok(LazyLoadingResult::Feilds(fields))
-                }
-            }
-        }
-    }
-}
-
-pub struct Options {
-    pub statics: Vec<stypes::FieldDesc>,
-    pub lazy: Option<LazyLoadingTask>,
-}
-
-impl Options {
-    pub fn new(statics: Vec<stypes::FieldDesc>) -> Self {
-        Self {
-            statics,
-            lazy: None,
-        }
-    }
-    pub fn set_lazy(
-        &mut self,
-        ident: stypes::Ident,
-        task: LazyFieldsTask,
-        cancel: &CancellationToken,
-    ) {
-        self.lazy = Some(LazyLoadingTask::new(ident, task, cancel, self.lazy_uuids()));
-    }
-    pub fn has_lazy(&self) -> bool {
-        self.statics
-            .iter()
-            .any(|f| matches!(f, stypes::FieldDesc::Lazy(..)))
-    }
-
-    fn lazy_uuids(&self) -> Vec<String> {
-        self.statics
-            .iter()
-            .filter_map(|f| {
-                if let stypes::FieldDesc::Lazy(f) = f {
-                    Some(f.id.to_owned())
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-}
+pub use scheme::*;
+pub use tys::*;
 
 #[derive(Default)]
 pub struct Components {
@@ -180,7 +53,7 @@ impl Components {
         &self,
         origin: stypes::SourceOrigin,
         mut targets: Vec<Uuid>,
-    ) -> Result<Vec<Options>, stypes::NativeError> {
+    ) -> Result<HashMap<Uuid, OptionsScheme>, stypes::NativeError> {
         let descriptors: Vec<&Box<dyn ComponentDescriptor + Send + 'static>> = self
             .components
             .iter()
@@ -207,9 +80,9 @@ impl Components {
                 )),
             });
         }
-        let mut list: Vec<Options> = Vec::new();
+        let mut list: HashMap<Uuid, OptionsScheme> = HashMap::new();
         for desc in descriptors.into_iter() {
-            let mut options = Options::new(desc.fields_getter(&origin)?);
+            let mut options = OptionsScheme::new(desc.fields_getter(&origin)?);
             if options.has_lazy() {
                 let cancel = CancellationToken::new();
                 options.set_lazy(
@@ -218,7 +91,7 @@ impl Components {
                     &cancel,
                 );
             }
-            list.push(options);
+            list.insert(desc.ident().uuid, options);
         }
         Ok(list)
     }
