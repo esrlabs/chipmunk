5ead88fb886492f0b0f320cd5527bdbdd8d605e9
DmitryAstafyev <dvastafyev@gmail.com>
2025-05-05 16:27:50 +0200

    Refactoring element definition

--- application/client/src/app/ui/elements/scheme/complex/nested_dictionary/component.ts
+++ application/client/src/app/ui/elements/scheme/complex/nested_dictionary/component.ts
@@ -4,7 +4,9 @@ import { Initial } from '@env/decorators/initial';
 import { ChangesDetector } from '@ui/env/extentions/changes';
 import { bytesToStr, timestampToUTC } from '@env/str';
 import { State } from './state';
-import { Element } from '../../element';
+import { Element, NestedDictionaryElement } from '../../element';
+
+import * as els from '../../element';
 
 @Component({
     selector: 'app-settings-scheme-nested-dictionary',
@@ -16,36 +18,26 @@ import { Element } from '../../element';
 @Ilc()
 export class NestedDictionary extends ChangesDetector implements AfterContentInit {
     @Input() element!: Element;
-    protected state: State | undefined;
+    @Input() inner!: NestedDictionaryElement<unknown>;
+
+    protected state!: State;
 
     constructor(cdRef: ChangeDetectorRef) {
         super(cdRef);
     }
 
     public ngAfterContentInit(): void {
-        this.state = this.element.nested_dictionary
-            ? new State(
-                  this.element.nested_dictionary.items,
-                  this.element.nested_dictionary.dictionary,
-              )
-            : undefined;
+        this.state = new State(this.inner.items, this.inner.dictionary);
     }
 
     public ngOnEntitySelect() {
-        if (!this.state) {
-            return;
-        }
         this.state.buildSummary().selected();
         this.detectChanges();
     }
 
     public ngContextMenu(event: MouseEvent) {
-        const state = this.state;
-        if (!state) {
-            return;
-        }
         const after = () => {
-            state.buildSummary().selected();
+            this.state.buildSummary().selected();
             this.detectChanges();
         };
         this.ilc().emitter.ui.contextmenu.open({
@@ -53,7 +45,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Select all',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             section.entities.forEach((e) => e.select());
                         });
                         after();
@@ -62,7 +54,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Unselect all',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             section.entities.forEach((e) => e.unselect());
                         });
                         after();
@@ -71,7 +63,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Reverse selection',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             section.entities.forEach((e) => e.toggle());
                         });
                         after();
@@ -81,7 +73,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Select with fotal',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             // section.entities.forEach((e) => {
                             //     e.log_fatal > 0 && e.select();
                             // });
@@ -92,7 +84,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Select with errors',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             // section.entities.forEach((e) => {
                             //     e.log_error > 0 && e.select();
                             // });
@@ -103,7 +95,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Select with warnings',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             // section.entities.forEach((e) => {
                             //     e.log_warning > 0 && e.select();
                             // });
@@ -115,7 +107,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Unselect without fotal',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             // section.entities.forEach((e) => {
                             //     e.log_fatal === 0 && e.unselect();
                             // });
@@ -126,7 +118,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Unselect without errors',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             // section.entities.forEach((e) => {
                             //     e.log_error === 0 && e.unselect();
                             // });
@@ -137,7 +129,7 @@ export class NestedDictionary extends ChangesDetector implements AfterContentIni
                 {
                     caption: 'Unselect without warnings',
                     handler: () => {
-                        state.structure.forEach((section) => {
+                        this.state.structure.forEach((section) => {
                             // section.entities.forEach((e) => {
                             //     e.log_warning === 0 && e.unselect();
                             // });
