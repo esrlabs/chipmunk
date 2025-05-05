47530c47a51dd958a202fa60f0b4314b3e267e92
DmitryAstafyev <dvastafyev@gmail.com>
2025-05-05 16:28:06 +0200

    Add files selector element

---
+++ application/client/src/app/ui/elements/scheme/complex/files_selector/module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
+
+import { FilesSelector } from './component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatChipsModule,
+        MatSelectModule,
+        MatListModule,
+        MatIcon,
+    ],
+    declarations: [FilesSelector],
+    exports: [FilesSelector],
+    bootstrap: [FilesSelector],
+})
+export class FilesSelectorModule {}
