15910e60e924455c3ff97134a32f1484a5f64aa5
DmitryAstafyev <dvastafyev@gmail.com>
2025-05-19 19:32:13 +0200

    Add documentation and cleanup

--- application/apps/indexer/session/src/handlers/mod.rs
+++ application/apps/indexer/session/src/handlers/mod.rs
@@ -1,7 +1,6 @@
 pub mod export_raw;
 pub mod extract;
 pub mod observe;
-mod observing;
 pub mod search;
 pub mod search_values;
 pub mod sleep;
--- application/apps/indexer/session/src/handlers/observe.rs
+++ application/apps/indexer/session/src/handlers/observe.rs
@@ -1,27 +1,49 @@
-use std::{mem, sync::Arc};
-
 use crate::{
-    handlers::observing,
     operations::{OperationAPI, OperationResult},
     state::SessionStateAPI,
+    tail,
 };
 use components::Components;
-use definitions::{Attachment, LogRecordOutput, LogRecordWriter};
-use log::error;
-use parsers::Parser;
+use definitions::*;
+use log::{trace, warn};
 use processor::producer::{sde::*, MessageProducer};
+use std::sync::Arc;
 use std::time::Instant;
-use stypes::NativeError;
+use stypes::{NativeError, SessionSetup, SourceOrigin};
+use tokio::{select, sync::mpsc::Receiver};
 
-use super::observing::run_producer;
-
+/// Duration (in milliseconds) between flushes to the session file.
 const SEND_DURATION: u128 = 500;
 
+/// A writer responsible for appending data to a session file.
+///
+/// This writer is linked to a session file through the `SessionStateAPI`.
+/// Instead of writing continuously, it accumulates updates and sends them
+/// in periodic batches defined by `SEND_DURATION` (in milliseconds).
+///
+/// To support this behavior, it uses two internal buffers:
+/// - `buffer` accumulates textual log entries.
+/// - `attachments` stores associated `Attachment` objects, which are sent
+///   only after flushing the text buffer to ensure synchronization between
+///   log lines and attachments.
 struct Writer {
+    /// Communication channel to the session file.
     state: SessionStateAPI,
+
+    /// Text buffer for log messages. Since session files are text-based,
+    /// the buffered data is stored as a `String`.
     buffer: String,
+
+    /// Buffer for received attachments. These are queued and sent only after
+    /// the buffered log messages have been flushed, preserving the logical
+    /// order between messages and attachments.
     attachments: Vec<Attachment>,
+
+    /// Timestamp used to track the last flush to the session file.
     recent: Instant,
+
+    /// Unique identifier for the data source. This is used on the client side
+    /// to visually group or distinguish data streams.
     id: u16,
 }
 
@@ -40,7 +62,7 @@ impl Writer {
 impl LogRecordWriter for Writer {
     fn write(&mut self, record: LogRecordOutput<'_>) -> Result<(), NativeError> {
         match record {
-            LogRecordOutput::Raw(inner) => {
+            LogRecordOutput::Raw(_inner) => {
                 // self.state
                 //     .write_session_file(0, format!("{:02X}\n", inner))
                 //     .await?;
@@ -102,16 +124,126 @@ impl LogRecordWriter for Writer {
 pub async fn start_observing(
     operation_api: OperationAPI,
     state: SessionStateAPI,
-    options: stypes::SessionSetup,
+    options: SessionSetup,
     components: Arc<Components<sources::Source, parsers::Parser>>,
     rx_sde: Option<SdeReceiver>,
 ) -> OperationResult<()> {
-    let (desciptor, source, parser) = components.setup(&options)?;
-    let producer = MessageProducer::new(
-        parser,
-        source,
-        Writer::new(state.clone(), state.add_source(desciptor).await?),
-    );
-    let result = run_producer(operation_api, state, producer, None, rx_sde).await?;
-    Ok(result)
+    match &options.origin {
+        SourceOrigin::File(..) => {
+            let (desciptor, source, parser) = components.setup(&options)?;
+            let producer = MessageProducer::new(
+                parser,
+                source,
+                Writer::new(state.clone(), state.add_source(desciptor).await?),
+            );
+            Ok(run_producer(operation_api, state, producer, None, None).await?)
+        }
+        SourceOrigin::Source => {
+            let (desciptor, source, parser) = components.setup(&options)?;
+            let producer = MessageProducer::new(
+                parser,
+                source,
+                Writer::new(state.clone(), state.add_source(desciptor).await?),
+            );
+            Ok(run_producer(operation_api, state, producer, None, rx_sde).await?)
+        }
+        SourceOrigin::Files(files) => {
+            // Replacement of concat feature
+            for file in files {
+                let (desciptor, source, parser) =
+                    components.setup(&options.inherit(SourceOrigin::File(file.to_owned())))?;
+                let producer = MessageProducer::new(
+                    parser,
+                    source,
+                    Writer::new(state.clone(), state.add_source(desciptor).await?),
+                );
+                run_producer(operation_api.clone(), state.clone(), producer, None, None).await?;
+            }
+            Ok(Some(()))
+        }
+    }
+}
+
+enum Next {
+    Parsed(usize, MessageStreamItem),
+    Waiting,
+    Sde(SdeMsg),
+}
+
+pub async fn run_producer<P: Parser, S: ByteSource, W: LogRecordWriter>(
+    operation_api: OperationAPI,
+    state: SessionStateAPI,
+    mut producer: MessageProducer<P, S, W>,
+    mut rx_tail: Option<Receiver<Result<(), tail::Error>>>,
+    mut rx_sde: Option<SdeReceiver>,
+) -> OperationResult<()> {
+    use log::debug;
+    state.set_session_file(None).await?;
+    operation_api.processing();
+    let cancel = operation_api.cancellation_token();
+    let cancel_on_tail = cancel.clone();
+    let started = Instant::now();
+    while let Some(next) = select! {
+        next_from_stream = async {
+            producer.read_next_segment().await
+                .map(|(consumed, results)|Next::Parsed(consumed, results))
+                .or_else(|| Some(Next::Waiting))
+        } => next_from_stream,
+        Some(sde_msg) = async {
+            if let Some(rx_sde) = rx_sde.as_mut() {
+                rx_sde.recv().await
+            } else {
+                None
+            }
+        } => Some(Next::Sde(sde_msg)),
+        _ = cancel.cancelled() => None,
+    } {
+        match next {
+            Next::Parsed(_consumed, results) => match results {
+                MessageStreamItem::Parsed(_results) => {
+                    //Just continue
+                }
+                MessageStreamItem::Done => {
+                    state.flush_session_file().await?;
+                    state.file_read().await?;
+                    debug!(
+                        "observe, message stream is done in {} ms",
+                        started.elapsed().as_millis()
+                    );
+                }
+                MessageStreamItem::Skipped => {
+                    trace!("observe: skipped a message");
+                }
+            },
+            Next::Waiting => {
+                if !state.is_closing() {
+                    state.flush_session_file().await?;
+                }
+                if let Some(mut rx_tail) = rx_tail.take() {
+                    if select! {
+                        next_from_stream = rx_tail.recv() => {
+                            if let Some(result) = next_from_stream {
+                                result.is_err()
+                            } else {
+                                true
+                            }
+                        },
+                        _ = cancel_on_tail.cancelled() => true,
+                    } {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            Next::Sde((msg, tx_response)) => {
+                let sde_res = producer.sde_income(msg).await.map_err(|e| e.to_string());
+                if tx_response.send(sde_res).is_err() {
+                    warn!("Fail to send back message from source");
+                }
+            }
+        }
+    }
+    debug!("listen done");
+    Ok(None)
 }
