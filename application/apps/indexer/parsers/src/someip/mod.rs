a4aa7cc0522fe5721a8f9dfd757592c7c5cfe980
DmitryAstafyev <dvastafyev@gmail.com>
2025-05-19 14:07:35 +0200

    Restore min_msg_len as parser method

--- application/apps/indexer/parsers/src/someip/mod.rs
+++ application/apps/indexer/parsers/src/someip/mod.rs
@@ -317,6 +317,9 @@ impl Parser for SomeipParser {
             data.map(|msg| LogRecordOutput::String(msg.to_string())),
         ))
     }
+    fn min_msg_len(&self) -> usize {
+        MIN_MSG_LEN
+    }
 }
 
 fn header_string(header: &Header) -> String {
