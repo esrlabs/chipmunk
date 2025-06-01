51fd6fa7013cee1c79a6d90770310ef9ca2b3811
DmitryAstafyev <dvastafyev@gmail.com>
2025-06-01 20:58:28 +0200

    Sync error handeling between rustcore and client (nested fields)

--- application/client/src/app/ui/elements/scheme/entry/inner/element/inner.ts
+++ application/client/src/app/ui/elements/scheme/entry/inner/element/inner.ts
@@ -1,12 +1,21 @@
 import { Value } from '@platform/types/bindings';
 import { Element } from './index';
+import { SchemeProvider } from '@ui/elements/scheme/provider';
+import { Subscriber } from '@platform/env/subscription';
 
-export abstract class ElementInner {
+export abstract class ElementInner extends Subscriber {
     public abstract getValue(): Value;
     public abstract getInnerValue(): any;
     public abstract setValue(value: any): void;
     public abstract isField(): boolean;
+    public destroy(): void {
+        this.unsubscribe();
+        // This method is optional. That's why it has default implementation.
+    }
     public setParentRef(_parent: Element): void {
+        // This method is optional. That's why it has default implementation.
+    }
+    public setProviderRef(_provider: SchemeProvider): void {
         // This method is optional. That's why it has default implementation.
     }
 }
