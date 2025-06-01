59b394cfa6902c5eabc3aa1ff300e02bf847bd2c
DmitryAstafyev <dvastafyev@gmail.com>
2025-06-01 20:58:28 +0200

    Sync error handeling between rustcore and client (nested fields)

--- application/client/src/app/ui/elements/scheme/entry/inner/element/fields_collection/index.ts
+++ application/client/src/app/ui/elements/scheme/entry/inner/element/fields_collection/index.ts
@@ -1,15 +1,32 @@
 import { Element } from '../index';
 import { ElementInner } from '../inner';
-import { StaticFieldDesc, Value, ValueInput } from '@platform/types/bindings';
+import { StaticFieldDesc, Value, ValueInput, Field } from '@platform/types/bindings';
 import { Provider } from './provider';
 import { Subscriber, Subject } from '@platform/env/subscription';
+import { SchemeProvider } from '@ui/elements/scheme/provider';
 
 class Collection extends Subscriber {
-    static from(spec: StaticFieldDesc[], idx: number, handler: () => void): Collection {
+    static from(
+        spec: StaticFieldDesc[],
+        uuid: string,
+        idx: number,
+        handler: () => void,
+    ): Collection {
         const elements: Element[] = spec.map(
-            (field, i) => new Element(`${idx}_${i}`, field.interface),
+            (field, i) => new Element(`${uuid}:${idx}:${i}`, field.interface),
         );
         return new Collection(elements, handler);
+    }
+
+    public checkErrors(errors: Map<string, string>) {
+        this.dropErrors();
+        errors.forEach((msg, uuid) => {
+            const element = this.elements.find((el) => el.uuid == uuid);
+            if (!element) {
+                return;
+            }
+            element.setError(msg);
+        });
     }
 
     constructor(public readonly elements: Element[], handler: () => void) {
@@ -17,14 +34,24 @@ class Collection extends Subscriber {
         this.register(...elements.map((el) => el.subjects.get().changed.subscribe(handler)));
     }
 
-    public getValues(): Value[] {
-        return this.elements.map((el) => el.getValue());
+    public getFields(uuid: string, idx: number): Value {
+        return {
+            Fields: this.elements.map((el, n) => {
+                return { id: `${uuid}:${idx}:${n}`, value: el.getValue() };
+            }),
+        };
+    }
+
+    public dropErrors() {
+        this.elements.forEach((el) => el.setError(undefined));
     }
 }
 
 export class FieldsCollectionElement extends ElementInner {
     protected update() {
-        this.values = this.collections.map((collection) => collection.getValues());
+        this.values = this.collections.map((collection, idx) =>
+            collection.getFields(this.parent.uuid, idx),
+        );
         this.parent.subjects
             .get()
             .changed.emit({ uuid: this.parent.uuid, inner: undefined, value: this.getValue() });
@@ -33,7 +60,7 @@ export class FieldsCollectionElement extends ElementInner {
 
     public parent!: Element;
     public collections: Collection[] = [];
-    public values: Value[][] = [];
+    public values: Value[] = [];
     public provider: Provider = new Provider();
 
     public constructor(
@@ -48,12 +75,26 @@ export class FieldsCollectionElement extends ElementInner {
         this.parent = parent;
     }
 
-    public destroy() {
+    public override setProviderRef(provider: SchemeProvider): void {
+        this.register(
+            provider.subjects.get().error.subscribe((errors: Map<string, string>) => {
+                this.collections.forEach((collection) => {
+                    collection.checkErrors(errors);
+                });
+                this.parent.emitError();
+            }),
+        );
+    }
+
+    public override destroy() {
         this.collections.forEach((collection) => collection.unsubscribe());
+        this.unsubscribe();
     }
 
     public add(): void {
-        this.collections.push(Collection.from(this.spec, this.collections.length, this.update));
+        this.collections.push(
+            Collection.from(this.spec, this.parent.uuid, this.collections.length, this.update),
+        );
     }
     public remove(idx: number) {
         this.collections.splice(idx, 1).forEach((collection) => collection.unsubscribe());
@@ -73,8 +114,11 @@ export class FieldsCollectionElement extends ElementInner {
 
     public getValue(): Value {
         return {
-            Values: this.collections.map((collection) => {
-                return { Values: collection.getValues() };
+            Fields: this.collections.map((collection, idx) => {
+                return {
+                    id: `${this.parent.uuid}:${idx}`,
+                    value: collection.getFields(this.parent.uuid, idx),
+                };
             }),
         };
     }
