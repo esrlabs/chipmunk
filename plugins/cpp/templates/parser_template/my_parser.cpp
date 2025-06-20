50031cd68fd5139d3ebc9e1c9147267dff54a970
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-06-20 17:17:01 +0200

    Plugins C/C++: Demonstrate configuration & render options
    * Add plugins configuration sections to plugin template to show sending
      configs schemas and log their values in init function.
    * Add Render options to plugins, providing two modes for messages with
      single line or multiple columns.

--- plugins/cpp/templates/parser_template/my_parser.cpp
+++ plugins/cpp/templates/parser_template/my_parser.cpp
@@ -1,6 +1,19 @@
 #include "parse.h"
 #include "stdio.h"
+#include <stdlib.h>
 #include <string.h>
+
+// This template demonstrates two modes for the parsed messages:
+// - Single column: For log messages which doesn't have structure.
+// - Multiple columns: For log messages with structure.
+
+#define SINGLE_COLUMN_MODE
+// #define MULTI_COLUMN_MODE
+
+#if (defined(SINGLE_COLUMN_MODE) && defined(MULTI_COLUMN_MODE)) ||             \
+    !defined(SINGLE_COLUMN_MODE) && !defined(MULTI_COLUMN_MODE)
+#error "Column mode definitions error: Exactly one must be defined."
+#endif
 
 // Basic solution to keep track on the current logging level provided by
 // Chipmunk host to avoid sending logs to host that won't be logged.
@@ -16,15 +29,67 @@ void exports_chipmunk_parser_parser_get_version(
 
 void exports_chipmunk_parser_parser_get_config_schemas(
     exports_chipmunk_parser_parser_list_config_schema_item_t *ret) {
-  // TODO AAZ: Temp solution until we are sure that it can be compiled.
-  ret->ptr = NULL;
-  ret->len = 0;
+  ret->len = 3;
+
+  // Boolean configuration item
+  auto &bool_item = ret->ptr[0];
+  parse_string_dup(&bool_item.id, "bool_id");
+  bool_item.description.is_some = true;
+  parse_string_dup(&bool_item.title, "Boolean Configuration");
+  parse_string_dup(&bool_item.description.val,
+                   "Demonstrate a boolean configuration item");
+  bool_item.input_type.tag =
+      CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_BOOLEAN;
+  bool_item.input_type.val.boolean = true;
+
+  // Text configuration item
+  auto &text_item = ret->ptr[1];
+  parse_string_dup(&text_item.id, "text_id");
+  text_item.description.is_some = true;
+  parse_string_dup(&text_item.title, "Text Configuration");
+  parse_string_dup(&text_item.description.val,
+                   "Demonstrate a text configuration item");
+  text_item.input_type.tag =
+      CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_TEXT;
+  parse_string_dup(&text_item.input_type.val.text, "Default text");
+
+  // Files configuration item
+  auto &files_item = ret->ptr[2];
+  parse_string_dup(&files_item.id, "files_id");
+  files_item.description.is_some = true;
+  parse_string_dup(&files_item.title, "Files Configuration");
+  parse_string_dup(&files_item.description.val,
+                   "Demonstrate files configuration item");
+  files_item.input_type.tag =
+      CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_FILES;
+  files_item.input_type.val.files.len = 0;
 }
 
 void exports_chipmunk_parser_parser_get_render_options(
     exports_chipmunk_parser_parser_render_options_t *ret) {
-  // TODO AAZ: Temp solution until we are sure that it can be compiled.
+#if defined(SINGLE_COLUMN_MODE)
   ret->columns_options.is_some = false;
+
+#elif defined(MULTI_COLUMN_MODE)
+  ret->columns_options.is_some = true;
+
+  ret->columns_options.val.max_width = 600;
+  ret->columns_options.val.min_width = 30;
+
+  auto &columns = ret->columns_options.val.columns;
+
+  columns.len = 2;
+  auto &first_column = columns.ptr[0];
+  parse_string_dup(&first_column.caption, "First Column");
+  parse_string_dup(&first_column.description, "First Column Description");
+  first_column.width = 30;
+
+  auto &second_column = columns.ptr[1];
+  parse_string_dup(&second_column.caption, "Second Column");
+  parse_string_dup(&second_column.description, "Second Column Description ");
+  second_column.width = 100;
+
+#endif
 }
 
 bool exports_chipmunk_parser_parser_init(
@@ -45,7 +110,51 @@ bool exports_chipmunk_parser_parser_init(
          general_configs->log_level);
 
   // *** Demonstrate printing to stderr ***
-  fprintf(stderr, "Provided configs length: %d\n", (int)plugin_configs->len);
+  size_t conf_len = plugin_configs->len;
+  fprintf(stderr, "Provided configs length: %zu\n", conf_len);
+
+  // *** Fetching configuration values ***
+  for (size_t i = 0; i < conf_len; i++) {
+    printf("Configuration item info:\n");
+    printf("  Index: %zu\n", i);
+    printf("  ID: %s\n", plugin_configs->ptr[i].id.ptr);
+    uint8_t val_tag = plugin_configs->ptr[i].value.tag;
+    printf("  TAG ID: %d\n", val_tag);
+    switch (val_tag) {
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_BOOLEAN:
+      printf("  Boolean value: %d\n", plugin_configs->ptr[i].value.val.boolean);
+      break;
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_INTEGER:
+      printf("  Integer value: %d\n", plugin_configs->ptr[i].value.val.integer);
+      break;
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_FLOAT:
+      printf("  Float value: %f\n", plugin_configs->ptr[i].value.val.float_);
+      break;
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_TEXT:
+      printf("  Text value: %s\n", plugin_configs->ptr[i].value.val.text.ptr);
+      break;
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_DIRECTORIES: {
+      size_t dir_len = plugin_configs->ptr[i].value.val.directories.len;
+      printf("  Directories length: %zu\n", dir_len);
+      for (size_t dir_idx = 0; dir_idx < dir_len; dir_len++) {
+        printf("    Directory path: %s\n",
+               plugin_configs->ptr[i].value.val.directories.ptr[dir_idx].ptr);
+      }
+    } break;
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_FILES: {
+      size_t files_len = plugin_configs->ptr[i].value.val.files.len;
+      printf("  Files length: %zu\n", files_len);
+      for (size_t file_idx = 0; file_idx < files_len; file_idx++) {
+        printf("    File path: %s\n",
+               plugin_configs->ptr[i].value.val.files.ptr[file_idx].ptr);
+      }
+    } break;
+    case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_DROPDOWN:
+      printf("  Dropdown value: %s\n",
+             plugin_configs->ptr[i].value.val.dropdown.ptr);
+      break;
+    }
+  }
 
   return true;
 }
@@ -55,12 +164,13 @@ bool exports_chipmunk_parser_parser_parse(
     exports_chipmunk_parser_parser_list_parse_return_t *ret,
     exports_chipmunk_parser_parser_parse_error_t *err) {
   // *** Demonstrate basic logging ***
-  // TODO AAZ: Change log level to debug.
-  if (global_log_level >= CHIPMUNK_SHARED_LOGGING_LEVEL_ERROR) {
+  if (global_log_level >= CHIPMUNK_SHARED_LOGGING_LEVEL_DEBUG) {
     parse_string_t log_msg;
     parse_string_set(&log_msg, "Parse called");
-    chipmunk_shared_logging_log(CHIPMUNK_SHARED_LOGGING_LEVEL_ERROR, &log_msg);
+    chipmunk_shared_logging_log(CHIPMUNK_SHARED_LOGGING_LEVEL_DEBUG, &log_msg);
   }
+
+#if defined(SINGLE_COLUMN_MODE)
 
   // *** Return length of provided bytes ***
   ret->len = 1;
@@ -80,4 +190,33 @@ bool exports_chipmunk_parser_parser_parse(
   parse_string_set(&item.value.val.val.message.val.line, msg_buff);
 
   return true;
+#elif defined(MULTI_COLUMN_MODE)
+
+  // *** Returns two columns ***
+  // - The first column contain a static text.
+  // - The second columns contains the length of provided bytes.
+  exports_chipmunk_parser_parser_parse_return_free(ret->ptr);
+  ret->ptr = (exports_chipmunk_parser_parser_parse_return_t *)malloc(
+      // alignof(exports_chipmunk_parser_parser_parse_return_t),
+      sizeof(exports_chipmunk_parser_parser_parse_return_t));
+  ret->len = 1;
+  auto &item = ret->ptr[0];
+  item.consumed = data->len;
+  item.value.is_some = true;
+
+  item.value.val.tag = CHIPMUNK_PARSER_PARSE_TYPES_PARSE_YIELD_MESSAGE;
+  item.value.val.val.message.tag =
+      CHIPMUNK_PARSER_PARSE_TYPES_PARSED_MESSAGE_COLUMNS;
+
+  auto &columns = item.value.val.val.message.val.columns;
+  columns.len = 2;
+  parse_string_dup(&columns.ptr[0], "static message");
+
+  char msg_buff[100];
+  snprintf(msg_buff, sizeof(msg_buff), "The length of provided bytes: %zu",
+           data->len);
+  parse_string_dup(&columns.ptr[1], msg_buff);
+
+  return true;
+#endif
 }
