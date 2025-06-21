4b6e5a7e29749d75b9e835573967ed4436ed4316
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-06-21 20:08:31 +0200

    Plugins C/C++: Separated template for each language
    C and C++ differ a lot in the build steps and each of them must have
    each own template.

--- plugins/cpp/templates/parser_template_cpp/my_parser.cpp
+++ plugins/cpp/templates/parser_template_cpp/my_parser.cpp
@@ -1,7 +1,6 @@
 #include "parse.h"
-#include "stdio.h"
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
 
 // This template demonstrates two modes for the parsed messages:
 // - Single column: For log messages which doesn't have structure.
@@ -29,6 +28,7 @@ void exports_chipmunk_parser_parser_get_version(
 
 void exports_chipmunk_parser_parser_get_config_schemas(
     exports_chipmunk_parser_parser_list_config_schema_item_t *ret) {
+  ret->ptr = new exports_chipmunk_parser_parser_config_schema_item_t[3];
   ret->len = 3;
 
   // Boolean configuration item
@@ -77,17 +77,16 @@ void exports_chipmunk_parser_parser_get_render_options(
   ret->columns_options.val.min_width = 30;
 
   auto &columns = ret->columns_options.val.columns;
-
+  columns.ptr = new chipmunk_parser_parse_types_column_info_t[2];
   columns.len = 2;
+
   auto &first_column = columns.ptr[0];
   parse_string_dup(&first_column.caption, "First Column");
-  parse_string_dup(&first_column.description, "First Column Description");
-  first_column.width = 30;
+  first_column.width = 110;
 
   auto &second_column = columns.ptr[1];
   parse_string_dup(&second_column.caption, "Second Column");
-  parse_string_dup(&second_column.description, "Second Column Description ");
-  second_column.width = 100;
+  second_column.width = -1;
 
 #endif
 }
@@ -106,52 +105,61 @@ bool exports_chipmunk_parser_parser_init(
   }
 
   // *** Demonstrate printing to stdout ***
-  printf("Inint function called with log level: %d\n",
-         general_configs->log_level);
+  std::cout << "Inint function called with log level: "
+            << std::to_string(general_configs->log_level) << std::endl;
 
   // *** Demonstrate printing to stderr ***
   size_t conf_len = plugin_configs->len;
-  fprintf(stderr, "Provided configs length: %zu\n", conf_len);
+  std::cerr << "Provided configs length: " << std::to_string(conf_len)
+            << std::endl;
 
   // *** Fetching configuration values ***
   for (size_t i = 0; i < conf_len; i++) {
-    printf("Configuration item info:\n");
-    printf("  Index: %zu\n", i);
-    printf("  ID: %s\n", plugin_configs->ptr[i].id.ptr);
+    std::cout << "Configuration item info:" << std::endl;
+    std::cout << "  Index: " << i << std::endl;
+    std::cout << "  ID: " << plugin_configs->ptr[i].id.ptr << std::endl;
     uint8_t val_tag = plugin_configs->ptr[i].value.tag;
-    printf("  TAG ID: %d\n", val_tag);
+    std::cout << "  TAG ID: " << std::to_string(val_tag) << std::endl;
     switch (val_tag) {
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_BOOLEAN:
-      printf("  Boolean value: %d\n", plugin_configs->ptr[i].value.val.boolean);
+      std::cout << "  Boolean value: "
+                << plugin_configs->ptr[i].value.val.boolean << std::endl;
       break;
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_INTEGER:
-      printf("  Integer value: %d\n", plugin_configs->ptr[i].value.val.integer);
+      std::cout << "  Integer value: "
+                << plugin_configs->ptr[i].value.val.integer << std::endl;
       break;
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_FLOAT:
-      printf("  Float value: %f\n", plugin_configs->ptr[i].value.val.float_);
+      std::cout << "  Float value: " << plugin_configs->ptr[i].value.val.float_
+                << std::endl;
       break;
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_TEXT:
-      printf("  Text value: %s\n", plugin_configs->ptr[i].value.val.text.ptr);
+      std::cout << "  Text value: " << plugin_configs->ptr[i].value.val.text.ptr
+                << std::endl;
       break;
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_DIRECTORIES: {
       size_t dir_len = plugin_configs->ptr[i].value.val.directories.len;
-      printf("  Directories length: %zu\n", dir_len);
+      std::cout << "  Directories length: " << std::to_string(dir_len)
+                << std::endl;
       for (size_t dir_idx = 0; dir_idx < dir_len; dir_len++) {
-        printf("    Directory path: %s\n",
-               plugin_configs->ptr[i].value.val.directories.ptr[dir_idx].ptr);
+        std::cout
+            << "    Directory path: "
+            << plugin_configs->ptr[i].value.val.directories.ptr[dir_idx].ptr
+            << std::endl;
       }
     } break;
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_FILES: {
       size_t files_len = plugin_configs->ptr[i].value.val.files.len;
-      printf("  Files length: %zu\n", files_len);
+      std::cout << "  Files length: " << std::to_string(files_len) << std::endl;
       for (size_t file_idx = 0; file_idx < files_len; file_idx++) {
-        printf("    File path: %s\n",
-               plugin_configs->ptr[i].value.val.files.ptr[file_idx].ptr);
+        std::cout << "    File path: "
+                  << plugin_configs->ptr[i].value.val.files.ptr[file_idx].ptr
+                  << std::endl;
       }
     } break;
     case CHIPMUNK_SHARED_SHARED_TYPES_CONFIG_SCHEMA_TYPE_DROPDOWN:
-      printf("  Dropdown value: %s\n",
-             plugin_configs->ptr[i].value.val.dropdown.ptr);
+      std::cout << "  Dropdown value: "
+                << plugin_configs->ptr[i].value.val.dropdown.ptr << std::endl;
       break;
     }
   }
@@ -181,13 +189,9 @@ bool exports_chipmunk_parser_parser_parse(
   item.value.val.val.message.tag =
       CHIPMUNK_PARSER_PARSE_TYPES_PARSED_MESSAGE_LINE;
 
-  // TODO AAZ: Std strings are failing to compile.
-  // std::string log_line = "Sent bytes length" + std::to_string(data->len);
-  // Deliver values with C API (snprintf) temporally:
-  char msg_buff[100];
-  snprintf(msg_buff, sizeof(msg_buff), "The length of provided bytes: %zu",
-           data->len);
-  parse_string_set(&item.value.val.val.message.val.line, msg_buff);
+  std::string log_line =
+      "The length of provided bytes: " + std::to_string(data->len);
+  parse_string_set(&item.value.val.val.message.val.line, log_line.c_str());
 
   return true;
 #elif defined(MULTI_COLUMN_MODE)
@@ -195,10 +199,6 @@ bool exports_chipmunk_parser_parser_parse(
   // *** Returns two columns ***
   // - The first column contain a static text.
   // - The second columns contains the length of provided bytes.
-  exports_chipmunk_parser_parser_parse_return_free(ret->ptr);
-  ret->ptr = (exports_chipmunk_parser_parser_parse_return_t *)malloc(
-      // alignof(exports_chipmunk_parser_parser_parse_return_t),
-      sizeof(exports_chipmunk_parser_parser_parse_return_t));
   ret->len = 1;
   auto &item = ret->ptr[0];
   item.consumed = data->len;
@@ -209,13 +209,13 @@ bool exports_chipmunk_parser_parser_parse(
       CHIPMUNK_PARSER_PARSE_TYPES_PARSED_MESSAGE_COLUMNS;
 
   auto &columns = item.value.val.val.message.val.columns;
+  columns.ptr = new parse_string_t[2];
   columns.len = 2;
   parse_string_dup(&columns.ptr[0], "static message");
 
-  char msg_buff[100];
-  snprintf(msg_buff, sizeof(msg_buff), "The length of provided bytes: %zu",
-           data->len);
-  parse_string_dup(&columns.ptr[1], msg_buff);
+  std::string log_line =
+      "The length of provided bytes: " + std::to_string(data->len);
+  parse_string_dup(&columns.ptr[1], log_line.c_str());
 
   return true;
 #endif
