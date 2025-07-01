c7967ee26ef25e126e72760c5f36a9e53757c5b4
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-07-01 16:45:14 +0200

    Components: Integrate Plugins in Electron

--- application/holder/src/service/components/plugins/invalid_plugin_info.ts
+++ application/holder/src/service/components/plugins/invalid_plugin_info.ts
@@ -1,6 +1,6 @@
 import { CancelablePromise } from 'platform/env/promise';
 import { Logger } from 'platform/log';
-import { unbound } from '@service/unbound';
+import { components } from '@service/components';
 
 import * as Requests from 'platform/ipc/request';
 
@@ -13,7 +13,7 @@ export const handler = Requests.InjectLogger<
         request: Requests.Plugins.InvalidPluginInfo.Request,
     ): CancelablePromise<Requests.Plugins.InvalidPluginInfo.Response> => {
         return new CancelablePromise((reslove, reject) => {
-            unbound.jobs
+            components.components
                 .invalidPluginsInfo(request.pluginPath)
                 .then((invalidPlugin) => {
                     reslove(new Requests.Plugins.InvalidPluginInfo.Response({ invalidPlugin }));
