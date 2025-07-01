4d7fe7a291775e573651ac3bf500c57bd7b04f75
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-07-01 16:45:14 +0200

    Components: Integrate Plugins in Electron

--- application/holder/src/service/components/plugins/list_installed.ts
+++ application/holder/src/service/components/plugins/list_installed.ts
@@ -1,6 +1,6 @@
 import { CancelablePromise } from 'platform/env/promise';
 import { Logger } from 'platform/log';
-import { unbound } from '@service/unbound';
+import { components } from '@service/components';
 
 import * as Requests from 'platform/ipc/request';
 
@@ -13,7 +13,7 @@ export const handler = Requests.InjectLogger<
         _request: Requests.Plugins.ListInstalled.Request,
     ): CancelablePromise<Requests.Plugins.ListInstalled.Response> => {
         return new CancelablePromise((reslove, reject) => {
-            unbound.jobs
+            components.components
                 .installedPluginsList()
                 .then((plugins) => {
                     reslove(new Requests.Plugins.ListInstalled.Response({ plugins }));
