55daf849047d98bcf82898d45be9bc64f950f6db
DmitryAstafyev <dvastafyev@gmail.com>
2025-07-07 17:19:12 +0200

    Update recent actions format

--- application/client/src/app/ui/elements/recent/component.ts
+++ application/client/src/app/ui/elements/recent/component.ts
@@ -87,7 +87,7 @@ export class RecentActions extends ChangesDetector implements AfterContentInit, 
             {
                 caption: 'Remove recent',
                 handler: () => {
-                    this.state.remove([action.uuid]);
+                    this.state.remove([action.hash]);
                 },
             },
             {
