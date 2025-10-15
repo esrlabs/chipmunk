a5bf78a51aa193b8fb17b07ff1bd8951e7fab74d
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-15 19:31:47 +0200

    Building Desktop App main parts
    * Rename crate to application
    * Define UI & Core & Service and State modules.
    * Define communication types (commands and events) for both general app
      and sessions.
    * Define communication channels for general app
    * Render Menu with one command to close the app.

---
+++ application/apps/indexer/gui/application/src/app.rs
@@ -0,0 +1,84 @@
+use eframe::NativeOptions;
+use egui::{Context, vec2};
+use tokio::sync::{mpsc::error::TryRecvError, watch};
+
+use crate::{
+    core::{
+        communication::{UiCommunication, UiReceivers},
+        events::AppEvent,
+    },
+    state::AppState,
+    ui::UiComponents,
+};
+
+const APP_TITLE: &str = "Chipmunk";
+
+#[derive(Debug)]
+pub struct ChipmunkApp {
+    receivers: UiReceivers,
+    ui: UiComponents,
+}
+
+impl ChipmunkApp {
+    pub fn run(comm: UiCommunication) -> eframe::Result<()> {
+        let UiCommunication { senders, receivers } = comm;
+
+        let native_options = NativeOptions {
+            viewport: egui::ViewportBuilder::default()
+                .with_title(APP_TITLE)
+                .with_inner_size(vec2(1200., 900.)),
+            ..Default::default()
+        };
+
+        eframe::run_native(
+            APP_TITLE,
+            native_options,
+            Box::new(|ctx| {
+                Self::spawn_repaint_listner(ctx.egui_ctx.clone(), receivers.app_state_rx.clone());
+
+                let ui = UiComponents::new(senders);
+                let app = Self { ui, receivers };
+
+                Ok(Box::new(app))
+            }),
+        )
+    }
+
+    fn spawn_repaint_listner(ctx: egui::Context, mut repaint_rx: watch::Receiver<AppState>) {
+        tokio::spawn(async move {
+            loop {
+                match repaint_rx.changed().await {
+                    Ok(()) => ctx.request_repaint(),
+                    Err(err) => {
+                        eprintln!("Communication error with app state channels: {err}");
+                        std::process::exit(1);
+                    }
+                }
+            }
+        });
+    }
+
+    fn handle_event(&mut self, event: AppEvent, ctx: &Context) {
+        match event {
+            AppEvent::Close => ctx.send_viewport_cmd(egui::ViewportCommand::Close),
+        }
+    }
+}
+
+impl eframe::App for ChipmunkApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        loop {
+            match self.receivers.event_rx.try_recv() {
+                Ok(event) => self.handle_event(event, ctx),
+                Err(TryRecvError::Empty) => break,
+                Err(TryRecvError::Disconnected) => {
+                    //TODO AAZ: better error handling.
+                    eprintln!("Communication error: State sender dropped");
+                    std::process::exit(1);
+                }
+            }
+        }
+
+        self.ui.update(ctx, frame);
+    }
+}
---
+++ application/apps/indexer/gui/application/src/core/events/app.rs
@@ -0,0 +1,6 @@
+/// Events related to general app to be sent from Core to UI.
+#[derive(Debug, Clone)]
+pub enum AppEvent {
+    /// Close the application.
+    Close,
+}
