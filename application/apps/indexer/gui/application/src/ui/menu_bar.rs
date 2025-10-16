822f7405a82d6c2251c8f9286befe74045d96a0f
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-16 19:42:15 +0200

    Simple implementation for Open File dialog.

---
+++ application/apps/indexer/gui/application/src/ui/menu_bar.rs
@@ -0,0 +1,55 @@
+use std::{path::PathBuf, pin::Pin};
+
+use egui::{MenuBar, Ui};
+use rfd::FileHandle;
+use tokio::sync::mpsc::Sender;
+
+use crate::core::commands::AppCommand;
+
+#[derive(Debug)]
+pub struct AppMenuBar {}
+
+impl AppMenuBar {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn render(&mut self, ui: &mut Ui, cmd_tx: &Sender<AppCommand>) {
+        MenuBar::new().ui(ui, |ui| {
+            ui.menu_button("Chipmunk", |ui| {
+                if ui.button("Close").clicked() {
+                    if let Err(err) = cmd_tx.try_send(AppCommand::Close) {
+                        // TODO AAZ: Better error handling.
+                        log::error!("Send app command failed: {err:?}");
+                    }
+                }
+            });
+
+            ui.menu_button("File", |ui| {
+                if ui.button("Open File(s)").clicked() {
+                    //TODO AAZ: App shouldn't be usable while dialog open.
+
+                    let handle = rfd::AsyncFileDialog::new().pick_files();
+
+                    let cmd_tx = cmd_tx.to_owned();
+                    tokio::spawn(async move {
+                        if let Some(files) = handle.await {
+                            log::trace!("Open file dialog return with {files:?}");
+
+                            if files.is_empty() {
+                                return;
+                            }
+
+                            let files: Vec<PathBuf> =
+                                files.iter().map(|file| file.into()).collect();
+
+                            if let Err(err) = cmd_tx.send(AppCommand::OpenFiles(files)).await {
+                                log::error!("Send app command failed: {err:?}");
+                            }
+                        }
+                    });
+                }
+            })
+        });
+    }
+}
