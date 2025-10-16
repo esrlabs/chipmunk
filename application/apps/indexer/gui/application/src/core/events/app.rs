d54a6c009b4695dbb9c5dc38f94d7b57238f1853
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-16 22:01:16 +0200

    Basic implementation for tabs & Open files as tabs
    + Ignore errors in repaint watcher since we will get and error once the
      states sender channel is dropped.

--- application/apps/indexer/gui/application/src/core/events/app.rs
+++ application/apps/indexer/gui/application/src/core/events/app.rs
@@ -1,6 +1,9 @@
 /// Events related to general app to be sent from Core to UI.
 #[derive(Debug, Clone)]
 pub enum AppEvent {
+    CreateSession {
+        title: String,
+    },
     /// Close the application.
     Close,
 }
