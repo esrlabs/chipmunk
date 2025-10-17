ef8d4e8191c9d9adfc72ce746b339a3f33fce67d
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-17 22:01:53 +0200

    Session UI & Wire up starting session and its events
    * Create the UI for rendering the content of the sessions using the
      communication channels.
    * Spawn a task for each session to make the communication for each
      session locally.
    * UI will just read the file and render it's content as lines.
    * Channels communication still need some thinking because we are tight
      to the render loop to receive the events.

--- application/apps/indexer/gui/application/src/session/mod.rs
+++ application/apps/indexer/gui/application/src/session/mod.rs
@@ -1,4 +1,40 @@
 //! This is the main module for session parts
+
+use std::path::PathBuf;
+
+use crate::session::{communication::UiHandle, data::SessionState, service::SessionService};
+
 pub mod command;
+pub mod communication;
+pub mod data;
+pub mod error;
 pub mod event;
+pub mod service;
 pub mod ui;
+
+#[derive(Debug, thiserror::Error)]
+pub enum InitSessionError {
+    #[error("IO error: {0}")]
+    IO(#[from] std::io::Error),
+}
+
+#[derive(Debug)]
+pub struct InitSessionParams {
+    pub file_path: PathBuf,
+    pub communication: UiHandle,
+}
+
+pub fn init_session(path: PathBuf) -> Result<InitSessionParams, InitSessionError> {
+    let state = SessionState::create(path.clone())?;
+
+    let (ui_handle, service_handle) = communication::init(state);
+
+    SessionService::spwan(service_handle);
+
+    let info = InitSessionParams {
+        file_path: path,
+        communication: ui_handle,
+    };
+
+    Ok(info)
+}
