5b82666225b861e184d11f8b1f63553df1ed7d0f
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-17 22:01:53 +0200

    Session UI & Wire up starting session and its events
    * Create the UI for rendering the content of the sessions using the
      communication channels.
    * Spawn a task for each session to make the communication for each
      session locally.
    * UI will just read the file and render it's content as lines.
    * Channels communication still need some thinking because we are tight
      to the render loop to receive the events.

--- application/apps/indexer/gui/application/src/host/ui/mod.rs
+++ application/apps/indexer/gui/application/src/host/ui/mod.rs
@@ -1,9 +1,13 @@
-use egui::{CentralPanel, Context, Frame, TopBottomPanel};
+use egui::{CentralPanel, Context, Frame, TopBottomPanel, Ui};
 
-use crate::host::communication::UiSenders;
+use crate::{
+    host::{communication::UiSenders, ui::home::HomeView},
+    session::{InitSessionParams, ui::SessionUI},
+};
 use menu::MainMenuBar;
 use state::{TabType, UiState};
 
+mod home;
 mod menu;
 mod state;
 
@@ -12,11 +16,7 @@ pub struct UiComponents {
     senders: UiSenders,
     menu: MainMenuBar,
     state: UiState,
-}
-
-#[derive(Debug)]
-pub struct SessionInfo {
-    pub title: String,
+    sessions: Vec<SessionUI>,
 }
 
 impl UiComponents {
@@ -27,51 +27,61 @@ impl UiComponents {
             menu,
             senders,
             state: UiState::default(),
+            sessions: Vec::new(),
         }
     }
 
-    pub fn add_session(&mut self, session: SessionInfo) {
-        self.state.sessions.push(session);
-        self.state.active_tab = TabType::Session(self.state.sessions.len() - 1);
+    pub fn add_session(&mut self, session: InitSessionParams) {
+        let session = SessionUI::new(session);
+        self.sessions.push(session);
+        self.state.active_tab = TabType::Session(self.sessions.len() - 1);
     }
 
     pub fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
-        self.render_menu(ctx);
-        self.render_tabs(ctx);
+        TopBottomPanel::top("menu_bar")
+            .frame(Frame::side_top_panel(&ctx.style()))
+            .show(ctx, |ui| {
+                self.render_menu(ui);
+            });
+
+        TopBottomPanel::top("tab_bar")
+            .frame(Frame::side_top_panel(&ctx.style()))
+            .show(ctx, |ui| {
+                self.render_tabs(ui);
+            });
 
         CentralPanel::default()
             .frame(Frame::central_panel(&ctx.style()))
             .show(ctx, |ui| {
-                ui.centered_and_justified(|ui| {
-                    ui.heading("Welcome to Chipmunk");
-                });
+                self.render_main(ui);
             });
     }
 
-    fn render_menu(&mut self, ctx: &Context) {
+    fn render_menu(&mut self, ui: &mut Ui) {
         let Self { senders, menu, .. } = self;
-        TopBottomPanel::top("menu_bar")
-            .frame(Frame::side_top_panel(&ctx.style()))
-            .show(ctx, |ui| {
-                menu.render(ui, &senders.cmd_tx);
-            });
+        menu.render(ui, &senders.cmd_tx);
     }
 
-    fn render_tabs(&mut self, ctx: &Context) {
-        let Self { state, .. } = self;
-        TopBottomPanel::top("tab_bar")
-            .frame(Frame::side_top_panel(&ctx.style()))
-            .show(ctx, |ui| {
-                ui.horizontal_wrapped(|ui| {
-                    ui.selectable_value(&mut state.active_tab, TabType::Home, "Home");
-                    for (idx, session) in state.sessions.iter().enumerate() {
-                        ui.selectable_value(
-                            &mut state.active_tab,
-                            TabType::Session(idx),
-                            format!("Session {}", session.title),
-                        );
-                    }
-                });
-            });
+    fn render_tabs(&mut self, ui: &mut Ui) {
+        let Self {
+            state, sessions, ..
+        } = self;
+        ui.horizontal_wrapped(|ui| {
+            ui.selectable_value(&mut state.active_tab, TabType::Home, "Home");
+            for (idx, session) in sessions.iter().enumerate() {
+                ui.selectable_value(
+                    &mut state.active_tab,
+                    TabType::Session(idx),
+                    format!("Session {}", session.get_info().title),
+                );
+            }
+        });
+    }
+
+    fn render_main(&mut self, ui: &mut Ui) {
+        match self.state.active_tab {
+            TabType::Home => HomeView::render_content(ui),
+            TabType::Session(idx) => self.sessions[idx].render_content(ui),
+        }
     }
 }
--- application/apps/indexer/gui/application/src/host/ui/state.rs
+++ application/apps/indexer/gui/application/src/host/ui/state.rs
@@ -1,16 +1,12 @@
-use crate::host::ui::SessionInfo;
-
 #[derive(Debug)]
 pub struct UiState {
     pub active_tab: TabType,
-    pub sessions: Vec<SessionInfo>,
 }
 
 impl Default for UiState {
     fn default() -> Self {
         Self {
             active_tab: TabType::Home,
-            sessions: Vec::new(),
         }
     }
 }
