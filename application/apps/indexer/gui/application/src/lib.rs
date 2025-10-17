be9c911138fda800533824d473072c9c8599f756
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-17 14:29:58 +0200

    Architecture: Split app into two main modules
    * Use two main modules for the app (host and session) and have each
      modules containing its UI, services, data and core modules as
      sub-modules.
    * This change will make it easier in the future to split the two main
      modules into separated crates.

--- application/apps/indexer/gui/application/src/lib.rs
+++ application/apps/indexer/gui/application/src/lib.rs
@@ -1,24 +1,23 @@
 use clap::Parser;
 
 use app::ChipmunkApp;
-use service::app_service::AppService;
+use host::service::HostService;
 
 mod app;
 mod cli;
-mod core;
 mod logging;
-mod service;
-mod state;
-mod ui;
+
+mod host;
+mod session;
 
 pub async fn run_app() -> anyhow::Result<()> {
     let _cli = cli::Cli::parse();
 
     logging::setup()?;
 
-    let (ui_comm, state_comm) = core::communication::init();
+    let (ui_comm, state_comm) = host::communication::init();
 
-    AppService::spawn(state_comm);
+    HostService::spawn(state_comm);
 
     ChipmunkApp::run(ui_comm).map_err(|err| anyhow::anyhow!("{err:?}"))
 }
