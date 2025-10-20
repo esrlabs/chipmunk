fedb7caddd013f590f6875323b1e895e71f6c423
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-20 12:28:09 +0200

    Native UI: Wake up UI on each event.
    Change the mechanism for waking up the UI on events to include egui
    context in all senders and call it explicitly on each send.

--- application/apps/indexer/gui/application/src/session/communication.rs
+++ application/apps/indexer/gui/application/src/session/communication.rs
@@ -26,29 +26,72 @@ pub struct UiReceivers {
 #[derive(Debug)]
 pub struct ServiceHandle {
     pub cmd_rx: mpsc::Receiver<SessionCommand>,
-    pub event_tx: mpsc::Sender<SessionEvent>,
-    pub session_state_tx: watch::Sender<SessionState>,
+    pub senders: ServiceSenders,
+}
+
+/// Provide functions to send event and update session state and waking up
+/// the UI on each change
+#[derive(Debug)]
+pub struct ServiceSenders {
+    event_tx: mpsc::Sender<SessionEvent>,
+    session_state_tx: watch::Sender<SessionState>,
+    egui_ctx: egui::Context,
+}
+
+impl ServiceSenders {
+    /// Send an event to the session UI waking it up.
+    pub async fn send_event(
+        &self,
+        event: SessionEvent,
+    ) -> Result<(), mpsc::error::SendError<SessionEvent>> {
+        self.event_tx.send(event).await?;
+        self.egui_ctx.request_repaint();
+
+        Ok(())
+    }
+
+    /// Modify session state with the provided `modify` function and notify
+    /// the listeners waking up the UI only if modified.
+    pub fn modify_state<F>(&self, modify: F) -> bool
+    where
+        F: FnOnce(&mut SessionState) -> bool,
+    {
+        let modified = self.session_state_tx.send_if_modified(modify);
+        if modified {
+            self.egui_ctx.request_repaint();
+        }
+
+        modified
+    }
 }
 
 /// Initialize communication channels for session application.
-pub fn init(state: SessionState) -> (UiHandle, ServiceHandle) {
+pub fn init(egui_ctx: egui::Context, state: SessionState) -> (UiHandle, ServiceHandle) {
     let (cmd_tx, cmd_rx) = mpsc::channel(CHANNELS_CAPACITY);
     let (event_tx, event_rx) = mpsc::channel(CHANNELS_CAPACITY);
     let (session_state_tx, session_state_rx) = watch::channel(state);
 
-    let senders = UiSenders { cmd_tx };
+    let ui_senders = UiSenders { cmd_tx };
 
-    let receivers = UiReceivers {
+    let ui_receivers = UiReceivers {
         event_rx,
         session_state_rx,
     };
 
-    let ui_comm = UiHandle { senders, receivers };
+    let ui_comm = UiHandle {
+        senders: ui_senders,
+        receivers: ui_receivers,
+    };
+
+    let service_senders = ServiceSenders {
+        event_tx,
+        session_state_tx,
+        egui_ctx,
+    };
 
     let state_comm = ServiceHandle {
         cmd_rx,
-        event_tx,
-        session_state_tx,
+        senders: service_senders,
     };
 
     (ui_comm, state_comm)
