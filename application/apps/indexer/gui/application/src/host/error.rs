3b25c5d4c6479c73404a22f4ab8ad8429708dbb2
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-20 16:28:12 +0200

    Native UI: Add notification support.

--- application/apps/indexer/gui/application/src/host/error.rs
+++ application/apps/indexer/gui/application/src/host/error.rs
@@ -1,3 +1,4 @@
+use stypes::NativeError;
 use thiserror::Error;
 
 use crate::{host::event::HostEvent, session::InitSessionError};
@@ -6,6 +7,12 @@ use crate::{host::event::HostEvent, session::InitSessionError};
 pub enum HostError {
     #[error("Session initialization error: {0}")]
     InitSessionError(#[from] InitSessionError),
+    //TODO AAZ: This can't be sent to the UI. I need to move this form here or
+    //have another struct to be sent to UI.
     #[error("Error while sending app event to UI")]
     SendEvent(HostEvent),
+    //TODO AAZ: Debug print the error for now
+    #[error("Core Error: severity {:?}, kind {:?}, msg {}", 
+        .0.severity, .0.kind, .0.message.as_deref().unwrap_or_default())]
+    NativeError(NativeError),
 }
