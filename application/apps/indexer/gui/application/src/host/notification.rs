bb13ade83e816e536b13d206f4562d60bc5ca8ef
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-21 15:29:45 +0200

    Native UI: Close session & Improve Notifications
    * Wire up close session command adding and ID for each session.
    * Improve notifications adding additional items and cleaning up the
      implementation in UI.

--- application/apps/indexer/gui/application/src/host/notification.rs
+++ application/apps/indexer/gui/application/src/host/notification.rs
@@ -1,7 +1,18 @@
-use crate::host::error::HostError;
+use uuid::Uuid;
+
+use crate::{host::error::HostError, session::error::SessionError};
 
 #[derive(Debug)]
 pub enum AppNotification {
     HostError(HostError),
+    SessionError {
+        session_id: Uuid,
+        error: SessionError,
+    },
+    /// General error notification.
+    Error(String),
+    /// General warning notification.
+    Warning(String),
+    /// General info notification.
     Info(String),
 }
