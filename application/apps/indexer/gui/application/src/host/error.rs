d8f3b5f427be9f2c4bf9cc0ce8d312c563f20e7d
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-21 16:04:33 +0200

    Native UI: Improve handing send errors
    * Log send errors in one function and embedded it in all senders

--- application/apps/indexer/gui/application/src/host/error.rs
+++ application/apps/indexer/gui/application/src/host/error.rs
@@ -1,16 +1,12 @@
 use stypes::NativeError;
 use thiserror::Error;
 
-use crate::{host::event::HostEvent, session::InitSessionError};
+use crate::session::InitSessionError;
 
 #[derive(Debug, Error)]
 pub enum HostError {
     #[error("Session initialization error: {0}")]
     InitSessionError(#[from] InitSessionError),
-    //TODO AAZ: This can't be sent to the UI. I need to move this form here or
-    //have another struct to be sent to UI.
-    #[error("Error while sending app event to UI")]
-    SendEvent(HostEvent),
     #[error("Core Error: {0}")]
     NativeError(NativeError),
 }
