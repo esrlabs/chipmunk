4756f700f30807354450861aef80d5d1c0716ad8
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-21 15:29:45 +0200

    Native UI: Close session & Improve Notifications
    * Wire up close session command adding and ID for each session.
    * Improve notifications adding additional items and cleaning up the
      implementation in UI.

--- application/apps/indexer/gui/application/src/session/service/mod.rs
+++ application/apps/indexer/gui/application/src/session/service/mod.rs
@@ -1,32 +1,73 @@
+use uuid::Uuid;
+
+use crate::host::event::HostEvent;
+use crate::host::notification::AppNotification;
+
 use super::{command::SessionCommand, error::SessionError};
 
 use super::communication::ServiceHandle;
 
 #[derive(Debug)]
 pub struct SessionService {
+    session_id: Uuid,
     communication: ServiceHandle,
 }
 
 impl SessionService {
-    pub fn spwan(communication: ServiceHandle) {
-        let session = Self { communication };
+    /// Spawn session service returning the session ID.
+    pub fn spwan(communication: ServiceHandle) -> Uuid {
+        let session_id = Uuid::new_v4();
+        let session = Self {
+            session_id,
+            communication,
+        };
 
         tokio::spawn(async move {
             session.run().await;
         });
+
+        session_id
     }
 
     async fn run(mut self) {
+        log::trace!("Start Session Service {}", self.session_id);
+
         while let Some(cmd) = self.communication.cmd_rx.recv().await {
-            if let Err(err) = self.handle_command(cmd).await {
-                //TODO AAZ: Better error handling.
-                log::error!("Error while handling session commands: {err:?}");
+            if let Err(error) = self.handle_command(cmd).await {
+                log::error!("Error while handling session commands: {error:?}");
+
+                let notifi = AppNotification::SessionError {
+                    session_id: self.session_id,
+                    error,
+                };
+
+                self.communication
+                    .senders
+                    .send_notification(notifi)
+                    .await
+                    .inspect_err(|err| {
+                        log::error!("Communication Error. {err}");
+                    })
+                    .ok();
             }
         }
+
+        log::trace!("Session Service {} has been dropped", self.session_id);
     }
 
     async fn handle_command(&mut self, cmd: SessionCommand) -> Result<(), SessionError> {
-        match cmd {}
+        match cmd {
+            SessionCommand::CloseSession => {
+                self.communication
+                    .senders
+                    .send_host_event(HostEvent::CloseSession {
+                        session_id: self.session_id,
+                    })
+                    .await
+                    .inspect_err(|err| log::error!("Communication Error: {err}"))
+                    .ok();
+            }
+        }
 
         Ok(())
     }
