e8b8dd3f77e053ca4227d05a6c29f7cee69dc9ca
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-29 11:44:33 +0100

    Integrating core session in native UI with text files (WIP)
    Building system to display logs in table (WIP): We load all logs
    without cleaning up the logs aren't needed.

--- application/apps/indexer/gui/application/src/session/data/mod.rs
+++ application/apps/indexer/gui/application/src/session/data/mod.rs
@@ -1,3 +1,5 @@
+use stypes::GrabbedElement;
+
 #[derive(Debug, Default)]
 pub struct SessionState {
     pub main_table: MainTable,
@@ -7,6 +9,13 @@ pub struct SessionState {
 #[derive(Debug, Default)]
 pub struct MainTable {
     /// The index of first line in logs window
-    pub idx_offset: usize,
+    pub idx_offset: u64,
     pub logs_window: Vec<String>,
+}
+
+impl MainTable {
+    pub fn append(&mut self, items: Vec<GrabbedElement>) {
+        self.logs_window
+            .extend(items.into_iter().map(|e| e.content));
+    }
 }
