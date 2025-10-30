07bd6392bb5fbd8e4c9b8906599ed3e3f03ef78d
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-30 16:39:10 +0100

    Logs Viewer with map & Request UI discard on fetch
    * Implement showing a window of logs simply using a map with indexes
      avoiding the complexity of keeping track on the existing logs.
    * Request discard rendering frame when new logs needed to be fetched to
      avoid rendering the rows where we don't have data.

--- application/apps/indexer/gui/application/src/session/data/mod.rs
+++ application/apps/indexer/gui/application/src/session/data/mod.rs
@@ -1,9 +1,10 @@
-use crate::session::data::logs_sliding_window::LogsSlidingWindow;
+use crate::session::data::logs_mapped::LogsMapped;
 
+mod logs_mapped;
 mod logs_sliding_window;
 
 #[derive(Debug, Default)]
 pub struct SessionState {
-    pub main_table: LogsSlidingWindow,
+    pub main_table: LogsMapped,
     pub logs_count: u64,
 }
--- application/apps/indexer/gui/application/src/session/ui/logs_table/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/logs_table/mod.rs
@@ -1,5 +1,7 @@
+use std::ops::Range;
+
 use egui::{Frame, Id, Margin, Ui};
-use egui_table::{AutoSizeMode, CellInfo, Column, HeaderCellInfo, TableDelegate};
+use egui_table::{AutoSizeMode, CellInfo, Column, HeaderCellInfo, PrefetchInfo, TableDelegate};
 use processor::grabber::LineRange;
 
 use crate::{
@@ -7,10 +9,16 @@ use crate::{
     session::{command::SessionCommand, communication::UiSenders, data::SessionState},
 };
 
-pub struct LogsTable;
+const LOGS_WINDOW_OFFSET: u64 = 5;
+
+#[derive(Debug, Default)]
+pub struct LogsTable {
+    last_visible_rows: Range<u64>,
+}
 
 impl LogsTable {
     pub fn render_content(
+        &mut self,
         data: &SessionState,
         senders: &UiSenders,
         ui: &mut Ui,
@@ -28,18 +36,28 @@ impl LogsTable {
         let mut delegate = LogsDelegate {
             session_data: data,
             to_fetch: None,
+            last_visible_rows: &mut self.last_visible_rows,
         };
 
         table.show(ui, &mut delegate);
 
-        if let Some(to_fetch) = delegate.to_fetch {
+        if let Some(to_fetch) = delegate.to_fetch.take() {
             let cmd = SessionCommand::GrabLines(to_fetch);
-            //TODO AAZ: Handle errors.
             if senders.cmd_tx.try_send(cmd).is_err() {
-                println!("*** Sending grab commands too fast ***");
+                log::warn!("Logs viewer is sending grab commands too fast for channel capacity");
             };
 
-            //TODO AAZ: Add UI action to wait until state is changed.
+            // Request discard this frame and start a new one with the fetched data.
+            //
+            // NOTE: We need to request discard when users are dragging the scrollbar.
+            // Avoiding this extra checks cause errors while resizing the application
+            // main window.
+            //
+            // TODO AAZ: This is an extra check that should be not needed.
+            // && (ui.ctx().is_pointer_over_area() || ui.ctx().is_using_pointer())
+            if !ui.ctx().will_discard() && ui.ctx().is_using_pointer() {
+                ui.ctx().request_discard("Fetching new rows");
+            }
         }
     }
 
@@ -54,15 +72,25 @@ impl LogsTable {
 struct LogsDelegate<'a> {
     session_data: &'a SessionState,
     to_fetch: Option<LineRange>,
+    last_visible_rows: &'a mut Range<u64>,
 }
 
 impl TableDelegate for LogsDelegate<'_> {
-    fn prepare(&mut self, info: &egui_table::PrefetchInfo) {
-        self.to_fetch = self
-            .session_data
-            .main_table
-            .check_fetch(&info.visible_rows, self.session_data.logs_count as usize)
-            .map(LineRange::from);
+    fn prepare(&mut self, info: &PrefetchInfo) {
+        // Check if we need the columns has changed from last frame
+        // and set the needed range to fetch if so.
+        let PrefetchInfo { visible_rows, .. } = info;
+        self.to_fetch = if self.last_visible_rows == visible_rows {
+            None
+        } else {
+            *self.last_visible_rows = info.visible_rows.to_owned();
+
+            let start = visible_rows.start.saturating_sub(LOGS_WINDOW_OFFSET);
+            let end = (visible_rows.end + LOGS_WINDOW_OFFSET).min(self.session_data.logs_count);
+
+            let rng = LineRange::from(start..=end.saturating_sub(1));
+            Some(rng)
+        }
     }
 
     fn header_cell_ui(&mut self, ui: &mut Ui, cell: &HeaderCellInfo) {
@@ -81,33 +109,30 @@ impl TableDelegate for LogsDelegate<'_> {
     fn cell_ui(&mut self, ui: &mut egui::Ui, cell: &egui_table::CellInfo) {
         let &CellInfo { col_nr, row_nr, .. } = cell;
 
-        if row_nr % 2 == 1 {
-            ui.painter()
-                .rect_filled(ui.max_rect(), 0.0, ui.visuals().faint_bg_color);
-        }
-
         Frame::NONE
             .inner_margin(Margin::symmetric(4, 0))
-            .show(ui, |ui| {
-                if col_nr == 0 {
+            .show(ui, |ui| match col_nr {
+                0 => {
                     ui.label(format!("{}", row_nr));
-                } else {
-                    let content = self
-                        .session_data
-                        .main_table
-                        .get_log(row_nr)
-                        .unwrap_or("Loading...");
+                }
+                1 => {
+                    let content =
+                        self.session_data
+                            .main_table
+                            .get_log(row_nr)
+                            .unwrap_or_else(|| {
+                                // Avoid showing place holder text in this frame since it will be
+                                // discarded to fetch new data.
+                                if self.to_fetch.is_some() {
+                                    ""
+                                } else {
+                                    "Loading..."
+                                }
+                            });
 
                     ui.label(content);
                 }
+                invalid => panic!("Invalid column number. {invalid}"),
             });
-    }
-
-    fn row_top_offset(&self, _ctx: &egui::Context, _table_id: egui::Id, row_nr: u64) -> f32 {
-        row_nr as f32 * self.default_row_height()
-    }
-
-    fn default_row_height(&self) -> f32 {
-        20.0
     }
 }
