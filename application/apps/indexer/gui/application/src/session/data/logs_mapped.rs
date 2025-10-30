3fdf682b33ee647b0d9914f91d62283ae6525d81
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-30 16:39:10 +0100

    Logs Viewer with map & Request UI discard on fetch
    * Implement showing a window of logs simply using a map with indexes
      avoiding the complexity of keeping track on the existing logs.
    * Request discard rendering frame when new logs needed to be fetched to
      avoid rendering the rows where we don't have data.

---
+++ application/apps/indexer/gui/application/src/session/data/logs_mapped.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+use stypes::GrabbedElement;
+
+/// Simple implementation for displaying a window of the logs to be used
+/// inside the logs viewer.
+/// This implementation keeps the logs inside a map with their positions as
+/// the keys.
+/// Once new elements arrive it will replace all exiting logs with
+/// the new ones blindly.
+#[derive(Debug, Default)]
+pub struct LogsMapped {
+    logs: HashMap<u64, String>,
+}
+
+impl LogsMapped {
+    pub fn append(&mut self, items: Vec<GrabbedElement>) {
+        self.logs.clear();
+
+        items.into_iter().for_each(|item| {
+            self.logs.insert(item.pos as u64, item.content);
+        });
+    }
+
+    pub fn get_log(&self, row_idx: u64) -> Option<&str> {
+        self.logs.get(&row_idx).map(|s| s.as_str())
+    }
+}
