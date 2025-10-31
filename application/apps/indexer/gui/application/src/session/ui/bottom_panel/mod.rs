5550a09aca37424708ffaa633dde2904d421a926
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-31 12:55:29 +0100

    Build search input control
    * Input controls follow the functionality and look if the current one.
    * It provides extra functionality for handling Ctrl+Backspace.

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/mod.rs
@@ -1,4 +1,4 @@
-use egui::Ui;
+use egui::{Frame, Margin, Ui};
 use state::{BottomTabType, BottomUiState};
 
 use crate::session::{communication::UiSenders, data::SessionState};
@@ -26,6 +26,7 @@ pub struct BottomPanelUI {
 impl BottomPanelUI {
     pub fn render_content(&mut self, data: &SessionState, senders: &UiSenders, ui: &mut Ui) {
         self.render_tabs(ui);
+
         match self.state.active_tab {
             BottomTabType::Search => self.search.render_content(data, senders, ui),
             BottomTabType::Details => self.details.render_content(data, senders, ui),
@@ -35,10 +36,14 @@ impl BottomPanelUI {
     }
 
     fn render_tabs(&mut self, ui: &mut Ui) {
-        ui.horizontal_wrapped(|ui| {
-            for tab in BottomTabType::all() {
-                ui.selectable_value(&mut self.state.active_tab, *tab, tab.to_string());
-            }
-        });
+        Frame::NONE
+            .inner_margin(Margin::symmetric(0, 2))
+            .show(ui, |ui| {
+                ui.horizontal_wrapped(|ui| {
+                    for tab in BottomTabType::all() {
+                        ui.selectable_value(&mut self.state.active_tab, *tab, tab.to_string());
+                    }
+                });
+            });
     }
 }
