a987ddbfcacab0e5d485180c27f7b12d59938784
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-10-31 12:55:29 +0100

    Build search input control
    * Input controls follow the functionality and look if the current one.
    * It provides extra functionality for handling Ctrl+Backspace.

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/search/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/search/mod.rs
@@ -2,11 +2,18 @@ use egui::Ui;
 
 use crate::session::{communication::UiSenders, data::SessionState};
 
+use search_bar::SearchBar;
+
+mod search_bar;
+
 #[derive(Debug, Default)]
-pub struct SearchUI;
+pub struct SearchUI {
+    bar: SearchBar,
+}
 
 impl SearchUI {
     pub fn render_content(&mut self, data: &SessionState, senders: &UiSenders, ui: &mut Ui) {
+        self.bar.render_content(senders, ui);
         ui.centered_and_justified(|ui| {
             ui.heading("Search UI");
         });
