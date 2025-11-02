56c7130eb351cde946903428754ee8e7b330c094
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-02 12:25:18 +0100

    Native UI: Commands for filter & Debug prints

--- application/apps/indexer/gui/application/src/session/command.rs
+++ application/apps/indexer/gui/application/src/session/command.rs
@@ -1,8 +1,10 @@
-use processor::grabber::LineRange;
+use processor::{grabber::LineRange, search::filter::SearchFilter};
 
 /// Represents session specific commands to be sent from UI to State.
 #[derive(Debug, Clone)]
 pub enum SessionCommand {
     GrabLines(LineRange),
+    ApplySearchFilter(Vec<SearchFilter>),
+    DropSearch,
     CloseSession,
 }
