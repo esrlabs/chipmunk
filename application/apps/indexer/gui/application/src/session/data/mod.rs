6c342e2454e0e3dc83c02ae6e217b63957a94a8f
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-03 20:13:59 +0100

    Wire filter callbacks & Display search status & Cancelling
    * Wire filter callback to keeping filter info in session data state.
    * Implement search status in search bar
    * Introduce entity to keep track for long running process.
    * Cancel search operation safely.
    * Cancel running operations on session closing.
    * TODOs and notes

--- application/apps/indexer/gui/application/src/session/data/mod.rs
+++ application/apps/indexer/gui/application/src/session/data/mod.rs
@@ -2,9 +2,13 @@ use crate::session::data::logs_mapped::LogsMapped;
 
 mod logs_mapped;
 mod logs_sliding_window;
+mod search;
+
+pub use search::{FilterIndex, SearchData};
 
 #[derive(Debug, Default)]
 pub struct SessionState {
     pub main_table: LogsMapped,
     pub logs_count: u64,
+    pub search: SearchData,
 }
