0aee31dbce7933d619727325631da4e55203e60e
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-04 14:23:21 +0100

    Table for search results
    * Implement table for search results adding the needed structs and
      commands to UI, data state and the services.

--- application/apps/indexer/gui/application/src/session/command.rs
+++ application/apps/indexer/gui/application/src/session/command.rs
@@ -1,9 +1,12 @@
+use std::ops::RangeInclusive;
+
 use processor::{grabber::LineRange, search::filter::SearchFilter};
 
 /// Represents session specific commands to be sent from UI to State.
 #[derive(Debug, Clone)]
 pub enum SessionCommand {
     GrabLines(LineRange),
+    GrabIndexedLines(RangeInclusive<u64>),
     ApplySearchFilter(Vec<SearchFilter>),
     DropSearch,
     CloseSession,
