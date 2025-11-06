56869823d0e5db5a527d4b3e25190e6833ef6ed2
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-06 16:46:16 +0100

    Change communication to fit grabbing into tables
    * Introduce new commands for blocking tasks which can be called in
      blocking UI rendering thread.
    * Session service has spawn two tasks now. One for async commands and
      the other for blocking commands to avoid dead-locks
    * Current logs window is part of the UI itself and not part of the data
      state.
    * Tell tokio runtime that the main task will block so it can's schedule
      any tasks on that thread. This enables us to use blocking function on
      tokio channels inside the tables.

--- application/apps/indexer/gui/application/src/session/communication.rs
+++ application/apps/indexer/gui/application/src/session/communication.rs
@@ -3,7 +3,11 @@ use tokio::sync::{mpsc, watch};
 use crate::{
     comm_utls::evaluate_send_res,
     host::{event::HostEvent, notification::AppNotification},
-    session::{command::SessionCommand, data::SessionState, event::SessionEvent},
+    session::{
+        command::{SessionBlockingCommand, SessionCommand},
+        data::SessionState,
+        event::SessionEvent,
+    },
 };
 
 const CHANNELS_CAPACITY: usize = 32;
@@ -41,6 +45,7 @@ pub struct UiHandle {
 #[derive(Debug)]
 pub struct UiSenders {
     pub cmd_tx: mpsc::Sender<SessionCommand>,
+    pub block_cmd_tx: mpsc::Sender<SessionBlockingCommand>,
 }
 
 #[derive(Debug)]
@@ -54,6 +59,7 @@ pub struct UiReceivers {
 pub struct ServiceHandle {
     pub cmd_rx: mpsc::Receiver<SessionCommand>,
     pub senders: ServiceSenders,
+    pub block_communication: ServiceBlockCommuniaction,
 }
 
 /// Provide functions to send event and update session state and waking up
@@ -116,6 +122,24 @@ impl ServiceSenders {
     }
 }
 
+/// Communication elements to be used in session service in the task
+/// which will handle blocking commands sent from UI.
+#[derive(Debug)]
+pub struct ServiceBlockCommuniaction {
+    pub block_cmd_rx: mpsc::Receiver<SessionBlockingCommand>,
+    notification_tx: mpsc::Sender<AppNotification>,
+    egui_ctx: egui::Context,
+}
+
+impl ServiceBlockCommuniaction {
+    pub async fn send_notification(&self, notifi: AppNotification) -> bool {
+        let res = self.notification_tx.send(notifi).await;
+
+        evaluate_send_res(&self.egui_ctx, res)
+    }
+}
+
+/// Communication types and functions needed for sessions.
 #[derive(Debug)]
 pub struct SessionCommunication {
     pub ui_handle: UiHandle,
@@ -125,10 +149,14 @@ pub struct SessionCommunication {
 /// Initialize communication channels for session application.
 pub fn init(shared_senders: SharedSenders) -> (UiHandle, ServiceHandle) {
     let (cmd_tx, cmd_rx) = mpsc::channel(CHANNELS_CAPACITY);
+    let (block_cmd_tx, block_cmd_rx) = mpsc::channel(CHANNELS_CAPACITY);
     let (session_event_tx, session_event_rx) = mpsc::channel(CHANNELS_CAPACITY);
     let (session_state_tx, session_state_rx) = watch::channel(SessionState::default());
 
-    let ui_senders = UiSenders { cmd_tx };
+    let ui_senders = UiSenders {
+        cmd_tx,
+        block_cmd_tx,
+    };
 
     let ui_receivers = UiReceivers {
         event_rx: session_event_rx,
@@ -149,13 +177,20 @@ pub fn init(shared_senders: SharedSenders) -> (UiHandle, ServiceHandle) {
     let service_senders = ServiceSenders {
         session_event_tx,
         host_event_tx,
-        notification_tx,
+        notification_tx: notification_tx.clone(),
         session_state_tx,
+        egui_ctx: egui_ctx.clone(),
+    };
+
+    let block_communication = ServiceBlockCommuniaction {
+        block_cmd_rx,
+        notification_tx,
         egui_ctx,
     };
 
     let state_comm = ServiceHandle {
         cmd_rx,
+        block_communication,
         senders: service_senders,
     };
 
