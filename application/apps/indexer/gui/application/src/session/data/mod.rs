fa6bb31409998164494ad1c24ba5523eddfd2202
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-06 16:46:16 +0100

    Change communication to fit grabbing into tables
    * Introduce new commands for blocking tasks which can be called in
      blocking UI rendering thread.
    * Session service has spawn two tasks now. One for async commands and
      the other for blocking commands to avoid dead-locks
    * Current logs window is part of the UI itself and not part of the data
      state.
    * Tell tokio runtime that the main task will block so it can's schedule
      any tasks on that thread. This enables us to use blocking function on
      tokio channels inside the tables.

--- application/apps/indexer/gui/application/src/session/data/mod.rs
+++ application/apps/indexer/gui/application/src/session/data/mod.rs
@@ -1,16 +1,10 @@
-use crate::session::data::logs_mapped::LogsMapped;
-
-mod indexed_mapped;
-mod logs_mapped;
 mod logs_sliding_window;
 mod search;
 
-pub use indexed_mapped::SearchTableIndex;
-pub use search::{FilterIndex, LogMainIndex, SearchData};
+pub use search::{LogMainIndex, SearchData};
 
 #[derive(Debug, Default)]
 pub struct SessionState {
-    pub main_table: LogsMapped,
     pub logs_count: u64,
     pub search: SearchData,
 }
