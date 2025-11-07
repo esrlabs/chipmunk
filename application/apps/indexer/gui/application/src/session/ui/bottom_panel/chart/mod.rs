5f67f79fde6f1cd9bbf8541a8ac9396b0086fbb4
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-07 17:06:32 +0100

    Select log with mouse & Scroll into it in both tables
    * Implement selecting a log entry with the mouse in both main and search
      tables with implementation to scroll to the selected log or to the
      nearest position of that log in case of scrolling in search table.

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/chart/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/chart/mod.rs
@@ -1,12 +1,12 @@
 use egui::Ui;
 
-use crate::session::{communication::UiSenders, data::SessionState};
+use crate::session::{communication::UiSenders, data::SessionDataState};
 
 #[derive(Debug, Default)]
 pub struct ChartUI;
 
 impl ChartUI {
-    pub fn render_content(&mut self, data: &SessionState, senders: &UiSenders, ui: &mut Ui) {
+    pub fn render_content(&mut self, data: &SessionDataState, senders: &UiSenders, ui: &mut Ui) {
         ui.centered_and_justified(|ui| {
             ui.heading("Charts UI");
         });
