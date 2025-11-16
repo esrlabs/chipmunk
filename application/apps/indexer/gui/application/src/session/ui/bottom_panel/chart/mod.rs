d59a78f2689eeb7650358b129fdb2dd5e133d7ff
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-16 22:19:14 +0100

    Charts UI and commands & Refactoring
    * Add charts to UI using egui_plot crate.
    * Commands to get charts values + Extend session data with the needed
      data for charts.
    * Load charts data from chipmunk into charts producing accurate charts
      as in current master.
    * Add session ID to session data because it's needed on multiple places
      as ID for UI elements.
    * Keep session ID saved on one place in session service

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/chart/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/chart/mod.rs
@@ -1,14 +1,108 @@
-use egui::Ui;
+use egui::{Color32, Direction, Label, Layout, Ui, Widget};
+use egui_plot::{Bar, BarChart, Legend, Plot};
 
-use crate::session::{communication::UiSenders, data::SessionDataState};
+use crate::{
+    host::ui::UiActions,
+    session::{command::SessionCommand, communication::UiSenders, data::SessionDataState},
+};
 
 #[derive(Debug, Default)]
-pub struct ChartUI;
+pub struct ChartUI {
+    requested_data: bool,
+}
 
 impl ChartUI {
-    pub fn render_content(&mut self, data: &SessionDataState, senders: &UiSenders, ui: &mut Ui) {
-        ui.centered_and_justified(|ui| {
-            ui.heading("Charts UI");
-        });
+    pub fn render_content(
+        &mut self,
+        data: &SessionDataState,
+        senders: &UiSenders,
+        actions: &mut UiActions,
+        ui: &mut Ui,
+    ) {
+        if data.search.is_search_active() {
+            self.chart(data, senders, actions, ui);
+        } else {
+            self.clear();
+            Self::place_holder(ui);
+        }
+    }
+
+    fn chart(
+        &mut self,
+        data: &SessionDataState,
+        senders: &UiSenders,
+        actions: &mut UiActions,
+        ui: &mut Ui,
+    ) {
+        if !self.requested_data {
+            // Taken from current Chipmunk: Matches are divided by 2
+            let dataset_len = (ui.available_width() / 2.0) as u16;
+            //TODO AAZ: Check if range is needed.
+            // let rng = Some(0..=data.logs_count);
+            let rng = None;
+            let chart_cmd = SessionCommand::GetChartMap {
+                dataset_len,
+                range: rng.clone(),
+            };
+
+            let chart_sent = actions.try_send_command(&senders.cmd_tx, chart_cmd);
+
+            let values_cmd = SessionCommand::GetChartValues {
+                dataset_len,
+                range: rng,
+            };
+            let values_sent = actions.try_send_command(&senders.cmd_tx, values_cmd);
+
+            self.requested_data = chart_sent && values_sent;
+        }
+
+        // Ratio describes how many logs will be represented in one unit of
+        // axe x on charts.
+        let ratio = data.logs_count as f64 / data.charts.bars.len() as f64;
+
+        let chart = BarChart::new(
+            "TODO: Search Name",
+            data.charts
+                .bars
+                .iter()
+                .enumerate()
+                .map(|(idx, bar)| {
+                    Bar::new(
+                        idx as f64 * ratio,
+                        bar.first().map(|a| a.matches_count).unwrap_or_default() as f64,
+                    )
+                    .width(ratio)
+                })
+                .collect(),
+        )
+        .color(Color32::LIGHT_BLUE);
+
+        Plot::new(data.session_id)
+            .legend(Legend::default())
+            .clamp_grid(false)
+            .show(ui, |plot_ui| plot_ui.bar_chart(chart));
+    }
+
+    fn place_holder(ui: &mut Ui) {
+        ui.with_layout(
+            Layout::centered_and_justified(Direction::TopDown).with_cross_justify(false),
+            |ui| {
+                ui.set_max_width(290.);
+
+                Label::new(
+                    "As soon as some filter will be created, \
+                the frequency of matching will be shown. \
+                Also as soon as some chart term will be created, \
+                the chart will be rendered.",
+                )
+                .halign(egui::Align::Min)
+                .selectable(false)
+                .ui(ui);
+            },
+        );
+    }
+
+    fn clear(&mut self) {
+        self.requested_data = false;
     }
 }
