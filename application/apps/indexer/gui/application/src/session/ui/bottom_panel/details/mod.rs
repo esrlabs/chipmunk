de1a08209f1d1c202491cef4932809ead5279924
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-19 21:25:03 +0100

    Refactor: Clone command senders instead of passing references.
    * Tokio Senders are cheap to clone so we took this trade off for cleaner
      APIs.
    * Other small refactoring removing not needed functions and using
      optional range in main table.

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/details/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/details/mod.rs
@@ -1,10 +1,6 @@
 use egui::{Label, RichText, Ui, Widget};
-use processor::grabber::LineRange;
-use tokio::sync::oneshot;
 
-use crate::session::{
-    command::SessionBlockingCommand, communication::UiSenders, data::SessionDataState,
-};
+use crate::session::data::SessionDataState;
 
 #[derive(Debug, Default)]
 pub struct DetailsUI {}
@@ -25,24 +21,5 @@ impl DetailsUI {
 
         let content = RichText::new(&log.content).strong();
         Label::new(content).ui(ui);
-    }
-
-    fn load_content(pos: u64, senders: &UiSenders) -> Option<String> {
-        let (elems_tx, elems_rx) = oneshot::channel();
-        let cmd = SessionBlockingCommand::GrabLines {
-            range: LineRange::from(pos..=pos),
-            sender: elems_tx,
-        };
-
-        if senders.block_cmd_tx.blocking_send(cmd).is_err() {
-            log::warn!("Communication error while sending grab command.");
-            return None;
-        };
-
-        elems_rx
-            .blocking_recv()
-            .ok()
-            .and_then(|items| items.into_iter().next())
-            .map(|m| m.content)
     }
 }
