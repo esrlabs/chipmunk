504a62365aa2c38651cf43b08c92892e9fe58739
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-19 21:25:03 +0100

    Refactor: Clone command senders instead of passing references.
    * Tokio Senders are cheap to clone so we took this trade off for cleaner
      APIs.
    * Other small refactoring removing not needed functions and using
      optional range in main table.

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/search/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/search/mod.rs
@@ -1,8 +1,14 @@
+use tokio::sync::mpsc::Sender;
+
 use egui::Ui;
 
 use crate::{
     host::ui::UiActions,
-    session::{communication::UiSenders, data::SessionDataState, ui::state::SessionUiState},
+    session::{
+        command::{SessionBlockingCommand, SessionCommand},
+        data::SessionDataState,
+        ui::state::SessionUiState,
+    },
 };
 
 use search_bar::SearchBar;
@@ -12,30 +18,37 @@ mod indexed_mapped;
 mod search_bar;
 mod search_table;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct SearchUI {
     bar: SearchBar,
     table: SearchTable,
 }
 
 impl SearchUI {
+    pub fn new(
+        cmd_tx: Sender<SessionCommand>,
+        block_cmd_tx: Sender<SessionBlockingCommand>,
+    ) -> Self {
+        Self {
+            bar: SearchBar::new(cmd_tx.clone()),
+            table: SearchTable::new(cmd_tx, block_cmd_tx),
+        }
+    }
     pub fn render_content(
         &mut self,
         data: &SessionDataState,
         ui_state: &mut SessionUiState,
         actions: &mut UiActions,
-        senders: &UiSenders,
         ui: &mut Ui,
     ) {
-        self.bar.render_content(data, senders, actions, ui);
+        self.bar.render_content(data, actions, ui);
 
         if data.search.is_search_active() {
             // We need to give a unique id for the direct parent of each table because
             // they will be used as identifiers for table state to avoid ID clashes between
             // tables from different tabs (different sessions).
             ui.push_id(data.session_id, |ui| {
-                self.table
-                    .render_content(data, ui_state, senders, actions, ui);
+                self.table.render_content(data, ui_state, actions, ui);
             });
         } else {
             self.table.clear();
