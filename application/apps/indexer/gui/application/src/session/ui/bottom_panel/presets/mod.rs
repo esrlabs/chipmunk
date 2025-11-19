77bc032381d1fc364c6e0a7b5dcad74606aacc24
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-19 21:25:03 +0100

    Refactor: Clone command senders instead of passing references.
    * Tokio Senders are cheap to clone so we took this trade off for cleaner
      APIs.
    * Other small refactoring removing not needed functions and using
      optional range in main table.

--- application/apps/indexer/gui/application/src/session/ui/bottom_panel/presets/mod.rs
+++ application/apps/indexer/gui/application/src/session/ui/bottom_panel/presets/mod.rs
@@ -1,12 +1,10 @@
 use egui::Ui;
 
-use crate::session::{communication::UiSenders, data::SessionDataState};
-
 #[derive(Debug, Default)]
 pub struct PresetsUI;
 
 impl PresetsUI {
-    pub fn render_content(&mut self, data: &SessionDataState, senders: &UiSenders, ui: &mut Ui) {
+    pub fn render_content(&mut self, ui: &mut Ui) {
         ui.centered_and_justified(|ui| {
             ui.heading("Presets UI");
         });
