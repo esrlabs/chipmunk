861ce10eada6d71eff8d1a294e5d7d1125c9d09e
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-20 11:51:24 +0100

    Change communications to Command & Callback
    * Move data state to be a part from the UI to avoid having its logic
      shared between UI and service and to avoid any potential dead-locks
      and to improve performance because services shouldn't wait to acquire
      lock to modify the data state.
    
    Host (Done):
    * Remove data state form host.
    * Rename event to message
    
    Session:
    * Rename event to message.

--- application/apps/indexer/gui/application/src/host/communication.rs
+++ application/apps/indexer/gui/application/src/host/communication.rs
@@ -1,10 +1,8 @@
-use tokio::sync::{mpsc, watch};
+use tokio::sync::mpsc;
 
 use crate::{
     comm_utls::evaluate_send_res,
-    host::{
-        command::HostCommand, data::HostState, event::HostEvent, notification::AppNotification,
-    },
+    host::{command::HostCommand, message::HostMessage, notification::AppNotification},
     session::communication::SharedSenders,
 };
 
@@ -24,9 +22,8 @@ pub struct UiSenders {
 
 #[derive(Debug)]
 pub struct UiReceivers {
-    pub event_rx: mpsc::Receiver<HostEvent>,
+    pub message_rx: mpsc::Receiver<HostMessage>,
     pub notification_rx: mpsc::Receiver<AppNotification>,
-    pub host_state_rx: watch::Receiver<HostState>,
 }
 
 /// Contains communication channels for the services to communicate with UI.
@@ -36,40 +33,24 @@ pub struct ServiceHandle {
     pub senders: ServiceSenders,
 }
 
-/// Provide functions to send event and update host state and waking up
-/// the UI on each change
+/// Provide functions to send host messages and waking up the UI on them.
 #[derive(Debug)]
 pub struct ServiceSenders {
-    event_tx: mpsc::Sender<HostEvent>,
+    message_tx: mpsc::Sender<HostMessage>,
     notification_tx: mpsc::Sender<AppNotification>,
-    host_state_tx: watch::Sender<HostState>,
     egui_ctx: egui::Context,
 }
 
 impl ServiceSenders {
-    /// Send an event to the host UI and waking it up.
+    /// Send a message to the host UI and waking it up.
     ///
     /// # Return
-    /// Returns `true` if the event is sent successfully. On send errors
+    /// Returns `true` if the message is sent successfully. On send errors
     /// it will log the error and return `false`.
-    pub async fn send_event(&self, event: HostEvent) -> bool {
-        let res = self.event_tx.send(event).await;
+    pub async fn send_message(&self, message: HostMessage) -> bool {
+        let res = self.message_tx.send(message).await;
 
         evaluate_send_res(&self.egui_ctx, res)
-    }
-
-    /// Modify host state with the provided `modify` function and notify
-    /// the listeners waking up the UI only if modified.
-    pub fn modify_state<F>(&self, modify: F) -> bool
-    where
-        F: FnOnce(&mut HostState) -> bool,
-    {
-        let modified = self.host_state_tx.send_if_modified(modify);
-        if modified {
-            self.egui_ctx.request_repaint();
-        }
-
-        modified
     }
 
     /// Send notification to host and waking up UI.
@@ -85,7 +66,7 @@ impl ServiceSenders {
     /// Create [`SharedSenders`] by cloning the needed internal channels.
     pub fn get_shared_senders(&self) -> SharedSenders {
         SharedSenders::new(
-            self.event_tx.clone(),
+            self.message_tx.clone(),
             self.notification_tx.clone(),
             self.egui_ctx.clone(),
         )
@@ -93,19 +74,16 @@ impl ServiceSenders {
 }
 
 /// Initialize communication channels for host application.
-pub fn init(egui_ctx: egui::Context, state: HostState) -> (UiHandle, ServiceHandle) {
+pub fn init(egui_ctx: egui::Context) -> (UiHandle, ServiceHandle) {
     let (cmd_tx, cmd_rx) = mpsc::channel(CHANNELS_CAPACITY);
-    let (event_tx, event_rx) = mpsc::channel(CHANNELS_CAPACITY);
+    let (message_tx, message_rx) = mpsc::channel(CHANNELS_CAPACITY);
     let (notification_tx, notification_rx) = mpsc::channel(CHANNELS_CAPACITY);
-
-    let (host_state_tx, host_state_rx) = watch::channel(state);
 
     let ui_senders = UiSenders { cmd_tx };
 
     let ui_receivers = UiReceivers {
-        event_rx,
+        message_rx,
         notification_rx,
-        host_state_rx,
     };
 
     let ui_handle = UiHandle {
@@ -114,9 +92,8 @@ pub fn init(egui_ctx: egui::Context, state: HostState) -> (UiHandle, ServiceHand
     };
 
     let service_senders = ServiceSenders {
-        event_tx,
+        message_tx,
         notification_tx,
-        host_state_tx,
         egui_ctx,
     };
 
