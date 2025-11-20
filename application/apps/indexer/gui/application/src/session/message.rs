a9e7793a6f6bf95fcb9da33d6f96c9141463fcc1
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-11-20 17:10:10 +0100

    Native UI: Rework communications in sessions
    * This commit targets removing whole shared session data watch channels
      keeping the whole logic in UI and replacing watch channels with
      command/message mpsc channels between session UI and service.
    * Merge session UI state and shared data in one struct called
      SessionShared to pass via reference to UI components in rendering
      routine
    * Session Service send the results back to UI where changes will be
      applied and errors will be handled.
    * Renaming & Refactoring & Removing unused items.

--- application/apps/indexer/gui/application/src/session/message.rs
+++ application/apps/indexer/gui/application/src/session/message.rs
@@ -1,8 +1,32 @@
-use stypes::NearestPosition;
+use stypes::{FilterMatch, GrabbedElement, NearestPosition};
+
+use crate::session::{error::SessionError, ui::chart::ChartBar};
 
 /// Messages sent to Session UI form services.
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum SessionMessage {
-    /// Nearest position jump to in the search table.
-    NearestPosition(NearestPosition),
+    /// Update to the total count of logs (stream/file).
+    LogsCount(u64),
+
+    /// Result from fetching a specific log line.
+    SelectedLog(Result<GrabbedElement, SessionError>),
+
+    // --- Search ---
+    //
+    /// Metadata about the current search (e.g., total found).
+    SearchState { found_count: u64 },
+
+    /// Search matches found.
+    SearchResults(Vec<FilterMatch>),
+
+    /// The nearest log index to jump to in search table.
+    NearestPosition(Result<Option<NearestPosition>, SessionError>),
+
+    // --- Charts ---
+    //
+    /// Bar data for histograms.
+    ChartHistogram(Result<Vec<Vec<ChartBar>>, SessionError>),
+
+    /// Point data for line charts.
+    ChartLinePlots(Result<Vec<(u8, Vec<stypes::Point>)>, SessionError>),
 }
