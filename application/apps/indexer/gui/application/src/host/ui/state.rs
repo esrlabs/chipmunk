5990e1892476e231f67aff05776cd62d2c1e48a9
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-12-03 11:08:10 +0100

    Change host tabs management & Session Setup Module
    * Change tabs management to have all tabs with one vector while having
      the sessions in a map. This is needed to include all tabs in one
      collection so we can add new tab types.
    * Rename modules simplifying them.
    * Add Session setup module empty for now.

--- application/apps/indexer/gui/application/src/host/ui/state.rs
+++ application/apps/indexer/gui/application/src/host/ui/state.rs
@@ -1,63 +1,74 @@
+use std::collections::HashMap;
+
 use uuid::Uuid;
 
-use crate::session::{InitSessionParams, ui::SessionUI};
+use crate::{
+    host::ui::tabs::TabType,
+    session::{InitSessionParams, ui::Session},
+};
+
+pub const HOME_TAB_IDX: usize = 0;
 
 #[derive(Debug)]
 pub struct HostState {
-    pub active_tab: TabType,
-    pub sessions: Vec<SessionUI>,
+    pub active_tab_idx: usize,
+    pub tabs: Vec<TabType>,
+    pub sessions: HashMap<Uuid, Session>,
 }
 
 impl HostState {
+    pub fn active_tab(&self) -> &TabType {
+        &self.tabs[self.active_tab_idx]
+    }
     pub fn add_session(&mut self, session: InitSessionParams) {
-        let session = SessionUI::new(session);
-        self.sessions.push(session);
-        self.active_tab = TabType::Session(self.sessions.len() - 1);
+        let session = Session::new(session);
+        let id = session.get_info().id;
+        self.tabs.push(TabType::Session(id));
+        self.sessions.insert(id, session);
+        self.active_tab_idx = self.tabs.len() - 1;
     }
 
     pub fn close_session(&mut self, session_id: Uuid) {
-        let session_idx = self
-            .sessions
+        let session_tab_idx = self
+            .tabs
             .iter()
-            .position(|s| s.get_info().id == session_id);
+            .position(|tab| matches!(tab, TabType::Session(id) if  *id == session_id));
 
-        let session_idx = match session_idx {
+        let session_tab_idx = match session_tab_idx {
             Some(idx) => idx,
             None => {
                 log::error!(
                     "Close Session Message: Session with ID {session_id}\
                     doesn't exist in host UI struct"
                 );
-                panic!("Recieved close session for unknown session ID");
+
+                if cfg!(debug_assertions) {
+                    panic!("Recieved close session for unknown session ID");
+                } else {
+                    return;
+                }
             }
         };
 
         // Handle current tab
-        if let TabType::Session(current_idx) = self.active_tab {
-            if current_idx == session_idx {
-                self.active_tab = TabType::Home;
-            }
+        if self.active_tab_idx == session_tab_idx {
+            self.active_tab_idx = HOME_TAB_IDX;
+        } else if self.active_tab_idx > session_tab_idx {
             // Tabs after the deleted one will be shifted one place to the left.
-            if current_idx > session_idx {
-                self.active_tab = TabType::Session(current_idx.saturating_sub(1));
-            }
+            self.active_tab_idx -= 1;
         }
 
-        self.sessions.remove(session_idx);
+        self.tabs.remove(session_tab_idx);
+        self.sessions.remove(&session_id);
     }
 }
 
 impl Default for HostState {
     fn default() -> Self {
         Self {
-            sessions: Vec::new(),
-            active_tab: TabType::Home,
+            active_tab_idx: 0,
+            tabs: vec![TabType::Home],
+            sessions: HashMap::new(),
         }
     }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum TabType {
-    Home,
-    Session(usize),
 }
