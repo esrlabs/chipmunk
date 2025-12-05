1b11b0042a035768bb47d4af8448212f3f538b1e
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-12-05 14:31:48 +0100

    General Session Setup view + Basic DLT implementation
    * Implement session setup view with the commands and services for files
      where users can select one of the suitable parsers.
    * DLT Session can be started now.
    * Configuration view aren't implemented yet and currently we are showing
      a place holder.
    * Configuration members are defined in the state without implementation
      in the view.

---
+++ application/apps/indexer/gui/application/src/host/common/parsers/dlt.rs
@@ -0,0 +1,69 @@
+use std::{fmt::Display, path::PathBuf};
+
+/// DLT Configurations to be used in front-end
+#[derive(Debug, Clone)]
+pub struct DltParserConfig {
+    pub with_storage_header: bool,
+    pub log_level: DltLogLevel,
+    pub fibex_file_paths: Vec<PathBuf>,
+    pub timezone: Option<String>,
+}
+
+impl DltParserConfig {
+    pub fn new(with_storage_header: bool) -> Self {
+        Self {
+            with_storage_header,
+            log_level: DltLogLevel::Verbose,
+            fibex_file_paths: Vec::new(),
+            timezone: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq)]
+pub enum DltLogLevel {
+    Fatal = 1,
+    Error = 2,
+    Warn = 3,
+    Info = 4,
+    Debug = 5,
+    Verbose = 6,
+}
+
+impl DltLogLevel {
+    pub const fn all() -> &'static [Self] {
+        // Reminder to update on new types
+        match DltLogLevel::Fatal {
+            DltLogLevel::Fatal => {}
+            DltLogLevel::Error => {}
+            DltLogLevel::Warn => {}
+            DltLogLevel::Info => {}
+            DltLogLevel::Debug => {}
+            DltLogLevel::Verbose => {}
+        };
+
+        &[
+            DltLogLevel::Fatal,
+            DltLogLevel::Error,
+            DltLogLevel::Warn,
+            DltLogLevel::Info,
+            DltLogLevel::Debug,
+            DltLogLevel::Verbose,
+        ]
+    }
+}
+
+impl Display for DltLogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match self {
+            DltLogLevel::Fatal => "Fatal",
+            DltLogLevel::Error => "Error",
+            DltLogLevel::Warn => "Warn",
+            DltLogLevel::Info => "Info",
+            DltLogLevel::Debug => "Debug",
+            DltLogLevel::Verbose => "Verbose",
+        };
+
+        f.write_str(name)
+    }
+}
