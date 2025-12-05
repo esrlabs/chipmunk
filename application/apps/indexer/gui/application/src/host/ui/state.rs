37befe1b3bae0e441ad199d451dca0576a2c25f0
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-12-05 14:31:48 +0100

    General Session Setup view + Basic DLT implementation
    * Implement session setup view with the commands and services for files
      where users can select one of the suitable parsers.
    * DLT Session can be started now.
    * Configuration view aren't implemented yet and currently we are showing
      a place holder.
    * Configuration members are defined in the state without implementation
      in the view.

--- application/apps/indexer/gui/application/src/host/ui/state.rs
+++ application/apps/indexer/gui/application/src/host/ui/state.rs
@@ -1,9 +1,16 @@
 use std::collections::HashMap;
 
+use tokio::sync::mpsc::Sender;
 use uuid::Uuid;
 
 use crate::{
-    host::ui::tabs::TabType,
+    host::{
+        command::HostCommand,
+        ui::{
+            session_setup::{SessionSetup, SessionSetupState},
+            tabs::TabType,
+        },
+    },
     session::{InitSessionParams, ui::Session},
 };
 
@@ -14,17 +21,44 @@ pub struct HostState {
     pub active_tab_idx: usize,
     pub tabs: Vec<TabType>,
     pub sessions: HashMap<Uuid, Session>,
+    pub session_setups: HashMap<Uuid, SessionSetup>,
 }
 
 impl HostState {
     pub fn active_tab(&self) -> &TabType {
         &self.tabs[self.active_tab_idx]
     }
-    pub fn add_session(&mut self, session: InitSessionParams) {
+
+    pub fn add_session(&mut self, session: InitSessionParams, session_setup_id: Option<Uuid>) {
         let session = Session::new(session);
         let id = session.get_info().id;
-        self.tabs.push(TabType::Session(id));
+
         self.sessions.insert(id, session);
+        if let Some(setup_id) = session_setup_id {
+            self.session_setups.remove(&setup_id);
+
+            // Replace Session Setup tab with the session itself.
+            let tab = self
+                .tabs
+                .iter_mut()
+                .find(|t| matches!(t, TabType::SessionSetup(uuid) if *uuid == setup_id))
+                .expect("Setup session tab should exist");
+            *tab = TabType::Session(id);
+        } else {
+            self.tabs.push(TabType::Session(id));
+            self.active_tab_idx = self.tabs.len() - 1;
+        }
+    }
+
+    pub fn add_session_setup(
+        &mut self,
+        setup_state: SessionSetupState,
+        cmd_tx: Sender<HostCommand>,
+    ) {
+        let id = setup_state.id;
+        let setup = SessionSetup::new(setup_state, cmd_tx);
+        self.tabs.push(TabType::SessionSetup(id));
+        self.session_setups.insert(id, setup);
         self.active_tab_idx = self.tabs.len() - 1;
     }
 
@@ -43,23 +77,54 @@ impl HostState {
                 );
 
                 if cfg!(debug_assertions) {
-                    panic!("Recieved close session for unknown session ID");
+                    panic!("Received close session for unknown session ID");
                 } else {
                     return;
                 }
             }
         };
 
-        // Handle current tab
-        if self.active_tab_idx == session_tab_idx {
-            self.active_tab_idx = HOME_TAB_IDX;
-        } else if self.active_tab_idx > session_tab_idx {
-            // Tabs after the deleted one will be shifted one place to the left.
-            self.active_tab_idx -= 1;
-        }
+        self.update_current_tab_on_close(session_tab_idx);
 
         self.tabs.remove(session_tab_idx);
         self.sessions.remove(&session_id);
+    }
+
+    pub fn close_session_setup(&mut self, setup_id: Uuid) {
+        let tab_idx = self
+            .tabs
+            .iter()
+            .position(|tab| matches!(tab, TabType::SessionSetup(id) if  *id == setup_id));
+
+        let tab_idx = match tab_idx {
+            Some(idx) => idx,
+            None => {
+                log::error!(
+                    "Close Session Setup Message: Session Setup with ID {setup_id}\
+                    doesn't exist in host UI struct"
+                );
+
+                if cfg!(debug_assertions) {
+                    panic!("Received close session for unknown session setup ID");
+                } else {
+                    return;
+                }
+            }
+        };
+
+        self.update_current_tab_on_close(tab_idx);
+
+        self.tabs.remove(tab_idx);
+        self.session_setups.remove(&setup_id);
+    }
+
+    fn update_current_tab_on_close(&mut self, removed_idx: usize) {
+        if self.active_tab_idx == removed_idx {
+            self.active_tab_idx = HOME_TAB_IDX;
+        } else if self.active_tab_idx > removed_idx {
+            // Tabs after the deleted one will be shifted one place to the left.
+            self.active_tab_idx -= 1;
+        }
     }
 }
 
@@ -69,6 +134,7 @@ impl Default for HostState {
             active_tab_idx: 0,
             tabs: vec![TabType::Home],
             sessions: HashMap::new(),
+            session_setups: HashMap::new(),
         }
     }
 }
