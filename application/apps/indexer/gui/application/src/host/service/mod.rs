e284b11651cfbcbf7f095024a0f759ed7195857c
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-12-05 14:31:48 +0100

    General Session Setup view + Basic DLT implementation
    * Implement session setup view with the commands and services for files
      where users can select one of the suitable parsers.
    * DLT Session can be started now.
    * Configuration view aren't implemented yet and currently we are showing
      a place holder.
    * Configuration members are defined in the state without implementation
      in the view.

--- application/apps/indexer/gui/application/src/host/service/mod.rs
+++ application/apps/indexer/gui/application/src/host/service/mod.rs
@@ -1,12 +1,21 @@
-use std::{path::PathBuf, thread};
+use std::{ops::Not, os::unix::fs::MetadataExt, path::PathBuf, thread};
 
-use stypes::{FileFormat, ObserveOptions, ParserType};
+use stypes::{DltParserSettings, FileFormat, ObserveOptions, ObserveOrigin, ParserType};
 use tokio::runtime::Handle;
+use uuid::Uuid;
 
 use crate::{
     host::{
-        command::HostCommand, communication::ServiceHandle, error::HostError, message::HostMessage,
+        command::HostCommand,
+        common::{
+            parsers::{DltParserConfig, ParserConfig, ParserNames},
+            sources::{ByteSourceType, SourceFileInfo},
+        },
+        communication::ServiceHandle,
+        error::HostError,
+        message::HostMessage,
         notification::AppNotification,
+        ui::session_setup::SessionSetupState,
     },
     session::{InitSessionError, init_session},
 };
@@ -28,7 +37,7 @@ impl HostService {
             let rt = tokio::runtime::Builder::new_multi_thread()
                 .enable_all()
                 .build()
-                .expect("Spawning tokio runtime falied");
+                .expect("Spawning tokio runtime failed");
 
             handle_tx
                 .send(rt.handle().clone())
@@ -64,6 +73,21 @@ impl HostService {
                     self.open_file(file).await?;
                 }
             }
+            HostCommand::StartSession {
+                session_setup_id,
+                parser,
+                source,
+            } => {
+                self.start_session(session_setup_id, source, parser).await?;
+            }
+            HostCommand::CloseSessionSetup(id) => {
+                // NOTE: We need to checks here for cleaning up session setups (Like cancelling
+                // DLT statistics process).
+                self.communication
+                    .senders
+                    .send_message(HostMessage::SessionSetupClosed { id })
+                    .await;
+            }
             HostCommand::Close => {
                 // Do any preparation before closing.
                 self.communication
@@ -82,28 +106,145 @@ impl HostService {
         let is_binary = file_tools::is_binary(&file_path).map_err(InitSessionError::IO)?;
 
         if is_binary {
-            return Err(HostError::InitSessionError(InitSessionError::Other(
-                "Binary files aren't supported yet".into(),
-            )));
+            let extension = file_path.extension();
+            // Open session setup view for binary files.
+            let format = match extension {
+                Some(ext) if ext.eq_ignore_ascii_case("pcap") => FileFormat::PcapLegacy,
+                Some(ext) if ext.eq_ignore_ascii_case("pcapng") => FileFormat::PcapNG,
+                _ => FileFormat::Binary,
+            };
+
+            let name = file_path
+                .file_name()
+                .map(|n| n.to_string_lossy().to_string())
+                .unwrap_or_else(|| String::from("Unknown"));
+
+            let size_bytes = std::fs::metadata(&file_path)
+                .map_err(InitSessionError::IO)?
+                .size();
+            let size_txt = format_file_size(size_bytes);
+
+            let parser = match format {
+                FileFormat::PcapNG | FileFormat::PcapLegacy => ParserConfig::SomeIP,
+                FileFormat::Text => ParserConfig::Text,
+                FileFormat::Binary => {
+                    if extension.is_some_and(|ext| ext.eq_ignore_ascii_case("dlt")) {
+                        ParserConfig::Dlt(DltParserConfig::new(true))
+                    } else {
+                        return Err(HostError::InitSessionError(InitSessionError::Other(
+                            "File extension is not supported".to_owned(),
+                        )));
+                    }
+                }
+            };
+
+            let file_info = SourceFileInfo::new(file_path, name, size_txt, format);
+            let source_type = ByteSourceType::File(file_info);
+
+            let supported_parsers = ParserNames::all()
+                .iter()
+                .filter(|f| f.support_binary_files())
+                .copied()
+                .collect();
+
+            let session_setup =
+                SessionSetupState::new(Uuid::new_v4(), source_type, parser, supported_parsers);
+
+            self.communication
+                .senders
+                .send_message(HostMessage::SessionSetupOpened(session_setup))
+                .await;
+        } else {
+            // Start sessions directly for text files.
+            let origin = ObserveOptions::file(file_path, FileFormat::Text, ParserType::Text(()));
+
+            let session_info =
+                init_session(self.communication.senders.get_shared_senders(), origin).await?;
+
+            self.communication
+                .senders
+                .send_message(HostMessage::SessionCreated {
+                    session_info,
+                    session_setup_id: None,
+                })
+                .await;
         }
 
-        let origin = ObserveOptions::file(file_path, FileFormat::Text, ParserType::Text(()));
+        Ok(())
+    }
+
+    async fn start_session(
+        &self,
+        session_setup_id: Uuid,
+        source: ByteSourceType,
+        parser: ParserConfig,
+    ) -> Result<(), HostError> {
+        let session_id = Uuid::new_v4();
+        let origin = match source {
+            ByteSourceType::File(source_file_info) => ObserveOrigin::File(
+                session_id.to_string(),
+                source_file_info.format,
+                source_file_info.path,
+            ),
+        };
+
+        let parser = match parser {
+            ParserConfig::Dlt(config) => {
+                let fibex_file_paths = config.fibex_file_paths.is_empty().not().then(|| {
+                    config
+                        .fibex_file_paths
+                        .into_iter()
+                        .map(|p| p.to_string_lossy().to_string())
+                        .collect()
+                });
+
+                let dlt_config = DltParserSettings {
+                    filter_config: None,
+                    fibex_file_paths,
+                    with_storage_header: config.with_storage_header,
+                    tz: config.timezone,
+                    fibex_metadata: None,
+                };
+
+                ParserType::Dlt(dlt_config)
+            }
+            ParserConfig::SomeIP | ParserConfig::Text | ParserConfig::Plugins => {
+                let parser_name = ParserNames::from(&parser);
+                return Err(HostError::InitSessionError(InitSessionError::Other(
+                    format!("Parser {parser_name:} isn't supported yet"),
+                )));
+            }
+        };
+
+        let origin = ObserveOptions { origin, parser };
 
         let session_info =
             init_session(self.communication.senders.get_shared_senders(), origin).await?;
 
         self.communication
             .senders
-            .send_message(HostMessage::SessionCreated(session_info))
+            .send_message(HostMessage::SessionCreated {
+                session_info,
+                session_setup_id: Some(session_setup_id),
+            })
             .await;
 
-        //TODO AAZ: Remove after prototyping.
-        self.communication
-            .senders
-            .send_notification(AppNotification::Info(String::from(
-                "TODO DEBUG: Session created for file",
-            )))
-            .await;
         Ok(())
+    }
+}
+
+fn format_file_size(bytes: u64) -> String {
+    const KB: u64 = 1024;
+    const MB: u64 = 1024 * KB;
+    const GB: u64 = 1024 * MB;
+
+    if bytes < KB {
+        format!("{} B", bytes)
+    } else if bytes < MB {
+        format!("{:.2} KB", bytes as f64 / KB as f64)
+    } else if bytes < GB {
+        format!("{:.2} MB", bytes as f64 / MB as f64)
+    } else {
+        format!("{:.2} GB", bytes as f64 / GB as f64)
     }
 }
---
+++ application/apps/indexer/gui/application/src/host/ui/session_setup/state/mod.rs
@@ -0,0 +1,68 @@
+use tokio::sync::mpsc::Sender;
+use uuid::Uuid;
+
+use crate::host::{
+    command::HostCommand,
+    common::{
+        parsers::{DltParserConfig, ParserConfig, ParserNames},
+        sources::ByteSourceType,
+    },
+    ui::UiActions,
+};
+
+#[derive(Debug)]
+pub struct SessionSetupState {
+    pub id: Uuid,
+    pub source: ByteSourceType,
+    pub parser: ParserConfig,
+    pub supported_parsers: Vec<ParserNames>,
+}
+
+impl SessionSetupState {
+    pub fn new(
+        id: Uuid,
+        source: ByteSourceType,
+        parser: ParserConfig,
+        supported_parsers: Vec<ParserNames>,
+    ) -> Self {
+        Self {
+            id,
+            source,
+            parser,
+            supported_parsers,
+        }
+    }
+
+    pub fn update_parser(&mut self, parser: ParserNames) {
+        self.parser = match parser {
+            ParserNames::Dlt => {
+                let with_headers = match self.source {
+                    ByteSourceType::File(..) => true,
+                };
+                ParserConfig::Dlt(DltParserConfig::new(with_headers))
+            }
+            ParserNames::SomeIP => ParserConfig::SomeIP,
+            ParserNames::Text => ParserConfig::Text,
+            ParserNames::Plugins => ParserConfig::Plugins,
+        };
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.source.is_valid() && self.parser.is_valid()
+    }
+
+    pub fn start_session(
+        &self,
+        cmd_tx: &Sender<crate::host::command::HostCommand>,
+        actions: &mut UiActions,
+    ) {
+        debug_assert!(self.is_valid());
+
+        let cmd = HostCommand::StartSession {
+            session_setup_id: self.id,
+            parser: self.parser.clone(),
+            source: self.source.clone(),
+        };
+        actions.try_send_command(cmd_tx, cmd);
+    }
+}
