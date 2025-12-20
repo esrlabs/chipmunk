318e52dd2f513b1a51b0699b3675c947feb35496
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2025-12-20 21:20:21 +0100

    TCP session setup & Validation errors
    * Add TCP session setup enabling running DLT from TCP streams.
    * Display validation errors on side bar.

--- application/apps/indexer/gui/application/src/host/command.rs
+++ application/apps/indexer/gui/application/src/host/command.rs
@@ -2,13 +2,19 @@ use std::path::PathBuf;
 
 use uuid::Uuid;
 
-use crate::host::ui::session_setup::state::{parsers::ParserConfig, sources::ByteSourceConfig};
+use crate::host::{
+    common::{parsers::ParserNames, sources::StreamNames},
+    ui::session_setup::state::{parsers::ParserConfig, sources::ByteSourceConfig},
+};
 
 /// Host commands to be sent from UI to its service.
 #[derive(Debug, Clone)]
 pub enum HostCommand {
     OpenFiles(Vec<PathBuf>),
-    OpenProcessCommand,
+    ConnectionSessionSetup {
+        stream: StreamNames,
+        parser: ParserNames,
+    },
     StartSession(Box<StartSessionParam>),
     CloseSessionSetup(Uuid),
     Close,
