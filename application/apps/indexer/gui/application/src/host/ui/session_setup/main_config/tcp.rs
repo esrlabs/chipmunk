e209fa6953745cfff2895359cd853db34c0e0d31
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2026-01-08 15:35:19 +0100

    Session Setup for UDP & Improve space allocation
    * Implement session setup for UDP.
    * Use generic function to render binding address for both TCP and UDP.
    * Avoid using set_min function and replace some of them with
      take_available functions

--- application/apps/indexer/gui/application/src/host/ui/session_setup/main_config/tcp.rs
+++ application/apps/indexer/gui/application/src/host/ui/session_setup/main_config/tcp.rs
@@ -1,46 +1,27 @@
-use egui::{Align, Color32, Key, Label, RichText, TextEdit, Ui, Widget};
+use egui::Ui;
 
-use crate::host::ui::session_setup::state::sources::TcpConfig;
+use crate::host::ui::session_setup::{ state::sources::TcpConfig};
 
-use super::RenderOutcome;
+use super::{ConfigBindAddress, RenderOutcome, render_socket_address};
+
+impl ConfigBindAddress for TcpConfig {
+    fn bind_addr(&mut self) -> &mut String {
+        &mut self.bind_addr
+    }
+
+    fn validate(&mut self) {
+        self.validate();
+    }
+
+    fn is_valid(&self) -> bool {
+        self.is_valid()
+    }
+
+    fn bind_err_msg(&self) -> Option<&str> {
+        self.get_err_msg()
+    }
+}
 
 pub fn render_connection(config: &mut TcpConfig, ui: &mut Ui) -> RenderOutcome {
-    let mut outcome = RenderOutcome::None;
-    ui.vertical(|ui| {
-        Label::new("Socket Address:").selectable(false).ui(ui);
-
-        let ip_txt_res = TextEdit::singleline(&mut config.bind_addr)
-            .vertical_align(Align::Center)
-            .desired_width(200.)
-            .hint_text("127.0.0.1:8080")
-            .show(ui)
-            .response;
-
-        if ip_txt_res.changed() {
-            config.validate();
-        }
-
-        if ip_txt_res.lost_focus() && ip_txt_res.ctx.input(|ui| ui.key_pressed(Key::Enter)) {
-            if config.is_valid() {
-                outcome = RenderOutcome::StartSession;
-            } else {
-                // Single line moves focus on enter. This is a quick fix
-                // in case command isn't valid.
-                ip_txt_res.request_focus();
-            }
-        }
-
-        ui.vertical(|ui| {
-            ui.set_height(20.);
-            if let Some(err_msg) = config.get_err_msg() {
-                let err_txt = RichText::new(err_msg)
-                    .size(10.5)
-                    .small()
-                    .color(Color32::YELLOW);
-                Label::new(err_txt).selectable(false).ui(ui);
-            }
-        });
-    });
-
-    outcome
+    render_socket_address(config, ui)
 }
