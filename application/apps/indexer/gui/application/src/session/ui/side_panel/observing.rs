08155c52bf69c56bf6ba2e054823bcc85d9ca121
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2026-01-12 15:52:48 +0100

    Session UI: Side panel with tab control
    * Implement the side panel inside the session with the vertical tab
      control.
    * Tab control is manually implemented because there is no tab control
      in egui by default.

---
+++ application/apps/indexer/gui/application/src/session/ui/side_panel/observing.rs
@@ -0,0 +1,19 @@
+use egui::Ui;
+use tokio::sync::mpsc;
+
+use crate::session::{command::SessionCommand, ui::shared::SessionShared};
+
+#[derive(Debug)]
+pub struct ObservingUi {
+    cmd_tx: mpsc::Sender<SessionCommand>,
+}
+
+impl ObservingUi {
+    pub fn new(cmd_tx: mpsc::Sender<SessionCommand>) -> Self {
+        Self { cmd_tx }
+    }
+
+    pub fn render_content(&mut self, shared: &mut SessionShared, ui: &mut Ui) {
+        ui.centered_and_justified(|ui| ui.heading("Observing UI coming soon."));
+    }
+}
