2e5f3bf5bcdaec19c71a42c188d1d90983e93301
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2026-01-14 22:25:44 +0100

    Native: Extend CLI arguments
    * Command to open single or multiple files
    * Command to run shell command.

--- application/apps/indexer/gui/application/src/cli/mod.rs
+++ application/apps/indexer/gui/application/src/cli/mod.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 mod command;
 
+use clap::{Subcommand, ValueHint};
 pub use command::CliCommand;
 
 const HELP_TEMPLATE: &str = "\
@@ -16,9 +17,46 @@ version: {version}
 #[derive(Debug, clap::Parser)]
 #[clap(name = "chipmunk", version, about, help_template = HELP_TEMPLATE)]
 pub struct Cli {
-    /// Specify file path to be opened.
-    #[arg(index = 1, name = "PATH")]
-    pub file_path: Option<PathBuf>,
+    #[command(subcommand)]
+    pub source: Option<SourcesCommand>,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+pub enum SourcesCommand {
+    /// Open file(s) in new session(s).
+    Files {
+        /// Paths to the source files.
+        #[arg(index = 1, name = "PATHS", required = true, value_hint = ValueHint::FilePath)]
+        paths: Vec<PathBuf>,
+    },
+    /// Execute shell command in new session.
+    #[clap(visible_alias = "command")]
+    Process {
+        #[arg(index = 1, name = "COMMAND", required = true, value_hint = ValueHint::DirPath)]
+        /// Command to run.
+        command: String,
+        #[arg(long)]
+        /// Current working directory
+        cwd: Option<PathBuf>,
+    },
+}
+
+impl Cli {
+    pub fn get_commands(mut self) -> Vec<CliCommand> {
+        let mut cli_cmds = Vec::new();
+
+        if let Some(source) = self.source.take() {
+            let cmd = match source {
+                SourcesCommand::Files { paths } => CliCommand::OpenFiles { paths },
+                SourcesCommand::Process { command, cwd } => {
+                    CliCommand::ProcessCommand { command, cwd }
+                }
+            };
+            cli_cmds.push(cmd);
+        }
+
+        cli_cmds
+    }
 }
 
 #[cfg(test)]
