2ecda7fa22075f5c0635ca8ef422b679fe978f53
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2026-02-13 18:45:51 +0100

    Session observe tab: Add TCP connection

--- application/apps/indexer/gui/application/src/session/ui/side_panel/observing/tcp.rs
+++ application/apps/indexer/gui/application/src/session/ui/side_panel/observing/tcp.rs
@@ -1,16 +1,29 @@
-use egui::{Id, Ui};
+use egui::{Align, Button, Id, Layout, RichText, ScrollArea, Ui, Widget};
+use stypes::Transport;
 use tokio::sync::mpsc;
 use uuid::Uuid;
 
 use crate::{
-    host::ui::UiActions,
-    session::{command::SessionCommand, ui::shared::SessionShared},
+    common::phosphor::icons,
+    host::ui::{
+        UiActions,
+        session_setup::{
+            RenderOutcome, main_config,
+            state::sources::{StreamConfig, TcpConfig},
+        },
+    },
+    session::{
+        command::{AttachSource, SessionCommand},
+        types::ObserveOperation,
+        ui::shared::SessionShared,
+    },
 };
 
 #[derive(Debug)]
 pub struct TcpObserveUi {
     id: Id,
-    _cmd_txa: mpsc::Sender<SessionCommand>,
+    cmd_tx: mpsc::Sender<SessionCommand>,
+    config: TcpConfig,
 }
 
 impl TcpObserveUi {
@@ -18,14 +31,15 @@ impl TcpObserveUi {
         let id = Id::new(format!("side_tcp_{id_salt}"));
         Self {
             id,
-            _cmd_txa: cmd_tx,
+            cmd_tx,
+            config: TcpConfig::new(),
         }
     }
 
     pub fn render_content(
         &mut self,
         ui: &mut Ui,
-        _shared: &mut SessionShared,
+        shared: &mut SessionShared,
         actions: &mut UiActions,
     ) {
         super::render_group_title(ui, "TCP Connections");
@@ -33,12 +47,108 @@ impl TcpObserveUi {
         ui.add_space(super::SPACE_BETWEEN_GROUPS);
 
         self.attach_tcp(ui, actions);
+
+        ui.add_space(super::SPACE_BETWEEN_GROUPS);
+
+        let (connected, disconnected): (Vec<_>, Vec<_>) = shared
+            .observe
+            .operations()
+            .iter()
+            .partition(|op| op.phase().is_running());
+
+        ScrollArea::vertical().show(ui, |ui| {
+            let mut idx = 0;
+            if !connected.is_empty() {
+                let connected_title = format!("Connected {}", connected.len());
+                ui.heading(RichText::new(connected_title).size(super::TITLE_SIZE));
+                for op in connected {
+                    self.render_tcp_item(ui, op, idx, actions);
+                    idx += 1;
+                }
+            }
+
+            if !disconnected.is_empty() {
+                ui.add_space(super::SPACE_BETWEEN_GROUPS);
+
+                let disconnected_title = format!("Disconnected {}", disconnected.len());
+                ui.heading(RichText::new(disconnected_title).size(super::TITLE_SIZE));
+
+                for op in disconnected {
+                    self.render_tcp_item(ui, op, idx, actions);
+                    idx += 1;
+                }
+            }
+        });
     }
 
-    fn attach_tcp(&self, ui: &mut Ui, _actions: &mut UiActions) {
+    fn attach_tcp(&mut self, ui: &mut Ui, actions: &mut UiActions) {
         super::render_attach_source(ui, self.id, "New Connection", |ui| {
-            //TODO AAZ:
-            ui.label("Attach new TCP Connections goes here");
+            let mut outcome = main_config::render_socket_address(&mut self.config, ui);
+            ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
+                if ui
+                    .add_enabled(self.config.is_valid(), Button::new("Connect"))
+                    .clicked()
+                {
+                    outcome = RenderOutcome::StartSession;
+                }
+            });
+
+            if outcome == RenderOutcome::StartSession {
+                let cmd = SessionCommand::AttachSource {
+                    source: AttachSource::Stream(Box::new(StreamConfig::Tcp(
+                        self.config.to_owned(),
+                    ))),
+                };
+                self.config.bind_addr.clear();
+                self.config.validate();
+
+                actions.try_send_command(&self.cmd_tx, cmd);
+            }
         });
+    }
+
+    fn render_tcp_item(
+        &self,
+        ui: &mut Ui,
+        operation: &ObserveOperation,
+        idx: usize,
+        actions: &mut UiActions,
+    ) {
+        let config = match &operation.origin {
+            stypes::ObserveOrigin::Stream(_, Transport::TCP(config)) => config,
+            _ => return,
+        };
+
+        super::render_observe_item(
+            ui,
+            idx,
+            "TCP",
+            |ui| {
+                ui.label(RichText::new(&config.bind_addr).strong());
+            },
+            |ui| {
+                if operation.phase().is_running() {
+                    let stop_res = super::get_item_button(icons::regular::STOP_CIRCLE)
+                        .ui(ui)
+                        .on_hover_text("Disconnect");
+                    if stop_res.clicked() {
+                        let cmd = SessionCommand::CancelOperation { id: operation.id };
+                        actions.try_send_command(&self.cmd_tx, cmd);
+                    }
+                } else {
+                    let run_res = super::get_item_button(icons::regular::PLAY_CIRCLE)
+                        .ui(ui)
+                        .on_hover_text("Reconnect");
+                    if run_res.clicked() {
+                        let to_send = TcpConfig::from(config);
+                        let cmd = SessionCommand::AttachSource {
+                            source: AttachSource::Stream(Box::new(StreamConfig::Tcp(to_send))),
+                        };
+
+                        actions.try_send_command(&self.cmd_tx, cmd);
+                    }
+                }
+            },
+        );
     }
 }
